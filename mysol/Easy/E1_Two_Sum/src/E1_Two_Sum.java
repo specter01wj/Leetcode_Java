e724368e47c796d1555570423f55e2732fc2d03d
specter01wj <specter01wj@gmail.com>
2019-06-17 12:37:48 -0400

    06/17/2019 v1

--- mysol/Easy/E1_Two_Sum/src/E1_Two_Sum.java
+++ mysol/Easy/E1_Two_Sum/src/E1_Two_Sum.java
@@ -35,6 +35,14 @@ public class E1_Two_Sum {
 	public static int[] twoSum(int[] numbers, int target) {
 		HashMap<Integer, Integer> map = new HashMap<>();
 		
+		for(int i = 0; i < numbers.length; i++) {
+			if(map.get(numbers[i]) != null) {
+				int[] result = {map.get(numbers[i]), i};
+				return result;
+			}
+			map.put(target - numbers[i], i);
+		}
+		
 		int[] result = {};
 		return result;
 	}
