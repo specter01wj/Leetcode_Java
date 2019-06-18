6966a847e6da97fd39d92b9406b9fb08a8c8fc8b
specter01wj <specter01wj@gmail.com>
2019-06-18 10:38:36 -0400

    06/18/2019 v1

--- mysol/Easy/E7_Reverse_Integer/src/E7_Reverse_Integer.java
+++ mysol/Easy/E7_Reverse_Integer/src/E7_Reverse_Integer.java
@@ -18,8 +18,30 @@ Output: 21*/
 public class E7_Reverse_Integer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int input = 321;
+        int output = reverseInteger(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output) );
+	}
+	
+	
+	/**
+     * @param n the integer to be reversed
+     * @return the reversed integer
+     */
+	public static int reverseInteger(int n) {
+		int reversed_n = 0;
+		
+		while(n != 0) {
+			int temp = reversed_n * 10 + n % 10;
+			n /= 10;
+			if(temp / 10 != reversed_n) {
+				reversed_n = 0;
+				break;
+			}
+			reversed_n = temp;
+		}
+		
+		return reversed_n;
 	}
 
 }
