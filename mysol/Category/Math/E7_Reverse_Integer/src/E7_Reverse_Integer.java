5985d5136ca35b58b3146d180f0e2efac654fbb9
specter01wj <specter01wj@gmail.com>
2019-06-18 13:20:16 -0400

    06/18/2019 v1

---
+++ mysol/Category/Math/E7_Reverse_Integer/src/E7_Reverse_Integer.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+/*Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21*/
+
+public class E7_Reverse_Integer {
+
+	public static void main(String[] args) {
+		int input = -123;
+        int output = reverseInteger(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output) );
+	}
+	
+	
+	/**
+     * @param n the integer to be reversed
+     * @return the reversed integer
+     */
+	public static int reverseInteger(int n) {
+		int reversed_n = 0;
+		
+		while(n != 0) {
+			int temp = reversed_n * 10 + n % 10;
+			n /= 10;
+			if(temp / 10 != reversed_n) {
+				reversed_n = 0;
+				break;
+			}
+			reversed_n = temp;
+		}
+		
+		return reversed_n;
+	}
+
+}
