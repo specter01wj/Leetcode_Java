09fc553224a35eedd5a4651af2793b4b763c3a52
specter01wj <specter01wj@gmail.com>
2019-06-19 11:23:49 -0400

    06/18/2019 v1

--- mysol/Category/Array/E27_Remove_Element/src/E27_Remove_Element.java
+++ mysol/Category/Array/E27_Remove_Element/src/E27_Remove_Element.java
@@ -39,6 +39,7 @@ public class E27_Remove_Element {
 	 */
 	public static int removeElement(int[] nums, int val) {
 		int i = 0;
+		
 		for(int j = 0; j < nums.length; j++) {
 			if(nums[j] != val) {
 				nums[i] = nums[j];
@@ -48,5 +49,20 @@ public class E27_Remove_Element {
 		
 		return i;
 	}
+	
+	/*public static int removeElement(int[] nums, int val) {
+		int i = 0;
+		int n = nums.length;
+		while(i < n) {
+			if(nums[i] == val) {
+				nums[i] = nums[n - 1];
+				n--;
+			} else {
+				i++;
+			}
+		}
+		
+		return n;
+	}*/
 
 }
