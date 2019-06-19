e7df240f70085a5f9ed800ac9282c1d26b1c73ef
specter01wj <specter01wj@gmail.com>
2019-06-19 15:49:42 -0400

    06/18/2019 v1

--- mysol/Category/Array/E26_Remove_Duplicates_from_Sorted_Array/src/E26_Remove_Duplicates_from_Sorted_Array.java
+++ mysol/Category/Array/E26_Remove_Duplicates_from_Sorted_Array/src/E26_Remove_Duplicates_from_Sorted_Array.java
@@ -23,8 +23,33 @@ It doesn't matter what values are set beyond the returned length.
 public class E26_Remove_Duplicates_from_Sorted_Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {0,0,1,1,1,2,2,3,3,4};
+        int output = removeDuplicates(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    由于有序，所以相同的数字排在一起。
+	用一个游标变量指向已经去重的部分的下一个空位，只要a[i] != a[i-1],就将a[i]填入之前的空位。
+    */
+	
+	/*
+     * @param nums: An ineger array
+     * @return: An integer
+    */
+	public static int removeDuplicates(int[] nums) {
+		if(nums.length == 0) {
+			return 0;
+		}
+		int i = 0;
+		for(int j = 1; j < nums.length; j++) {
+			if(nums[j] != nums[i]) {
+				nums[++i] = nums[j];
+			}
+		}
+		
+		return i + 1;
 	}
 
 }
