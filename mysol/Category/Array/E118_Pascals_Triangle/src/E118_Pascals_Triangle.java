74465a27d2419e7e0f807ae4f6d617a60ffa8642
specter01wj <specter01wj@gmail.com>
2019-06-21 11:49:40 -0400

    06/21/2019 v1

---
+++ mysol/Category/Array/E118_Pascals_Triangle/src/E118_Pascals_Triangle.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+/*Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+Example:
+
+Input: 5
+Output:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+*/
+
+public class E118_Pascals_Triangle {
+
+	public static void main(String[] args) {
+		int input = 5;
+        int[] output = generate(input);
+        System.out.println("input: " + (input) + "\noutput: " + Arrays.toString(output));
+	}
+	
+	/*
+    solution:
+    Using Extra Array
+	Algorithm
+	We use an extra array in which we place every element of the array 
+	at its correct position i.e. the number at index ii in the original 
+	array is placed at the index (i+k)%(length of array)(i+k). 
+	Then, we copy the new array to the original one.
+    */
+	
+	/**
+     * @param nums: an array
+     * @param k: an integer
+     * @return: rotate the array to the right by k steps
+     */
+	
+
+}
