b3b6a54f9403e74a5c02bf71e68c76468c0ab8f0
specter01wj <specter01wj@gmail.com>
2019-06-22 12:37:19 -0400

    06/18/2019 v1

--- mysol/Category/Array/E169_Majority_Element/src/E169_Majority_Element.java
+++ mysol/Category/Array/E169_Majority_Element/src/E169_Majority_Element.java
@@ -22,6 +22,21 @@ public class E169_Majority_Element {
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    We know that the majority element occurs more than n/2 times, and a HashMap 
+    allows us to count element occurrences efficiently.
+    
+    Algorithm
+    We can use a HashMap that maps elements to counts in order to count occurrences 
+    in linear time by looping over nums. 
+    Then, we simply return the key with maximum value.
+    */
+	
+	/*
+     * @param nums: a list of integers
+     * @return: find a  majority number
+     */
 	public static int majorityElement(int[] nums) {
 		Map<Integer, Integer> counts = countNums(nums);
 		Map.Entry<Integer, Integer> majorityEntry = null;
