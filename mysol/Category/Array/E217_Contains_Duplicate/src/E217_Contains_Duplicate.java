41c4c964c2eacd5be0d29d1b6e2a285a1337e43d
specter01wj <specter01wj@gmail.com>
2019-06-23 20:37:05 -0400

    06/18/2019 v1

--- mysol/Category/Array/E217_Contains_Duplicate/src/E217_Contains_Duplicate.java
+++ mysol/Category/Array/E217_Contains_Duplicate/src/E217_Contains_Duplicate.java
@@ -1,9 +1,40 @@
+import java.util.*;
+
+/*Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true
+Example 2:
+
+Input: [1,2,3,4]
+Output: false
+Example 3:
+
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+*/
 
 public class E217_Contains_Duplicate {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {1,1,1,3,3,4,3,2,4,2};
+        boolean output = containsDuplicate(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	/*
+    solution:
+    使用HashSet判断重复
+    */
+	
+	/**
+     * @param nums: the given array
+     * @return: if any value appears at least twice in the array
+     */
+	
 
 }
