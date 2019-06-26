0ab12db935f7c6329f049f49b6a8ce673906b4a5
specter01wj <specter01wj@gmail.com>
2019-06-26 12:52:04 -0400

    06/26/2019 v1

--- mysol/Category/Array/E283_Move_Zeroes/src/E283_Move_Zeroes.java
+++ mysol/Category/Array/E283_Move_Zeroes/src/E283_Move_Zeroes.java
@@ -1,9 +1,47 @@
+import java.util.*;
+
+/*Given an array nums, write a function to move all 0's to the end of it 
+while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+*/
 
 public class E283_Move_Zeroes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {0,1,0,3,12};
+        int[] output = moveZeroes(input);
+//		moveZeroes(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + Arrays.toString(output));
+	}
+	
+	/*
+    solution:
+    双指针，left代表新数组，right代表老数组
+    */
+	
+	/*
+     * @param nums: an integer array
+     * @return nothing, do this in-place
+     */
+	public static int[] moveZeroes(int[] nums) {
+		int left = 0, right = 0;
+		int[] result = nums.clone();
+		
+		while(right < nums.length) {
+			if(result[right] != 0) {
+				int temp = result[left];
+				result[left] = result[right];
+				result[right] = temp;
+				left++;
+			}
+			right++;
+		}
+		
+		return result;
 	}
 
 }
