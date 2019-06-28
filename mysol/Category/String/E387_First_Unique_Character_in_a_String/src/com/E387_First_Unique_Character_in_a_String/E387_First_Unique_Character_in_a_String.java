b44769c4a3fd62d9b34959d8f24270f35f712f92
specter01wj <specter01wj@gmail.com>
2019-06-28 14:54:29 -0400

    06/28/2019 v1

--- mysol/Category/String/E387_First_Unique_Character_in_a_String/src/com/E387_First_Unique_Character_in_a_String/E387_First_Unique_Character_in_a_String.java
+++ mysol/Category/String/E387_First_Unique_Character_in_a_String/src/com/E387_First_Unique_Character_in_a_String/E387_First_Unique_Character_in_a_String.java
@@ -42,7 +42,21 @@ public class E387_First_Unique_Character_in_a_String {
      * @return: integer, the first unique character in a given string
      */
 	public static int firstUniqChar(String s) {
+		HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+		int n = s.length();
 		
+		for(int i = 0; i < n; i++) {
+			char c = s.charAt(i);
+			count.put(c, count.getOrDefault(c, 0) + 1);
+		}
+		
+		for(int i = 0; i < n; i++) {
+			if(count.get(s.charAt(i)) == 1) {
+				return i;
+			}
+		}
+		
+		return -1;
 	}
 
 }
