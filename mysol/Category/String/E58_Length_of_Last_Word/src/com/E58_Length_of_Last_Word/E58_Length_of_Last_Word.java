ae1bd7cd5bcd7fa0ae973e35a74beb1f52b2a4d3
specter01wj <specter01wj@gmail.com>
2019-06-28 12:08:16 -0400

    06/28/2019 v1

--- mysol/Category/String/E58_Length_of_Last_Word/src/com/E58_Length_of_Last_Word/E58_Length_of_Last_Word.java
+++ mysol/Category/String/E58_Length_of_Last_Word/src/com/E58_Length_of_Last_Word/E58_Length_of_Last_Word.java
@@ -17,8 +17,45 @@ Output: 5
 public class E58_Length_of_Last_Word {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "Hello World";
+        int output = lengthOfLastWord(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    The points of interest are the peaks and valleys in the given graph. 
+    We need to find the largest peak following the smallest valley. 
+    We can maintain two variables - minprice and maxprofit corresponding 
+    to the smallest valley and maximum profit (maximum difference between 
+    selling price and minprice) obtained so far respectively.
+    */
+	
+	/*
+     * @param prices: a list of integers
+     * @return: find a maximum profit
+     */
+	public static int lengthOfLastWord(String s) {
+		int length = 0;
+		char[] chars = s.toCharArray();
+		
+		for(int i = s.length() - 1; i >= 0; i--) {
+			if(length == 0) {
+				if(chars[i] == ' ') {
+					continue;
+				} else {
+					length++;
+				}
+			} else {
+				if(chars[i] == ' ') {
+					break;
+				} else {
+					length++;
+				}
+			}
+		}
+		
+		return length;
 	}
 
 }
