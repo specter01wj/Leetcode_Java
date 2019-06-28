2edbaca7508672e5ab5941cbf44e8fd59fe6b8b9
specter01wj <specter01wj@gmail.com>
2019-06-28 11:56:18 -0400

    06/28/2019 v1

---
+++ mysol/Category/String/E58_Length_of_Last_Word/src/com/E58_Length_of_Last_Word/E58_Length_of_Last_Word.java
@@ -0,0 +1,24 @@
+package com.E58_Length_of_Last_Word;
+
+import java.util.*;
+
+/*Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters only.
+
+Example:
+
+Input: "Hello World"
+Output: 5
+*/
+
+public class E58_Length_of_Last_Word {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
