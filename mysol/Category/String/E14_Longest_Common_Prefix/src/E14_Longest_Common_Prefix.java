e38288782bc83cf4c055171ce0d13d48adfb4bb2
specter01wj <specter01wj@gmail.com>
2019-06-28 00:32:18 -0400

    04/10/2019 v1

---
+++ mysol/Category/String/E14_Longest_Common_Prefix/src/E14_Longest_Common_Prefix.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+/*Write a function to find the longest common prefix string 
+ * amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+Input: ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+*/
+
+public class E14_Longest_Common_Prefix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
