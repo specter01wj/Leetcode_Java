349460bd0356d0525ab42733ec2e37f4df4066d5
specter01wj <specter01wj@gmail.com>
2019-06-29 21:47:00 -0400

    06/29/2019 v1

--- mysol/Category/String/E344_Reverse_String/src/com/E344_Reverse_String/E344_Reverse_String.java
+++ mysol/Category/String/E344_Reverse_String/src/com/E344_Reverse_String/E344_Reverse_String.java
@@ -1,10 +1,28 @@
 package com.E344_Reverse_String;
 
+import java.util.*;
+
+
+
 public class E344_Reverse_String {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		char[] input = {'h','e','l','l','o'};
+        char[] output = reverseString(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + Arrays.toString(output));
 	}
+	
+	public static char[] reverseString(char[] s) {
+        char[] str = new char[s.length];
+        
+        for (int i = 0; i < s.length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
+        str = s;
+        
+        return str;
+    }
 
 }
