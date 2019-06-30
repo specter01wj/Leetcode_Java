3ab959ef56d6905bd9a3b08a0f398beda89c4c1e
specter01wj <specter01wj@gmail.com>
2019-06-29 22:26:29 -0400

    06/29/2019 v1

--- mysol/Category/String/E345_Reverse_Vowels_of_a_String/src/com/E345_Reverse_Vowels_of_a_String/E345_Reverse_Vowels_of_a_String.java
+++ mysol/Category/String/E345_Reverse_Vowels_of_a_String/src/com/E345_Reverse_Vowels_of_a_String/E345_Reverse_Vowels_of_a_String.java
@@ -17,8 +17,45 @@ Output: "leotcede"
 public class E345_Reverse_Vowels_of_a_String {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "hello";
+        String output = reverseVowels(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    双指针解法，头尾两个指针相向而行，遇到元音字符则交换。
+    */
+	
+	/*
+     * @param s: a String
+     * @return chars: reversed vowels of a string
+     */
+	public static String reverseVowels(String s) {
+		if(s == null || s.length()==0) {
+			return s;
+		}
+		String vowels = "aeiouAEIOU";
+		// 将字符串转化成char类型数组
+		char[] chars = s.toCharArray();
+		int start = 0, end = s.length() - 1;
+		
+		while(start < end) {
+			// 双指针相向而行找元音字符
+			while(start < end && !vowels.contains(chars[start] + "")) {
+				start++;
+			}
+			while(start < end && !vowels.contains(chars[end] + "")) {
+				end--;
+			}
+			char temp = chars[start];
+			chars[start] = chars[end];
+			chars[end] = temp;
+			start++;
+			end--;
+		}
+		
+		return new String(chars);
 	}
 
 }
