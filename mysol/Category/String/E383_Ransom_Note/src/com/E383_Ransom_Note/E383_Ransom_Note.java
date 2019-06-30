3603762f492be135a63de0acddfee793a0203054
specter01wj <specter01wj@gmail.com>
2019-06-29 20:42:04 -0400

    06/30/2019 v1

--- mysol/Category/String/E383_Ransom_Note/src/com/E383_Ransom_Note/E383_Ransom_Note.java
+++ mysol/Category/String/E383_Ransom_Note/src/com/E383_Ransom_Note/E383_Ransom_Note.java
@@ -1,10 +1,42 @@
 package com.E383_Ransom_Note;
 
+import java.util.*;
+
+/*Given an arbitrary ransom note string and another string containing letters 
+ * from all the magazines, write a function that will return true if the 
+ * ransom note can be constructed from the magazines ; otherwise, 
+ * it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+*/
+
 public class E383_Ransom_Note {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {7,1,5,3,6,4};//{7,6,4,3,1};
+        int output = maxProfit(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	/*
+    solution:
+    开一个arr数组，遍历magazine的内容记录每个字符出现的个数，
+    然后继续遍历ransomNote的内容减去相应字符的个数，
+    若出现某个字符的个数小于0，则返回False
+    */
+	
+	/*
+     * @param ransomNote: a string
+     * @param magazine: a string
+     * @return boolean: whether the ransom note can be constructed from the magazines
+     */
+	
 
 }
