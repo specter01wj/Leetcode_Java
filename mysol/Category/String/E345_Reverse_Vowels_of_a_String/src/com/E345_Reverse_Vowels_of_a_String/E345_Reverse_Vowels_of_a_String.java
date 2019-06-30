8fba6a1d796d6a8ae975a0ae5fafa0e137c33714
specter01wj <specter01wj@gmail.com>
2019-06-29 22:09:00 -0400

    06/29/2019 v1

--- mysol/Category/String/E345_Reverse_Vowels_of_a_String/src/com/E345_Reverse_Vowels_of_a_String/E345_Reverse_Vowels_of_a_String.java
+++ mysol/Category/String/E345_Reverse_Vowels_of_a_String/src/com/E345_Reverse_Vowels_of_a_String/E345_Reverse_Vowels_of_a_String.java
@@ -1,5 +1,19 @@
 package com.E345_Reverse_Vowels_of_a_String;
 
+import java.util.*;
+
+/*Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+
+Input: "hello"
+Output: "holle"
+Example 2:
+
+Input: "leetcode"
+Output: "leotcede"
+*/
+
 public class E345_Reverse_Vowels_of_a_String {
 
 	public static void main(String[] args) {
