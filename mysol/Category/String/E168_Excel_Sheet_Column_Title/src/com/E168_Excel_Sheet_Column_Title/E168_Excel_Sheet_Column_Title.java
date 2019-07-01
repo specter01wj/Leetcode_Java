3ca7d526270e40cc456211ed96b817f8ddd8f5cd
specter01wj <specter01wj@gmail.com>
2019-07-01 16:33:37 -0400

    06/30/2019 v1

--- mysol/Category/String/E168_Excel_Sheet_Column_Title/src/com/E168_Excel_Sheet_Column_Title/E168_Excel_Sheet_Column_Title.java
+++ mysol/Category/String/E168_Excel_Sheet_Column_Title/src/com/E168_Excel_Sheet_Column_Title/E168_Excel_Sheet_Column_Title.java
@@ -49,8 +49,8 @@ public class E168_Excel_Sheet_Column_Title {
 		
 		while(n > 0) {
 			n--;
-			int t1 = (n % 26) + 'A';
-			char r1 = (char)((n % 26) + 'A');
+			/*int t1 = (n % 26) + 'A';
+			char r1 = (char)((n % 26) + 'A');*/
 			str.append( (char)((n % 26) + 'A') );
 			n /= 26;
 		}
