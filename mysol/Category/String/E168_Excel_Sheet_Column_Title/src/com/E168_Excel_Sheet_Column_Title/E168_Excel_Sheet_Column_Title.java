b3ad5e06e3dd4ee27fac223fcf3e8e5fa78d97af
specter01wj <specter01wj@gmail.com>
2019-07-01 13:23:02 -0400

    06/30/2019 v1

--- mysol/Category/String/E168_Excel_Sheet_Column_Title/src/com/E168_Excel_Sheet_Column_Title/E168_Excel_Sheet_Column_Title.java
+++ mysol/Category/String/E168_Excel_Sheet_Column_Title/src/com/E168_Excel_Sheet_Column_Title/E168_Excel_Sheet_Column_Title.java
@@ -1,10 +1,59 @@
 package com.E168_Excel_Sheet_Column_Title;
 
+import java.util.*;
+
+/*Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+For example:
+
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+    ...
+Example 1:
+
+Input: 1
+Output: "A"
+Example 2:
+
+Input: 28
+Output: "AB"
+Example 3:
+
+Input: 701
+Output: "ZY"*/
+
 public class E168_Excel_Sheet_Column_Title {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int input = 28;
+        String output = convertToTitle(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    
+    */
+	
+	/*
+     * @param n: an integer
+     * @return: return its corresponding column title as appear in an Excel sheet
+     */
+	public static String convertToTitle(int n) {
+		StringBuilder str = new StringBuilder();
+		
+		while(n > 0) {
+			n--;
+			str.append( (char)((n % 26) + 'A') );
+			n /= 26;
+		}
+		
+		return str.reverse().toString();
 	}
 
 }
