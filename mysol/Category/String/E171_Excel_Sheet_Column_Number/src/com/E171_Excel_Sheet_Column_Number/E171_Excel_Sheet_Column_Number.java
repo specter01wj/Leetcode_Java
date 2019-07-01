6d41ecfd57f86435c783ab9591e78cf491affd44
specter01wj <specter01wj@gmail.com>
2019-07-01 15:54:31 -0400

    06/30/2019 v1

---
+++ mysol/Category/String/E171_Excel_Sheet_Column_Number/src/com/E171_Excel_Sheet_Column_Number/E171_Excel_Sheet_Column_Number.java
@@ -0,0 +1,10 @@
+package com.E171_Excel_Sheet_Column_Number;
+
+public class E171_Excel_Sheet_Column_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
