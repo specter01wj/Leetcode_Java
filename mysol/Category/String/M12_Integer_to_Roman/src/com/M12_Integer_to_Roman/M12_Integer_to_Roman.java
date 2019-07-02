6e78846aebf6522d13c4eb2e398d4becefd8e3d2
specter01wj <specter01wj@gmail.com>
2019-07-02 13:13:56 -0400

    06/30/2019 v1

--- mysol/Category/String/M12_Integer_to_Roman/src/com/M12_Integer_to_Roman/M12_Integer_to_Roman.java
+++ mysol/Category/String/M12_Integer_to_Roman/src/com/M12_Integer_to_Roman/M12_Integer_to_Roman.java
@@ -1,10 +1,96 @@
 package com.M12_Integer_to_Roman;
 
+import java.util.*;
+
+/*Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, two is written as II in Roman numeral, just two one's added together. 
+Twelve is written as, XII, which is simply X + II. 
+The number twenty seven is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. 
+However, the numeral for four is not IIII. Instead, the number four is written as IV. 
+Because the one is before the five we subtract it making four. 
+The same principle applies to the number nine, which is written as IX. 
+There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given an integer, convert it to a roman numeral. 
+Input is guaranteed to be within the range from 1 to 3999.
+
+Example 1:
+
+Input: 3
+Output: "III"
+Example 2:
+
+Input: 4
+Output: "IV"
+Example 3:
+
+Input: 9
+Output: "IX"
+Example 4:
+
+Input: 58
+Output: "LVIII"
+Explanation: L = 50, V = 5, III = 3.
+Example 5:
+
+Input: 1994
+Output: "MCMXCIV"
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.*/
+
 public class M12_Integer_to_Roman {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int input = 1994;
+        String output = intToRoman(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    The points of interest are the peaks and valleys in the given graph. 
+    We need to find the largest peak following the smallest valley. 
+    We can maintain two variables - minprice and maxprofit corresponding 
+    to the smallest valley and maximum profit (maximum difference between 
+    selling price and minprice) obtained so far respectively.
+    */
+	
+	/*
+     * @param prices: a list of integers
+     * @return: find a maximum profit
+     */
+	public static String intToRoman(int num) {
+		if(num <= 0) {
+			return "";
+		}
+		int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+	    String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+	    StringBuilder res = new StringBuilder();
+	    int digit = 0;
+	    
+	    while(num > 0) {
+	    		int times = num / nums[digit];
+	    		num -= nums[digit] * times;
+	    		for(; times > 0; times--) {
+	    			res.append(symbols[digit]);
+	    		}
+	    		digit++;
+	    }
+	    
+	    return res.toString();
 	}
 
 }
