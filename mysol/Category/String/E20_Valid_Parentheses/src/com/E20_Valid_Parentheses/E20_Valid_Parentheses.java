3a97c99da13b52076e4452aaf3ed1a1ffe0b5a1e
specter01wj <specter01wj@gmail.com>
2019-07-09 17:28:33 -0400

    06/30/2019 v1

--- mysol/Category/String/E20_Valid_Parentheses/src/com/E20_Valid_Parentheses/E20_Valid_Parentheses.java
+++ mysol/Category/String/E20_Valid_Parentheses/src/com/E20_Valid_Parentheses/E20_Valid_Parentheses.java
@@ -35,8 +35,64 @@ Output: true*/
 public class E20_Valid_Parentheses {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {7,1,5,3,6,4};//{7,6,4,3,1};
+        int output = maxProfit(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    Approach 1: Stacks
+	An interesting property about a valid parenthesis expression is that a sub-expression 
+	of a valid expression should also be a valid expression. 
+	(Not every sub-expression) e.g.
+	
+	
+	Also, if you look at the above structure carefully, the color coded cells mark the 
+	opening and closing pairs of parenthesis. The entire expression is valid, 
+	but sub portions of it are also valid in themselves. 
+	This lends a sort of a recursive structure to the problem. 
+	For e.g. Consider the expression enclosed within the two green parenthesis 
+	in the diagram above. The opening bracket at index 1 and the corresponding 
+	closing bracket at index 6.
+	
+	What if whenever we encounter a matching pair of parenthesis in the expression, 
+	we simply remove it from the expression?
+	
+	Let's have a look at this idea below where remove the smaller expressions one 
+	at a time from the overall expression and since this is a valid expression, 
+	we would be left with an empty string in the end.
+	
+	The stack data structure can come in handy here in representing this recursive 
+	structure of the problem. We can't really process this from the inside out 
+	because we don't have an idea about the overall structure. 
+	But, the stack can help us process this recursively i.e. from outside to inwards.
+	
+	Let us have a look at the algorithm for this problem using stacks as the 
+	intermediate data structure.
+	
+	Algorithm
+	
+	1. Initialize a stack S.
+	2. Process each bracket of the expression one at a time.
+	3. If we encounter an opening bracket, we simply push it onto the stack. 
+	This means we will process it later, let us simply move onto the sub-expression ahead.
+	4. If we encounter a closing bracket, then we check the element on top of the stack. 
+	If the element at the top of the stack is an opening bracket of the same type, 
+	then we pop it off the stack and continue processing. Else, this implies an invalid 
+	expression.
+	5. In the end, if we are left with a stack still having elements, then this implies 
+	an invalid expression.
+	
+	We'll have a look a dry run for the algorithm and then move onto the implementation.
+    */
+	
+	/*
+     * @param s: a string
+     * @return: boolean, whether the string is a valid parentheses
+     */
+	public static boolean isValid(String s) {
+		
 	}
 
 }
