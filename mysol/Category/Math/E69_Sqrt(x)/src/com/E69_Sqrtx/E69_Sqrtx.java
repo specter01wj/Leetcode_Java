4cda8b9f3cbe868cd7960b0cb521ee2f48bf01d6
specter01wj <specter01wj@gmail.com>
2019-07-12 13:48:36 -0400

    07/12/2019 v1

--- mysol/Category/Math/E69_Sqrt(x)/src/com/E69_Sqrtx/E69_Sqrtx.java
+++ mysol/Category/Math/E69_Sqrt(x)/src/com/E69_Sqrtx/E69_Sqrtx.java
@@ -1,10 +1,46 @@
 package com.E69_Sqrtx;
 
+import java.util.*;
+
+/*Implement int sqrt(int x).
+
+Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+
+Since the return type is an integer, the decimal digits are truncated and only the integer 
+part of the result is returned.
+
+Example 1:
+
+Input: 4
+Output: 2
+Example 2:
+
+Input: 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since 
+             the decimal part is truncated, 2 is returned.*/
+
 public class E69_Sqrtx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int input = 8;
+        int output = mySqrt(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	/*
+    solution:
+    The points of interest are the peaks and valleys in the given graph. 
+    We need to find the largest peak following the smallest valley. 
+    We can maintain two variables - minprice and maxprofit corresponding 
+    to the smallest valley and maximum profit (maximum difference between 
+    selling price and minprice) obtained so far respectively.
+    */
+	
+	/*
+     * @param prices: a list of integers
+     * @return: find a maximum profit
+     */
+	
 
 }
