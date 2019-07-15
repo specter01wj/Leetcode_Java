ab96fb566e016f511bbf3d4664f899ec2e5b3781
specter01wj <specter01wj@gmail.com>
2019-07-15 12:13:52 -0400

    07/15/2019 v1

--- mysol/Category/Math/E367_Valid_Perfect_Square/src/com/E367_Valid_Perfect_Square/E367_Valid_Perfect_Square.java
+++ mysol/Category/Math/E367_Valid_Perfect_Square/src/com/E367_Valid_Perfect_Square/E367_Valid_Perfect_Square.java
@@ -1,10 +1,39 @@
 package com.E367_Valid_Perfect_Square;
 
+import java.util.*;
+
+/*Given a positive integer num, write a function which returns True 
+ * if num is a perfect square else False.
+
+Note: Do not use any built-in library function such as sqrt.
+
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 14
+Output: false*/
+
 public class E367_Valid_Perfect_Square {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int input = 16;
+        boolean output = isPerfectSquare(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	/*
+    solution:
+    利用二分法求解。利用二分法找到一个最小的ans，满足ans的平方不小于num,
+    然后判断ans的平方是否等于num即可
+    */
+	
+	/*
+     * @param num: a positive integer
+     * @return: boolean, if num is a perfect square else False
+     */
+	
 
 }
