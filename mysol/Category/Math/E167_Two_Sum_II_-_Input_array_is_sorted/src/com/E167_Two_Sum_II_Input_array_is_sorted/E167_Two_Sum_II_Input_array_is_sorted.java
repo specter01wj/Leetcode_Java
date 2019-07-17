69a6475d2afa7dcf4f2359f7f04e7a4222109758
specter01wj <specter01wj@gmail.com>
2019-07-17 16:25:40 -0400

    07/15/2019 v1

---
+++ mysol/Category/Math/E167_Two_Sum_II_-_Input_array_is_sorted/src/com/E167_Two_Sum_II_Input_array_is_sorted/E167_Two_Sum_II_Input_array_is_sorted.java
@@ -0,0 +1,10 @@
+package com.E167_Two_Sum_II_Input_array_is_sorted;
+
+public class E167_Two_Sum_II_Input_array_is_sorted {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
