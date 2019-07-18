d07ea9aa4e4654dc6092a66c9631e0ab2a4632fa
specter01wj <specter01wj@gmail.com>
2019-07-18 11:54:00 -0400

    07/15/2019 v1

--- mysol/Category/Math/E202_Happy_Number/src/com/E202_Happy_Number/E202_Happy_Number.java
+++ mysol/Category/Math/E202_Happy_Number/src/com/E202_Happy_Number/E202_Happy_Number.java
@@ -27,5 +27,41 @@ public class E202_Happy_Number {
         boolean output = isHappy(input);
         System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	/*
+    solution:
+    The points of interest are the peaks and valleys in the given graph. 
+    We need to find the largest peak following the smallest valley. 
+    We can maintain two variables - minprice and maxprofit corresponding 
+    to the smallest valley and maximum profit (maximum difference between 
+    selling price and minprice) obtained so far respectively.
+    */
+	
+	/*
+     * @param prices: a list of integers
+     * @return: find a maximum profit
+     */
+	public boolean isHappy(int n) {
+		HashSet<Integer> hash = new HashSet<Integer>();
+		
+		while(n != 1) {
+			if(hash.contains(n)) {
+				return false;
+			}
+			hash.add(n);
+			n = getNextHappy(n);
+		}
+		
+		return true;
+	}
+	
+	private int getNextHappy(int n) {
+		int sum = 0;
+		while(n != 0) {
+			sum += (n % 10) * (n % 10);
+			n /= 10;
+		}
+		return sum;
+	}
 
 }
