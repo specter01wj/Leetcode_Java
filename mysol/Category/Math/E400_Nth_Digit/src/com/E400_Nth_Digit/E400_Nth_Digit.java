5b15dc74f39ba9fb65b9ab05c99029a00342d0bb
specter01wj <specter01wj@gmail.com>
2019-07-19 18:36:11 -0400

    07/15/2019 v1

--- mysol/Category/Math/E400_Nth_Digit/src/com/E400_Nth_Digit/E400_Nth_Digit.java
+++ mysol/Category/Math/E400_Nth_Digit/src/com/E400_Nth_Digit/E400_Nth_Digit.java
@@ -42,7 +42,7 @@ public class E400_Nth_Digit {
 	
 	/*
      * @param n: a positive integer
-     * @return: the nth digit of the infinite integer sequence
+     * @return: integer, the nth digit of the infinite integer sequence
      */
 	public static int findNthDigit(int n) {
         // 初始化一位数的整数有9个，从1开始
