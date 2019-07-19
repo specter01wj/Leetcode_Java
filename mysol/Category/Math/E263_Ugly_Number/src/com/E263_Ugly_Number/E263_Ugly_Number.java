64dd3a1bfe2028cdb54d854d570ac34ba197dc72
specter01wj <specter01wj@gmail.com>
2019-07-19 12:45:40 -0400

    07/15/2019 v1

--- mysol/Category/Math/E263_Ugly_Number/src/com/E263_Ugly_Number/E263_Ugly_Number.java
+++ mysol/Category/Math/E263_Ugly_Number/src/com/E263_Ugly_Number/E263_Ugly_Number.java
@@ -1,10 +1,44 @@
 package com.E263_Ugly_Number;
 
+import java.util.*;
+
+/*Write a program to check whether a given number is an ugly number.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+
+Example 1:
+
+Input: 6
+Output: true
+Explanation: 6 = 2 × 3
+Example 2:
+
+Input: 8
+Output: true
+Explanation: 8 = 2 × 2 × 2
+Example 3:
+
+Input: 14
+Output: false 
+Explanation: 14 is not ugly since it includes another prime factor 7.*/
+
 public class E263_Ugly_Number {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int input = 6;
+        boolean output = isUgly(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	/*
+    solution:
+    看这个数是否只有2，3，5的因子
+    */
+	
+	/*
+     * @param num: an integer
+     * @return: boolean, true if num is an ugly number or false
+     */
+	
 
 }
