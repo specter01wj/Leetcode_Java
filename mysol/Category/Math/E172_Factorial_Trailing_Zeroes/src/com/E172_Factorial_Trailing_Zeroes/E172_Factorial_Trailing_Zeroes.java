7f016fbc5d38adf0c9fcdb19319c7e2a4a382daf
specter01wj <specter01wj@gmail.com>
2019-07-22 12:53:36 -0400

    07/15/2019 v1

---
+++ mysol/Category/Math/E172_Factorial_Trailing_Zeroes/src/com/E172_Factorial_Trailing_Zeroes/E172_Factorial_Trailing_Zeroes.java
@@ -0,0 +1,10 @@
+package com.E172_Factorial_Trailing_Zeroes;
+
+public class E172_Factorial_Trailing_Zeroes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
