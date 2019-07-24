8115f0470d13961dd335c2c0c1e6a65072fbe3c7
specter01wj <specter01wj@gmail.com>
2019-07-24 12:40:08 -0400

    07/15/2019 v1

--- mysol/Category/Bit Manipulation/E268_Missing_Number/src/com/E268_Missing_Number/E268_Missing_Number.java
+++ mysol/Category/Bit Manipulation/E268_Missing_Number/src/com/E268_Missing_Number/E268_Missing_Number.java
@@ -1,10 +1,49 @@
 package com.E268_Missing_Number;
 
+import java.util.*;
+
+/*Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, 
+ * find the one that is missing from the array.
+
+Example 1:
+
+Input: [3,0,1]
+Output: 2
+Example 2:
+
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8*/
+
 public class E268_Missing_Number {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {9,6,4,2,3,5,7,0,1};//{27,26,29,30,31};
+        int output = missingNumber(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	/*
+    solution:
+    The basic idea is to use XOR operation. 
+    We all know that a^b^b =a, which means two xor operations with the same number 
+    will eliminate the number and reveal the original number.
+	In this solution, I apply XOR operation to both the index and value of the array. 
+	In a complete array with no missing numbers, the index and value should be perfectly 
+	corresponding( nums[index] = index), so in a missing array, what left finally is 
+	the missing number.
+    */
+	
+	/*
+     * @param nums: an array of integers
+     * @return: an integer
+     */
+	public static int missingNumber(int[] nums) {
+        int xor = 0, i = 0;
+        for (i = 0; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+
+        return xor ^ i;
+    }
 
 }
