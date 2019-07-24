3e3f80109b2e6334d95bb45d73f4d85b49ce5972
specter01wj <specter01wj@gmail.com>
2019-07-24 10:55:29 -0400

    07/15/2019 v1

--- mysol/Category/Bit Manipulation/E371_Sum_of_Two_Integers/src/com/E371_Sum_of_Two_Integers/E371_Sum_of_Two_Integers.java
+++ mysol/Category/Bit Manipulation/E371_Sum_of_Two_Integers/src/com/E371_Sum_of_Two_Integers/E371_Sum_of_Two_Integers.java
@@ -1,10 +1,37 @@
 package com.E371_Sum_of_Two_Integers;
 
+import java.util.*;
+
+/*Calculate the sum of two integers a and b, 
+ * but you are not allowed to use the operator + and -.
+
+Example 1:
+
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+
+Input: a = -2, b = 3
+Output: 1*/
+
 public class E371_Sum_of_Two_Integers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {7,1,5,3,6,4};//{7,6,4,3,1};
+        int output = maxProfit(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	/*
+    solution:
+    
+    */
+	
+	/*
+     * @param a: an integer
+     * @param b: an integer
+     * @return: sum of two integers a and b
+     */
+	
 
 }
