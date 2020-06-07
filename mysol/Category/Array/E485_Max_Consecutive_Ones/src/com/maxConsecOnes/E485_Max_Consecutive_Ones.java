e640c0b3a6a7c09352d99e662b70f20e4005905f
specter01wj <specter01wj@gmail.com>
2020-06-07 16:31:17 -0600

    06/04/2020 v1

---
+++ mysol/Category/Array/E485_Max_Consecutive_Ones/src/com/maxConsecOnes/E485_Max_Consecutive_Ones.java
@@ -0,0 +1,35 @@
+package com.maxConsecOnes;
+import java.util.*;
+
+/*Given a binary array, find the maximum number of consecutive 1s in this array.
+
+Example 1:
+Input: [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s.
+    The maximum number of consecutive 1s is 3.
+Note:
+
+The input array will only contain 0 and 1.
+The length of input array is a positive integer and will not exceed 10,000*/
+
+public class E485_Max_Consecutive_Ones {
+
+	public static void main(String[] args) {
+		int[] input = {1,1,0,1,1,1};
+        int output = findMaxConsecutiveOnes(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	/*
+	solution:
+	动态规划思想。 同时记录count和result，若当前数为1，count加一，若count此时大于result，
+	将result更新为count。若不为1，重置连续数count为0。
+	*/
+	
+	/**
+	 * @param {number[]} nums
+	 * @return {number}
+	 */
+
+}
