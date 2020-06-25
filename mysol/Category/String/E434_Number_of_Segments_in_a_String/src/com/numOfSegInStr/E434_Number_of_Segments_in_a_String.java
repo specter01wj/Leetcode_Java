ebad526f708330c374e5c0513854476ac472dbf7
specter01wj <specter01wj@gmail.com>
2020-06-25 11:19:08 -0600

    06/04/2020 v1

--- mysol/Category/String/E434_Number_of_Segments_in_a_String/src/com/numOfSegInStr/E434_Number_of_Segments_in_a_String.java
+++ mysol/Category/String/E434_Number_of_Segments_in_a_String/src/com/numOfSegInStr/E434_Number_of_Segments_in_a_String.java
@@ -34,8 +34,12 @@ public class E434_Number_of_Segments_in_a_String {
 	public static int countSegments(String s) {
 		int res = 0;
 		for(int i = 0; i < s.length(); i++) {
-			if(s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' '))
+			if(s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' ')) {
+				res++;
+			}
 		}
+		
+		return res;
 	}
 
 }
