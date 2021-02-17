634490e7c1c039705a16480aa23e213e76b5cd7e
Jin Wang <specter01wj@gmail.com>
2021-02-17 11:22:25 -0500

    02/12/2021 v1

--- mysol/Category/Array/E228_Summary_Ranges/src/com/summaryRanges/E228_Summary_Ranges.java
+++ mysol/Category/Array/E228_Summary_Ranges/src/com/summaryRanges/E228_Summary_Ranges.java
@@ -52,6 +52,20 @@ public class E228_Summary_Ranges {
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    思路就是维持start和end两个指针, 然后:
+	如果end的后面一个数是end + 1, 那么继续扩展end
+	如果end后面一个数不是end + 1: 1. 判断start和end的关系决定输出: a. 
+	如果start和end相等, 那么就直接输出这个数 b. 如果start和end不相等, 那么就输出"start->end"
+	把start和end置为当前数
+	最后别忘了处理一下剩下的最后一组start和end就行了 []代表的是这个下标所在的值, start和end是两个下标
+    */
+	
+	/**
+     * @param nums:  a sorted integer array without duplicates
+     * @return: the summary of its ranges
+     */
 	public static List<String> summaryRanges(int[] nums) {
 		List<String> list = new ArrayList();
 		if(nums.length == 1) {
