aecda2f61e1f0d2b361d8a612dc77b6702e89eb1
Jin Wang <specter01wj@gmail.com>
2021-02-20 12:15:42 -0500

    02/12/2021 v1

--- mysol/Category/Array/E628_Maximum_Product_of_Three_Numbers/src/com/maximumProductofThreeNumbers/E628_Maximum_Product_of_Three_Numbers.java
+++ mysol/Category/Array/E628_Maximum_Product_of_Three_Numbers/src/com/maximumProductofThreeNumbers/E628_Maximum_Product_of_Three_Numbers.java
@@ -27,6 +27,19 @@ public class E628_Maximum_Product_of_Three_Numbers {
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    我们将数组从小到大排序后， 
+    1、假设数组元素全为负数或只有一个负数或全为正数，此时将数组中最大的三个元素相乘即为最大乘积； 
+    2、假设只有一个正数或两个正数，此时将数组的前两个负数与最后一个正数相乘即为最大乘积； 
+    3、假设正数多于三个且负数多于两个，则最大乘积应为前两个负数与最后一个正数的乘积与最大的
+    三个元素的乘积中的最大值。
+    */
+	
+	/**
+     * @param nums: an integer array
+     * @return: the maximum product
+     */
 	public static int maximumProduct(int[] nums) {
 		int n = nums.length;
 		Arrays.sort(nums);
