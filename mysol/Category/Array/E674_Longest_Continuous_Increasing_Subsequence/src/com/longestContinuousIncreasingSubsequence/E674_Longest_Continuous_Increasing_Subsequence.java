65fb0d8a12218f9a253b26092c6ec303844ff055
Jin Wang <specter01wj@gmail.com>
2021-02-21 12:18:32 -0500

    02/12/2021 v1

--- mysol/Category/Array/E674_Longest_Continuous_Increasing_Subsequence/src/com/longestContinuousIncreasingSubsequence/E674_Longest_Continuous_Increasing_Subsequence.java
+++ mysol/Category/Array/E674_Longest_Continuous_Increasing_Subsequence/src/com/longestContinuousIncreasingSubsequence/E674_Longest_Continuous_Increasing_Subsequence.java
@@ -35,5 +35,19 @@ public class E674_Longest_Continuous_Increasing_Subsequence {
         int output = findLengthOfLCIS(input);
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	public static int findLengthOfLCIS(int[] nums) {
+		int res = 0, cnt = 0;
+		
+		for(int i = 0; i < nums.length; i++) {
+			if(i == 0 || nums[i - 1] < nums[i]) {
+				res = Math.max(res, ++cnt);
+			} else {
+				cnt = 1;
+			}
+		}
+		
+		return res;
+	}
 
 }
