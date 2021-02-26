e1ee9b66ca98316aa7b9d179c429b98f27338b70
Jin Wang <specter01wj@gmail.com>
2021-02-26 18:19:14 -0500

    02/12/2021 v1

--- mysol/Category/Array/E867_Transpose_Matrix/src/com/transposeMatrix/E867_Transpose_Matrix.java
+++ mysol/Category/Array/E867_Transpose_Matrix/src/com/transposeMatrix/E867_Transpose_Matrix.java
@@ -1,10 +1,45 @@
 package com.transposeMatrix;
+import java.util.*;
+
+/*Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal, 
+switching the matrix's row and column indices.
+
+
+
+ 
+
+Example 1:
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+
+Example 2:
+
+Input: matrix = [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]*/
 
 public class E867_Transpose_Matrix {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[][] input = {{1,2,3},{4,5,6},{7,8,9}};
+        int[][] output = transpose(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	public static int[][] transpose(int[][] matrix) {
+		int n = matrix.length;
+		int m = matrix[0].length;
+		int[][] res = new int[m][n];
+		
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < n; j++) {
+				res[i][j] = matrix[j][i];
+			}
+		}
+		
+		return res;
 	}
 
 }
