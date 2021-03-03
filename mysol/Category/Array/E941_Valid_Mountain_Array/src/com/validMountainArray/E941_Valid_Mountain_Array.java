6a7f182f2ab5e562a1e389c5c8beaad1d76a2a75
Jin Wang <specter01wj@gmail.com>
2021-03-03 11:35:27 -0500

    02/12/2021 v1

---
+++ mysol/Category/Array/E941_Valid_Mountain_Array/src/com/validMountainArray/E941_Valid_Mountain_Array.java
@@ -0,0 +1,59 @@
+package com.validMountainArray;
+import java.util.*;
+
+/*Given an array of integers arr, return true if and only 
+if it is a valid mountain array.
+
+Recall that arr is a mountain array if and only if:
+
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+ 
+
+Example 1:
+
+Input: arr = [2,1]
+Output: false
+
+Example 2:
+
+Input: arr = [3,5,5]
+Output: false
+
+Example 3:
+
+Input: arr = [0,3,2,1]
+Output: true*/
+
+public class E941_Valid_Mountain_Array {
+
+	public static void main(String[] args) {
+		int[] input = {0,3,2,1};
+        boolean output = validMountainArray(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	public static boolean validMountainArray(int[] arr) {
+		if(arr == null || arr.length < 3) return false;
+		
+		int i = 0;
+		
+		while( (i+1) <= arr.length - 1 && arr[i] < arr[i+1]) {
+			i++;
+		}
+		
+		if(i == arr.length - 1 || i == 0) return false;
+		
+		while( (i+1) <= arr.length - 1 && arr[i] > arr[i+1]) {
+			i++;
+		}
+		
+		if(i == arr.length - 1) return true;
+		
+		return false;
+	}
+
+}
