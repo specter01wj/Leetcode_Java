567f4cf5203152133d021e036b5e48be9fe086a3
Jin Wang <specter01wj@gmail.com>
2021-03-03 12:05:02 -0500

    02/12/2021 v1

---
+++ mysol/Category/Array/E977_Squares_of_a_Sorted_Array/src/com/squaresofaSortedArray/E977_Squares_of_a_Sorted_Array.java
@@ -0,0 +1,41 @@
+package com.squaresofaSortedArray;
+import java.util.*;
+
+/*Given an integer array nums sorted in non-decreasing order, 
+return an array of the squares of each number sorted in 
+non-decreasing order.
+
+		 
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]*/
+
+public class E977_Squares_of_a_Sorted_Array {
+
+	public static void main(String[] args) {
+		int[] input = {-4,-1,0,3,10};
+		System.out.println("input: " + Arrays.toString(input) );
+        int[] output = sortedSquares(input);
+        System.out.println("output: " + Arrays.toString(output));
+	}
+	
+	public static int[] sortedSquares(int[] nums) {
+		int length = nums.length;
+		for(int i = 0; i < length; i++) {
+			nums[i] = nums[i] * nums[i];
+		}
+		Arrays.sort(nums);
+		
+		return nums;
+	}
+
+}
