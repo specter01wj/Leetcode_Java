5fbdd5b5ec50bb173e7c81b0c48ffd5b97815f2c
Jin Wang <specter01wj@gmail.com>
2021-03-05 10:40:07 -0500

    02/12/2021 v1

--- mysol/Category/Array/E999_Available_Captures_for_Rook/src/com/availableCapturesforRook/E999_Available_Captures_for_Rook.java
+++ mysol/Category/Array/E999_Available_Captures_for_Rook/src/com/availableCapturesforRook/E999_Available_Captures_for_Rook.java
@@ -59,9 +59,44 @@ Explanation: The rook is attacking the pawns at positions b5, d6, and f5.*/
 public class E999_Available_Captures_for_Rook {
 
 	public static void main(String[] args) {
-		int[] input = {7,1,5,3,6,4};//{7,6,4,3,1};
+		char[][] input = {{'.','.','.','.','.','.','.','.'},
+						 	{'.','.','.','p','.','.','.','.'},
+						 	{'.','.','.','p','.','.','.','.'},
+						 	{'p','p','.','R','.','p','B','.'},
+						 	{'.','.','.','.','.','.','.','.'},
+						 	{'.','.','.','B','.','.','.','.'},
+						 	{'.','.','.','p','.','.','.','.'},
+						 	{'.','.','.','.','.','.','.','.'}};
         int output = numRookCaptures(input);
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    1. Find index of the white rook
+	2. Search in 4 direction until 8 steps or meet any piece.
+    */
+	
+	/*
+     * @param board: a list of chars
+     * @return: available captures for rook
+     */
+	public static int numRookCaptures(char[][] board) {
+		int x0 = 0, y0 = 0, res = 0;
+        for (int i = 0; i < 8; ++i)
+            for (int j = 0; j < 8; ++j)
+                if (board[i][j] == 'R') {
+                    x0 = i;
+                    y0 = j;
+                }
+
+        for (int[] d : new int[][] {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}) {
+            for (int x = x0 + d[0], y = y0 + d[1]; 0 <= x && x < 8 && 0 <= y && y < 8; x += d[0], y += d[1]) {
+                if (board[x][y] == 'p') res++;
+                if (board[x][y] != '.') break;
+            }
+        }
+        return res;
 	}
 
 }
