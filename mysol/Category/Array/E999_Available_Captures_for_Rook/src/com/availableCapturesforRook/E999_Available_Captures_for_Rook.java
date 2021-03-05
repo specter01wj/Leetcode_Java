98e9c110aba35ecd3012481dec25890462b4f69d
Jin Wang <specter01wj@gmail.com>
2021-03-05 10:45:30 -0500

    02/12/2021 v1

--- mysol/Category/Array/E999_Available_Captures_for_Rook/src/com/availableCapturesforRook/E999_Available_Captures_for_Rook.java
+++ mysol/Category/Array/E999_Available_Captures_for_Rook/src/com/availableCapturesforRook/E999_Available_Captures_for_Rook.java
@@ -83,12 +83,14 @@ public class E999_Available_Captures_for_Rook {
      */
 	public static int numRookCaptures(char[][] board) {
 		int x0 = 0, y0 = 0, res = 0;
-        for (int i = 0; i < 8; ++i)
-            for (int j = 0; j < 8; ++j)
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
                 if (board[i][j] == 'R') {
                     x0 = i;
                     y0 = j;
                 }
+            }
+        }
 
         for (int[] d : new int[][] {{1, 0}, {0, 1}, {-1, 0}, {0, -1}}) {
             for (int x = x0 + d[0], y = y0 + d[1]; 0 <= x && x < 8 && 0 <= y && y < 8; x += d[0], y += d[1]) {
