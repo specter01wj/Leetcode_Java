98114c8f374ba2d5734ff07f04c5cd5fd42277f7
Jin Wang <specter01wj@gmail.com>
2021-03-11 03:10:31 -0500

    02/12/2021 v1

---
+++ mysol/Category/String/E709_To_Lower_Case/src/com/toLowerCase/E709_To_Lower_Case.java
@@ -0,0 +1,31 @@
+package com.toLowerCase;
+import java.util.*;
+
+/*Implement function ToLowerCase() that has a string parameter str, 
+and returns the same string in lowercase.
+
+
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+
+Example 2:
+
+Input: "here"
+Output: "here"
+
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"*/
+
+public class E709_To_Lower_Case {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
