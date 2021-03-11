2754136726377161a5d4ffecd794e5af3d4a49b8
Jin Wang <specter01wj@gmail.com>
2021-03-11 03:13:02 -0500

    02/12/2021 v1

--- mysol/Category/String/E709_To_Lower_Case/src/com/toLowerCase/E709_To_Lower_Case.java
+++ mysol/Category/String/E709_To_Lower_Case/src/com/toLowerCase/E709_To_Lower_Case.java
@@ -24,8 +24,21 @@ Output: "lovely"*/
 public class E709_To_Lower_Case {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "Hello";
+        String output = toLowerCase(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
+	}
+	
+	public static String toLowerCase(String str) {
+		char[] a = str.toCharArray();
+		
+        for(int i = 0; i < a.length; i++) {
+            if(a[i] >= 'A' && a[i] <= 'Z') {
+            	a[i] = (char) (a[i] - 'A' + 'a');
+            }
+        }
+        
+        return new String(a);
 	}
 
 }
