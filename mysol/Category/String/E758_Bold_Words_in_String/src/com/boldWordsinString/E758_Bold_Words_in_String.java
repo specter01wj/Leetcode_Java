6c98c520f3f842f12ab9ebf950f868350c639118
Jin Wang <specter01wj@gmail.com>
2021-03-12 02:38:17 -0500

    02/12/2021 v1

--- mysol/Category/String/E758_Bold_Words_in_String/src/com/boldWordsinString/E758_Bold_Words_in_String.java
+++ mysol/Category/String/E758_Bold_Words_in_String/src/com/boldWordsinString/E758_Bold_Words_in_String.java
@@ -24,7 +24,7 @@ public class E758_Bold_Words_in_String {
 	}
 	
 	public static String boldWords(String[] words, String S) {
-		int[] s = new int[S.length()+1];
+		int[] s = new int[S.length() + 1];
 		
         for(String w : words) {
             int i = 0;
@@ -34,15 +34,24 @@ public class E758_Bold_Words_in_String {
                 i++;
             }
         }
+        
         StringBuilder sb = new StringBuilder();
         int pre = 0, sum = 0;
+        
         for(int i = 0; i <= S.length(); i++) {
             sum += s[i];
-            if (sum > 0 && pre == 0) sb.append("<b>");
-            if (sum == 0 && pre > 0) sb.append("</b>");
-            if (i < S.length()) sb.append(S.charAt(i));
+            if(sum > 0 && pre == 0) {
+            	sb.append("<b>");
+            }
+            if(sum == 0 && pre > 0) {
+            	sb.append("</b>");
+            }
+            if (i < S.length()) {
+            	sb.append(S.charAt(i));
+            }
             pre = sum;
         }
+        
         return sb.toString();
 	}
 
