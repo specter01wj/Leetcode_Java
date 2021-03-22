0a46708bd82610d2ea6f1eb4c8651af08ee52041
Jin Wang <specter01wj@gmail.com>
2021-03-21 20:08:39 -0400

    02/12/2021 v1

--- mysol/Category/String/E819_Most_Common_Word/src/com/mostCommonWord/E819_Most_Common_Word.java
+++ mysol/Category/String/E819_Most_Common_Word/src/com/mostCommonWord/E819_Most_Common_Word.java
@@ -33,18 +33,38 @@ public class E819_Most_Common_Word {
 
 	public static void main(String[] args) {
 		String input = "Bob hit a ball, the hit BALL flew far after it was hit.";
-        String output = mostCommonWord(input);
+        String[] banned = {"hit"};
+		String output = mostCommonWord(input, banned);
         System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    利用哈希表记录每个单词出现的次数即可
+    4 steps:
+		1.remove all punctuations
+		2.change to lowercase
+		3.words count for each word not in banned set
+		4.return the most common word
+    */
+	
+	/**
+     * @param paragraph:
+     * @param banned:
+     * @return: nothing
+     */
 	public static String mostCommonWord(String paragraph, String[] banned) {
 		Set<String> ban = new HashSet<> (Arrays.asList(banned));
         Map<String, Integer> count = new HashMap<>();
         String[] words = paragraph.replaceAll ("\\pP", " ").toLowerCase().split ("\\s+");
         
-        for (String w : words) if (!ban.contains (w)) count.put (w, count.getOrDefault (w, 0) + 1);
+        for(String w : words) {
+        	if(!ban.contains(w)) {
+        		count.put(w, count.getOrDefault(w, 0) + 1);
+        	}
+        }
         
-        return Collections.max (count.entrySet(), Map.Entry.comparingByValue()).getKey();
+        return Collections.max(count.entrySet(), Map.Entry.comparingByValue()).getKey();
 	}
 
 }
