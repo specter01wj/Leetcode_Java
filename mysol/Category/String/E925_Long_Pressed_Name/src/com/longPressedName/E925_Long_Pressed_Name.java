3ad147bb100d0d389cd817c6b1a914da24ba35c4
Jin Wang <specter01wj@gmail.com>
2021-04-04 23:35:23 -0400

    02/12/2021 v1

--- mysol/Category/String/E925_Long_Pressed_Name/src/com/longPressedName/E925_Long_Pressed_Name.java
+++ mysol/Category/String/E925_Long_Pressed_Name/src/com/longPressedName/E925_Long_Pressed_Name.java
@@ -44,6 +44,22 @@ public class E925_Long_Pressed_Name {
         System.out.println("input: " + (input) + " typed: " + (typed) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    采用双指针的做法来解决本题，假设name的指针为 i,typed的指针为 j,两者均从 0 开始。 
+    使结果为false的情况主要有四种： 
+    1. typed的第一个字符与name的第一个字符不相等 
+    2. namei != typedj 成立，且typedj != typedj-1成立，
+       比如：name = "ab", i = 1, typed = "ac", j = 1 
+    3. name遍历完成，typed还没有遍历完成且出现了与name最后一个字符不一样的字符，
+       比如：name = "ab", typed = "abbbbc" 4. typed遍历完成，而name还没有遍历完成
+    */
+	
+	/**
+     * @param name: your friend's name
+     * @param typed: the typed string
+     * @return: Return True if it is possible that it was your friends name
+     */
 	public static boolean isLongPressedName(String name, String typed) {
 		int m = name.length(), n = typed.length();
 		int i = 0;
