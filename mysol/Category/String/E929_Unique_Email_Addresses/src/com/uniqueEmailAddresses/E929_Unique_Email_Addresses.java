afab642a7cecefda33ae4fe1b1401516c36ad31f
Jin Wang <specter01wj@gmail.com>
2021-04-05 11:01:37 -0400

    02/12/2021 v1

--- mysol/Category/String/E929_Unique_Email_Addresses/src/com/uniqueEmailAddresses/E929_Unique_Email_Addresses.java
+++ mysol/Category/String/E929_Unique_Email_Addresses/src/com/uniqueEmailAddresses/E929_Unique_Email_Addresses.java
@@ -1,10 +1,67 @@
 package com.uniqueEmailAddresses;
+import java.util.*;
+
+/*Every valid email consists of a local name and a domain name, 
+separated by the '@' sign. Besides lowercase letters, 
+the email may contain one or more '.' or '+'.
+
+For example, in "alice@leetcode.com", "alice" is the local name, 
+and "leetcode.com" is the domain name.
+
+If you add periods '.' between some characters in the local name 
+part of an email address, mail sent there will be forwarded to 
+the same address without dots in the local name. Note that this 
+rule does not apply to domain names.
+
+For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward 
+to the same email address.
+
+If you add a plus '+' in the local name, everything after the first 
+plus sign will be ignored. This allows certain emails to be filtered. 
+Note that this rule does not apply to domain names.
+
+For example, "m.y+name@email.com" will be forwarded to "my@email.com".
+
+It is possible to use both of these rules at the same time.
+
+Given an array of strings emails where we send one email to each email[i], 
+return the number of different addresses that actually receive mails.
+
+ 
+
+Example 1:
+
+Input: emails = ["test.email+alex@leetcode.com",
+"test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+Output: 2
+Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" 
+actually receive mails.
+
+Example 2:
+
+Input: emails = ["a@leetcode.com","b@leetcode.com","c@leetcode.com"]
+Output: 3*/
 
 public class E929_Unique_Email_Addresses {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[] input = {"test.email+alex@leetcode.com",
+				"test.e.mail+bob.cathy@leetcode.com",
+				"testemail+david@lee.tcode.com"};
+        int output = numUniqueEmails(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	public static int numUniqueEmails(String[] emails) {
+		Set<String> diff = new HashSet<>();
+		
+		for(String email : emails) {
+			String[] parts = email.split("@");
+			String[] local = parts[0].split("\\+");
+			diff.add(local[0].replace(".", "") + "@" + parts[1]);
+		}
+		
+		return diff.size();
 	}
 
 }
