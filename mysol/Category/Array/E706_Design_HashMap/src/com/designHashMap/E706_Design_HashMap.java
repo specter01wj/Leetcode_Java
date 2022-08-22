70fe8be72d0b87a75186b7b8eaf2bcf48238b555
Jin Wang <specter01wj@gmail.com>
2022-08-22 12:34:24 -0400

    v6

--- mysol/Category/Array/E706_Design_HashMap/src/com/designHashMap/E706_Design_HashMap.java
+++ mysol/Category/Array/E706_Design_HashMap/src/com/designHashMap/E706_Design_HashMap.java
@@ -1,13 +1,56 @@
 package com.designHashMap;
+import java.util.*;
+
+/*
+Design a HashMap without using any built-in hash table libraries.
+
+Implement the MyHashMap class:
+
+MyHashMap() initializes the object with an empty map.
+void put(int key, int value) inserts a (key, value) pair into the HashMap. 
+If the key already exists in the map, update the corresponding value.
+int get(int key) returns the value to which the specified key is mapped, 
+or -1 if this map contains no mapping for the key.
+void remove(key) removes the key and its corresponding value if the map 
+contains the mapping for the key.
+
+
+Example 1:
+
+Input
+["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+[[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+Output
+[null, null, null, 1, -1, null, 1, null, -1]
+
+Explanation
+MyHashMap myHashMap = new MyHashMap();
+myHashMap.put(1, 1); // The map is now [[1,1]]
+myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+myHashMap.get(1);    // return 1, The map is now [[1,1], [2,2]]
+myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
+myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
+*/
 
 public class E706_Design_HashMap {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MyHashMap myHashMap = new MyHashMap();
+		int test1 = -1;
+		myHashMap.put(1, 1); // The map is now [[1,1]]
+		myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+		test1 = myHashMap.get(1);    // return 1, The map is now [[1,1], [2,2]]
+		test1 = myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+		myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+		test1 = myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
+		myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+		test1 = myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
 	}
 	
-	class ListNode {
+	static class ListNode {
 	    int key, val;
 	    ListNode next;
 	    public ListNode(int key, int val, ListNode next) {
@@ -16,7 +59,7 @@ public class E706_Design_HashMap {
 	        this.next = next;
 	    }
 	}
-	class MyHashMap {
+	static class MyHashMap {
 	    static final int size = 19997;
 	    static final int mult = 12582917;
 	    ListNode[] data;
