76224647fca8f1c0f2e0dffd97d2ff5061313ae5
Jin Wang <specter01wj@gmail.com>
2022-08-22 11:33:00 -0400

    v1

---
+++ mysol/Category/Array/E704_Binary_Search/src/com/binarySearch/E704_Binary_Search.java
@@ -0,0 +1,60 @@
+package com.binarySearch;
+import java.util.*;
+
+/*
+Given an array of integers nums which is sorted in ascending order, 
+and an integer target, write a function to search target in nums. 
+If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+*/
+
+public class E704_Binary_Search {
+
+	public static void main(String[] args) {
+		int[] input = {-1,0,3,5,9,12};
+        int output = search(input, 9);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	
+	public static int search(int[] nums, int target) {
+		if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                end = mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        
+        if (nums[start] == target) {
+            return start;
+        }
+        if (nums[end] == target) {
+            return end;
+        }
+        
+        return -1;
+	}
+
+}
