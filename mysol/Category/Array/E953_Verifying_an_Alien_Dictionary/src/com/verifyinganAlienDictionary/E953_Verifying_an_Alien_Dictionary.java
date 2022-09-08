84175a9ff1732bbf5589703167b7a335b0fed094
Jin Wang <specter01wj@gmail.com>
2022-09-08 17:42:49 -0400

    v5

---
+++ mysol/Category/Array/E953_Verifying_an_Alien_Dictionary/src/com/verifyinganAlienDictionary/E953_Verifying_an_Alien_Dictionary.java
@@ -0,0 +1,10 @@
+package com.verifyinganAlienDictionary;
+
+public class E953_Verifying_an_Alien_Dictionary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
