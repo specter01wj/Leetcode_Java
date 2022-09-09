8826724a69451f79bd747be91befd3669eedd026
Jin Wang <specter01wj@gmail.com>
2022-09-09 19:23:27 -0400

    v9

--- mysol/Category/Array/E976_Largest_Perimeter_Triangle/src/com/largestPerimeterTriangle/E976_Largest_Perimeter_Triangle.java
+++ mysol/Category/Array/E976_Largest_Perimeter_Triangle/src/com/largestPerimeterTriangle/E976_Largest_Perimeter_Triangle.java
@@ -26,7 +26,19 @@ public class E976_Largest_Perimeter_Triangle {
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    1. We sort the A
+	2. Try to get a triangle with 3 biggest numbers.
+	3. If A[n-1] < A[n-2] + A[n-3], we get a triangle.
+	   If A[n-1] >= A[n-2] + A[n-3] >= A[i] + A[j], we cannot get any triangle with A[n-1]
+	4. repeat step2 and step3 with the left numbers.
+    */
 	
+	/**
+     * @param nums: an array
+     * @return: the largest perimeter of a triangle with a non-zero area
+     */
 	public static int largestPerimeter(int[] nums) {
 		Arrays.sort(nums);
         for (int i = nums.length - 1; i > 1; --i) {
