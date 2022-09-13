c4e776e8284a056981a0340575376227eed765f1
Jin Wang <specter01wj@gmail.com>
2022-09-13 19:09:46 -0400

    v8

--- mysol/Category/String/E412_Fizz_Buzz/src/com/fizzBuzz/E412_Fizz_Buzz.java
+++ mysol/Category/String/E412_Fizz_Buzz/src/com/fizzBuzz/E412_Fizz_Buzz.java
@@ -35,7 +35,15 @@ public class E412_Fizz_Buzz {
         System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    能同时被3跟5整除的数，肯定也能被3整除，也能被5整除，所以要先判断能否被15整除
+    */
 	
+	/**
+     * param n: As description.
+     * return: A list of strings.
+     */
 	public static List<String> fizzBuzz(int n) {
 		ArrayList<String> results = new ArrayList<String>();
         for (int i = 1; i <= n; i++) {
