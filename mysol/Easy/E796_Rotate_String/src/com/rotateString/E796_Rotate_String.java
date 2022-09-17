503e5a64811ab5e274cae905a4d9acc2e48d018c
Jin Wang <specter01wj@gmail.com>
2022-09-17 13:20:35 -0400

    v17

---
+++ mysol/Easy/E796_Rotate_String/src/com/rotateString/E796_Rotate_String.java
@@ -0,0 +1,48 @@
+package com.rotateString;
+import java.util.*;
+
+/*
+Given two strings s and goal, return true if and only if s can become 
+goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the 
+rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+
+
+Example 1:
+
+Input: s = "abcde", goal = "cdeab"
+Output: true
+
+Example 2:
+
+Input: s = "abcde", goal = "abced"
+Output: false
+*/
+
+public class E796_Rotate_String {
+
+	public static void main(String[] args) {
+		String input1 = "abcde";
+		String input2 = "cdeab";
+        boolean output = rotateString(input1, input2);
+        System.out.println("input1: " + (input1) + "; input2: " + input2 + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    it is rotated if B can be found in (A + A).
+    */
+	
+	/**
+     * @param s: an array of char
+     * @param goal: an array of char
+     * @return: boolean whether s can become goal after some number of shifts on s
+     */
+	public static boolean rotateString(String s, String goal) {
+		return (s.length() == goal.length()) && (s + s).contains(goal);
+	}
+
+}
