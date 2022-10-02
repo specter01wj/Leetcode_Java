92853bc0773e423276f165e90a005061bcdd8ee5
Jin Wang <specter01wj@gmail.com>
2022-10-02 18:40:27 -0400

    v1

---
+++ mysol/Category/String/E1071_Greatest_Common_Divisor_of_Strings/src/com/greatestCommonDivisorofStrings/E1071_Greatest_Common_Divisor_of_Strings.java
@@ -0,0 +1,10 @@
+package com.greatestCommonDivisorofStrings;
+
+public class E1071_Greatest_Common_Divisor_of_Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
