c7546b973bcf6e070c3f0c646590ad750d24de66
Jin Wang <specter01wj@gmail.com>
2022-10-02 18:49:20 -0400

    v2

--- mysol/Category/String/E1071_Greatest_Common_Divisor_of_Strings/src/com/greatestCommonDivisorofStrings/E1071_Greatest_Common_Divisor_of_Strings.java
+++ mysol/Category/String/E1071_Greatest_Common_Divisor_of_Strings/src/com/greatestCommonDivisorofStrings/E1071_Greatest_Common_Divisor_of_Strings.java
@@ -3,8 +3,28 @@ package com.greatestCommonDivisorofStrings;
 public class E1071_Greatest_Common_Divisor_of_Strings {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input1 = "ABABAB";
+		String input2 = "ABAB";
+        String output = gcdOfStrings(input1, input2);
+        System.out.println("input1: " + (input1) + " Input2: " + (input2) + "\noutput: " + (output));   
 	}
+	
+	
+	public static String gcdOfStrings(String str1, String str2) {
+        if (!(str1+str2).equals(str2+str1)) {
+            return "";
+        }
+    
+        int gcdVal = gcd(str1.length() , str2.length());
+        return str2.substring(0, gcdVal);
+    }
+    
+    public static int gcd(int p, int q) {
+        if (q == 0) {
+            return p;
+        } else {
+            return gcd(q, p % q);
+        }
+    }
 
 }
