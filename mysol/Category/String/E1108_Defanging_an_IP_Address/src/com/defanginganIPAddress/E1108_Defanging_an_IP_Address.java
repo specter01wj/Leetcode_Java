0cbd4cc9945b6387a0840ae2f12ee0427fe8fac8
Jin Wang <specter01wj@gmail.com>
2022-10-03 12:16:29 -0400

    v4

--- mysol/Category/String/E1108_Defanging_an_IP_Address/src/com/defanginganIPAddress/E1108_Defanging_an_IP_Address.java
+++ mysol/Category/String/E1108_Defanging_an_IP_Address/src/com/defanginganIPAddress/E1108_Defanging_an_IP_Address.java
@@ -1,9 +1,22 @@
 package com.defanginganIPAddress;
 import java.util.*;
 
+/*
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
 
 
+Example 1:
 
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+
+Example 2:
+
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+*/
 
 public class E1108_Defanging_an_IP_Address {
 
