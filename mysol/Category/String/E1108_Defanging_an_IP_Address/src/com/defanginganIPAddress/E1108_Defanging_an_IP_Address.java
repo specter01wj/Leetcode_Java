85350c2eee64dbb21064dd9d3a2e45e8d037c430
Jin Wang <specter01wj@gmail.com>
2022-10-03 12:13:42 -0400

    v2

--- mysol/Category/String/E1108_Defanging_an_IP_Address/src/com/defanginganIPAddress/E1108_Defanging_an_IP_Address.java
+++ mysol/Category/String/E1108_Defanging_an_IP_Address/src/com/defanginganIPAddress/E1108_Defanging_an_IP_Address.java
@@ -1,10 +1,25 @@
 package com.defanginganIPAddress;
+import java.util.*;
+
+
+
+
 
 public class E1108_Defanging_an_IP_Address {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "1.1.1.1";
+        String output = defangIPaddr(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	
+	public static String defangIPaddr(String address) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : address.toCharArray()) {
+            sb.append(c == '.' ? "[.]" : c);
+        }
+        return sb.toString();
+    }
 
 }
