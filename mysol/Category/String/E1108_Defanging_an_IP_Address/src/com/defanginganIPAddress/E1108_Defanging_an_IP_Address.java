6b61f4d51c3a00c758680f639cdf1aee7e8283cc
Jin Wang <specter01wj@gmail.com>
2022-10-03 12:15:55 -0400

    v3

--- mysol/Category/String/E1108_Defanging_an_IP_Address/src/com/defanginganIPAddress/E1108_Defanging_an_IP_Address.java
+++ mysol/Category/String/E1108_Defanging_an_IP_Address/src/com/defanginganIPAddress/E1108_Defanging_an_IP_Address.java
@@ -13,7 +13,17 @@ public class E1108_Defanging_an_IP_Address {
         System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    Create an empty list. Iterate through the characters in the address, 
+    numbers get appended to the list while periods signal that "[.]" chars 
+    must be appended.
+    */
 	
+	/**
+     * @param s: a string
+     * @return: A defanged IP address replaces every period "." with "[.]"
+     */
 	public static String defangIPaddr(String address) {
         StringBuilder sb = new StringBuilder();
         for (char c : address.toCharArray()) {
