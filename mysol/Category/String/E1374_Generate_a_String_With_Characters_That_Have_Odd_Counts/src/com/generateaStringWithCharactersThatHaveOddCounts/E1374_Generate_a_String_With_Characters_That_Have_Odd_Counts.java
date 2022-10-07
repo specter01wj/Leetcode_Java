0038b7341c099d02b19e86f257f348a38ddd85fc
Jin Wang <specter01wj@gmail.com>
2022-10-07 18:32:37 -0400

    v8

--- mysol/Category/String/E1374_Generate_a_String_With_Characters_That_Have_Odd_Counts/src/com/generateaStringWithCharactersThatHaveOddCounts/E1374_Generate_a_String_With_Characters_That_Have_Odd_Counts.java
+++ mysol/Category/String/E1374_Generate_a_String_With_Characters_That_Have_Odd_Counts/src/com/generateaStringWithCharactersThatHaveOddCounts/E1374_Generate_a_String_With_Characters_That_Have_Odd_Counts.java
@@ -1,10 +1,38 @@
 package com.generateaStringWithCharactersThatHaveOddCounts;
+import java.util.*;
+
+
+
 
 public class E1374_Generate_a_String_With_Characters_That_Have_Odd_Counts {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int input = 4;
+        String output = generateTheString(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	/*
+    solution:
+    opened count the number of opened parenthesis.
+	Add every char to the result,
+	unless the first left parenthesis,
+	and the last right parenthesis.
+    */
+	
+	/**
+     * @param s: a string
+     * @return: after removing the outermost parentheses
+     */
+	public static String generateTheString(int n) {
+		// need Java11
+        //return "b" + "ab".substring(n % 2, 1 + n % 2).repeat(n - 1);
+		char[] str = new char[n];
+	    Arrays.fill(str, 'a');
+	    if(n % 2 == 0) {
+	        str[0] = 'b';
+	    }
+	    return new String(str);
+    }
 
 }
