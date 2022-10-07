09777dbd077156026df7390a53e6428d126a9fbe
Jin Wang <specter01wj@gmail.com>
2022-10-07 18:34:46 -0400

    v9

--- mysol/Category/String/E1374_Generate_a_String_With_Characters_That_Have_Odd_Counts/src/com/generateaStringWithCharactersThatHaveOddCounts/E1374_Generate_a_String_With_Characters_That_Have_Odd_Counts.java
+++ mysol/Category/String/E1374_Generate_a_String_With_Characters_That_Have_Odd_Counts/src/com/generateaStringWithCharactersThatHaveOddCounts/E1374_Generate_a_String_With_Characters_That_Have_Odd_Counts.java
@@ -1,8 +1,35 @@
 package com.generateaStringWithCharactersThatHaveOddCounts;
 import java.util.*;
 
+/*
+Given an integer n, return a string with n characters such that 
+each character in such string occurs an odd number of times.
+
+The returned string must contain only lowercase English letters. 
+If there are multiples valid strings, return any of them.  
 
 
+Example 1:
+
+Input: n = 4
+Output: "pppz"
+Explanation: "pppz" is a valid string since the character 'p' occurs 
+three times and the character 'z' occurs once. 
+Note that there are many other valid strings such as "ohhh" and "love".
+
+Example 2:
+
+Input: n = 2
+Output: "xy"
+Explanation: "xy" is a valid string since the characters 'x' and 'y' 
+occur once. 
+Note that there are many other valid strings such as "ag" and "ur".
+
+Example 3:
+
+Input: n = 7
+Output: "holasss"
+*/
 
 public class E1374_Generate_a_String_With_Characters_That_Have_Odd_Counts {
 
@@ -14,15 +41,13 @@ public class E1374_Generate_a_String_With_Characters_That_Have_Odd_Counts {
 	
 	/*
     solution:
-    opened count the number of opened parenthesis.
-	Add every char to the result,
-	unless the first left parenthesis,
-	and the last right parenthesis.
+    If n is odd, we return "bbbb....b".
+	If n is even, we return "baaa...a".
     */
 	
 	/**
-     * @param s: a string
-     * @return: after removing the outermost parentheses
+     * @param n: an integer
+     * @return: a string with n characters
      */
 	public static String generateTheString(int n) {
 		// need Java11
