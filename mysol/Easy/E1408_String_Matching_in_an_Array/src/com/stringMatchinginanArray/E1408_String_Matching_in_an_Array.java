43a34784705e3c80fda5cd158a143b8d3792aa77
Jin Wang <specter01wj@gmail.com>
2022-10-08 19:33:23 -0400

    v15

---
+++ mysol/Easy/E1408_String_Matching_in_an_Array/src/com/stringMatchinginanArray/E1408_String_Matching_in_an_Array.java
@@ -0,0 +1,71 @@
+package com.stringMatchinginanArray;
+import java.util.*;
+
+/*
+Given an array of string words. Return all strings in words 
+which is substring of another word in any order. 
+
+String words[i] is substring of words[j], if can be obtained 
+removing some characters to left and/or right side of words[j].
+
+
+Example 1:
+
+Input: words = ["mass","as","hero","superhero"]
+Output: ["as","hero"]
+Explanation: "as" is substring of "mass" and "hero" is substring of "superhero".
+["hero","as"] is also a valid answer.
+
+Example 2:
+
+Input: words = ["leetcode","et","code"]
+Output: ["et","code"]
+Explanation: "et", "code" are substring of "leetcode".
+
+Example 3:
+
+Input: words = ["blue","green","bu"]
+Output: []
+*/
+
+public class E1408_String_Matching_in_an_Array {
+
+	public static void main(String[] args) {
+//		String[] input = {"mass","as","hero","superhero"};
+		String[] input = {"leetcoder","leetcode","od","hamlet","am"};
+		List<String> output = stringMatching(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    using HashSet
+    */
+	
+	/**
+     * @param words: a string array
+     * @return: all strings in words which is substring of another word in any order
+     */
+	public static List<String> stringMatching(String[] words) {
+        HashSet<String> set = new HashSet<>();
+        
+        for(int i = 0; i<words.length-1; i++) {
+            String currWord = words[i];
+            for(int j = i+1; j<words.length; j++) {
+                String nextWord = words[j];
+                
+                if(currWord.contains(nextWord)) {
+                    set.add(nextWord);
+                }
+                
+                if(nextWord.contains(currWord)) {
+                    set.add(currWord);
+                }
+            }
+        }
+        
+        List<String> res = new ArrayList<String>(set);
+        return res;
+    }
+
+}
---
+++ mysol/Easy/E1417_Reformat_The_String/src/com/reformatTheString/E1417_Reformat_The_String.java
@@ -0,0 +1,92 @@
+package com.reformatTheString;
+import java.util.*;
+
+/*
+You are given an alphanumeric string s. (Alphanumeric string 
+is a string consisting of lowercase English letters and digits).
+
+You have to find a permutation of the string where no letter is 
+followed by another letter and no digit is followed by another digit. 
+That is, no two adjacent characters have the same type.
+
+Return the reformatted string or return an empty string if it is 
+impossible to reformat the string.
+
+
+Example 1:
+
+Input: s = "a0b1c2"
+Output: "0a1b2c"
+Explanation: No two adjacent characters have the same type 
+in "0a1b2c". "a0b1c2", "0a1b2c", "0c2a1b" are also valid permutations.
+
+Example 2:
+
+Input: s = "leetcode"
+Output: ""
+Explanation: "leetcode" has only characters so we cannot separate them by digits.
+
+Example 3:
+
+Input: s = "1229857369"
+Output: ""
+Explanation: "1229857369" has only digits so we cannot separate them by characters.
+*/
+
+public class E1417_Reformat_The_String {
+
+	public static void main(String[] args) {
+		String input = "a0b1c2";
+        String output = reformat(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    1. Reformat the string with alternate digit and character, and vice versa.
+	2. Create 2 lists one for digits and the second one as characters
+	3. Traverse the string and segregate the items to reformat
+	4. Use a boolean to switch between character and digit and append all the items
+    */
+	
+	/**
+     * @param s: a string
+     * @return: the reformatted string
+     */
+	public static String reformat(String s) {
+        if(s == null || s.length() <= 1) {
+            return s;
+        }
+        List<Character> digits = new ArrayList<>(s.length());
+        List<Character> characters = new ArrayList<>(s.length());
+        // Check if it is a digit or character
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                digits.add(ch);
+            } else  {
+                characters.add(ch);
+            }
+        }
+        // it is impossible to reformat the string.
+        if(Math.abs(digits.size() - characters.size()) >= 2) return "";
+        
+        StringBuilder result = new StringBuilder();
+        // boolean to decide if we should start with characters or digits
+        boolean digitOrChar = (digits.size() >= characters.size()) ? true : false; 
+
+       for (int i = 0; i < s.length(); i++) {
+            if (digitOrChar) {
+                if (digits.size() > 0) {
+                    result.append(digits.remove(0));    
+                }
+            } else {
+                if (characters.size() > 0) {
+                    result.append(characters.remove(0));    
+                }
+            }
+            digitOrChar = !digitOrChar;
+        }
+        return result.toString();
+    }
+
+}
