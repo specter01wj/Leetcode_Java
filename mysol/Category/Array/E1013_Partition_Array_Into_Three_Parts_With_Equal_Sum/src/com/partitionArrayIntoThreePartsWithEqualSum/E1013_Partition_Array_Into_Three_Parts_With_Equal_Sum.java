f0a5fad837736b29f815af0fe7961189e850cfd1
Jin Wang <specter01wj@gmail.com>
2022-10-10 17:44:32 -0400

    v2

--- mysol/Category/Array/E1013_Partition_Array_Into_Three_Parts_With_Equal_Sum/src/com/partitionArrayIntoThreePartsWithEqualSum/E1013_Partition_Array_Into_Three_Parts_With_Equal_Sum.java
+++ mysol/Category/Array/E1013_Partition_Array_Into_Three_Parts_With_Equal_Sum/src/com/partitionArrayIntoThreePartsWithEqualSum/E1013_Partition_Array_Into_Three_Parts_With_Equal_Sum.java
@@ -32,8 +32,22 @@ Explanation: 3 + 3 = 6 = 5 - 2 + 2 + 5 + 1 - 9 + 4
 public class E1013_Partition_Array_Into_Three_Parts_With_Equal_Sum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {0,2,1,-6,6,-7,9,1,2,0,1};
+		boolean output = canThreePartsEqualSum(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public static boolean canThreePartsEqualSum(int[] arr) {
+        int sum = Arrays.stream(arr).sum(), part = 0, average = sum / 3, cnt = 0;
+        for (int a : arr) {
+            part += a;
+            if (part == average) { // find the average: sum / 3
+                ++cnt; // find an average, increase the counter.
+                part = 0; // reset part.
+            }
+        }
+        return cnt >= 3 && sum % 3 == 0;
+    }
 
 }
