ef07084aeee1314386e5a80e98b84b0468b137e2
Jin Wang <specter01wj@gmail.com>
2022-10-09 22:21:15 -0400

    v1

---
+++ mysol/Category/String/E1422_Maximum_Score_After_Splitting_a_String/src/com/maximumScoreAfterSplittingaString/E1422_Maximum_Score_After_Splitting_a_String.java
@@ -0,0 +1,10 @@
+package com.maximumScoreAfterSplittingaString;
+
+public class E1422_Maximum_Score_After_Splitting_a_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
