a4a3d3f5afbc24c503763c65d4286a40fd9c2f19
Jin Wang <specter01wj@gmail.com>
2022-10-09 22:23:41 -0400

    v3

--- mysol/Category/String/E1422_Maximum_Score_After_Splitting_a_String/src/com/maximumScoreAfterSplittingaString/E1422_Maximum_Score_After_Splitting_a_String.java
+++ mysol/Category/String/E1422_Maximum_Score_After_Splitting_a_String/src/com/maximumScoreAfterSplittingaString/E1422_Maximum_Score_After_Splitting_a_String.java
@@ -38,8 +38,25 @@ Output: 3
 public class E1422_Maximum_Score_After_Splitting_a_String {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "011101";
+        int output = maxScore(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	
+	public static int maxScore(String s) {
+        int zeros = 0, ones = 0, max = Integer.MIN_VALUE;
+        for(int i = 0;i < s.length();i++) {
+            if(s.charAt(i) == '0') {
+                zeros++;
+            } else {
+                ones++;
+            }
+            if(i != s.length()-1) {
+                max = Math.max(zeros - ones, max);
+            }
+        }
+        return max + ones;
+    }
 
 }
