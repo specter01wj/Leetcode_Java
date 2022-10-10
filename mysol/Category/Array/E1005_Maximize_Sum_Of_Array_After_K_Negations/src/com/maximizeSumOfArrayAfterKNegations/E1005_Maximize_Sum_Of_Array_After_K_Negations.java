798fb94298474f5b7341add9ec231519d9e1ce04
Jin Wang <specter01wj@gmail.com>
2022-10-10 18:18:07 -0400

    v8

---
+++ mysol/Category/Array/E1005_Maximize_Sum_Of_Array_After_K_Negations/src/com/maximizeSumOfArrayAfterKNegations/E1005_Maximize_Sum_Of_Array_After_K_Negations.java
@@ -0,0 +1,10 @@
+package com.maximizeSumOfArrayAfterKNegations;
+
+public class E1005_Maximize_Sum_Of_Array_After_K_Negations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
