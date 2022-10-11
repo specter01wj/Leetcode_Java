babc2cf9b9f58b1e2ac83ce6e1e1a82becbd86a3
Jin Wang <specter01wj@gmail.com>
2022-10-11 17:44:08 -0400

    v1

---
+++ mysol/Category/Array/E1018_Binary_Prefix_Divisible_By_5/src/com/binaryPrefixDivisibleBy5/E1018_Binary_Prefix_Divisible_By_5.java
@@ -0,0 +1,10 @@
+package com.binaryPrefixDivisibleBy5;
+
+public class E1018_Binary_Prefix_Divisible_By_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
