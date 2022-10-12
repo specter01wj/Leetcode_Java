1e196e5125eac72ac0099610fc6cf989fdfe66cf
Jin Wang <specter01wj@gmail.com>
2022-10-12 12:07:15 -0400

    v2

--- mysol/Category/Array/E1037_Valid_Boomerang/src/com/validBoomerang/E1037_Valid_Boomerang.java
+++ mysol/Category/Array/E1037_Valid_Boomerang/src/com/validBoomerang/E1037_Valid_Boomerang.java
@@ -1,10 +1,36 @@
 package com.validBoomerang;
+import java.util.*;
+
+/*
+Given an array points where points[i] = [xi, yi] represents 
+a point on the X-Y plane, return true if these points are a boomerang.
+
+A boomerang is a set of three points that are all distinct and not 
+in a straight line.
+
+
+Example 1:
+
+Input: points = [[1,1],[2,3],[3,2]]
+Output: true
+
+Example 2:
+
+Input: points = [[1,1],[2,2],[3,3]]
+Output: false
+*/
 
 public class E1037_Valid_Boomerang {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[][] input = {{1,1},{2,3},{3,2}};
+		boolean output = isBoomerang(input);
+        System.out.println("input: " + Arrays.deepToString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public static boolean isBoomerang(int[][] points) {
+        return (points[0][0] - points[1][0]) * (points[0][1] - points[2][1]) != (points[0][0] - points[2][0]) * (points[0][1] - points[1][1]);
+    }
 
 }
