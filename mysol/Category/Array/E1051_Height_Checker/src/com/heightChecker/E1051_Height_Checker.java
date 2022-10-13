45b64d2d2877ff188d481658f9a36997fd3b2575
Jin Wang <specter01wj@gmail.com>
2022-10-13 18:27:10 -0400

    v6

--- mysol/Category/Array/E1051_Height_Checker/src/com/heightChecker/E1051_Height_Checker.java
+++ mysol/Category/Array/E1051_Height_Checker/src/com/heightChecker/E1051_Height_Checker.java
@@ -8,11 +8,12 @@ non-decreasing order by height. Let this ordering be represented
 by the integer array expected where expected[i] is the expected 
 height of the ith student in line.
 
-You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+You are given an integer array heights representing the current 
+order that the students are standing in. Each heights[i] is the 
+height of the ith student in line (0-indexed).
 
 Return the number of indices where heights[i] != expected[i].
 
- 
 
 Example 1:
 
@@ -22,6 +23,7 @@ Explanation:
 heights:  [1,1,4,2,1,3]
 expected: [1,1,1,2,3,4]
 Indices 2, 4, and 5 do not match.
+
 Example 2:
 
 Input: heights = [5,1,2,3,4]
@@ -30,6 +32,7 @@ Explanation:
 heights:  [5,1,2,3,4]
 expected: [1,2,3,4,5]
 All indices do not match.
+
 Example 3:
 
 Input: heights = [1,2,3,4,5]
