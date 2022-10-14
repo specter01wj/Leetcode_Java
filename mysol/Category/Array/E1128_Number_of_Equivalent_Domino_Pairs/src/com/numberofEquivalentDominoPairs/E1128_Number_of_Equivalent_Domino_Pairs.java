3149bb777171e145f15ffc2c7fcb6dc182fbde32
Jin Wang <specter01wj@gmail.com>
2022-10-14 18:18:14 -0400

    v9

---
+++ mysol/Category/Array/E1128_Number_of_Equivalent_Domino_Pairs/src/com/numberofEquivalentDominoPairs/E1128_Number_of_Equivalent_Domino_Pairs.java
@@ -0,0 +1,10 @@
+package com.numberofEquivalentDominoPairs;
+
+public class E1128_Number_of_Equivalent_Domino_Pairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
