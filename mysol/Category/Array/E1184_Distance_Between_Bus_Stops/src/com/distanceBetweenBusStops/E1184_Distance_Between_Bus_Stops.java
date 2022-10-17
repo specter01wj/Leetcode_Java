a3e586bc5a481dfa22b9b4e5099b14fdae115804
Jin Wang <specter01wj@gmail.com>
2022-10-16 20:10:25 -0400

    v1

---
+++ mysol/Category/Array/E1184_Distance_Between_Bus_Stops/src/com/distanceBetweenBusStops/E1184_Distance_Between_Bus_Stops.java
@@ -0,0 +1,10 @@
+package com.distanceBetweenBusStops;
+
+public class E1184_Distance_Between_Bus_Stops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
