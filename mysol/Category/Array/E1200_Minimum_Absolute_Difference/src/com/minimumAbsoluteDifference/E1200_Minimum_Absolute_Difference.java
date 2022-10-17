0882b979a0882f97b8702b728861b4903c529fdf
Jin Wang <specter01wj@gmail.com>
2022-10-17 00:01:46 -0400

    v1

---
+++ mysol/Category/Array/E1200_Minimum_Absolute_Difference/src/com/minimumAbsoluteDifference/E1200_Minimum_Absolute_Difference.java
@@ -0,0 +1,10 @@
+package com.minimumAbsoluteDifference;
+
+public class E1200_Minimum_Absolute_Difference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
