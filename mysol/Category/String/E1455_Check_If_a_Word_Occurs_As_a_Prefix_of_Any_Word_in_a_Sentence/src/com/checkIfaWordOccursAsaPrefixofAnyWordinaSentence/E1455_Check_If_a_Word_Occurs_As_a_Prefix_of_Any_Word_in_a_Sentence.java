8899f9c8307d73697514a66cc8a7619f50fbd125
Jin Wang <specter01wj@gmail.com>
2022-10-18 11:22:06 -0400

    v7

---
+++ mysol/Category/String/E1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence/src/com/checkIfaWordOccursAsaPrefixofAnyWordinaSentence/E1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.java
@@ -0,0 +1,10 @@
+package com.checkIfaWordOccursAsaPrefixofAnyWordinaSentence;
+
+public class E1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
