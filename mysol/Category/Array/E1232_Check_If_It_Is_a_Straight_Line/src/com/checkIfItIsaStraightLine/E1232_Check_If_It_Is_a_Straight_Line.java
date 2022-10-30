dd1ed1b64863d7292ef4c81be6ba4199291e646f
Jin Wang <specter01wj@gmail.com>
2022-10-30 13:36:59 -0400

    v6

--- mysol/Category/Array/E1232_Check_If_It_Is_a_Straight_Line/src/com/checkIfItIsaStraightLine/E1232_Check_If_It_Is_a_Straight_Line.java
+++ mysol/Category/Array/E1232_Check_If_It_Is_a_Straight_Line/src/com/checkIfItIsaStraightLine/E1232_Check_If_It_Is_a_Straight_Line.java
@@ -22,6 +22,7 @@ public class E1232_Check_If_It_Is_a_Straight_Line {
 
 	public static void main(String[] args) {
 		int[][] input = {{1,2},{2,3},{3,4},{4,5},{5,6},{6,7}};
+		int[][] input = {{1,2},{2,3},{3,4},{4,5},{5,6},{6,7}};
 		boolean output = checkStraightLine(input);
         System.out.println("input: " + Arrays.deepToString(input) + "\noutput: " + (output));
 	}
@@ -41,16 +42,16 @@ public class E1232_Check_If_It_Is_a_Straight_Line {
      * @return: check if these points make a straight line in the XY plane
      */
 	public static boolean checkStraightLine(int[][] coordinates) {
-      int x0 = coordinates[0][0], y0 = coordinates[0][1], 
+		int x0 = coordinates[0][0], y0 = coordinates[0][1], 
             x1 = coordinates[1][0], y1 = coordinates[1][1];
-        int dx = x1 - x0, dy = y1 - y0;
-        for (int[] co : coordinates) {
-            int x = co[0], y = co[1];
-            if (dx * (y - y1) != dy * (x - x1)) {
-                return false;
-            }
-        }
-        return true;  
+	    int dx = x1 - x0, dy = y1 - y0;
+	    for (int[] co : coordinates) {
+	        int x = co[0], y = co[1];
+	        if (dx * (y - y1) != dy * (x - x1)) {
+	            return false;
+	        }
+	    }
+	    return true;  
     }
 
 }
