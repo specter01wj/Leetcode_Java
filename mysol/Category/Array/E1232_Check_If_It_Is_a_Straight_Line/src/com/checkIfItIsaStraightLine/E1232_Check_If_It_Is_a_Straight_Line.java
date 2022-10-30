0e3badc94feb4cc3cabc385a232e87fc9f53cdca
Jin Wang <specter01wj@gmail.com>
2022-10-30 13:21:02 -0400

    v1

---
+++ mysol/Category/Array/E1232_Check_If_It_Is_a_Straight_Line/src/com/checkIfItIsaStraightLine/E1232_Check_If_It_Is_a_Straight_Line.java
@@ -0,0 +1,10 @@
+package com.checkIfItIsaStraightLine;
+
+public class E1232_Check_If_It_Is_a_Straight_Line {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
