9106db408e20bff21c16e2a54e3ed15dd5a45a5e
Jin Wang <specter01wj@gmail.com>
2022-10-30 15:45:24 -0400

    v8

---
+++ mysol/Category/Array/E1252_Cells_with_Odd_Values_in_a_Matrix/src/com/cellswithOddValuesinaMatrix/E1252_Cells_with_Odd_Values_in_a_Matrix.java
@@ -0,0 +1,10 @@
+package com.cellswithOddValuesinaMatrix;
+
+public class E1252_Cells_with_Odd_Values_in_a_Matrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
