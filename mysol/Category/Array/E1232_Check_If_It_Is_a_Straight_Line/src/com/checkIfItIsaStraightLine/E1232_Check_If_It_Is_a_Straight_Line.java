7ce47614fd973d0d311e8f5bd8f988b9e58dda7e
Jin Wang <specter01wj@gmail.com>
2022-10-30 13:33:39 -0400

    v4

--- mysol/Category/Array/E1232_Check_If_It_Is_a_Straight_Line/src/com/checkIfItIsaStraightLine/E1232_Check_If_It_Is_a_Straight_Line.java
+++ mysol/Category/Array/E1232_Check_If_It_Is_a_Straight_Line/src/com/checkIfItIsaStraightLine/E1232_Check_If_It_Is_a_Straight_Line.java
@@ -26,18 +26,31 @@ public class E1232_Check_If_It_Is_a_Straight_Line {
         System.out.println("input: " + Arrays.deepToString(input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    The slope for a line through any 2 points (x0, y0) and (x1, y1) is (y1 - y0) / (x1 - x0); 
+    Therefore, for any given 3 points (denote the 3rd point as (x, y)), if they are in a 
+    straight line, the slopes of the lines from the 3rd point to the 2nd point and the 2nd 
+    point to the 1st point must be equal.
+    Now imagine connecting the 2nd points respectively with others one by one, 
+    Check if all of the slopes are equal.
+    */
 	
-		public static boolean checkStraightLine(int[][] coordinates) {
-		      int x0 = coordinates[0][0], y0 = coordinates[0][1], 
-		            x1 = coordinates[1][0], y1 = coordinates[1][1];
-		        int dx = x1 - x0, dy = y1 - y0;
-		        for (int[] co : coordinates) {
-		            int x = co[0], y = co[1];
-		            if (dx * (y - y1) != dy * (x - x1)) {
-		                return false;
-		            }
-		        }
-		        return true;  
-		    }
+	/*
+     * @param coordinates: a list of 2D integer
+     * @return: check if these points make a straight line in the XY plane
+     */
+	public static boolean checkStraightLine(int[][] coordinates) {
+      int x0 = coordinates[0][0], y0 = coordinates[0][1], 
+            x1 = coordinates[1][0], y1 = coordinates[1][1];
+        int dx = x1 - x0, dy = y1 - y0;
+        for (int[] co : coordinates) {
+            int x = co[0], y = co[1];
+            if (dx * (y - y1) != dy * (x - x1)) {
+                return false;
+            }
+        }
+        return true;  
+    }
 
 }
