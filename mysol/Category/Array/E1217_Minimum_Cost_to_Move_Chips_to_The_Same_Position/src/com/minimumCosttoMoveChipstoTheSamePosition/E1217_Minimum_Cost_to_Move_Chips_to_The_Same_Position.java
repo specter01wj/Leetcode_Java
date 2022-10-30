ba6c22daf74a8d844fbb05fd95e325bc6de2d988
Jin Wang <specter01wj@gmail.com>
2022-10-29 23:52:41 -0400

    v6

---
+++ mysol/Category/Array/E1217_Minimum_Cost_to_Move_Chips_to_The_Same_Position/src/com/minimumCosttoMoveChipstoTheSamePosition/E1217_Minimum_Cost_to_Move_Chips_to_The_Same_Position.java
@@ -0,0 +1,10 @@
+package com.minimumCosttoMoveChipstoTheSamePosition;
+
+public class E1217_Minimum_Cost_to_Move_Chips_to_The_Same_Position {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
