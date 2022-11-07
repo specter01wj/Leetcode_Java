dbc68b5a715443272e7b4472c2a6bf2bf781e82d
Jin Wang <specter01wj@gmail.com>
2022-11-06 22:15:34 -0500

    v7

---
+++ mysol/Category/Array/E1266_Minimum_Time_Visiting_All_Points/src/com/minimumTimeVisitingAllPoints/E1266_Minimum_Time_Visiting_All_Points.java
@@ -0,0 +1,10 @@
+package com.minimumTimeVisitingAllPoints;
+
+public class E1266_Minimum_Time_Visiting_All_Points {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
