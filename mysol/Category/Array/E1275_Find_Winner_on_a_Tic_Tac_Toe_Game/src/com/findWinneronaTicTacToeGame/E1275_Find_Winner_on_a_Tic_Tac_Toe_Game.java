54395ea36650deba77ff4198860ef96af190558d
Jin Wang <specter01wj@gmail.com>
2022-11-16 11:05:57 -0500

    v3

--- mysol/Category/Array/E1275_Find_Winner_on_a_Tic_Tac_Toe_Game/src/com/findWinneronaTicTacToeGame/E1275_Find_Winner_on_a_Tic_Tac_Toe_Game.java
+++ mysol/Category/Array/E1275_Find_Winner_on_a_Tic_Tac_Toe_Game/src/com/findWinneronaTicTacToeGame/E1275_Find_Winner_on_a_Tic_Tac_Toe_Game.java
@@ -57,8 +57,9 @@ public class E1275_Find_Winner_on_a_Tic_Tac_Toe_Game {
         int[] d1 = new int[2], d2 = new int[2];
         for (int i = 0; i < moves.length; ++i) {
             int r = moves[i][0], c = moves[i][1], id = i % 2;
-            if (++row[id][r] == 3 || ++col[id][c] == 3 || r == c && ++d1[id] == 3 || r + c == 2 && ++d2[id] == 3) 
+            if (++row[id][r] == 3 || ++col[id][c] == 3 || r == c && ++d1[id] == 3 || r + c == 2 && ++d2[id] == 3) {
                 return id == 0 ? "A" : "B";
+            }
         }
         return moves.length == 9 ? "Draw" : "Pending";   
     }
