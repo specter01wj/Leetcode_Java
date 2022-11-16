e8bfc877c97367bdf063282fc729133b39a92cc8
Jin Wang <specter01wj@gmail.com>
2022-11-16 10:53:17 -0500

    v1

---
+++ mysol/Category/Array/E1275_Find_Winner_on_a_Tic_Tac_Toe_Game/src/com/findWinneronaTicTacToeGame/E1275_Find_Winner_on_a_Tic_Tac_Toe_Game.java
@@ -0,0 +1,54 @@
+package com.findWinneronaTicTacToeGame;
+import java.util.*;
+
+/*
+Tic-tac-toe is played by two players A and B on a 3 x 3 grid. 
+The rules of Tic-Tac-Toe are:
+
+. Players take turns placing characters into empty squares ' '.
+. The first player A always places 'X' characters, 
+  while the second player B always places 'O' characters.
+. 'X' and 'O' characters are always placed into empty squares, never on filled ones.
+. The game ends when there are three of the same (non-empty) character filling any row, 
+  column, or diagonal.
+. The game also ends if all squares are non-empty.
+. No more moves can be played if the game is over.
+
+Given a 2D integer array moves where moves[i] = [rowi, coli] indicates that 
+the ith move will be played on grid[rowi][coli]. 
+return the winner of the game if it exists (A or B). 
+In case the game ends in a draw return "Draw". If there are still movements to 
+play return "Pending".
+
+You can assume that moves is valid (i.e., it follows the rules of Tic-Tac-Toe), 
+the grid is initially empty, and A will play first.
+
+
+Example 1:
+
+Input: moves = [[0,0],[2,0],[1,1],[2,1],[2,2]]
+Output: "A"
+Explanation: A wins, they always play first.
+
+Example 2:
+
+Input: moves = [[0,0],[1,1],[0,1],[0,2],[1,0],[2,0]]
+Output: "B"
+Explanation: B wins.
+
+Example 3:
+
+Input: moves = [[0,0],[1,1],[2,0],[1,0],[1,2],[2,1],[0,1],[0,2],[2,2]]
+Output: "Draw"
+Explanation: The game ends in a draw since there are no moves to make.
+*/
+
+public class E1275_Find_Winner_on_a_Tic_Tac_Toe_Game {
+
+	public static void main(String[] args) {
+		int[][] input = {{0,0},{2,0},{1,1},{2,1},{2,2}};
+		List<String> output = commonChars(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+
+}
