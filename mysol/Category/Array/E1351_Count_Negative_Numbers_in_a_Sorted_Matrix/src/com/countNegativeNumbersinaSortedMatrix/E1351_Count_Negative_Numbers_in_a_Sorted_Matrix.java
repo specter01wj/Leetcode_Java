21b56a70d54186bd3fa9d5677b71052b2879537b
Jin Wang <specter01wj@gmail.com>
2022-12-03 00:48:21 -0500

    v1

---
+++ mysol/Category/Array/E1351_Count_Negative_Numbers_in_a_Sorted_Matrix/src/com/countNegativeNumbersinaSortedMatrix/E1351_Count_Negative_Numbers_in_a_Sorted_Matrix.java
@@ -0,0 +1,10 @@
+package com.countNegativeNumbersinaSortedMatrix;
+
+public class E1351_Count_Negative_Numbers_in_a_Sorted_Matrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
