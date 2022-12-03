cdd628264c540996c0e56f56c13fa79b13d5df26
Jin Wang <specter01wj@gmail.com>
2022-12-03 00:52:32 -0500

    v3

--- mysol/Category/Array/E1351_Count_Negative_Numbers_in_a_Sorted_Matrix/src/com/countNegativeNumbersinaSortedMatrix/E1351_Count_Negative_Numbers_in_a_Sorted_Matrix.java
+++ mysol/Category/Array/E1351_Count_Negative_Numbers_in_a_Sorted_Matrix/src/com/countNegativeNumbersinaSortedMatrix/E1351_Count_Negative_Numbers_in_a_Sorted_Matrix.java
@@ -26,7 +26,16 @@ public class E1351_Count_Negative_Numbers_in_a_Sorted_Matrix {
         System.out.println("input: " + Arrays.deepToString(input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    This solution uses the fact that the negative regions of the matrix will form a staircase shape.
+    What this solution then does is to trace the outline of the staircase.
+    */
 	
+	/*
+     * @param grid: a list of 2D integers
+     * @return: the number of negative numbers in grid
+     */
 	public static int countNegatives(int[][] grid) {
         int m = grid.length, n = grid[0].length, r = m - 1, c = 0, cnt = 0;
         while (r >= 0 && c < n) {
