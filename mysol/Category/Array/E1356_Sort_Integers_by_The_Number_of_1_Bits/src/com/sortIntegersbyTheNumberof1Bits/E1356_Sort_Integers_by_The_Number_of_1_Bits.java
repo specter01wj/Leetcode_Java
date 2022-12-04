58f0ce6369ace28296ed50cbebf1de2bc67bf470
Jin Wang <specter01wj@gmail.com>
2022-12-04 13:32:27 -0500

    v3

--- mysol/Category/Array/E1356_Sort_Integers_by_The_Number_of_1_Bits/src/com/sortIntegersbyTheNumberof1Bits/E1356_Sort_Integers_by_The_Number_of_1_Bits.java
+++ mysol/Category/Array/E1356_Sort_Integers_by_The_Number_of_1_Bits/src/com/sortIntegersbyTheNumberof1Bits/E1356_Sort_Integers_by_The_Number_of_1_Bits.java
@@ -35,7 +35,20 @@ public class E1356_Sort_Integers_by_The_Number_of_1_Bits {
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + Arrays.toString(output));
 	}
 	
+	/*
+    solution:
+     Since the input values can range from 0 to 10000, we can add to each element 
+     on the array the value of the bit count (number of 1s) multiplied by 10001.
+     10001 is not a magic number, it is merely the max possible number the array 
+     may have + 1, which ensures that the bit count has the maximum priority,
+     and that we do not mistakenly erase a number when reverting the array back 
+     to its original values (after sorting).
+    */
 	
+	/*
+     * @param arr: a list of integers
+     * @return: the array after sorting it
+     */
 	public static int[] sortByBits(int[] arr) {
         int n = arr.length, res[] = new int[n];
         for (int i = 0; i < n; i++) {
