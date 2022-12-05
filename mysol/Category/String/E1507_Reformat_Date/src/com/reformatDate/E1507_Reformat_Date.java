2d24a4fbd7c287af578e20e7bc878772ceab24f3
Jin Wang <specter01wj@gmail.com>
2022-12-05 12:51:30 -0500

    v9

--- mysol/Category/String/E1507_Reformat_Date/src/com/reformatDate/E1507_Reformat_Date.java
+++ mysol/Category/String/E1507_Reformat_Date/src/com/reformatDate/E1507_Reformat_Date.java
@@ -1,10 +1,41 @@
 package com.reformatDate;
+import java.util.*;
 
 public class E1507_Reformat_Date {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "20th Oct 2052";
+        String output = reformatDate(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	
+	public static String reformatDate(String date) {
+        String[] ss = date.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        sb.append(ss[2]).append("-");
+        sb.append(months.get(ss[1])).append("-");
+        sb.append(ss[0].length() == 3 ? ("0" + ss[0].substring(0, 1)) : ss[0].substring(0, 2));
+        return sb.toString();
+    }
+	
+    private static final Map<String, String> months = getMonths();
+    
+    private static Map<String, String> getMonths(){
+        Map<String, String> months = new HashMap<>();
+        months.put("Jan", "01");
+        months.put("Feb", "02");
+        months.put("Mar", "03");
+        months.put("Apr", "04");
+        months.put("May", "05");
+        months.put("Jun", "06");
+        months.put("Jul", "07");
+        months.put("Aug", "08");
+        months.put("Sep", "09");
+        months.put("Oct", "10");
+        months.put("Nov", "11");
+        months.put("Dec", "12");
+        return months;
+    }
 
 }
