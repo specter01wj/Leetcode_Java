53299c679d98a1390d2baf88c3270b20a362f64d
Jin Wang <specter01wj@gmail.com>
2022-12-05 12:54:18 -0500

    v10

--- mysol/Category/String/E1507_Reformat_Date/src/com/reformatDate/E1507_Reformat_Date.java
+++ mysol/Category/String/E1507_Reformat_Date/src/com/reformatDate/E1507_Reformat_Date.java
@@ -1,6 +1,36 @@
 package com.reformatDate;
 import java.util.*;
 
+/*
+Given a date string in the form Day Month Year, where:
+
+ .Day is in the set {"1st", "2nd", "3rd", "4th", ..., "30th", "31st"}.
+ .Month is in the set {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}.
+ .Year is in the range [1900, 2100].
+
+Convert the date string to the format YYYY-MM-DD, where:
+
+ .YYYY denotes the 4 digit year.
+ .MM denotes the 2 digit month.
+ .DD denotes the 2 digit day.
+
+
+Example 1:
+
+Input: date = "20th Oct 2052"
+Output: "2052-10-20"
+
+Example 2:
+
+Input: date = "6th Jun 1933"
+Output: "1933-06-06"
+
+Example 3:
+
+Input: date = "26th May 1960"
+Output: "1960-05-26"
+*/
+
 public class E1507_Reformat_Date {
 
 	public static void main(String[] args) {
@@ -9,7 +39,15 @@ public class E1507_Reformat_Date {
         System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    Map
+    */
 	
+	/**
+     * @param date: a string
+     * @return: the date string to the format YYYY-MM-DD
+     */
 	public static String reformatDate(String date) {
         String[] ss = date.split("\\s+");
         StringBuilder sb = new StringBuilder();
