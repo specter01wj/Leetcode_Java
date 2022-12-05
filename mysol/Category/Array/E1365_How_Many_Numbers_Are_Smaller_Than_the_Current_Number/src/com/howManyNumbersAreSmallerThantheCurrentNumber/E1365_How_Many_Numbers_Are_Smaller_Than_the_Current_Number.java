7cc06079186cbaf0f6acbb9f82434bec6245f7f7
Jin Wang <specter01wj@gmail.com>
2022-12-05 00:49:01 -0500

    v1

---
+++ mysol/Category/Array/E1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number/src/com/howManyNumbersAreSmallerThantheCurrentNumber/E1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.java
@@ -0,0 +1,10 @@
+package com.howManyNumbersAreSmallerThantheCurrentNumber;
+
+public class E1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
