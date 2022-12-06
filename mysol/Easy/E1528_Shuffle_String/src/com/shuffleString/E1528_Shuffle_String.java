1d9625b05ebaf61200b016d740ed9ec0f1904ef7
Jin Wang <specter01wj@gmail.com>
2022-12-06 13:48:00 -0500

    v13

---
+++ mysol/Easy/E1528_Shuffle_String/src/com/shuffleString/E1528_Shuffle_String.java
@@ -0,0 +1,57 @@
+package com.shuffleString;
+import java.util.*;
+
+/*
+You are given a string s and an integer array indices of the same length. 
+The string s will be shuffled such that the character at the ith position 
+moves to indices[i] in the shuffled string.
+
+Return the shuffled string.
+
+
+Example 1:
+
+
+Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+Output: "leetcode"
+Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+
+Example 2:
+
+Input: s = "abc", indices = [0,1,2]
+Output: "abc"
+Explanation: After shuffling, each character remains in its position.
+*/
+
+public class E1528_Shuffle_String {
+
+	public static void main(String[] args) {
+		String input = "codeleet";
+		int[] indices = {4,5,6,7,0,2,1,3};
+        String output = restoreString(input, indices);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    shift string
+    */
+	
+	/**
+     * @param s: a string
+     * @param indices: a list of integers
+     * @return: the shuffled string
+     */
+	public static String restoreString(String s, int[] indices) {
+        StringBuilder s1 = new StringBuilder(s); 
+        StringBuilder s2 = new StringBuilder(s); 
+        int n = indices.length;
+        for ( int i = 0; i <n; i++ ) {
+            int in = indices[i];
+            s2.setCharAt(in, s1.charAt(i)); 
+            
+        }
+        return s2.toString();
+    }
+
+}
---
+++ mysol/Easy/E1544_Make_The_String_Great/src/com/makeTheStringGreat/E1544_Make_The_String_Great.java
@@ -0,0 +1,80 @@
+package com.makeTheStringGreat;
+import java.util.*;
+
+/*
+Given a string s of lower and upper case English letters.
+
+A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+
+ .0 <= i <= s.length - 2
+ .s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+
+To make the string good, you can choose two adjacent characters that make the string bad 
+and remove them. You can keep doing this until the string becomes good.
+
+Return the string after making it good. The answer is guaranteed to be unique under 
+the given constraints.
+
+Notice that an empty string is also good.
+
+
+Example 1:
+
+Input: s = "leEeetcode"
+Output: "leetcode"
+Explanation: In the first step, either you choose i = 1 or i = 2, 
+both will result "leEeetcode" to be reduced to "leetcode".
+
+Example 2:
+
+Input: s = "abBAcC"
+Output: ""
+Explanation: We have many possible scenarios, and all lead to the same answer. For example:
+"abBAcC" --> "aAcC" --> "cC" --> ""
+"abBAcC" --> "abBA" --> "aA" --> ""
+
+Example 3:
+
+Input: s = "s"
+Output: "s"
+*/
+
+public class E1544_Make_The_String_Great {
+
+	public static void main(String[] args) {
+		String input = "leEeetcode";
+        String output = makeGood(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    The difference between the any lowercase and uppercase alphabet is 32. 
+    Example - ASCII value of a is 97 and A is 65 , 97-65 = 32
+    Using same trick, we can delete adjacent characters with absolute difference of 32.
+    If current character is A and a is at top of stack, we pop a and dont insert A.
+	Otherwise, we insert the current character in stack.
+	Collect result in String at the end.
+    */
+	
+	/**
+     * @param s: a string
+     * @return: the string after making it good
+     */
+	public static String makeGood(String s) {
+        Stack<Character> stack = new Stack();
+        for(int i=0;i<s.length();i++){
+            if(!stack.isEmpty() && Math.abs(stack.peek()-s.charAt(i)) == 32)
+                stack.pop();
+            else
+                stack.push(s.charAt(i));
+        }
+        char res[] = new char[stack.size()];
+        int index = stack.size()-1;
+        while(!stack.isEmpty()){
+            res[index--] = stack.pop();
+        }
+        return new String(res);
+    }
+
+}
