57ef0b1d66e1f9a045d96c63cf4120cb31d5cc9a
Jin Wang <specter01wj@gmail.com>
2022-12-07 18:35:51 -0500

    v6

---
+++ mysol/Category/String/E1576_Replace_All_to_Avoid_Consecutive_Repeating_Characters/src/com/replaceAlltoAvoidConsecutiveRepeatingCharacters/E1576_Replace_All_to_Avoid_Consecutive_Repeating_Characters.java
@@ -0,0 +1,10 @@
+package com.replaceAlltoAvoidConsecutiveRepeatingCharacters;
+
+public class E1576_Replace_All_to_Avoid_Consecutive_Repeating_Characters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
