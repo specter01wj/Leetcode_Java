d74c3b5c760c1db749187185eaad7c58c018b402
Jin Wang <specter01wj@gmail.com>
2022-12-07 18:57:24 -0500

    v10

---
+++ mysol/Easy/E1556_Thousand_Separator/src/com/thousandSeparator/E1556_Thousand_Separator.java
@@ -0,0 +1,48 @@
+package com.thousandSeparator;
+import java.util.*;
+
+/*
+Given an integer n, add a dot (".") as the thousands separator and return it in string format.
+
+
+Example 1:
+
+Input: n = 987
+Output: "987"
+
+Example 2:
+
+Input: n = 1234
+Output: "1.234"
+*/
+
+public class E1556_Thousand_Separator {
+
+	public static void main(String[] args) {
+		int input = 1234;
+		String output = thousandSeparator(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    StringBuilder
+    */
+	
+	/**
+     * @param n: a string
+     * @return: add a dot as the thousands separator and return it in string format
+     */
+	public static String thousandSeparator(int n) {
+        String num = Integer.toString(n);
+        StringBuilder sb = new StringBuilder();
+        for (int i = num.length(); i > 0; i -= 3) {
+            if (sb.length() > 0) {
+            	sb.insert(0, ".");
+            }
+            sb.insert(0, num.substring(Math.max(0, i - 3), i));
+        }
+        return sb.toString();
+    }
+
+}
