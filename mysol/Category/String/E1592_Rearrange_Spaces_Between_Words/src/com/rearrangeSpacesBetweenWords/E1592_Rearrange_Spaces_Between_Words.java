176f8b35ce63ee361042afc112cf7af7b877f327
Jin Wang <specter01wj@gmail.com>
2022-12-08 13:13:26 -0500

    v1

---
+++ mysol/Category/String/E1592_Rearrange_Spaces_Between_Words/src/com/rearrangeSpacesBetweenWords/E1592_Rearrange_Spaces_Between_Words.java
@@ -0,0 +1,10 @@
+package com.rearrangeSpacesBetweenWords;
+
+public class E1592_Rearrange_Spaces_Between_Words {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
