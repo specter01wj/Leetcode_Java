e5720ce1a34787589e7967437aabea97b2643f86
Jin Wang <specter01wj@gmail.com>
2022-12-12 12:26:24 -0500

    v7

---
+++ mysol/Category/Array/E1385_Find_the_Distance_Value_Between_Two_Arrays/src/com/findtheDistanceValueBetweenTwoArrays/E1385_Find_the_Distance_Value_Between_Two_Arrays.java
@@ -0,0 +1,10 @@
+package com.findtheDistanceValueBetweenTwoArrays;
+
+public class E1385_Find_the_Distance_Value_Between_Two_Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
