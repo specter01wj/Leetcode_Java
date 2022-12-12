b73ec3cdc5c209a71f4315c984bb094a812b2079
Jin Wang <specter01wj@gmail.com>
2022-12-11 22:16:01 -0500

    v2

--- mysol/Category/String/E1614_Maximum_Nesting_Depth_of_the_Parentheses/src/com/maximumNestingDepthoftheParentheses/E1614_Maximum_Nesting_Depth_of_the_Parentheses.java
+++ mysol/Category/String/E1614_Maximum_Nesting_Depth_of_the_Parentheses/src/com/maximumNestingDepthoftheParentheses/E1614_Maximum_Nesting_Depth_of_the_Parentheses.java
@@ -1,10 +1,58 @@
 package com.maximumNestingDepthoftheParentheses;
+import java.util.*;
+
+/*
+A string is a valid parentheses string (denoted VPS) if it meets one of the following:
+
+	.It is an empty string "", or a single character not equal to "(" or ")",
+	.It can be written as AB (A concatenated with B), where A and B are VPS's, or
+	.It can be written as (A), where A is a VPS.
+	
+We can similarly define the nesting depth depth(S) of any VPS S as follows:
+
+	.depth("") = 0
+	.depth(C) = 0, where C is a string with a single character not equal to "(" or ")".
+	.depth(A + B) = max(depth(A), depth(B)), where A and B are VPS's.
+	.depth("(" + A + ")") = 1 + depth(A), where A is a VPS.
+
+For example, "", "()()", and "()(()())" are VPS's (with nesting depths 0, 1, and 2), 
+and ")(" and "(()" are not VPS's.
+
+Given a VPS represented as string s, return the nesting depth of s.
+
+
+Example 1:
+
+Input: s = "(1+(2*3)+((8)/4))+1"
+Output: 3
+Explanation: Digit 8 is inside of 3 nested parentheses in the string.
+
+Example 2:
+
+Input: s = "(1)+((2))+(((3)))"
+Output: 3
+*/
 
 public class E1614_Maximum_Nesting_Depth_of_the_Parentheses {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "(1+(2*3)+((8)/4))+1";
+        int output = maxDepth(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	
+	public static int maxDepth(String s) {
+        int res = 0, cur = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '(') {
+                res = Math.max(res, ++cur);
+            }
+            if (s.charAt(i) == ')') {
+                cur--;
+            }
+        }
+        return res;
+    }
 
 }
