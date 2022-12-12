357ea4dffe9f6d41029d5a94a2f30a030914ab49
Jin Wang <specter01wj@gmail.com>
2022-12-12 01:15:04 -0500

    v1

---
+++ mysol/Category/String/E1624_Largest_Substring_Between_Two_Equal_Characters/src/com/largestSubstringBetweenTwoEqualCharacters/E1624_Largest_Substring_Between_Two_Equal_Characters.java
@@ -0,0 +1,10 @@
+package com.largestSubstringBetweenTwoEqualCharacters;
+
+public class E1624_Largest_Substring_Between_Two_Equal_Characters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
