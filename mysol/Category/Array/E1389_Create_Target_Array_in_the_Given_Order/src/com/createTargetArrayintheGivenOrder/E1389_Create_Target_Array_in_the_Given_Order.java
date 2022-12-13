19d6f4ab2e9a7bf78142ba7f99dfafc54810fccc
Jin Wang <specter01wj@gmail.com>
2022-12-13 11:20:12 -0500

    v1

---
+++ mysol/Category/Array/E1389_Create_Target_Array_in_the_Given_Order/src/com/createTargetArrayintheGivenOrder/E1389_Create_Target_Array_in_the_Given_Order.java
@@ -0,0 +1,10 @@
+package com.createTargetArrayintheGivenOrder;
+
+public class E1389_Create_Target_Array_in_the_Given_Order {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
