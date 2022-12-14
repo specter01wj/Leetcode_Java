ab365cedabb0273044cac9ea0ff9e710b33e9a00
Jin Wang <specter01wj@gmail.com>
2022-12-14 01:33:43 -0500

    v1

---
+++ mysol/Category/Array/E1394_Find_Lucky_Integer_in_an_Array/src/com/findLuckyIntegerinanArray/E1394_Find_Lucky_Integer_in_an_Array.java
@@ -0,0 +1,10 @@
+package com.findLuckyIntegerinanArray;
+
+public class E1394_Find_Lucky_Integer_in_an_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
