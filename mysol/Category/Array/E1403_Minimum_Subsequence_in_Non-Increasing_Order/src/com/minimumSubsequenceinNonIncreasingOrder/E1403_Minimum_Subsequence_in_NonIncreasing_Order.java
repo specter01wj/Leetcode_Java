274cc27e629d912fe7e56c40fcdcf5c4d9e34b3a
Jin Wang <specter01wj@gmail.com>
2022-12-14 11:24:31 -0500

    v1

---
+++ mysol/Category/Array/E1403_Minimum_Subsequence_in_Non-Increasing_Order/src/com/minimumSubsequenceinNonIncreasingOrder/E1403_Minimum_Subsequence_in_NonIncreasing_Order.java
@@ -0,0 +1,10 @@
+package com.minimumSubsequenceinNonIncreasingOrder;
+
+public class E1403_Minimum_Subsequence_in_NonIncreasing_Order {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
