8b3846f9fe1127c406b71fb68ae736f3e833d10a
Jin Wang <specter01wj@gmail.com>
2022-12-15 13:28:45 -0500

    v6

---
+++ mysol/Category/Array/E1431_Kids_With_the_Greatest_Number_of_Candies/src/com/kidsWiththeGreatestNumberofCandies/E1431_Kids_With_the_Greatest_Number_of_Candies.java
@@ -0,0 +1,10 @@
+package com.kidsWiththeGreatestNumberofCandies;
+
+public class E1431_Kids_With_the_Greatest_Number_of_Candies {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
