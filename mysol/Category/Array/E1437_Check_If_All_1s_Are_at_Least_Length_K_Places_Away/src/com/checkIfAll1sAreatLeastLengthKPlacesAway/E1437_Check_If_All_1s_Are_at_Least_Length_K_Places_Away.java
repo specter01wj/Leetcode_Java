21560ec27717195725acef19e1af0198e5755b89
Jin Wang <specter01wj@gmail.com>
2022-12-16 12:16:10 -0500

    v1

---
+++ mysol/Category/Array/E1437_Check_If_All_1s_Are_at_Least_Length_K_Places_Away/src/com/checkIfAll1sAreatLeastLengthKPlacesAway/E1437_Check_If_All_1s_Are_at_Least_Length_K_Places_Away.java
@@ -0,0 +1,10 @@
+package com.checkIfAll1sAreatLeastLengthKPlacesAway;
+
+public class E1437_Check_If_All_1s_Are_at_Least_Length_K_Places_Away {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
