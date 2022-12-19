c49bf4ec2755a8bb2319b60f028da2070f69594c
Jin Wang <specter01wj@gmail.com>
2022-12-18 22:21:15 -0500

    v5

---
+++ mysol/Category/Array/E1464_Maximum_Product_of_Two_Elements_in_an_Array/src/com/maximumProductofTwoElementsinanArray/E1464_Maximum_Product_of_Two_Elements_in_an_Array.java
@@ -0,0 +1,10 @@
+package com.maximumProductofTwoElementsinanArray;
+
+public class E1464_Maximum_Product_of_Two_Elements_in_an_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
