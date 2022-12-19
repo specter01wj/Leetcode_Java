77b9ae69d6e0e02210c9aa480b4ad932c10f071c
Jin Wang <specter01wj@gmail.com>
2022-12-18 21:47:01 -0500

    v1

---
+++ mysol/Category/Array/E1460_Make_Two_Arrays_Equal_by_Reversing_Subarrays/src/com/makeTwoArraysEqualbyReversingSubarrays/E1460_Make_Two_Arrays_Equal_by_Reversing_Subarrays.java
@@ -0,0 +1,10 @@
+package com.makeTwoArraysEqualbyReversingSubarrays;
+
+public class E1460_Make_Two_Arrays_Equal_by_Reversing_Subarrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
