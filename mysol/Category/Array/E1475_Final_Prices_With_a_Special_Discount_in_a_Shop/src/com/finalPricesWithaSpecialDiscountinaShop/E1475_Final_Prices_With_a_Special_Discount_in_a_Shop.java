1f2e897333def77a7fe590afea4cb40fad62da0b
Jin Wang <specter01wj@gmail.com>
2022-12-19 13:06:04 -0500

    v5

---
+++ mysol/Category/Array/E1475_Final_Prices_With_a_Special_Discount_in_a_Shop/src/com/finalPricesWithaSpecialDiscountinaShop/E1475_Final_Prices_With_a_Special_Discount_in_a_Shop.java
@@ -0,0 +1,10 @@
+package com.finalPricesWithaSpecialDiscountinaShop;
+
+public class E1475_Final_Prices_With_a_Special_Discount_in_a_Shop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
