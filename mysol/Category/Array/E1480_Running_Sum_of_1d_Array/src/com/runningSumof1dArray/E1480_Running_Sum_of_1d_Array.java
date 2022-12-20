01687ef1171b1b28b53cc1a2caa74917389c0fc5
Jin Wang <specter01wj@gmail.com>
2022-12-20 11:25:32 -0500

    v2

--- mysol/Category/Array/E1480_Running_Sum_of_1d_Array/src/com/runningSumof1dArray/E1480_Running_Sum_of_1d_Array.java
+++ mysol/Category/Array/E1480_Running_Sum_of_1d_Array/src/com/runningSumof1dArray/E1480_Running_Sum_of_1d_Array.java
@@ -1,10 +1,46 @@
 package com.runningSumof1dArray;
+import java.util.*;
+
+/*
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+Example 3:
+
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+*/
 
 public class E1480_Running_Sum_of_1d_Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {1,2,3,4};
+		int[] output = runningSum(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + Arrays.toString(output));
 	}
+	
+	
+	public static int[] runningSum(int[] nums) {
+        int i = 1;
+        while (i < nums.length){
+            nums[i] += nums[i-1];
+            i++;
+        }
+        return nums;
+    }
 
 }
