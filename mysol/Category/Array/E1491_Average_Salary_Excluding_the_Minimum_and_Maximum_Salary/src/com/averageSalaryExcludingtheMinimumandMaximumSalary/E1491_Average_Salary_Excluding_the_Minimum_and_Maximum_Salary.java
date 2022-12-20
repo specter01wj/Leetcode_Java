a9d68b9a645af32840fcdc70e512cc25e7e4768e
Jin Wang <specter01wj@gmail.com>
2022-12-20 12:18:37 -0500

    v6

---
+++ mysol/Category/Array/E1491_Average_Salary_Excluding_the_Minimum_and_Maximum_Salary/src/com/averageSalaryExcludingtheMinimumandMaximumSalary/E1491_Average_Salary_Excluding_the_Minimum_and_Maximum_Salary.java
@@ -0,0 +1,10 @@
+package com.averageSalaryExcludingtheMinimumandMaximumSalary;
+
+public class E1491_Average_Salary_Excluding_the_Minimum_and_Maximum_Salary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
