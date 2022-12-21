bafdb2052f278913cef95396a5bdff5aab524a09
Jin Wang <specter01wj@gmail.com>
2022-12-21 11:43:43 -0500

    v1

---
+++ mysol/Category/Array/E1502_Can_Make_Arithmetic_Progression_From_Sequence/src/com/canMakeArithmeticProgressionFromSequence/E1502_Can_Make_Arithmetic_Progression_From_Sequence.java
@@ -0,0 +1,10 @@
+package com.canMakeArithmeticProgressionFromSequence;
+
+public class E1502_Can_Make_Arithmetic_Progression_From_Sequence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
