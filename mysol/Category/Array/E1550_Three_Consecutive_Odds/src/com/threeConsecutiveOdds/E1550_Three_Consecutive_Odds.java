9becdec18570343e7ffcbde6cf254c5920627aa1
Jin Wang <specter01wj@gmail.com>
2022-12-28 11:30:18 -0500

    v2

--- mysol/Category/Array/E1550_Three_Consecutive_Odds/src/com/threeConsecutiveOdds/E1550_Three_Consecutive_Odds.java
+++ mysol/Category/Array/E1550_Three_Consecutive_Odds/src/com/threeConsecutiveOdds/E1550_Three_Consecutive_Odds.java
@@ -1,10 +1,43 @@
 package com.threeConsecutiveOdds;
+import java.util.*;
+
+/*
+Given an integer array arr, return true if there are three consecutive 
+odd numbers in the array. Otherwise, return false.
+
+
+Example 1:
+
+Input: arr = [2,6,4,1]
+Output: false
+Explanation: There are no three consecutive odds.
+
+Example 2:
+
+Input: arr = [1,2,34,3,4,5,7,23,12]
+Output: true
+Explanation: [5,7,23] are three consecutive odds.
+*/
 
 public class E1550_Three_Consecutive_Odds {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {1,2,34,3,4,5,7,23,12};
+		boolean output = threeConsecutiveOdds(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public static boolean threeConsecutiveOdds(int[] arr) {
+        for (int i = 0, cnt = 0; i < arr.length; i++) {
+        	if (arr[i] % 2 == 0) {
+        		cnt = 0;
+        	} else if (++cnt == 3) {
+        		return true;
+        	}
+        }
+        
+        return false;
+    }
 
 }
