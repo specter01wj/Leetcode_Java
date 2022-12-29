6c84d0a9232b7af7a0bf990d157c67bb140f64fe
Jin Wang <specter01wj@gmail.com>
2022-12-29 12:19:06 -0500

    v1

---
+++ mysol/Category/Array/E1566_Detect_Pattern_of_Length_M_Repeated_K_or_More_Times/src/com/detectPatternofLengthMRepeatedKorMoreTimes/E1566_Detect_Pattern_of_Length_M_Repeated_K_or_More_Times.java
@@ -0,0 +1,10 @@
+package com.detectPatternofLengthMRepeatedKorMoreTimes;
+
+public class E1566_Detect_Pattern_of_Length_M_Repeated_K_or_More_Times {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
