1f3875158e724512f1e30d75e743e145382ada5e
Jin Wang <specter01wj@gmail.com>
2022-12-30 11:14:41 -0500

    v2

--- mysol/Category/Array/E1582_Special_Positions_in_a_Binary_Matrix/src/com/specialPositionsinaBinaryMatrix/E1582_Special_Positions_in_a_Binary_Matrix.java
+++ mysol/Category/Array/E1582_Special_Positions_in_a_Binary_Matrix/src/com/specialPositionsinaBinaryMatrix/E1582_Special_Positions_in_a_Binary_Matrix.java
@@ -1,10 +1,55 @@
 package com.specialPositionsinaBinaryMatrix;
+import java.util.*;
+
+/*
+Given an m x n binary matrix mat, return the number of special positions in mat.
+
+A position (i, j) is called special if mat[i][j] == 1 and all other elements in 
+row i and column j are 0 (rows and columns are 0-indexed).
+
+
+Example 1:
+
+Input: mat = [[1,0,0],[0,0,1],[1,0,0]]
+Output: 1
+Explanation: (1, 2) is a special position because mat[1][2] == 1 and 
+all other elements in row 1 and column 2 are 0.
+
+Example 2:
+
+Input: mat = [[1,0,0],[0,1,0],[0,0,1]]
+Output: 3
+Explanation: (0, 0), (1, 1) and (2, 2) are special positions.
+*/
 
 public class E1582_Special_Positions_in_a_Binary_Matrix {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[][] input = {{1,0,0},{0,0,1},{1,0,0}};
+		int output = numSpecial(input);
+        System.out.println("input: " + Arrays.deepToString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public static int numSpecial(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int res = 0, col[] = new int[n], row[] = new int[m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    col[j]++;
+                    row[i]++;
+                } 
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
 
 }
