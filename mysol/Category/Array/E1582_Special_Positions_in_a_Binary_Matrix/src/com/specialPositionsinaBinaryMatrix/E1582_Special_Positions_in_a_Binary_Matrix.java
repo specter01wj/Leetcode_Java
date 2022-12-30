8add8d81ad8af5b2eb6d675eaf8771187dc75d06
Jin Wang <specter01wj@gmail.com>
2022-12-30 11:07:08 -0500

    v1

---
+++ mysol/Category/Array/E1582_Special_Positions_in_a_Binary_Matrix/src/com/specialPositionsinaBinaryMatrix/E1582_Special_Positions_in_a_Binary_Matrix.java
@@ -0,0 +1,10 @@
+package com.specialPositionsinaBinaryMatrix;
+
+public class E1582_Special_Positions_in_a_Binary_Matrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
