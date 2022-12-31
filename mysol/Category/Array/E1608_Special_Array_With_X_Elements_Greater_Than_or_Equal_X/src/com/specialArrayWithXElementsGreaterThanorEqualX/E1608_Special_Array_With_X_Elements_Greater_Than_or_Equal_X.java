a6d5a16422f9899d3122ca09d9bb50b980d3e2a9
Jin Wang <specter01wj@gmail.com>
2022-12-31 13:50:54 -0500

    v1

---
+++ mysol/Category/Array/E1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X/src/com/specialArrayWithXElementsGreaterThanorEqualX/E1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X.java
@@ -0,0 +1,10 @@
+package com.specialArrayWithXElementsGreaterThanorEqualX;
+
+public class E1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
