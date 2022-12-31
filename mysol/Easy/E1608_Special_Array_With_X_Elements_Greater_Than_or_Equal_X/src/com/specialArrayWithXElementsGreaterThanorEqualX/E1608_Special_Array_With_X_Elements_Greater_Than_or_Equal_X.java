70061afc5f2e1952fd081042d331e125c2668007
Jin Wang <specter01wj@gmail.com>
2022-12-31 14:26:53 -0500

    v7

---
+++ mysol/Easy/E1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X/src/com/specialArrayWithXElementsGreaterThanorEqualX/E1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X.java
@@ -0,0 +1,80 @@
+package com.specialArrayWithXElementsGreaterThanorEqualX;
+import java.util.*;
+
+/*
+You are given an array nums of non-negative integers. nums is considered special 
+if there exists a number x such that there are exactly x numbers in nums that 
+are greater than or equal to x.
+
+Notice that x does not have to be an element in nums.
+
+Return x if the array is special, otherwise, return -1. It can be proven 
+that if nums is special, the value for x is unique.
+
+
+Example 1:
+
+Input: nums = [3,5]
+Output: 2
+Explanation: There are 2 values (3 and 5) that are greater than or equal to 2.
+
+Example 2:
+
+Input: nums = [0,0]
+Output: -1
+Explanation: No numbers fit the criteria for x.
+If x = 0, there should be 0 numbers >= x, but there are 2.
+If x = 1, there should be 1 number >= x, but there are 0.
+If x = 2, there should be 2 numbers >= x, but there are 0.
+x cannot be greater since there are only 2 numbers in nums.
+
+Example 3:
+
+Input: nums = [0,4,3,0,4]
+Output: 3
+Explanation: There are 3 values that are greater than or equal to 3.
+*/
+
+public class E1608_Special_Array_With_X_Elements_Greater_Than_or_Equal_X {
+
+	public static void main(String[] args) {
+		int[] input = {0,4,3,0,4};
+		int output = specialArray(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    while counts[counts.length - 1] store the occurences for all numbers >= counts.length - 1
+    x is the max possible answer, so no need to count occurences for each number >= x
+    Consider the index i as x, and the goal is to find when (res = current number of elements >= x) == x
+    */
+	
+	/*
+     * @param nums: a list of integers
+     * @return: x if the array is special, otherwise, return -1
+     */
+	public static int specialArray(int[] nums) {
+        int x = nums.length;
+        int[] counts = new int[x+1];
+        
+        for (int elem : nums) {
+            if (elem >= x) {
+                counts[x]++;
+            } else {
+                counts[elem]++;
+            }
+        }
+        
+        int res = 0;
+        for (int i = counts.length - 1; i > 0; i--) {
+            res += counts[i];
+            if(res == i) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+
+}
