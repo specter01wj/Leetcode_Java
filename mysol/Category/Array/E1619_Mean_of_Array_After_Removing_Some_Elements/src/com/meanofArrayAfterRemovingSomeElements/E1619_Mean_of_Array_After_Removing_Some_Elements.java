c74134a0fdc4a1108e67ebd54f9f593e6b3a90e3
Jin Wang <specter01wj@gmail.com>
2023-01-01 13:49:58 -0500

    v1

---
+++ mysol/Category/Array/E1619_Mean_of_Array_After_Removing_Some_Elements/src/com/meanofArrayAfterRemovingSomeElements/E1619_Mean_of_Array_After_Removing_Some_Elements.java
@@ -0,0 +1,10 @@
+package com.meanofArrayAfterRemovingSomeElements;
+
+public class E1619_Mean_of_Array_After_Removing_Some_Elements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
