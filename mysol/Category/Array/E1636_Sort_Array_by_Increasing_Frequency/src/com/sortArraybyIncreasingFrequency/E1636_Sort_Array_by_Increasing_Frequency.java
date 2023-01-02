99be159734caadc3c16d785128dd0b10039d9429
Jin Wang <specter01wj@gmail.com>
2023-01-02 13:33:50 -0500

    v5

---
+++ mysol/Category/Array/E1636_Sort_Array_by_Increasing_Frequency/src/com/sortArraybyIncreasingFrequency/E1636_Sort_Array_by_Increasing_Frequency.java
@@ -0,0 +1,10 @@
+package com.sortArraybyIncreasingFrequency;
+
+public class E1636_Sort_Array_by_Increasing_Frequency {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
