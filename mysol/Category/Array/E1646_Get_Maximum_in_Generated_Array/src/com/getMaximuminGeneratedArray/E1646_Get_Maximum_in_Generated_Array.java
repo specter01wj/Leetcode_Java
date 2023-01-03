71bb4b2b39efe7afb10cdd10229cefa7b6f97183
Jin Wang <specter01wj@gmail.com>
2023-01-03 13:12:51 -0500

    v6

---
+++ mysol/Category/Array/E1646_Get_Maximum_in_Generated_Array/src/com/getMaximuminGeneratedArray/E1646_Get_Maximum_in_Generated_Array.java
@@ -0,0 +1,10 @@
+package com.getMaximuminGeneratedArray;
+
+public class E1646_Get_Maximum_in_Generated_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
