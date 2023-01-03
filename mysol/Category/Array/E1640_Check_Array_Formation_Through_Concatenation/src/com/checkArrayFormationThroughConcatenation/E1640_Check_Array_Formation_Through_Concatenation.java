c018df2e1f3b71a8e868491d1ab1ade2ef8cd4cd
Jin Wang <specter01wj@gmail.com>
2023-01-03 11:55:00 -0500

    v1

---
+++ mysol/Category/Array/E1640_Check_Array_Formation_Through_Concatenation/src/com/checkArrayFormationThroughConcatenation/E1640_Check_Array_Formation_Through_Concatenation.java
@@ -0,0 +1,10 @@
+package com.checkArrayFormationThroughConcatenation;
+
+public class E1640_Check_Array_Formation_Through_Concatenation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
