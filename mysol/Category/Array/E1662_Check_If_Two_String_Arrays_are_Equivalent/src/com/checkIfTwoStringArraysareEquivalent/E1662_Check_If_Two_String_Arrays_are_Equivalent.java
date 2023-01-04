8baad2d1edd524110cc2fe0bd34d2b5e62e61c57
Jin Wang <specter01wj@gmail.com>
2023-01-04 13:22:46 -0500

    v8

---
+++ mysol/Category/Array/E1662_Check_If_Two_String_Arrays_are_Equivalent/src/com/checkIfTwoStringArraysareEquivalent/E1662_Check_If_Two_String_Arrays_are_Equivalent.java
@@ -0,0 +1,10 @@
+package com.checkIfTwoStringArraysareEquivalent;
+
+public class E1662_Check_If_Two_String_Arrays_are_Equivalent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
