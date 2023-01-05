25ba06187dae7e1080090d7098aa89e61f4f2dfd
Jin Wang <specter01wj@gmail.com>
2023-01-05 12:22:13 -0500

    v6

---
+++ mysol/Category/Array/E1684_Count_the_Number_of_Consistent_Strings/src/com/counttheNumberofConsistentStrings/E1684_Count_the_Number_of_Consistent_Strings.java
@@ -0,0 +1,10 @@
+package com.counttheNumberofConsistentStrings;
+
+public class E1684_Count_the_Number_of_Consistent_Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
