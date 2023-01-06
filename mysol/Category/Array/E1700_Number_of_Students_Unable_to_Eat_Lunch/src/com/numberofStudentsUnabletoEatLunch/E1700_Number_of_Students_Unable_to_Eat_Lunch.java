e7aa4fa946f8c0c88f7ab1aca081327a12310f48
Jin Wang <specter01wj@gmail.com>
2023-01-06 11:49:16 -0500

    v1

---
+++ mysol/Category/Array/E1700_Number_of_Students_Unable_to_Eat_Lunch/src/com/numberofStudentsUnabletoEatLunch/E1700_Number_of_Students_Unable_to_Eat_Lunch.java
@@ -0,0 +1,10 @@
+package com.numberofStudentsUnabletoEatLunch;
+
+public class E1700_Number_of_Students_Unable_to_Eat_Lunch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
