45407a36ba2c6b4bf9de5dc34b9b62cd3a0e57fa
Jin Wang <specter01wj@gmail.com>
2023-01-09 11:32:25 -0500

    v1

---
+++ mysol/Category/Array/E1668_Maximum_Repeating_Substring/src/com/maximumRepeatingSubstring/E1668_Maximum_Repeating_Substring.java
@@ -0,0 +1,10 @@
+package com.maximumRepeatingSubstring;
+
+public class E1668_Maximum_Repeating_Substring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
