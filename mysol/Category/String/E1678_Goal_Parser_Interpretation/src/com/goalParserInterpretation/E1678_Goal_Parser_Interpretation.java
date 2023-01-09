6be8eaf07e07c1bf3cdf971e21a52769b10a3568
Jin Wang <specter01wj@gmail.com>
2023-01-09 12:07:35 -0500

    v8

--- mysol/Category/String/E1678_Goal_Parser_Interpretation/src/com/goalParserInterpretation/E1678_Goal_Parser_Interpretation.java
+++ mysol/Category/String/E1678_Goal_Parser_Interpretation/src/com/goalParserInterpretation/E1678_Goal_Parser_Interpretation.java
@@ -1,10 +1,69 @@
 package com.goalParserInterpretation;
+import java.util.*;
+
+/*
+You own a Goal Parser that can interpret a string command. 
+The command consists of an alphabet of "G", "()" and/or "(al)" in some order. 
+The Goal Parser will interpret "G" as the string "G", "()" as the string "o", 
+and "(al)" as the string "al". 
+The interpreted strings are then concatenated in the original order.
+
+Given the string command, return the Goal Parser's interpretation of command.
+
+
+Example 1:
+
+Input: command = "G()(al)"
+Output: "Goal"
+Explanation: The Goal Parser interprets the command as follows:
+G -> G
+() -> o
+(al) -> al
+The final concatenated result is "Goal".
+
+Example 2:
+
+Input: command = "G()()()()(al)"
+Output: "Gooooal"
+
+Example 3:
+
+Input: command = "(al)G(al)()()G"
+Output: "alGalooG"
+*/
 
 public class E1678_Goal_Parser_Interpretation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "G()()()()(al)";
+        String output = interpret(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	/*
+    solution:
+    StringBuilder
+    */
+	
+	/**
+     * @param command: a string
+     * @return: the Goal Parser's interpretation of command
+     */
+	public static String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < command.length(); ++i) {
+            char c = command.charAt(i);
+            if (c == 'G') {
+                sb.append(c);
+            } else if (c == '(' && command.charAt(i + 1) == ')') {
+                sb.append("o");
+                ++i;
+            } else {
+                sb.append("al");
+                i += 3;
+            }
+        }
+        return sb.toString();
+    }
 
 }
