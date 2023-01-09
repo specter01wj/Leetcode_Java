954ad9a9898c94816702cbd5dfdef07e5f5c5800
Jin Wang <specter01wj@gmail.com>
2023-01-09 12:01:56 -0500

    v6

---
+++ mysol/Category/String/E1678_Goal_Parser_Interpretation/src/com/goalParserInterpretation/E1678_Goal_Parser_Interpretation.java
@@ -0,0 +1,10 @@
+package com.goalParserInterpretation;
+
+public class E1678_Goal_Parser_Interpretation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
