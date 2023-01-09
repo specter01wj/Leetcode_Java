fdd8af5484c3ed2457eacb0f32cc5e3e609bda22
Jin Wang <specter01wj@gmail.com>
2023-01-08 21:44:33 -0500

    v1

---
+++ mysol/Category/Array/E1725_Number_Of_Rectangles_That_Can_Form_The_Largest_Square/src/com/numberOfRectanglesThatCanFormTheLargestSquare/E1725_Number_Of_Rectangles_That_Can_Form_The_Largest_Square.java
@@ -0,0 +1,10 @@
+package com.numberOfRectanglesThatCanFormTheLargestSquare;
+
+public class E1725_Number_Of_Rectangles_That_Can_Form_The_Largest_Square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
