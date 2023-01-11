7a2663d64e4e5bd4a3bec2c5f857c79a0527c920
Jin Wang <specter01wj@gmail.com>
2023-01-11 13:53:40 -0500

    v7

---
+++ mysol/Category/String/E1758_Minimum_Changes_To_Make_Alternating_Binary_String/src/com/minimumChangesToMakeAlternatingBinaryString/E1758_Minimum_Changes_To_Make_Alternating_Binary_String.java
@@ -0,0 +1,10 @@
+package com.minimumChangesToMakeAlternatingBinaryString;
+
+public class E1758_Minimum_Changes_To_Make_Alternating_Binary_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
