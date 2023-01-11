2e1e7a136ef4c6d197100d7ad3b24ea959e9ea20
Jin Wang <specter01wj@gmail.com>
2023-01-11 13:59:01 -0500

    v8

--- mysol/Category/String/E1758_Minimum_Changes_To_Make_Alternating_Binary_String/src/com/minimumChangesToMakeAlternatingBinaryString/E1758_Minimum_Changes_To_Make_Alternating_Binary_String.java
+++ mysol/Category/String/E1758_Minimum_Changes_To_Make_Alternating_Binary_String/src/com/minimumChangesToMakeAlternatingBinaryString/E1758_Minimum_Changes_To_Make_Alternating_Binary_String.java
@@ -38,7 +38,30 @@ public class E1758_Minimum_Changes_To_Make_Alternating_Binary_String {
         System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    Small observation that the sequence of index is [0,1,2,3..],
+	if we get its module by 2, we get [0,1,0,1,0..],
+	which is the alternating binary sequence we want.
 	
+	So we iterate the string,
+	check if the characters[i] is same as the i % 2.
+	Note that s[i] is a character,
+	and s[i] - '0' making it to integer.
+	
+	We accumulate the number of difference,
+	which is the number of operation to make it into 01 string.
+	
+	We can do the same to find out res,
+	the number of operation for 10 string.
+	But we don't have to,
+	becaue this equals to s.length - res.
+    */
+	
+	/**
+     * @param s: a string
+     * @return: the minimum number of operations needed to make s alternating
+     */
 	public static int minOperations(String s) {
         int res = 0, n = s.length();
         for (int i = 0; i < n; ++i) {
