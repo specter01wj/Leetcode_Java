d38b9032dbdd1e63f78849d942f092e29ee533d5
Jin Wang <specter01wj@gmail.com>
2023-01-13 12:13:18 -0500

    v7

---
+++ mysol/Category/String/E1784_Check_if_Binary_String_Has_at_Most_One_Segment_of_Ones/src/com/checkifBinaryStringHasatMostOneSegmentofOnes/E1784_Check_if_Binary_String_Has_at_Most_One_Segment_of_Ones.java
@@ -0,0 +1,10 @@
+package com.checkifBinaryStringHasatMostOneSegmentofOnes;
+
+public class E1784_Check_if_Binary_String_Has_at_Most_One_Segment_of_Ones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
