fd018d1242b2a6bf653726f89224be0528d670f6
Jin Wang <specter01wj@gmail.com>
2023-01-13 11:26:14 -0500

    v1

---
+++ mysol/Category/String/E1773_Count_Items_Matching_a_Rule/src/com/countItemsMatchingaRule/E1773_Count_Items_Matching_a_Rule.java
@@ -0,0 +1,10 @@
+package com.countItemsMatchingaRule;
+
+public class E1773_Count_Items_Matching_a_Rule {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
