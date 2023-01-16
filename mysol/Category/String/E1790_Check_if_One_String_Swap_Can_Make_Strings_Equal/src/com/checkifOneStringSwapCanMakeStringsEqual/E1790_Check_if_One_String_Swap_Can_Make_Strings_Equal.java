34715df12acf0a0f8c612815e6d369a1be42b177
Jin Wang <specter01wj@gmail.com>
2023-01-16 12:35:31 -0500

    v1

---
+++ mysol/Category/String/E1790_Check_if_One_String_Swap_Can_Make_Strings_Equal/src/com/checkifOneStringSwapCanMakeStringsEqual/E1790_Check_if_One_String_Swap_Can_Make_Strings_Equal.java
@@ -0,0 +1,10 @@
+package com.checkifOneStringSwapCanMakeStringsEqual;
+
+public class E1790_Check_if_One_String_Swap_Can_Make_Strings_Equal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
