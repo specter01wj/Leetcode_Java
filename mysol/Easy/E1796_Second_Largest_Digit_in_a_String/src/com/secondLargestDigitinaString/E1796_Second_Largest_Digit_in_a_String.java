7e3e646b3854e61d2cb6b3b9652006a7b5628ac2
Jin Wang <specter01wj@gmail.com>
2023-01-17 16:39:10 -0500

    v12

---
+++ mysol/Easy/E1796_Second_Largest_Digit_in_a_String/src/com/secondLargestDigitinaString/E1796_Second_Largest_Digit_in_a_String.java
@@ -0,0 +1,58 @@
+package com.secondLargestDigitinaString;
+import java.util.*;
+
+/*
+Given an alphanumeric string s, return the second largest numerical 
+digit that appears in s, or -1 if it does not exist.
+
+An alphanumeric string is a string consisting of lowercase English letters and digits.
+
+
+Example 1:
+
+Input: s = "dfa12321afd"
+Output: 2
+Explanation: The digits that appear in s are [1, 2, 3]. The second largest digit is 2.
+
+Example 2:
+
+Input: s = "abc1111"
+Output: -1
+Explanation: The digits that appear in s are [1]. There is no second largest digit.
+*/
+
+public class E1796_Second_Largest_Digit_in_a_String {
+
+	public static void main(String[] args) {
+		String input = "dfa12321afd";
+        int output = secondHighest(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    loop and count
+    */
+	
+	/**
+     * @param s: a string
+     * @return: the second largest numerical digit that appears in s, or -1 if it does not exist
+     */
+	public static int secondHighest(String s) {
+        int first = -1, sec = -1;
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i); 
+            if (Character.isDigit(c)) {
+                int d = c - '0';
+                if (first < d) {
+                    sec = first;
+                    first = d;
+                } else if (sec < d && d < first) {
+                    sec = d;
+                }
+            }
+        }
+        return sec;
+    }
+
+}
---
+++ mysol/Easy/E1805_Number_of_Different_Integers_in_a_String/src/com/numberofDifferentIntegersinaString/E1805_Number_of_Different_Integers_in_a_String.java
@@ -0,0 +1,72 @@
+package com.numberofDifferentIntegersinaString;
+import java.util.*;
+
+/*
+You are given a string word that consists of digits and lowercase English letters.
+
+You will replace every non-digit character with a space. For example, "a123bc34d8ef34" 
+will become " 123  34 8  34". Notice that you are left with some integers that are 
+separated by at least one space: "123", "34", "8", and "34".
+
+Return the number of different integers after performing the replacement operations on word.
+
+Two integers are considered different if their decimal representations without any 
+leading zeros are different.
+
+
+Example 1:
+
+Input: word = "a123bc34d8ef34"
+Output: 3
+Explanation: The three different integers are "123", "34", and "8". 
+Notice that "34" is only counted once.
+
+Example 2:
+
+Input: word = "leet1234code234"
+Output: 2
+
+Example 3:
+
+Input: word = "a1b01c001"
+Output: 1
+Explanation: The three integers "1", "01", and "001" all represent the same integer because
+the leading zeros are ignored when comparing their decimal values.
+*/
+
+public class E1805_Number_of_Different_Integers_in_a_String {
+
+	public static void main(String[] args) {
+		String input = "a123bc34d8ef34";
+        int output = numDifferentIntegers(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    1. Replace all the non-numeric character with a single space.
+	2. Split the word by spaces and store it in a String[]
+	3. Ignore empty strings
+	4. Replace all 0 from the beginning, if any
+	5. Save the new word in the set
+	6. Return the set size
+    */
+	
+	/**
+     * @param word: a string
+     * @return: the number of different integers after performing the replacement operations on word
+     */
+	public static int numDifferentIntegers(String word) {
+        String[] arr = word.replaceAll("[a-zA-Z]", " ").split("\\s+");
+        Set<String> set = new HashSet<String>();
+            
+        for (String str : arr) {
+            if (!str.isEmpty()) {
+                set.add(String.valueOf(str.replaceAll("^0*","")));
+            }
+        }
+                
+        return set.size();
+    }
+
+}
