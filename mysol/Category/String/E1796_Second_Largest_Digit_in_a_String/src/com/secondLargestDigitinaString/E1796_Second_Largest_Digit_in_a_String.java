29a730739d51fd1b0ee226e3c718e0f232f47d67
Jin Wang <specter01wj@gmail.com>
2023-01-17 13:02:06 -0500

    v1

---
+++ mysol/Category/String/E1796_Second_Largest_Digit_in_a_String/src/com/secondLargestDigitinaString/E1796_Second_Largest_Digit_in_a_String.java
@@ -0,0 +1,10 @@
+package com.secondLargestDigitinaString;
+
+public class E1796_Second_Largest_Digit_in_a_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
