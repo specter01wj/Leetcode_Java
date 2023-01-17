38d9090e1b6575c1dc9942dc17329e9b10f334d0
Jin Wang <specter01wj@gmail.com>
2023-01-17 15:59:07 -0500

    v6

---
+++ mysol/Category/String/E1805_Number_of_Different_Integers_in_a_String/src/com/numberofDifferentIntegersinaString/E1805_Number_of_Different_Integers_in_a_String.java
@@ -0,0 +1,10 @@
+package com.numberofDifferentIntegersinaString;
+
+public class E1805_Number_of_Different_Integers_in_a_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
