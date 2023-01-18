260af2d643bd856038f367218ab58086794fa2bf
Jin Wang <specter01wj@gmail.com>
2023-01-18 12:04:25 -0500

    v6

--- mysol/Category/String/E1816_Truncate_Sentence/src/com/truncateSentence/E1816_Truncate_Sentence.java
+++ mysol/Category/String/E1816_Truncate_Sentence/src/com/truncateSentence/E1816_Truncate_Sentence.java
@@ -1,10 +1,69 @@
 package com.truncateSentence;
+import java.util.*;
+
+/*
+A sentence is a list of words that are separated by a single space with no leading 
+or trailing spaces. Each of the words consists of only uppercase and lowercase 
+English letters (no punctuation).
+
+	.For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+
+You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+
+
+Example 1:
+
+Input: s = "Hello how are you Contestant", k = 4
+Output: "Hello how are you"
+Explanation:
+The words in s are ["Hello", "how" "are", "you", "Contestant"].
+The first 4 words are ["Hello", "how", "are", "you"].
+Hence, you should return "Hello how are you".
+
+Example 2:
+
+Input: s = "What is the solution to this problem", k = 4
+Output: "What is the solution"
+Explanation:
+The words in s are ["What", "is" "the", "solution", "to", "this", "problem"].
+The first 4 words are ["What", "is", "the", "solution"].
+Hence, you should return "What is the solution".
+
+Example 3:
+
+Input: s = "chopper is not a tanuki", k = 5
+Output: "chopper is not a tanuki"
+*/
 
 public class E1816_Truncate_Sentence {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "What is the solution to this problem";
+		int k = 4;
+        String output = truncateSentence(input, 4);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	/*
+    solution:
+    opened count the number of opened parenthesis.
+	Add every char to the result,
+	unless the first left parenthesis,
+	and the last right parenthesis.
+    */
+	
+	/**
+     * @param s: a string
+     * @param k: an integer
+     * @return: after removing the outermost parentheses
+     */
+	public static String truncateSentence(String s, int k) {
+        String[] words = s.split(" ");
+        StringBuilder sb = new StringBuilder(words[0]);
+        for (int i = 1; i < k; ++i) {
+            sb.append(" " + words[i]);
+        }
+        return sb.toString();
+    }
 
 }
