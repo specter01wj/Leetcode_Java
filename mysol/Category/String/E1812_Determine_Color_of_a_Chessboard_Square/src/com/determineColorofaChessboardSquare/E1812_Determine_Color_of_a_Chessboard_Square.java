a63ef11101c6fa0f11850e6272e615f98c78beef
Jin Wang <specter01wj@gmail.com>
2023-01-18 11:42:13 -0500

    v1

---
+++ mysol/Category/String/E1812_Determine_Color_of_a_Chessboard_Square/src/com/determineColorofaChessboardSquare/E1812_Determine_Color_of_a_Chessboard_Square.java
@@ -0,0 +1,10 @@
+package com.determineColorofaChessboardSquare;
+
+public class E1812_Determine_Color_of_a_Chessboard_Square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
