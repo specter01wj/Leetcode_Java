46c6964c07ce98e5d97a1a458f7529d1046491c0
Jin Wang <specter01wj@gmail.com>
2023-01-19 12:42:58 -0500

    v5

---
+++ mysol/Category/String/E1844_Replace_All_Digits_with_Characters/src/com/replaceAllDigitswithCharacters/E1844_Replace_All_Digits_with_Characters.java
@@ -0,0 +1,10 @@
+package com.replaceAllDigitswithCharacters;
+
+public class E1844_Replace_All_Digits_with_Characters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
