55401eb4fc339cd0afa8742610a2152f77c93e7b
Jin Wang <specter01wj@gmail.com>
2023-01-19 12:00:37 -0500

    v2

--- mysol/Category/String/E1832_Check_if_the_Sentence_Is_Pangram/src/com/checkiftheSentenceIsPangram/E1832_Check_if_the_Sentence_Is_Pangram.java
+++ mysol/Category/String/E1832_Check_if_the_Sentence_Is_Pangram/src/com/checkiftheSentenceIsPangram/E1832_Check_if_the_Sentence_Is_Pangram.java
@@ -1,10 +1,40 @@
 package com.checkiftheSentenceIsPangram;
+import java.util.*;
+
+/*
+A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence 
+is a pangram, or false otherwise.
+
+
+Example 1:
+
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+Explanation: sentence contains at least one of every letter of the English alphabet.
+
+Example 2:
+
+Input: sentence = "leetcode"
+Output: false
+*/
 
 public class E1832_Check_if_the_Sentence_Is_Pangram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "thequickbrownfoxjumpsoverthelazydog";
+        boolean output = checkIfPangram(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	
+	public static boolean checkIfPangram(String sentence) {
+        Set<Character> s = new HashSet<>();
+        for (int i = 0; i < sentence.length(); ++i) {
+            s.add(sentence.charAt(i));
+        }
+        return s.size() == 26;
+    }
 
 }
