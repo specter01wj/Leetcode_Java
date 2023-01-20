f79b99000c9eb1191c73f8ae5b81a1bac7c9fa5f
Jin Wang <specter01wj@gmail.com>
2023-01-20 11:45:23 -0500

    v4

---
+++ mysol/Category/String/E1869_Longer_Contiguous_Segments_of_Ones_than_Zeros/src/com/longerContiguousSegmentsofOnesthanZeros/E1869_Longer_Contiguous_Segments_of_Ones_than_Zeros.java
@@ -0,0 +1,10 @@
+package com.longerContiguousSegmentsofOnesthanZeros;
+
+public class E1869_Longer_Contiguous_Segments_of_Ones_than_Zeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
