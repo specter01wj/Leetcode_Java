619a90fa247c6bc587b8d6f92d7c6a01ee2f0072
Jin Wang <specter01wj@gmail.com>
2023-01-21 10:54:17 -0500

    v1

---
+++ mysol/Category/String/E1876_Substrings_of_Size_Three_with_Distinct_Characters/src/com/substringsofSizeThreewithDistinctCharacters/E1876_Substrings_of_Size_Three_with_Distinct_Characters.java
@@ -0,0 +1,10 @@
+package com.substringsofSizeThreewithDistinctCharacters;
+
+public class E1876_Substrings_of_Size_Three_with_Distinct_Characters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
