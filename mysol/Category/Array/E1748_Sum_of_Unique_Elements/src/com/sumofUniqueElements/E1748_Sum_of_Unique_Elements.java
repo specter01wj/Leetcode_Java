bfcc550a6e1668e418a907f4afddabbb6b5e7b1a
Jin Wang <specter01wj@gmail.com>
2023-01-23 11:33:11 -0500

    v7

--- mysol/Category/Array/E1748_Sum_of_Unique_Elements/src/com/sumofUniqueElements/E1748_Sum_of_Unique_Elements.java
+++ mysol/Category/Array/E1748_Sum_of_Unique_Elements/src/com/sumofUniqueElements/E1748_Sum_of_Unique_Elements.java
@@ -1,10 +1,53 @@
 package com.sumofUniqueElements;
+import java.util.*;
+
+/*
+You are given an integer array nums. The unique elements of an array 
+are the elements that appear exactly once in the array.
+
+Return the sum of all the unique elements of nums.
+
+
+Example 1:
+
+Input: nums = [1,2,3,2]
+Output: 4
+Explanation: The unique elements are [1,3], and the sum is 4.
+
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: 0
+Explanation: There are no unique elements, and the sum is 0.
+
+Example 3:
+
+Input: nums = [1,2,3,4,5]
+Output: 15
+Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+*/
 
 public class E1748_Sum_of_Unique_Elements {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {1,2,3,2};
+		int output = sumOfUnique(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public static int sumOfUnique(int[] nums) {
+        int[] cnt = new int[101];
+        for (int n : nums) {
+            ++cnt[n];
+        }
+        int sum = 0;
+        for (int i = 1; i <= 100; ++i) {
+            if (cnt[i] == 1) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
 
 }
