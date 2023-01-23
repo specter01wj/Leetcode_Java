52f81875b12cd3e92f969037b646a75868a71752
Jin Wang <specter01wj@gmail.com>
2023-01-22 22:02:21 -0500

    v1

---
+++ mysol/Category/String/E1880_Check_if_Word_Equals_Summation_of_Two_Words/src/com/checkifWordEqualsSummationofTwoWords/E1880_Check_if_Word_Equals_Summation_of_Two_Words.java
@@ -0,0 +1,10 @@
+package com.checkifWordEqualsSummationofTwoWords;
+
+public class E1880_Check_if_Word_Equals_Summation_of_Two_Words {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
