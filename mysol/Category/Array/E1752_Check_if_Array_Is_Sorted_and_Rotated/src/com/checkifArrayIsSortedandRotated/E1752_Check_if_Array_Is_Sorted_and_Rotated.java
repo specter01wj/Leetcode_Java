a2e2e6260ad7f5b8c0b2b6eee1e813c801b86715
Jin Wang <specter01wj@gmail.com>
2023-01-24 11:38:27 -0500

    v2

--- mysol/Category/Array/E1752_Check_if_Array_Is_Sorted_and_Rotated/src/com/checkifArrayIsSortedandRotated/E1752_Check_if_Array_Is_Sorted_and_Rotated.java
+++ mysol/Category/Array/E1752_Check_if_Array_Is_Sorted_and_Rotated/src/com/checkifArrayIsSortedandRotated/E1752_Check_if_Array_Is_Sorted_and_Rotated.java
@@ -1,10 +1,58 @@
 package com.checkifArrayIsSortedandRotated;
+import java.util.*;
+
+/*
+Given an array nums, return true if the array was originally sorted in 
+non-decreasing order, then rotated some number of positions (including zero). 
+Otherwise, return false.
+
+There may be duplicates in the original array.
+
+Note: An array A rotated by x positions results in an array B of the same length 
+such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
+
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: true
+Explanation: [1,2,3,4,5] is the original sorted array.
+You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].
+
+Example 2:
+
+Input: nums = [2,1,3,4]
+Output: false
+Explanation: There is no sorted array once rotated that can make nums.
+
+Example 3:
+
+Input: nums = [1,2,3]
+Output: true
+Explanation: [1,2,3] is the original sorted array.
+You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
+*/
 
 public class E1752_Check_if_Array_Is_Sorted_and_Rotated {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {3,4,5,1,2};
+		boolean output = check(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public static boolean check(int[] nums) {
+       int k = 0, n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                k++;
+            }
+            if (k > 1) {
+                return false;
+            }
+        }
+        return true; 
+    }
 
 }
