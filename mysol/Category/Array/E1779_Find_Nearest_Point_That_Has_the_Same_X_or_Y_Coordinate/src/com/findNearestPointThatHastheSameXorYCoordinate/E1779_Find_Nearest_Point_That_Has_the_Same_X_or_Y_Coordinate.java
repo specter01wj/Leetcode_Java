2fa3173a7ea18283b40633a449318b69567a61c6
Jin Wang <specter01wj@gmail.com>
2023-01-24 12:06:49 -0500

    v5

---
+++ mysol/Category/Array/E1779_Find_Nearest_Point_That_Has_the_Same_X_or_Y_Coordinate/src/com/findNearestPointThatHastheSameXorYCoordinate/E1779_Find_Nearest_Point_That_Has_the_Same_X_or_Y_Coordinate.java
@@ -0,0 +1,10 @@
+package com.findNearestPointThatHastheSameXorYCoordinate;
+
+public class E1779_Find_Nearest_Point_That_Has_the_Same_X_or_Y_Coordinate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
