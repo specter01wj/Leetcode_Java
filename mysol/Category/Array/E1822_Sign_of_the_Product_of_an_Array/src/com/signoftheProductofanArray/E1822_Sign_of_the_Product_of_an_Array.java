58ec404ef8211919838daea09d8870631b3b5693
Jin Wang <specter01wj@gmail.com>
2023-01-25 11:03:02 -0500

    v7

---
+++ mysol/Category/Array/E1822_Sign_of_the_Product_of_an_Array/src/com/signoftheProductofanArray/E1822_Sign_of_the_Product_of_an_Array.java
@@ -0,0 +1,10 @@
+package com.signoftheProductofanArray;
+
+public class E1822_Sign_of_the_Product_of_an_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
