249387907546a6e61fa374558454a9c1f68d5628
Jin Wang <specter01wj@gmail.com>
2023-01-26 10:37:11 -0500

    v1

---
+++ mysol/Category/Array/E1827_Minimum_Operations_to_Make_the_Array_Increasing/src/com/minimumOperationstoMaketheArrayIncreasing/E1827_Minimum_Operations_to_Make_the_Array_Increasing.java
@@ -0,0 +1,10 @@
+package com.minimumOperationstoMaketheArrayIncreasing;
+
+public class E1827_Minimum_Operations_to_Make_the_Array_Increasing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
