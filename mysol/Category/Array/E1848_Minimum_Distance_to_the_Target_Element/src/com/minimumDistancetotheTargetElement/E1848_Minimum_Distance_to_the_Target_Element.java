0563658448c778e270d4bbbe8057d359c857beb1
Jin Wang <specter01wj@gmail.com>
2023-01-26 10:58:57 -0500

    v5

---
+++ mysol/Category/Array/E1848_Minimum_Distance_to_the_Target_Element/src/com/minimumDistancetotheTargetElement/E1848_Minimum_Distance_to_the_Target_Element.java
@@ -0,0 +1,10 @@
+package com.minimumDistancetotheTargetElement;
+
+public class E1848_Minimum_Distance_to_the_Target_Element {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
