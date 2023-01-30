d390935157f05f5afe83f9f9e647227275dc6acb
Jin Wang <specter01wj@gmail.com>
2023-01-30 11:02:52 -0500

    v1

---
+++ mysol/Category/Array/E1863_Sum_of_All_Subset_XOR_Totals/src/com/sumofAllSubsetXORTotals/E1863_Sum_of_All_Subset_XOR_Totals.java
@@ -0,0 +1,10 @@
+package com.sumofAllSubsetXORTotals;
+
+public class E1863_Sum_of_All_Subset_XOR_Totals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
