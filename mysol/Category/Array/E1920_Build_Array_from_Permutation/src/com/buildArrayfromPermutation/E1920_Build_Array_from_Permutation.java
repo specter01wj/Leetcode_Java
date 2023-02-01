8bbbb37eacac86911e3794eaf4c9241d6cc17167
Jin Wang <specter01wj@gmail.com>
2023-02-01 11:48:21 -0500

    v5

---
+++ mysol/Category/Array/E1920_Build_Array_from_Permutation/src/com/buildArrayfromPermutation/E1920_Build_Array_from_Permutation.java
@@ -0,0 +1,10 @@
+package com.buildArrayfromPermutation;
+
+public class E1920_Build_Array_from_Permutation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
