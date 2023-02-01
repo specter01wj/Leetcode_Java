02d8a1744b2ba537f5896211a08e52ff825f99a7
Jin Wang <specter01wj@gmail.com>
2023-02-01 11:54:21 -0500

    v6

--- mysol/Category/Array/E1920_Build_Array_from_Permutation/src/com/buildArrayfromPermutation/E1920_Build_Array_from_Permutation.java
+++ mysol/Category/Array/E1920_Build_Array_from_Permutation/src/com/buildArrayfromPermutation/E1920_Build_Array_from_Permutation.java
@@ -1,10 +1,56 @@
 package com.buildArrayfromPermutation;
+import java.util.*;
+
+/*
+Given a zero-based permutation nums (0-indexed), build an array ans of the same 
+length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+A zero-based permutation nums is an array of distinct integers from 0 to 
+nums.length - 1 (inclusive).
+
+
+Example 1:
+
+Input: nums = [0,2,1,5,3,4]
+Output: [0,1,2,4,5,3]
+Explanation: The array ans is built as follows: 
+ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+    = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
+    = [0,1,2,4,5,3]
+
+Example 2:
+
+Input: nums = [5,0,1,2,3,4]
+Output: [4,5,0,1,2,3]
+Explanation: The array ans is built as follows:
+ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+    = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
+    = [4,5,0,1,2,3]
+*/
 
 public class E1920_Build_Array_from_Permutation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {0,2,1,5,3,4};
+		int[] output = buildArray(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + Arrays.toString(output));
 	}
+	
+	/*
+    solution:
+    loop and build a new array
+    */
+	
+	/*
+     * @param nums: a list of integers
+     * @return: an array ans of the same length where ans[i] = nums[nums[i]]
+     */
+	public static int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for(int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
 
 }
