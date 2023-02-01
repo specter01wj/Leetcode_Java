8e0561b7c660a94025e08f62b2db4c2ffb644fb2
Jin Wang <specter01wj@gmail.com>
2023-02-01 11:08:47 -0500

    v1

---
+++ mysol/Category/Array/E1913_Maximum_Product_Difference_Between_Two_Pairs/src/com/maximumProductDifferenceBetweenTwoPairs/E1913_Maximum_Product_Difference_Between_Two_Pairs.java
@@ -0,0 +1,10 @@
+package com.maximumProductDifferenceBetweenTwoPairs;
+
+public class E1913_Maximum_Product_Difference_Between_Two_Pairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
