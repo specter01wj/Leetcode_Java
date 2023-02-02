f75e89b34e438f78f4dc72668c245bf326fa2a85
Jin Wang <specter01wj@gmail.com>
2023-02-02 12:09:10 -0500

    v5

---
+++ mysol/Category/Array/E1961_Check_If_String_Is_a_Prefix_of_Array/src/com/checkIfStringIsaPrefixofArray/E1961_Check_If_String_Is_a_Prefix_of_Array.java
@@ -0,0 +1,10 @@
+package com.checkIfStringIsaPrefixofArray;
+
+public class E1961_Check_If_String_Is_a_Prefix_of_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
