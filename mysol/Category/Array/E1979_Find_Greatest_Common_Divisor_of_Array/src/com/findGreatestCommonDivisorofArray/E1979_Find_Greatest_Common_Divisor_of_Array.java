543f7bd0a2c7cd4559e6b0cbe8218ebe8722e474
Jin Wang <specter01wj@gmail.com>
2023-02-03 11:06:38 -0500

    v1

---
+++ mysol/Category/Array/E1979_Find_Greatest_Common_Divisor_of_Array/src/com/findGreatestCommonDivisorofArray/E1979_Find_Greatest_Common_Divisor_of_Array.java
@@ -0,0 +1,10 @@
+package com.findGreatestCommonDivisorofArray;
+
+public class E1979_Find_Greatest_Common_Divisor_of_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
