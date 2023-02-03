b59c08156fe1214c055587fc3c7e3a7c9c9d1957
Jin Wang <specter01wj@gmail.com>
2023-02-03 11:33:38 -0500

    v5

---
+++ mysol/Category/Array/E1984_Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores/src/com/minimumDifferenceBetweenHighestandLowestofKScores/E1984_Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores.java
@@ -0,0 +1,10 @@
+package com.minimumDifferenceBetweenHighestandLowestofKScores;
+
+public class E1984_Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
