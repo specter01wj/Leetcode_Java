9de45d5a9226c0e86d6153e7bfc2c287e4cbb293
Jin Wang <specter01wj@gmail.com>
2023-02-05 13:52:44 -0500

    v1

---
+++ mysol/Category/Array/E1991_Find_the_Middle_Index_in_Array/src/com/findtheMiddleIndexinArray/E1991_Find_the_Middle_Index_in_Array.java
@@ -0,0 +1,10 @@
+package com.findtheMiddleIndexinArray;
+
+public class E1991_Find_the_Middle_Index_in_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
