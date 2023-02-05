97ec719c8e4bc1909af5758fed48bfaabdd8a92c
Jin Wang <specter01wj@gmail.com>
2023-02-05 14:26:03 -0500

    v6

--- mysol/Category/Array/E1995_Count_Special_Quadruplets/src/com/countSpecialQuadruplets/E1995_Count_Special_Quadruplets.java
+++ mysol/Category/Array/E1995_Count_Special_Quadruplets/src/com/countSpecialQuadruplets/E1995_Count_Special_Quadruplets.java
@@ -1,10 +1,65 @@
 package com.countSpecialQuadruplets;
+import java.util.*;
+
+/*
+Given a 0-indexed integer array nums, return the number of distinct 
+quadruplets (a, b, c, d) such that:
+
+	.nums[a] + nums[b] + nums[c] == nums[d], and
+	.a < b < c < d
+
+
+Example 1:
+
+Input: nums = [1,2,3,6]
+Output: 1
+Explanation: The only quadruplet that satisfies the requirement is (0, 1, 2, 3) 
+because 1 + 2 + 3 == 6.
+
+Example 2:
+
+Input: nums = [3,3,6,4,5]
+Output: 0
+Explanation: There are no such quadruplets in [3,3,6,4,5].
+
+Example 3:
+
+Input: nums = [1,1,1,3,5]
+Output: 4
+Explanation: The 4 quadruplets that satisfy the requirement are:
+- (0, 1, 2, 3): 1 + 1 + 1 == 3
+- (0, 1, 3, 4): 1 + 1 + 3 == 5
+- (0, 2, 3, 4): 1 + 1 + 3 == 5
+- (1, 2, 3, 4): 1 + 1 + 3 == 5
+*/
 
 public class E1995_Count_Special_Quadruplets {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {1,1,1,3,5};
+		int output = countQuadruplets(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public static int countQuadruplets(int[] nums) {
+        int res = 0;
+        int len = nums.length;
+        
+        Map<Integer, Integer> count = new HashMap<>();
+        count.put(nums[len-1] - nums[len-2], 1);
+        
+        for (int b = len - 3; b >= 1; b--) {
+            for (int a = b - 1; a >= 0; a--) {
+                res += count.getOrDefault(nums[a] + nums[b], 0);
+            }
+            
+            for (int x = len - 1; x > b; x--) {
+                count.put(nums[x] - nums[b], count.getOrDefault(nums[x] - nums[b], 0) + 1);
+            }
+        }
+        
+        return res;
+    }
 
 }
