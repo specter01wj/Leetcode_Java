0e8b9a35b3a4562414d589e183fa09fa62a08c4f
Jin Wang <specter01wj@gmail.com>
2023-02-06 11:55:43 -0500

    v6

--- mysol/Category/String/E1903_Largest_Odd_Number_in_String/src/com/largestOddNumberinString/E1903_Largest_Odd_Number_in_String.java
+++ mysol/Category/String/E1903_Largest_Odd_Number_in_String/src/com/largestOddNumberinString/E1903_Largest_Odd_Number_in_String.java
@@ -1,10 +1,49 @@
 package com.largestOddNumberinString;
+import java.util.*;
+
+/*
+You are given a string num, representing a large integer. Return the largest-valued 
+odd integer (as a string) that is a non-empty substring of num, or an empty 
+string "" if no odd integer exists.
+
+A substring is a contiguous sequence of characters within a string.
+
+
+Example 1:
+
+Input: num = "52"
+Output: "5"
+Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
+
+Example 2:
+
+Input: num = "4206"
+Output: ""
+Explanation: There are no odd numbers in "4206".
+
+Example 3:
+
+Input: num = "35427"
+Output: "35427"
+Explanation: "35427" is already an odd number.
+*/
 
 public class E1903_Largest_Odd_Number_in_String {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "35436";
+		String output = largestOddNumber(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	
+	public static String largestOddNumber(String num) {
+        for (int i = num.length() - 1; i >= 0; i--) {
+            if (num.charAt(i) % 2 != 0) {
+                return num.substring(0, i + 1);
+            }
+        }
+        return "";
+    }
 
 }
