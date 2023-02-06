f991f922ef1ae2faf1cc1e0435f67966a8ef4589
Jin Wang <specter01wj@gmail.com>
2023-02-06 10:56:52 -0500

    v1

---
+++ mysol/Category/String/E1897_Redistribute_Characters_to_Make_All_Strings_Equal/src/com/redistributeCharacterstoMakeAllStringsEqual/E1897_Redistribute_Characters_to_Make_All_Strings_Equal.java
@@ -0,0 +1,10 @@
+package com.redistributeCharacterstoMakeAllStringsEqual;
+
+public class E1897_Redistribute_Characters_to_Make_All_Strings_Equal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
