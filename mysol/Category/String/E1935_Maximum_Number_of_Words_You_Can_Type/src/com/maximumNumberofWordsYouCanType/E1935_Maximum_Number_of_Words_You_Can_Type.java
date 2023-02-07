989283d85fdc5c171c37dd4c5c3dd3b705e8750a
Jin Wang <specter01wj@gmail.com>
2023-02-07 10:58:30 -0500

    v1

---
+++ mysol/Category/String/E1935_Maximum_Number_of_Words_You_Can_Type/src/com/maximumNumberofWordsYouCanType/E1935_Maximum_Number_of_Words_You_Can_Type.java
@@ -0,0 +1,10 @@
+package com.maximumNumberofWordsYouCanType;
+
+public class E1935_Maximum_Number_of_Words_You_Can_Type {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
