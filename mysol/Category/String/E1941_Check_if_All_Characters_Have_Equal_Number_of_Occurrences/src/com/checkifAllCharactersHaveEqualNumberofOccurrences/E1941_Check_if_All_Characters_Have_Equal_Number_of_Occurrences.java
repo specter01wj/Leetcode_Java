2672bdb3e5725badbb870beafe5826e5b533f854
Jin Wang <specter01wj@gmail.com>
2023-02-07 11:34:56 -0500

    v5

---
+++ mysol/Category/String/E1941_Check_if_All_Characters_Have_Equal_Number_of_Occurrences/src/com/checkifAllCharactersHaveEqualNumberofOccurrences/E1941_Check_if_All_Characters_Have_Equal_Number_of_Occurrences.java
@@ -0,0 +1,10 @@
+package com.checkifAllCharactersHaveEqualNumberofOccurrences;
+
+public class E1941_Check_if_All_Characters_Have_Equal_Number_of_Occurrences {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
