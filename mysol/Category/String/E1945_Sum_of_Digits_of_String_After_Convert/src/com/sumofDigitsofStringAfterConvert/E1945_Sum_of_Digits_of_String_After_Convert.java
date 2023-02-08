86c7431903cfcbea6f640108e3b7f1d458c633ee
Jin Wang <specter01wj@gmail.com>
2023-02-08 10:21:06 -0500

    v1

---
+++ mysol/Category/String/E1945_Sum_of_Digits_of_String_After_Convert/src/com/sumofDigitsofStringAfterConvert/E1945_Sum_of_Digits_of_String_After_Convert.java
@@ -0,0 +1,10 @@
+package com.sumofDigitsofStringAfterConvert;
+
+public class E1945_Sum_of_Digits_of_String_After_Convert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
