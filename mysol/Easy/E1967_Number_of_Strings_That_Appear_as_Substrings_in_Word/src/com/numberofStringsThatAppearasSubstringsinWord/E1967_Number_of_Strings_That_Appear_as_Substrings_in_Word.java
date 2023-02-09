6ead85f878c8a035b3a086a3632b9b0eff75a6df
Jin Wang <specter01wj@gmail.com>
2023-02-09 11:39:45 -0500

    v10

---
+++ mysol/Easy/E1967_Number_of_Strings_That_Appear_as_Substrings_in_Word/src/com/numberofStringsThatAppearasSubstringsinWord/E1967_Number_of_Strings_That_Appear_as_Substrings_in_Word.java
@@ -0,0 +1,68 @@
+package com.numberofStringsThatAppearasSubstringsinWord;
+import java.util.*;
+
+/*
+Given an array of strings patterns and a string word, return the number of 
+strings in patterns that exist as a substring in word.
+
+A substring is a contiguous sequence of characters within a string.
+
+
+Example 1:
+
+Input: patterns = ["a","abc","bc","d"], word = "abc"
+Output: 3
+Explanation:
+- "a" appears as a substring in "abc".
+- "abc" appears as a substring in "abc".
+- "bc" appears as a substring in "abc".
+- "d" does not appear as a substring in "abc".
+3 of the strings in patterns appear as a substring in word.
+
+Example 2:
+
+Input: patterns = ["a","b","c"], word = "aaaaabbbbb"
+Output: 2
+Explanation:
+- "a" appears as a substring in "aaaaabbbbb".
+- "b" appears as a substring in "aaaaabbbbb".
+- "c" does not appear as a substring in "aaaaabbbbb".
+2 of the strings in patterns appear as a substring in word.
+
+Example 3:
+
+Input: patterns = ["a","a","a"], word = "ab"
+Output: 3
+Explanation: Each of the patterns appears as a substring in word "ab".
+*/
+
+public class E1967_Number_of_Strings_That_Appear_as_Substrings_in_Word {
+
+	public static void main(String[] args) {
+		String[] input = {"a","abc","bc","d"};
+		String word = "abc";
+        int output = numOfStrings(input, word);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    loop and check if contains
+    */
+	
+	/**
+     * @param patterns: a list of strings
+     * @param word: a string
+     * @return: the number of strings in patterns that exist as a substring in word
+     */
+	public static int numOfStrings(String[] patterns, String word) {
+        int res = 0;
+        for(String p: patterns){
+            if(word.contains(p)){
+                res++;
+            }
+        }
+        return res; 
+    }
+
+}
