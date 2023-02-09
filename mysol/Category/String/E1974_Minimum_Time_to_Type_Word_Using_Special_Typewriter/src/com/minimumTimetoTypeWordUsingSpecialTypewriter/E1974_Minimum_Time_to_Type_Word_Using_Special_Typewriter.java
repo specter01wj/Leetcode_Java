5e6966cdd4f4860de5336ecaf549a592b50f9a0a
Jin Wang <specter01wj@gmail.com>
2023-02-09 11:16:34 -0500

    v5

---
+++ mysol/Category/String/E1974_Minimum_Time_to_Type_Word_Using_Special_Typewriter/src/com/minimumTimetoTypeWordUsingSpecialTypewriter/E1974_Minimum_Time_to_Type_Word_Using_Special_Typewriter.java
@@ -0,0 +1,10 @@
+package com.minimumTimetoTypeWordUsingSpecialTypewriter;
+
+public class E1974_Minimum_Time_to_Type_Word_Using_Special_Typewriter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
