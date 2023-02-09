57ecef09dd441f736319465452ea850b8b7f9a60
Jin Wang <specter01wj@gmail.com>
2023-02-09 10:24:13 -0500

    v1

---
+++ mysol/Category/String/E1967_Number_of_Strings_That_Appear_as_Substrings_in_Word/src/com/numberofStringsThatAppearasSubstringsinWord/E1967_Number_of_Strings_That_Appear_as_Substrings_in_Word.java
@@ -0,0 +1,10 @@
+package com.numberofStringsThatAppearasSubstringsinWord;
+
+public class E1967_Number_of_Strings_That_Appear_as_Substrings_in_Word {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
