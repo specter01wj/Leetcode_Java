1ecae8b03bf804cc147e7807a02be03a975ea5c5
Jin Wang <specter01wj@gmail.com>
2023-02-12 21:40:25 -0500

    v1

---
+++ mysol/Category/String/E2027_Minimum_Moves_to_Convert_String/src/com/minimumMovestoConvertString/E2027_Minimum_Moves_to_Convert_String.java
@@ -0,0 +1,10 @@
+package com.minimumMovestoConvertString;
+
+public class E2027_Minimum_Moves_to_Convert_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
