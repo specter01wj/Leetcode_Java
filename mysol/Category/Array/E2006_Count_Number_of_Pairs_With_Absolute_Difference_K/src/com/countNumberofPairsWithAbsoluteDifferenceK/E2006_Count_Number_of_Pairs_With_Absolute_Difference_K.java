318a94fa7becfe7b85646635fb4595e3312afa43
Jin Wang <specter01wj@gmail.com>
2023-02-13 11:27:20 -0500

    v1

---
+++ mysol/Category/Array/E2006_Count_Number_of_Pairs_With_Absolute_Difference_K/src/com/countNumberofPairsWithAbsoluteDifferenceK/E2006_Count_Number_of_Pairs_With_Absolute_Difference_K.java
@@ -0,0 +1,10 @@
+package com.countNumberofPairsWithAbsoluteDifferenceK;
+
+public class E2006_Count_Number_of_Pairs_With_Absolute_Difference_K {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
