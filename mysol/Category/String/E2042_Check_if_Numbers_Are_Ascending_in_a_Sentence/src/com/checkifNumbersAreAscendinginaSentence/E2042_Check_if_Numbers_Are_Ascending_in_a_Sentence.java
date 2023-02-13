c1d1d9bcf0418018f8464133abf6b8f8dba3ab73
Jin Wang <specter01wj@gmail.com>
2023-02-12 21:59:30 -0500

    v4

---
+++ mysol/Category/String/E2042_Check_if_Numbers_Are_Ascending_in_a_Sentence/src/com/checkifNumbersAreAscendinginaSentence/E2042_Check_if_Numbers_Are_Ascending_in_a_Sentence.java
@@ -0,0 +1,10 @@
+package com.checkifNumbersAreAscendinginaSentence;
+
+public class E2042_Check_if_Numbers_Are_Ascending_in_a_Sentence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
