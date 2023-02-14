b590f4bef56b01649dacd181059eec78c95aeff3
Jin Wang <specter01wj@gmail.com>
2023-02-14 13:38:26 -0500

    v1

---
+++ mysol/Category/Array/E2022_Convert_1D_Array_Into_2D_Array/src/com/convert1DArrayInto2DArray/E2022_Convert_1D_Array_Into_2D_Array.java
@@ -0,0 +1,10 @@
+package com.convert1DArrayInto2DArray;
+
+public class E2022_Convert_1D_Array_Into_2D_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
