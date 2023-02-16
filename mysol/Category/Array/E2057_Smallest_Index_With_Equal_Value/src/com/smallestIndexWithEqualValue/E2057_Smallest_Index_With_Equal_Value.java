b4952682c2bc43d0d93496725debeb121f9d59a9
Jin Wang <specter01wj@gmail.com>
2023-02-16 10:58:22 -0500

    v1

---
+++ mysol/Category/Array/E2057_Smallest_Index_With_Equal_Value/src/com/smallestIndexWithEqualValue/E2057_Smallest_Index_With_Equal_Value.java
@@ -0,0 +1,10 @@
+package com.smallestIndexWithEqualValue;
+
+public class E2057_Smallest_Index_With_Equal_Value {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
