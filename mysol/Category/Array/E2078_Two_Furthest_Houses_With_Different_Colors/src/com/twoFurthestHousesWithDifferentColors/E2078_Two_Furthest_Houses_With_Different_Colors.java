0514b84e21e730c550c833a4a4ed140b7e2c7a10
Jin Wang <specter01wj@gmail.com>
2023-02-17 10:51:46 -0500

    v1

---
+++ mysol/Category/Array/E2078_Two_Furthest_Houses_With_Different_Colors/src/com/twoFurthestHousesWithDifferentColors/E2078_Two_Furthest_Houses_With_Different_Colors.java
@@ -0,0 +1,10 @@
+package com.twoFurthestHousesWithDifferentColors;
+
+public class E2078_Two_Furthest_Houses_With_Different_Colors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
