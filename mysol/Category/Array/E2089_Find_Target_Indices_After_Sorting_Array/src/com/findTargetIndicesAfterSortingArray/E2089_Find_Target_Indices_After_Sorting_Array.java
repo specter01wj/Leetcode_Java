ccaa902364289366a52bfde0c535b14a46352b8e
Jin Wang <specter01wj@gmail.com>
2023-02-19 17:47:31 -0500

    v1

---
+++ mysol/Category/Array/E2089_Find_Target_Indices_After_Sorting_Array/src/com/findTargetIndicesAfterSortingArray/E2089_Find_Target_Indices_After_Sorting_Array.java
@@ -0,0 +1,10 @@
+package com.findTargetIndicesAfterSortingArray;
+
+public class E2089_Find_Target_Indices_After_Sorting_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
