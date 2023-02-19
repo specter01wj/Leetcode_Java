60de55e57c862ad0b52d9eff88af0da51a0f2d63
Jin Wang <specter01wj@gmail.com>
2023-02-19 18:24:35 -0500

    v5

---
+++ mysol/Category/Array/E2094_Finding_3-Digit_Even_Numbers/src/com/finding3DigitEvenNumbers/E2094_Finding_3Digit_Even_Numbers.java
@@ -0,0 +1,10 @@
+package com.finding3DigitEvenNumbers;
+
+public class E2094_Finding_3Digit_Even_Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
