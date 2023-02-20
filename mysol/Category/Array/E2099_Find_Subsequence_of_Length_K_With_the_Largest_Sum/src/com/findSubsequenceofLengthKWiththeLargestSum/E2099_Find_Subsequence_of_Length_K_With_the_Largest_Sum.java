7cd2d3df1690ea636f3fa96561f995fa52745cea
Jin Wang <specter01wj@gmail.com>
2023-02-20 12:00:57 -0500

    v2

--- mysol/Category/Array/E2099_Find_Subsequence_of_Length_K_With_the_Largest_Sum/src/com/findSubsequenceofLengthKWiththeLargestSum/E2099_Find_Subsequence_of_Length_K_With_the_Largest_Sum.java
+++ mysol/Category/Array/E2099_Find_Subsequence_of_Length_K_With_the_Largest_Sum/src/com/findSubsequenceofLengthKWiththeLargestSum/E2099_Find_Subsequence_of_Length_K_With_the_Largest_Sum.java
@@ -1,10 +1,63 @@
 package com.findSubsequenceofLengthKWiththeLargestSum;
+import java.util.*;
+
+/*
+You are given an integer array nums and an integer k. 
+You want to find a subsequence of nums of length k that has the largest sum.
+
+Return any such subsequence as an integer array of length k.
+
+A subsequence is an array that can be derived from another array by deleting 
+some or no elements without changing the order of the remaining elements.
+
+
+Example 1:
+
+Input: nums = [2,1,3,3], k = 2
+Output: [3,3]
+Explanation:
+The subsequence has the largest sum of 3 + 3 = 6.
+
+Example 2:
+
+Input: nums = [-1,-2,3,4], k = 3
+Output: [-1,3,4]
+Explanation: 
+The subsequence has the largest sum of -1 + 3 + 4 = 6.
+
+Example 3:
+
+Input: nums = [3,4,3,3], k = 2
+Output: [3,4]
+Explanation:
+The subsequence has the largest sum of 3 + 4 = 7. 
+Another possible subsequence is [4, 3].
+*/
 
 public class E2099_Find_Subsequence_of_Length_K_With_the_Largest_Sum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {2,1,3,3};
+		int k = 2;
+		int[] output = maxSubsequence(input, k);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + Arrays.toString(output));
 	}
+	
+	
+	public static int[] maxSubsequence(int[] nums, int k) {
+        int n = nums.length;
+        int[][] indexAndVal = new int[n][2];
+        for (int i = 0; i < n; ++i) {
+            indexAndVal[i] = new int[]{i, nums[i]};
+        }
+        Arrays.sort(indexAndVal, Comparator.comparingInt(a -> -a[1]));
+        int[][] maxK = Arrays.copyOf(indexAndVal, k);
+        Arrays.sort(maxK, Comparator.comparingInt(a -> a[0]));
+        int[] seq = new int[k];
+        for (int i = 0; i < k; ++i) {
+            seq[i] = maxK[i][1];
+        }
+        return seq;
+    }
 
 }
