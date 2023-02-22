8fb86dbe4ba42e4cc7d649b1febae26d2afd3b60
Jin Wang <specter01wj@gmail.com>
2023-02-22 12:00:51 -0500

    v1

---
+++ mysol/Category/Array/E2114_Maximum_Number_of_Words_Found_in_Sentences/src/com/maximumNumberofWordsFoundinSentences/E2114_Maximum_Number_of_Words_Found_in_Sentences.java
@@ -0,0 +1,10 @@
+package com.maximumNumberofWordsFoundinSentences;
+
+public class E2114_Maximum_Number_of_Words_Found_in_Sentences {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
