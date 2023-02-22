457c4bf00c357ff216c4e994e3eaa8a097ac3f59
Jin Wang <specter01wj@gmail.com>
2023-02-22 12:26:20 -0500

    v7

--- mysol/Category/Array/E2133_Check_if_Every_Row_and_Column_Contains_All_Numbers/src/com/checkifEveryRowandColumnContainsAllNumbers/E2133_Check_if_Every_Row_and_Column_Contains_All_Numbers.java
+++ mysol/Category/Array/E2133_Check_if_Every_Row_and_Column_Contains_All_Numbers/src/com/checkifEveryRowandColumnContainsAllNumbers/E2133_Check_if_Every_Row_and_Column_Contains_All_Numbers.java
@@ -1,10 +1,50 @@
 package com.checkifEveryRowandColumnContainsAllNumbers;
+import java.util.*;
+
+/*
+An n x n matrix is valid if every row and every column contains 
+all the integers from 1 to n (inclusive).
+
+Given an n x n integer matrix matrix, return true if the matrix is valid. Otherwise, return false.
+
+
+Example 1:
+
+
+Input: matrix = [[1,2,3],[3,1,2],[2,3,1]]
+Output: true
+Explanation: In this case, n = 3, and every row and column contains the numbers 1, 2, and 3.
+Hence, we return true.
+
+Example 2:
+
+
+Input: matrix = [[1,1,1],[1,2,3],[1,2,3]]
+Output: false
+Explanation: In this case, n = 3, but the first row and the first column do not contain the numbers 2 or 3.
+Hence, we return false.
+*/
 
 public class E2133_Check_if_Every_Row_and_Column_Contains_All_Numbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[][] input = {{1,2,3},{3,1,2},{2,3,1}};
+		boolean output = checkValid(input);
+        System.out.println("input: " + Arrays.deepToString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public static boolean checkValid(int[][] matrix) {
+        for (int r = 0, n = matrix.length; r < n; ++r) {
+            Set<Integer> row = new HashSet<>();
+            Set<Integer> col = new HashSet<>();
+            for (int c = 0; c < n; ++c) {
+                if (!row.add(matrix[r][c]) || !col.add(matrix[c][r])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
 }
