a0b4f927c360c5a44631159e1b844e63e46ba581
Jin Wang <specter01wj@gmail.com>
2023-02-24 14:36:55 -0500

    v1

---
+++ mysol/Category/Array/E2144_Minimum_Cost_of_Buying_Candies_With_Discount/src/com/minimumCostofBuyingCandiesWithDiscount/E2144_Minimum_Cost_of_Buying_Candies_With_Discount.java
@@ -0,0 +1,10 @@
+package com.minimumCostofBuyingCandiesWithDiscount;
+
+public class E2144_Minimum_Cost_of_Buying_Candies_With_Discount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
