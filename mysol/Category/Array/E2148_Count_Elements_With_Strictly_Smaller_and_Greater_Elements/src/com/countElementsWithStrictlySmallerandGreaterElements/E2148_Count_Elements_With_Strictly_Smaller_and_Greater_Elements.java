f03d2cba129dfc056dc3a6dc01b2df79329812b3
Jin Wang <specter01wj@gmail.com>
2023-02-24 14:52:09 -0500

    v5

---
+++ mysol/Category/Array/E2148_Count_Elements_With_Strictly_Smaller_and_Greater_Elements/src/com/countElementsWithStrictlySmallerandGreaterElements/E2148_Count_Elements_With_Strictly_Smaller_and_Greater_Elements.java
@@ -0,0 +1,10 @@
+package com.countElementsWithStrictlySmallerandGreaterElements;
+
+public class E2148_Count_Elements_With_Strictly_Smaller_and_Greater_Elements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
