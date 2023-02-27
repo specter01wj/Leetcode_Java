00b9b2f26f13ecba2a06a6bcf9c768ff81ce3278
Jin Wang <specter01wj@gmail.com>
2023-02-27 10:31:13 -0500

    v1

---
+++ mysol/Category/Array/E2154_Keep_Multiplying_Found_Values_by_Two/src/com/keepMultiplyingFoundValuesbyTwo/E2154_Keep_Multiplying_Found_Values_by_Two.java
@@ -0,0 +1,10 @@
+package com.keepMultiplyingFoundValuesbyTwo;
+
+public class E2154_Keep_Multiplying_Found_Values_by_Two {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
