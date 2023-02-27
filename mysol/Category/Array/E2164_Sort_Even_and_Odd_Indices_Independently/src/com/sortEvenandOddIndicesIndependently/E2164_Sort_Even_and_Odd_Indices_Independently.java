1cffa6f6f6c95beb9f316a98f78d0d061a44f1ab
Jin Wang <specter01wj@gmail.com>
2023-02-27 11:23:15 -0500

    v5

---
+++ mysol/Category/Array/E2164_Sort_Even_and_Odd_Indices_Independently/src/com/sortEvenandOddIndicesIndependently/E2164_Sort_Even_and_Odd_Indices_Independently.java
@@ -0,0 +1,10 @@
+package com.sortEvenandOddIndicesIndependently;
+
+public class E2164_Sort_Even_and_Odd_Indices_Independently {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
