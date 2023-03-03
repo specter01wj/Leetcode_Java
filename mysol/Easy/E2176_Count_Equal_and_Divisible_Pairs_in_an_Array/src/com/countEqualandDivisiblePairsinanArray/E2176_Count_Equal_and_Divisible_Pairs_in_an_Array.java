b9e3212a502d6584623d807b2fa96d381d88323d
Jin Wang <specter01wj@gmail.com>
2023-03-03 12:09:44 -0500

    v12

---
+++ mysol/Easy/E2176_Count_Equal_and_Divisible_Pairs_in_an_Array/src/com/countEqualandDivisiblePairsinanArray/E2176_Count_Equal_and_Divisible_Pairs_in_an_Array.java
@@ -0,0 +1,66 @@
+package com.countEqualandDivisiblePairsinanArray;
+import java.util.*;
+
+/*
+Given a 0-indexed integer array nums of length n and an integer k, 
+return the number of pairs (i, j) where 0 <= i < j < n, 
+such that nums[i] == nums[j] and (i * j) is divisible by k.
+
+
+Example 1:
+
+Input: nums = [3,1,2,2,2,1,3], k = 2
+Output: 4
+Explanation:
+There are 4 pairs that meet all the requirements:
+- nums[0] == nums[6], and 0 * 6 == 0, which is divisible by 2.
+- nums[2] == nums[3], and 2 * 3 == 6, which is divisible by 2.
+- nums[2] == nums[4], and 2 * 4 == 8, which is divisible by 2.
+- nums[3] == nums[4], and 3 * 4 == 12, which is divisible by 2.
+
+Example 2:
+
+Input: nums = [1,2,3,4], k = 1
+Output: 0
+Explanation: Since no value in nums is repeated, there are no pairs (i,j) 
+that meet all the requirements.
+*/
+
+public class E2176_Count_Equal_and_Divisible_Pairs_in_an_Array {
+
+	public static void main(String[] args) {
+		int[] input = {3,1,2,2,2,1,3};
+		int k = 2;
+		int output = countPairs(input, k);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    Traverse indices with same values
+    */
+	
+	/*
+     * @param nums: a list of integers
+     * @param k: an integer
+     * @return: the number of pairs (i, j)
+     */
+	public static int countPairs(int[] nums, int k) {
+        Map<Integer, List<Integer>> indices = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            indices.computeIfAbsent(nums[i], l -> new ArrayList<>()).add(i);
+        } 
+        int cnt = 0;
+        for (List<Integer> ind : indices.values()) {
+            for (int i = 0; i < ind.size(); ++i) {
+                for (int j = 0; j < i; ++j) {
+                    if (ind.get(i) * ind.get(j) % k == 0) {
+                        ++cnt;
+                    }
+                }
+            }
+        }
+        return cnt; 
+    }
+
+}
---
+++ mysol/Easy/E2185_Counting_Words_With_a_Given_Prefix/src/com/countingWordsWithaGivenPrefix/E2185_Counting_Words_With_a_Given_Prefix.java
@@ -0,0 +1,52 @@
+package com.countingWordsWithaGivenPrefix;
+import java.util.*;
+
+/*
+You are given an array of strings words and a string pref.
+
+Return the number of strings in words that contain pref as a prefix.
+
+A prefix of a string s is any leading contiguous substring of s.
+
+
+Example 1:
+
+Input: words = ["pay","attention","practice","attend"], pref = "at"
+Output: 2
+Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
+
+Example 2:
+
+Input: words = ["leetcode","win","loops","success"], pref = "code"
+Output: 0
+Explanation: There are no strings that contain "code" as a prefix.
+*/
+
+public class E2185_Counting_Words_With_a_Given_Prefix {
+
+	public static void main(String[] args) {
+		String[] input = {"pay","attention","practice","attend"};
+		String pref = "at";
+		int output = prefixCount(input, pref);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    loop and find the index
+    */
+	
+	/*
+     * @param words: a list of strings
+     * @param pref: a string
+     * @return: the number of good triplets
+     */
+	public static int prefixCount(String[] words, String pref) {
+        int ans = 0;
+        for (String w: words) {
+            ans += (w.indexOf(pref) == 0) ? 1 : 0;
+        }
+        return ans;
+    }
+
+}
