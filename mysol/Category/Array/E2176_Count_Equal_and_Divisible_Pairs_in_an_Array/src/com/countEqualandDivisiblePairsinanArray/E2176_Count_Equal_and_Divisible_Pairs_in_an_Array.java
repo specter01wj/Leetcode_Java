30a53db8ce21a64a2978b7abe9b370f26843155a
Jin Wang <specter01wj@gmail.com>
2023-03-03 11:41:11 -0500

    v1

---
+++ mysol/Category/Array/E2176_Count_Equal_and_Divisible_Pairs_in_an_Array/src/com/countEqualandDivisiblePairsinanArray/E2176_Count_Equal_and_Divisible_Pairs_in_an_Array.java
@@ -0,0 +1,10 @@
+package com.countEqualandDivisiblePairsinanArray;
+
+public class E2176_Count_Equal_and_Divisible_Pairs_in_an_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
