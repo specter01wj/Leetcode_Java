a3501fa5bbc7cc8fa46d79118fb60856e838e85e
Jin Wang <specter01wj@gmail.com>
2023-03-03 12:00:18 -0500

    v5

---
+++ mysol/Category/Array/E2185_Counting_Words_With_a_Given_Prefix/src/com/countingWordsWithaGivenPrefix/E2185_Counting_Words_With_a_Given_Prefix.java
@@ -0,0 +1,10 @@
+package com.countingWordsWithaGivenPrefix;
+
+public class E2185_Counting_Words_With_a_Given_Prefix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
