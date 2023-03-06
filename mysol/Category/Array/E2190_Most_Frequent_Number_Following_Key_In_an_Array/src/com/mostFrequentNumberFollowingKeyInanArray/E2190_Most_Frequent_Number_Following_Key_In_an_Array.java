64c4072f77bc21312b5d12a0eb3a97abb935b2ac
Jin Wang <specter01wj@gmail.com>
2023-03-06 10:28:54 -0500

    v1

---
+++ mysol/Category/Array/E2190_Most_Frequent_Number_Following_Key_In_an_Array/src/com/mostFrequentNumberFollowingKeyInanArray/E2190_Most_Frequent_Number_Following_Key_In_an_Array.java
@@ -0,0 +1,10 @@
+package com.mostFrequentNumberFollowingKeyInanArray;
+
+public class E2190_Most_Frequent_Number_Following_Key_In_an_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
