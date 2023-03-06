32e8912515f9cf371d31233c6329e7fce6f8bac2
Jin Wang <specter01wj@gmail.com>
2023-03-06 10:52:26 -0500

    v5

---
+++ mysol/Category/Array/E2200_Find_All_KDistant_Indices_in_an_Array/src/com/findAllKDistantIndicesinanArray/E2200_Find_All_KDistant_Indices_in_an_Array.java
@@ -0,0 +1,10 @@
+package com.findAllKDistantIndicesinanArray;
+
+public class E2200_Find_All_KDistant_Indices_in_an_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
