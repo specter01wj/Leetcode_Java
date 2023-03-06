fed67cb001330d0b0f2a2187adf3330f3b32a667
Jin Wang <specter01wj@gmail.com>
2023-03-06 11:01:20 -0500

    v12

---
+++ mysol/Easy/E2190_Most_Frequent_Number_Following_Key_In_an_Array/src/com/mostFrequentNumberFollowingKeyInanArray/E2190_Most_Frequent_Number_Following_Key_In_an_Array.java
@@ -0,0 +1,73 @@
+package com.mostFrequentNumberFollowingKeyInanArray;
+import java.util.*;
+
+/*
+You are given a 0-indexed integer array nums. You are also given an integer key, 
+which is present in nums.
+
+For every unique integer target in nums, count the number of times target 
+immediately follows an occurrence of key in nums. In other words, 
+count the number of indices i such that:
+
+	.0 <= i <= nums.length - 2,
+	.nums[i] == key and,
+	.nums[i + 1] == target.
+
+Return the target with the maximum count. The test cases will be generated 
+such that the target with maximum count is unique.
+
+
+Example 1:
+
+Input: nums = [1,100,200,1,100], key = 1
+Output: 100
+Explanation: For target = 100, there are 2 occurrences at indices 1 and 4 which 
+follow an occurrence of key.
+No other integers follow an occurrence of key, so we return 100.
+
+Example 2:
+
+Input: nums = [2,2,2,2,3], key = 2
+Output: 2
+Explanation: For target = 2, there are 3 occurrences at indices 1, 2, and 3 which 
+follow an occurrence of key.
+For target = 3, there is only one occurrence at index 4 which follows an occurrence of key.
+target = 2 has the maximum number of occurrences following an occurrence of key, so we return 2.
+*/
+
+public class E2190_Most_Frequent_Number_Following_Key_In_an_Array {
+
+	public static void main(String[] args) {
+		int[] input = {1,100,200,1,100};
+		int key = 1;
+		int output = mostFrequent(input, key);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    loop and calculate
+    */
+	
+	/*
+     * @param nums: a list of integers
+     * @param key: an integer
+     * @return: the target with the maximum count
+     */
+	public static int mostFrequent(int[] nums, int key) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        int mostFreq = -1;
+        for (int i = 0, n = nums.length, max = 0; i + 1 < n; ++i) {
+            if (nums[i] == key) {
+                int candidate = nums[i + 1];
+                freq.put(candidate, 1 + freq.getOrDefault(candidate, 0));
+                if (freq.get(candidate) > max) {
+                    max = freq.get(candidate);
+                    mostFreq = candidate;
+                }
+            }
+        }
+        return mostFreq;
+    }
+
+}
---
+++ mysol/Easy/E2200_Find_All_KDistant_Indices_in_an_Array/src/com/findAllKDistantIndicesinanArray/E2200_Find_All_KDistant_Indices_in_an_Array.java
@@ -0,0 +1,79 @@
+package com.findAllKDistantIndicesinanArray;
+import java.util.*;
+
+/*
+You are given a 0-indexed integer array nums and two integers key and k. 
+A k-distant index is an index i of nums for which there exists at least 
+one index j such that |i - j| <= k and nums[j] == key.
+
+Return a list of all k-distant indices sorted in increasing order.
+
+
+Example 1:
+
+Input: nums = [3,4,9,1,3,9,5], key = 9, k = 1
+Output: [1,2,3,4,5,6]
+Explanation: Here, nums[2] == key and nums[5] == key.
+- For index 0, |0 - 2| > k and |0 - 5| > k, so there is no j where |0 - j| <= k 
+and nums[j] == key. Thus, 0 is not a k-distant index.
+- For index 1, |1 - 2| <= k and nums[2] == key, so 1 is a k-distant index.
+- For index 2, |2 - 2| <= k and nums[2] == key, so 2 is a k-distant index.
+- For index 3, |3 - 2| <= k and nums[2] == key, so 3 is a k-distant index.
+- For index 4, |4 - 5| <= k and nums[5] == key, so 4 is a k-distant index.
+- For index 5, |5 - 5| <= k and nums[5] == key, so 5 is a k-distant index.
+- For index 6, |6 - 5| <= k and nums[5] == key, so 6 is a k-distant index.
+Thus, we return [1,2,3,4,5,6] which is sorted in increasing order. 
+
+Example 2:
+
+Input: nums = [2,2,2,2,2], key = 2, k = 2
+Output: [0,1,2,3,4]
+Explanation: For all indices i in nums, there exists some index j such that 
+|i - j| <= k and nums[j] == key, so every index is a k-distant index. 
+Hence, we return [0,1,2,3,4].
+*/
+
+public class E2200_Find_All_KDistant_Indices_in_an_Array {
+
+	public static void main(String[] args) {
+		int[] input = {3,4,9,1,3,9,5};
+		int key = 9, k = 1;
+		List<Integer> output = findKDistantIndices(input, key, k);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    If we find all the indices of the keys in a given array in increasing order, 
+    then we can iterate k elements on the right and left sides of that key index 
+    to add them to answer in increasing order. For the next key index we can start 
+    iterating from the maximum(last key index + k , current key index - k) so that 
+    we will not process the already iterated array index.
+    */
+	
+	/*
+     * @param nums: a list of integers
+     * @param key: an integer
+     * @param k: an integer
+     * @return: a list of all k-distant indices sorted in increasing order
+     */
+	public static List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+        List<Integer> idx = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0 ; i < nums.length; i++){
+            if(nums[i] == key){
+                idx.add(i);
+            }
+        }
+        int last = 0;
+        for(int ind : idx){
+            int i = Math.max(last,ind-k);
+            for(; i <= ind+k && i < nums.length; i++){
+                ans.add(i);
+            }
+            last = i;
+        }
+        return ans;
+    }
+
+}
