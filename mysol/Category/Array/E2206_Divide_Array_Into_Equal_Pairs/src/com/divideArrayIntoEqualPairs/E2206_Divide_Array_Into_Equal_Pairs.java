b15e00baeb5f543e5cac05ae8f49414288c71b32
Jin Wang <specter01wj@gmail.com>
2023-03-08 13:24:01 -0500

    v2

--- mysol/Category/Array/E2206_Divide_Array_Into_Equal_Pairs/src/com/divideArrayIntoEqualPairs/E2206_Divide_Array_Into_Equal_Pairs.java
+++ mysol/Category/Array/E2206_Divide_Array_Into_Equal_Pairs/src/com/divideArrayIntoEqualPairs/E2206_Divide_Array_Into_Equal_Pairs.java
@@ -1,10 +1,52 @@
 package com.divideArrayIntoEqualPairs;
+import java.util.*;
+
+/*
+You are given an integer array nums consisting of 2 * n integers.
+
+You need to divide nums into n pairs such that:
+
+	.Each element belongs to exactly one pair.
+	.The elements present in a pair are equal.
+
+Return true if nums can be divided into n pairs, otherwise return false.
+
+
+Example 1:
+
+Input: nums = [3,2,3,2,2,2]
+Output: true
+Explanation: 
+There are 6 elements in nums, so they should be divided into 6 / 2 = 3 pairs.
+If nums is divided into the pairs (2, 2), (3, 3), and (2, 2), it will satisfy 
+all the conditions.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Explanation: 
+There is no way to divide nums into 4 / 2 = 2 pairs such that the pairs satisfy 
+every condition.
+*/
 
 public class E2206_Divide_Array_Into_Equal_Pairs {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {3,2,3,2,2,2};
+		boolean output = divideArray(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public static boolean divideArray(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (!seen.add(num)) {
+                seen.remove(num);
+            }
+        }
+        return seen.isEmpty();
+    }
 
 }
