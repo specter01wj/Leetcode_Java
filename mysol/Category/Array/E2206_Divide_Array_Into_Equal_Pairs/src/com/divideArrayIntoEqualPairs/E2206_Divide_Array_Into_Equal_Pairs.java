3837ed90986b01831ca00f98cacc6bcb056f5959
Jin Wang <specter01wj@gmail.com>
2023-03-08 13:08:31 -0500

    v1

---
+++ mysol/Category/Array/E2206_Divide_Array_Into_Equal_Pairs/src/com/divideArrayIntoEqualPairs/E2206_Divide_Array_Into_Equal_Pairs.java
@@ -0,0 +1,10 @@
+package com.divideArrayIntoEqualPairs;
+
+public class E2206_Divide_Array_Into_Equal_Pairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
