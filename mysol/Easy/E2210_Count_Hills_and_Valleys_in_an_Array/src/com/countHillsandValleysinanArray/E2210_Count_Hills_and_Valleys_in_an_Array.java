66400a70aee38b86cbfb11e362257b574225871d
Jin Wang <specter01wj@gmail.com>
2023-03-08 19:35:35 -0500

    v10

---
+++ mysol/Easy/E2210_Count_Hills_and_Valleys_in_an_Array/src/com/countHillsandValleysinanArray/E2210_Count_Hills_and_Valleys_in_an_Array.java
@@ -0,0 +1,73 @@
+package com.countHillsandValleysinanArray;
+import java.util.*;
+
+/*
+You are given a 0-indexed integer array nums. An index i is part of a hill 
+in nums if the closest non-equal neighbors of i are smaller than nums[i]. 
+Similarly, an index i is part of a valley in nums if the closest non-equal 
+neighbors of i are larger than nums[i]. Adjacent indices i and j are part 
+of the same hill or valley if nums[i] == nums[j].
+
+Note that for an index to be part of a hill or valley, it must have a non-equal 
+neighbor on both the left and right of the index.
+
+Return the number of hills and valleys in nums.
+
+
+Example 1:
+
+Input: nums = [2,4,1,1,6,5]
+Output: 3
+Explanation:
+At index 0: There is no non-equal neighbor of 2 on the left, so index 0 is neither a hill nor a valley.
+At index 1: The closest non-equal neighbors of 4 are 2 and 1. Since 4 > 2 and 4 > 1, index 1 is a hill. 
+At index 2: The closest non-equal neighbors of 1 are 4 and 6. Since 1 < 4 and 1 < 6, index 2 is a valley.
+At index 3: The closest non-equal neighbors of 1 are 4 and 6. Since 1 < 4 and 1 < 6, index 3 is a valley, 
+but note that it is part of the same valley as index 2.
+At index 4: The closest non-equal neighbors of 6 are 1 and 5. Since 6 > 1 and 6 > 5, index 4 is a hill.
+At index 5: There is no non-equal neighbor of 5 on the right, so index 5 is neither a hill nor a valley. 
+There are 3 hills and valleys so we return 3.
+
+Example 2:
+
+Input: nums = [6,6,5,5,4,1]
+Output: 0
+Explanation:
+At index 0: There is no non-equal neighbor of 6 on the left, so index 0 is neither a hill nor a valley.
+At index 1: There is no non-equal neighbor of 6 on the left, so index 1 is neither a hill nor a valley.
+At index 2: The closest non-equal neighbors of 5 are 6 and 4. Since 5 < 6 and 5 > 4, index 2 is neither a hill nor a valley.
+At index 3: The closest non-equal neighbors of 5 are 6 and 4. Since 5 < 6 and 5 > 4, index 3 is neither a hill nor a valley.
+At index 4: The closest non-equal neighbors of 4 are 5 and 1. Since 4 < 5 and 4 > 1, index 4 is neither a hill nor a valley.
+At index 5: There is no non-equal neighbor of 1 on the right, so index 5 is neither a hill nor a valley.
+There are 0 hills and valleys so we return 0.
+*/
+
+public class E2210_Count_Hills_and_Valleys_in_an_Array {
+
+	public static void main(String[] args) {
+		int[] input = {2,4,1,1,6,5};
+		int output = countHillValley(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    loop and compare
+    */
+	
+	/*
+     * @param nums: a list of integers
+     * @return: the number of hills and valleys in nums
+     */
+	public static int countHillValley(int[] nums) {
+        int result = 0, left = nums[0];
+        for (int i = 1; i < nums.length - 1; i++) {
+            if ((left < nums[i] && nums[i] > nums[i + 1]) || (left > nums[i] && nums[i] < nums[i + 1])) {
+            	result++;
+                left = nums[i];
+            }
+        }
+        return result;
+    }
+
+}
