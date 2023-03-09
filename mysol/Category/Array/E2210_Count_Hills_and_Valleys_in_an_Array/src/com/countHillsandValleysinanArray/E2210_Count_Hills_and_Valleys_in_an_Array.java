532f8a32504bc8ce235abf5dfca960b6e02b2b67
Jin Wang <specter01wj@gmail.com>
2023-03-08 19:24:05 -0500

    v5

---
+++ mysol/Category/Array/E2210_Count_Hills_and_Valleys_in_an_Array/src/com/countHillsandValleysinanArray/E2210_Count_Hills_and_Valleys_in_an_Array.java
@@ -0,0 +1,10 @@
+package com.countHillsandValleysinanArray;
+
+public class E2210_Count_Hills_and_Valleys_in_an_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
