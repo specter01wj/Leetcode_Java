f697ff5413b44b49abc965099414fd1b5f2d6d2a
Jin Wang <specter01wj@gmail.com>
2023-03-12 15:17:52 -0400

    v5

---
+++ mysol/Category/Array/E2255_Count_Prefixes_of_a_Given_String/src/com/countPrefixesofaGivenString/E2255_Count_Prefixes_of_a_Given_String.java
@@ -0,0 +1,10 @@
+package com.countPrefixesofaGivenString;
+
+public class E2255_Count_Prefixes_of_a_Given_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
