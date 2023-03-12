930fc2cc9116b3f42e1c09377eea50ac2c300f40
Jin Wang <specter01wj@gmail.com>
2023-03-12 14:58:37 -0400

    v1

---
+++ mysol/Category/Array/E2248_Intersection_of_Multiple_Arrays/src/com/intersectionofMultipleArrays/E2248_Intersection_of_Multiple_Arrays.java
@@ -0,0 +1,10 @@
+package com.intersectionofMultipleArrays;
+
+public class E2248_Intersection_of_Multiple_Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
