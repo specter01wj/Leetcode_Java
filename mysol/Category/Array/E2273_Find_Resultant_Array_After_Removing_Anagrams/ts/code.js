adbcfb911bfdba85ce6ba3ee5eb9f92c70f91bf2
Jin Wang <specter01wj@gmail.com>
2023-09-30 14:54:58 -0400

    v5

--- mysol/Category/Array/E2273_Find_Resultant_Array_After_Removing_Anagrams/ts/code.js
+++ mysol/Category/Array/E2273_Find_Resultant_Array_After_Removing_Anagrams/ts/code.js
@@ -1,30 +1,23 @@
-
-function removeAnagrams(words: string[]): string[] {
-    let i = 1; // start from the second word
+function removeAnagrams(words) {
+    var i = 1; // start from the second word
     while (i < words.length) {
         if (areAnagrams(words[i], words[i - 1])) {
             // remove word at position i
             words.splice(i, 1);
-        } else {
+        }
+        else {
             i++;
         }
     }
     return words;
 }
-
-function areAnagrams(s1: string, s2: string): boolean {
+function areAnagrams(s1, s2) {
     return sortString(s1) === sortString(s2);
 }
-
-function sortString(str: string): string {
+function sortString(str) {
     return str.split('').sort().join('');
 }
-
-
-let input = ["abba","baba","bbaa","cd","cd"];
-let output = removeAnagrams(input);
-
-let webHeading = document.querySelector('#t1');
+var input = ["abba", "baba", "bbaa", "cd", "cd"];
+var output = removeAnagrams(input);
+var webHeading = document.querySelector('#t1');
 webHeading.textContent = 'Output: ' + output.toString();
-
-
