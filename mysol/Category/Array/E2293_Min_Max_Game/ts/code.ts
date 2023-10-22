96eb8fb51a20c39d86fa4d4244821da74f696090
Jin Wang <specter01wj@gmail.com>
2023-10-22 19:15:37 -0400

    v5

--- mysol/Category/Array/E2293_Min_Max_Game/ts/code.ts
+++ mysol/Category/Array/E2293_Min_Max_Game/ts/code.ts
@@ -1,28 +1,25 @@
 
-function removeAnagrams(words: string[]): string[] {
-    let i = 1; // start from the second word
-    while (i < words.length) {
-        if (areAnagrams(words[i], words[i - 1])) {
-            // remove word at position i
-            words.splice(i, 1);
-        } else {
-            i++;
+function minMaxGame(nums: number[]): number {
+    while (nums.length > 1) {
+        let n = nums.length;
+        let newNums: number[] = new Array(Math.floor(n / 2)).fill(0);
+        
+        for (let i = 0; i < Math.floor(n / 2); i++) {
+            if (i % 2 === 0) {
+                newNums[i] = Math.min(nums[2 * i], nums[2 * i + 1]);
+            } else {
+                newNums[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
+            }
         }
+            
+        nums = newNums;
     }
-    return words;
+
+    return nums[0];
 }
 
-function areAnagrams(s1: string, s2: string): boolean {
-    return sortString(s1) === sortString(s2);
-}
-
-function sortString(str: string): string {
-    return str.split('').sort().join('');
-}
-
-
-let input = ["abba","baba","bbaa","cd","cd"];
-let output = removeAnagrams(input);
+let input = [1,3,5,2,4,8,2,2];
+let output = minMaxGame(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.textContent = 'Output: ' + output.toString();
