8b535ec79f846de8d473f81673f3e98ab871660a
Jin Wang <specter01wj@gmail.com>
2023-10-28 10:25:55 -0400

    v1

---
+++ mysol/Category/Array/E2303_Calculate_Amount_Paid_in_Taxes/src/com/calculateAmountPaidinTaxes/E2303_Calculate_Amount_Paid_in_Taxes.java
@@ -0,0 +1,10 @@
+package com.calculateAmountPaidinTaxes;
+
+public class E2303_Calculate_Amount_Paid_in_Taxes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
