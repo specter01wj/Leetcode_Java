f8399b2c82a095c2dc6009fdf64ee0a5576f8471
Jin Wang <specter01wj@gmail.com>
2023-11-06 01:36:41 -0500

    v1

---
+++ mysol/Category/Array/E2335_Minimum_Amount_of_Time_to_Fill_Cups/src/com/minimumAmountofTimetoFillCups/E2335_Minimum_Amount_of_Time_to_Fill_Cups.java
@@ -0,0 +1,10 @@
+package com.minimumAmountofTimetoFillCups;
+
+public class E2335_Minimum_Amount_of_Time_to_Fill_Cups {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
