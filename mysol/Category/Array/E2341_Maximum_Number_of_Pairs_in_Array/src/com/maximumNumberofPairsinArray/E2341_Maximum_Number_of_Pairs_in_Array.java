c502c68c4846e07f42b36c5e8a4883c114cc3b8e
Jin Wang <specter01wj@gmail.com>
2023-11-10 17:29:08 -0500

    v1

---
+++ mysol/Category/Array/E2341_Maximum_Number_of_Pairs_in_Array/src/com/maximumNumberofPairsinArray/E2341_Maximum_Number_of_Pairs_in_Array.java
@@ -0,0 +1,10 @@
+package com.maximumNumberofPairsinArray;
+
+public class E2341_Maximum_Number_of_Pairs_in_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
