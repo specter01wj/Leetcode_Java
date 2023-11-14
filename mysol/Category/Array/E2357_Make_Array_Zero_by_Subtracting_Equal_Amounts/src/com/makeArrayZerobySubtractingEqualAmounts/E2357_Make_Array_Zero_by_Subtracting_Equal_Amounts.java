39fe3b7b60d9e11cb17091d047f4bd80dfcce0f0
Jin Wang <specter01wj@gmail.com>
2023-11-14 01:22:57 -0500

    v1

---
+++ mysol/Category/Array/E2357_Make_Array_Zero_by_Subtracting_Equal_Amounts/src/com/makeArrayZerobySubtractingEqualAmounts/E2357_Make_Array_Zero_by_Subtracting_Equal_Amounts.java
@@ -0,0 +1,10 @@
+package com.makeArrayZerobySubtractingEqualAmounts;
+
+public class E2357_Make_Array_Zero_by_Subtracting_Equal_Amounts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
