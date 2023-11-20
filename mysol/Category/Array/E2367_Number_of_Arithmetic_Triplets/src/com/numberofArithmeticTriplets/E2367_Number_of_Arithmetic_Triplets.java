92e7f2fa1650dbf644f915f28dab213d08de6c1e
Jin Wang <specter01wj@gmail.com>
2023-11-19 21:16:41 -0500

    v1

---
+++ mysol/Category/Array/E2367_Number_of_Arithmetic_Triplets/src/com/numberofArithmeticTriplets/E2367_Number_of_Arithmetic_Triplets.java
@@ -0,0 +1,10 @@
+package com.numberofArithmeticTriplets;
+
+public class E2367_Number_of_Arithmetic_Triplets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
