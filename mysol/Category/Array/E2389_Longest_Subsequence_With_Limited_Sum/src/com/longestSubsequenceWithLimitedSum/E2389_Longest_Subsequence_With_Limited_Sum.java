5acc4d7c1810679af3ee00ec88ce3c3d103bb193
Jin Wang <specter01wj@gmail.com>
2023-11-22 18:12:00 -0500

    v1

---
+++ mysol/Category/Array/E2389_Longest_Subsequence_With_Limited_Sum/src/com/longestSubsequenceWithLimitedSum/E2389_Longest_Subsequence_With_Limited_Sum.java
@@ -0,0 +1,10 @@
+package com.longestSubsequenceWithLimitedSum;
+
+public class E2389_Longest_Subsequence_With_Limited_Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
