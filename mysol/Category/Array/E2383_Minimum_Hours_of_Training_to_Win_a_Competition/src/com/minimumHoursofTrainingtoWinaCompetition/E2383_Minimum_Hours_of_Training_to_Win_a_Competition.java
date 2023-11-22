fe4fd39c389ea77fd5a407dbeb136a33ba970258
Jin Wang <specter01wj@gmail.com>
2023-11-22 00:35:06 -0500

    v1

---
+++ mysol/Category/Array/E2383_Minimum_Hours_of_Training_to_Win_a_Competition/src/com/minimumHoursofTrainingtoWinaCompetition/E2383_Minimum_Hours_of_Training_to_Win_a_Competition.java
@@ -0,0 +1,10 @@
+package com.minimumHoursofTrainingtoWinaCompetition;
+
+public class E2383_Minimum_Hours_of_Training_to_Win_a_Competition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
