9519b9ac0ba374cd0d13d23b5bd8c2fe96f72a3e
Jin Wang <specter01wj@gmail.com>
2023-11-24 12:42:57 -0500

    v2

--- mysol/30 Days Challenge/30 Days of JavaScript/1_Closures/E2667_Create_Hello_World_Function/ts/code.ts
+++ mysol/30 Days Challenge/30 Days of JavaScript/1_Closures/E2667_Create_Hello_World_Function/ts/code.ts
@@ -1,33 +1,12 @@
 
-function answerQueries(nums: number[], queries: number[]): number[] {
-  // Sort the nums array
-  nums.sort((a, b) => a - b);
-
-  // Convert nums into a prefix sum array
-  for (let i = 1; i < nums.length; ++i) {
-      nums[i] += nums[i - 1];
+function createHelloWorld() {
+  return function(...args) {
+    return 'Hello World';
   }
-
-  // Process each query using a modified binary search
-  return queries.map(query => {
-      let left = 0, right = nums.length - 1, result = 0;
-
-      while (left <= right) {
-          let mid = Math.floor((left + right) / 2);
-          if (nums[mid] <= query) {
-              result = mid + 1;
-              left = mid + 1;
-          } else {
-              right = mid - 1;
-          }
-      }
-
-      return result;
-  });
 }
 
-let nums = [4,5,2,1], queries = [3,10,21];
-let output = answerQueries(nums, queries);
+let input = '';
+let output = createHelloWorld()();
 
 let webHeading = document.querySelector('#t1');
 webHeading.textContent = 'Output: ' + output.toString();
