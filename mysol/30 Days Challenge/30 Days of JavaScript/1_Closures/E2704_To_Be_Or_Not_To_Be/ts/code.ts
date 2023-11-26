6676972b87e66ee3d83103ca5e662256d6df7baf
Jin Wang <specter01wj@gmail.com>
2023-11-25 23:51:50 -0500

    v3

---
+++ mysol/30 Days Challenge/30 Days of JavaScript/1_Closures/E2704_To_Be_Or_Not_To_Be/ts/code.ts
@@ -0,0 +1,31 @@
+
+function expect(val) {
+  return {
+    toBe: function(expected) {
+      if (val === expected) {
+        return true;
+      } else {
+        throw new Error("Not Equal");
+      }
+    },
+    notToBe: function(expected) {
+      if (val !== expected) {
+        return true;
+      } else {
+        throw new Error("Equal");
+      }
+    }
+  }
+}
+
+let input = '';
+let output1 = expect(5).toBe(5);
+let output2 = expect(5).notToBe(5);
+
+let webHeading1 = document.querySelector('#t1');
+webHeading1.textContent = 'Output: ' + output1.toString();
+
+let webHeading2 = document.querySelector('#t2');
+webHeading2.textContent = 'Output: ' + output2.toString();
+
+
