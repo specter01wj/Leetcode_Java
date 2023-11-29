eef40be6c60af652558b498ecc78436fba1fba6d
Jin Wang <specter01wj@gmail.com>
2023-11-29 00:10:56 -0500

    v9

--- mysol/30 Days Challenge/30 Days of JavaScript/3_Function Transformations/E2666_Allow_One_Function_Call/ts/code.ts
+++ mysol/30 Days Challenge/30 Days of JavaScript/3_Function Transformations/E2666_Allow_One_Function_Call/ts/code.ts
@@ -16,10 +16,16 @@ function once(fn: Function): OnceFn {
 	};
 }
 
-let output = argumentsLength({}, null, "3");
+let fn = (a,b,c) => (a + b + c);
+let onceFn = once(fn);
+let output1 = onceFn(1,2,3);
+let output2 = onceFn(2,3,6);
 
 let webHeading1 = document.querySelector('#t1');
-webHeading1.textContent = 'Output: ' + output.toString();
+webHeading1.textContent = 'Output: ' + output1.toString();
+
+let webHeading2 = document.querySelector('#t2');
+webHeading2.textContent = 'Output: ' + output2.toString();
 
 
 
