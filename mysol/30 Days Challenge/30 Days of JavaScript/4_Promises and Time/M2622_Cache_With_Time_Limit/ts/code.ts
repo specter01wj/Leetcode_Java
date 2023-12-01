00545a5ffc44af1a547d3d57be7dff1af8c5c570
Jin Wang <specter01wj@gmail.com>
2023-12-01 16:47:47 -0500

    v5

--- mysol/30 Days Challenge/30 Days of JavaScript/4_Promises and Time/M2622_Cache_With_Time_Limit/ts/code.ts
+++ mysol/30 Days Challenge/30 Days of JavaScript/4_Promises and Time/M2622_Cache_With_Time_Limit/ts/code.ts
@@ -1,24 +1,56 @@
-type Fn = (...params: any[]) => Promise<any>;
+class TimeLimitedCache {
+  private cache: Record<number, number>;
+  private expiryTimes: Record<number, number>;
 
-function timeLimit(fn: Fn, t: number): Fn {
-    
-	return async function(...args) {
-    let timeoutPromise = new Promise<any>((_, reject) => {
-      setTimeout(() => reject("Time Limit Exceeded"), t);
-    });
-
-    let fnPromise = fn(...args);
-
-    return Promise.race([fnPromise, timeoutPromise]);
+  constructor() {
+      this.cache = {};
+      this.expiryTimes = {};
   }
-};
+  
+  set(key: number, value: number, duration: number): boolean {
+      const now = new Date().getTime();
+      const alreadyExistsAndUnexpired = this.cache.hasOwnProperty(key) && now < this.expiryTimes[key];
+
+      this.cache[key] = value;
+      this.expiryTimes[key] = now + duration;
+
+      return alreadyExistsAndUnexpired;
+  }
+
+  get(key: number): number {
+      const now = new Date().getTime();
+      if (this.cache.hasOwnProperty(key) && now < this.expiryTimes[key]) {
+          return this.cache[key];
+      }
+      return -1;
+  }
+
+  count(): number {
+      const now = new Date().getTime();
+      return Object.keys(this.expiryTimes).reduce((count, key) => {
+          const numericKey = parseInt(key);
+          if (now < this.expiryTimes[numericKey]) {
+              count++;
+          }
+          return count;
+      }, 0);
+  }
+}
 
 const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+const timeLimitedCache = new TimeLimitedCache()
+let output1 = timeLimitedCache.set(1, 42, 1000); // false
+let output2 = timeLimitedCache.get(1) // 42
+let output3 = timeLimitedCache.count() // 1
 
-limited(150).catch((result) => {
-  let output1 = result;
-  let webHeading1 = document.querySelector('#t1');
-  webHeading1.textContent = 'Output: ' + (output1);
-});
+let webHeading1 = document.querySelector('#t1');
+webHeading1.textContent = 'Output: ' + (output1);
+
+let webHeading2 = document.querySelector('#t2');
+webHeading2.textContent = 'Output: ' + (output2);
+
+let webHeading3 = document.querySelector('#t3');
+webHeading3.textContent = 'Output: ' + (output3);
+
 
 
