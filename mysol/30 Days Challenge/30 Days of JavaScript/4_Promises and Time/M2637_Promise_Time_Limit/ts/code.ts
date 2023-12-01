b2676a946fa91021c1fdb46a45567011b570ba55
Jin Wang <specter01wj@gmail.com>
2023-12-01 11:08:53 -0500

    v4

--- mysol/30 Days Challenge/30 Days of JavaScript/4_Promises and Time/M2637_Promise_Time_Limit/ts/code.ts
+++ mysol/30 Days Challenge/30 Days of JavaScript/4_Promises and Time/M2637_Promise_Time_Limit/ts/code.ts
@@ -13,32 +13,12 @@ function timeLimit(fn: Fn, t: number): Fn {
   }
 };
 
+const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
 
-const result = [];
-const fn = (x) => x * 2;
-const args = [4], t = 35, cancelT = 190;
-const start = performance.now();
-
-const log = (...argsArr) => {
-  const diff = Math.floor(performance.now() - start);
-  result.push({"time": diff, "returned": fn(...argsArr)});
-};
-
-const cancel = cancellable(log, args, t);
-
-const maxT = Math.max(t, cancelT);
-          
-setTimeout(() => {
-    cancel();
-}, cancelT);
-
-setTimeout(() => {
-    console.log(result);
-    let output1 = result;
-    let webHeading1 = document.querySelector('#t1');
-    webHeading1.textContent = 'Output: ' + 'Time - ' + (output1[0].time) + '; Returned - ' + (output1[0].returned);
-}, maxT + 15);
-
-
+limited(150).catch((result) => {
+  let output1 = result;
+  let webHeading1 = document.querySelector('#t1');
+  webHeading1.textContent = 'Output: ' + (output1);
+});
 
 
