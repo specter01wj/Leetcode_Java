ea4eabcd3c294bd53e44b7e4a9c452d41be45ec6
Jin Wang <specter01wj@gmail.com>
2023-12-03 17:40:52 -0500

    v9

--- mysol/30 Days Challenge/30 Days of JavaScript/5_JSON/E2677_Chunk_Array/ts/code.ts
+++ mysol/30 Days Challenge/30 Days of JavaScript/5_JSON/E2677_Chunk_Array/ts/code.ts
@@ -1,21 +1,21 @@
-
 type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
-type Obj = Record<string, JSONValue> | JSONValue[]
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
 
-function isEmpty(obj: Obj): boolean {
-	return Object.keys(obj).length === 0;
+function chunk(arr: Obj[], size: number): Obj[][] {
+	let chunkedArr: Obj[][] = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+        chunkedArr.push(arr.slice(i, i + size) as Obj[]);
+    }
+
+    return chunkedArr;
 };
 
 
-let input1 = {"x": 5, "y": 42}, input2 = {};
-let output1 = isEmpty(input1);
-let output2 = isEmpty(input2);
+let inpu1 = [1,9,6,3,2], size = 3;
+let output1 = chunk(input1, size);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + output1.toString();
-
-let webHeading2 = document.querySelector('#t2');
-webHeading2.textContent = 'Output: ' + output2.toString();
-
 
 
