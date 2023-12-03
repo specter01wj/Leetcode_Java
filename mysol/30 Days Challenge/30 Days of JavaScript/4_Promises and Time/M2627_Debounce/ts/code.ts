3d9602bf671ced68f7c6ebed0cd06f0630eebcf1
Jin Wang <specter01wj@gmail.com>
2023-12-03 00:11:33 -0500

    v4

--- mysol/30 Days Challenge/30 Days of JavaScript/4_Promises and Time/M2627_Debounce/ts/code.ts
+++ mysol/30 Days Challenge/30 Days of JavaScript/4_Promises and Time/M2627_Debounce/ts/code.ts
@@ -1,55 +1,26 @@
-class TimeLimitedCache {
-  private cache: Record<number, number>;
-  private expiryTimes: Record<number, number>;
+type F = (...args: number[]) => void
 
-  constructor() {
-      this.cache = {};
-      this.expiryTimes = {};
-  }
-  
-  set(key: number, value: number, duration: number): boolean {
-      const now = new Date().getTime();
-      const alreadyExistsAndUnexpired = this.cache.hasOwnProperty(key) && now < this.expiryTimes[key];
+function debounce(fn: F, t: number): F {
+    let timeoutID: number | undefined;
 
-      this.cache[key] = value;
-      this.expiryTimes[key] = now + duration;
+    return function(...args: number[]) {
+        
+        clearTimeout(timeoutID);
 
-      return alreadyExistsAndUnexpired;
-  }
+        timeoutID = setTimeout(() => {
+            fn(...args);
+        }, t) as unknown as number;
+    };
+};
 
-  get(key: number): number {
-      const now = new Date().getTime();
-      if (this.cache.hasOwnProperty(key) && now < this.expiryTimes[key]) {
-          return this.cache[key];
-      }
-      return -1;
-  }
-
-  count(): number {
-      const now = new Date().getTime();
-      return Object.keys(this.expiryTimes).reduce((count, key) => {
-          const numericKey = parseInt(key);
-          if (now < this.expiryTimes[numericKey]) {
-              count++;
-          }
-          return count;
-      }, 0);
-  }
-}
-
-const timeLimitedCache = new TimeLimitedCache()
-let output1 = timeLimitedCache.set(1, 42, 1000); // false
-let output2 = timeLimitedCache.get(1) // 42
-let output3 = timeLimitedCache.count() // 1
-
-let webHeading1 = document.querySelector('#t1');
-webHeading1.textContent = 'Output: ' + (output1);
-
-let webHeading2 = document.querySelector('#t2');
-webHeading2.textContent = 'Output: ' + (output2);
-
-let webHeading3 = document.querySelector('#t3');
-webHeading3.textContent = 'Output: ' + (output3);
-
+let output = '';
+const log = debounce((item) => {
+  output = item;
+  let webHeading1 = document.querySelector('#t1');
+  webHeading1.textContent = 'Output: ' + (output);
+}, 100);
+log('Hello1'); // cancelled
+log('Hello2'); // cancelled
+output = log('Hello3'); // Logged at t=100ms
 
 
