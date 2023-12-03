88f1097e9d75eeb0bca44c5ddb93630dab2f3419
Jin Wang <specter01wj@gmail.com>
2023-12-03 17:23:23 -0500

    v4

--- mysol/30 Days Challenge/30 Days of JavaScript/5_JSON/E2727_Is_Object_Empty/ts/code.ts
+++ mysol/30 Days Challenge/30 Days of JavaScript/5_JSON/E2727_Is_Object_Empty/ts/code.ts
@@ -1,23 +1,21 @@
 
-function reduce(nums: number[], fn: (item: number, index: number) => number): number {
-  let val = init;
-  
-  for (let i = 0; i < nums.length; i++) {
-    val = fn(val, nums[i]);
-  }
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | JSONValue[]
 
-  return val;
-}
+function isEmpty(obj: Obj): boolean {
+	return Object.keys(obj).length === 0;
+};
 
-function fn(a, b) {
-  return a + b;
-}
 
-let input = [1, 2, 3, 4], init = 10;
-let output1 = reduce(input, fn, init);
+let input1 = {"x": 5, "y": 42}, input2 = {};
+let output1 = isEmpty(input1);
+let output2 = isEmpty(input2);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + output1.toString();
+
+let webHeading2 = document.querySelector('#t2');
+webHeading2.textContent = 'Output: ' + output2.toString();
 
 
 
