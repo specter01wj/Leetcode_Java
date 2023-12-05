e22fcc04c37fb86eb6e7c7a6a60549577803011d
Jin Wang <specter01wj@gmail.com>
2023-12-05 01:19:17 -0500

    v3

--- mysol/30 Days Challenge/30 Days of JavaScript/5_JSON/M2631_Group_By/ts/code.ts
+++ mysol/30 Days Challenge/30 Days of JavaScript/5_JSON/M2631_Group_By/ts/code.ts
@@ -1,15 +1,36 @@
+declare global {
+  interface Array<T> {
+      groupBy(fn: (item: T) => string): Record<string, T[]>
+  }
+}
 
-type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
-type Obj = Record<string, JSONValue> | JSONValue[]
+Array.prototype.groupBy = function(fn) {
+  const grouped = {};
 
-function isEmpty(obj: Obj): boolean {
-	return Object.keys(obj).length === 0;
+  for (let i = 0; i < this.length; i++) {
+      const key = fn(this[i]);
+
+      if (!grouped[key]) {
+          grouped[key] = [];
+      }
+
+      grouped[key].push(this[i]);
+  }
+
+  return grouped;
+}
+
+
+let input1 = [
+  {"id":"1"},
+  {"id":"1"},
+  {"id":"2"}
+];
+let fn = function (item) { 
+  return item.id; 
 };
-
-
-let input1 = {"x": 5, "y": 42}, input2 = {};
-let output1 = isEmpty(input1);
-let output2 = isEmpty(input2);
+let output1 = [1,2,3].groupBy(String);
+let output2 = input1.groupBy(fn);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + output1.toString();
