b63a6f965196c2c11efa726880c3d250b244ff2d
Jin Wang <specter01wj@gmail.com>
2023-12-05 01:41:21 -0500

    v8

---
+++ mysol/30 Days Challenge/30 Days of JavaScript/5_JSON/E2724_Sort_By/ts/code.ts
@@ -0,0 +1,42 @@
+declare global {
+  interface Array<T> {
+      groupBy(fn: (item: T) => string): Record<string, T[]>
+  }
+}
+
+Array.prototype.groupBy = function(fn) {
+  const grouped = {};
+
+  for (let i = 0; i < this.length; i++) {
+      const key = fn(this[i]);
+
+      if (!grouped[key]) {
+          grouped[key] = [];
+      }
+
+      grouped[key].push(this[i]);
+  }
+
+  return grouped;
+}
+
+
+let input1 = [
+  {"id":"1"},
+  {"id":"1"},
+  {"id":"2"}
+];
+let fn = function (item) { 
+  return item.id; 
+};
+let output1 = [1,2,3].groupBy(String);
+let output2 = input1.groupBy(fn);
+
+let webHeading1 = document.querySelector('#t1');
+webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
+
+let webHeading2 = document.querySelector('#t2');
+webHeading2.textContent = 'Output: ' + JSON.stringify(output2);
+
+
+
