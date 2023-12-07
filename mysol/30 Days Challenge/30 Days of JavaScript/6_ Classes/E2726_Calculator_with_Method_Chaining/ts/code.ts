14a26a96573bf3655144df70c24dfe94ea51c8a3
Jin Wang <specter01wj@gmail.com>
2023-12-07 14:07:00 -0500

    v10

--- mysol/30 Days Challenge/30 Days of JavaScript/6_ Classes/E2726_Calculator_with_Method_Chaining/ts/code.ts
+++ mysol/30 Days Challenge/30 Days of JavaScript/6_ Classes/E2726_Calculator_with_Method_Chaining/ts/code.ts
@@ -1,26 +1,48 @@
-class ArrayWrapper {
-  private nums: number[];
-  constructor(nums: number[]) {
-    this.nums = nums;
-  }
+class Calculator {
+  private result: number;
 
-  valueOf(): number {
-    return this.nums.reduce((acc, num) => acc + num, 0);
+	constructor(value: number) {
+		this.result = value;
   }
+    
+	add(value: number): Calculator {
+		this.result += value;
+    return this;
+	}
+    
+	subtract(value: number): Calculator {
+		this.result -= value;
+    return this;
+	}
+    
+	multiply(value: number): Calculator {
+		this.result *= value;
+    return this;
+	}
 
-  toString(): string {
-    return '[' + this.nums.join(',') + ']';
-  }
-};
+	divide(value: number): Calculator {
+		if (value === 0) {
+      throw new Error("Division by zero is not allowed");
+    }
+    this.result /= value;
+    return this;
+	}
+    
+	power(value: number): Calculator {
+		this.result **= value;
+    return this;
+	}
+
+	getResult(): number {
+		return this.result;
+	}
+}
 
 
 let input1 = '';
-const obj1 = new ArrayWrapper([1,2]);
-const obj2 = new ArrayWrapper([3,4]);
-
-let output1 = obj1 + obj2; // 10
-let output2 = String(obj1); // "[1,2]"
-let output3 = String(obj2); // "[3,4]"
+let output1 = new Calculator(10).add(5).subtract(7).getResult(); // 10 + 5 - 7 = 8
+let output2 = new Calculator(2).multiply(5).power(2).getResult(); // (2 * 5) ^ 2 = 100
+let output3 = new Calculator(20).divide(0).getResult(); // 20 / 0 
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
