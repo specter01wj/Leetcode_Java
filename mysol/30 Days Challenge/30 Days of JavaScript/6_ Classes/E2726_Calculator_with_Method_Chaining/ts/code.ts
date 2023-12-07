939d5bd62239878656b35fc361b8598c92d332e0
Jin Wang <specter01wj@gmail.com>
2023-12-07 14:07:36 -0500

    v11

--- mysol/30 Days Challenge/30 Days of JavaScript/6_ Classes/E2726_Calculator_with_Method_Chaining/ts/code.ts
+++ mysol/30 Days Challenge/30 Days of JavaScript/6_ Classes/E2726_Calculator_with_Method_Chaining/ts/code.ts
@@ -42,7 +42,7 @@ class Calculator {
 let input1 = '';
 let output1 = new Calculator(10).add(5).subtract(7).getResult(); // 10 + 5 - 7 = 8
 let output2 = new Calculator(2).multiply(5).power(2).getResult(); // (2 * 5) ^ 2 = 100
-let output3 = new Calculator(20).divide(0).getResult(); // 20 / 0 
+// let output3 = new Calculator(20).divide(0).getResult(); // 20 / 0 
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
@@ -50,8 +50,8 @@ webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
 let webHeading2 = document.querySelector('#t2');
 webHeading2.textContent = 'Output: ' + JSON.stringify(output2);
 
-let webHeading3 = document.querySelector('#t3');
-webHeading3.textContent = 'Output: ' + JSON.stringify(output3);
+// let webHeading3 = document.querySelector('#t3');
+// webHeading3.textContent = 'Output: ' + JSON.stringify(output3);
 
 
 
