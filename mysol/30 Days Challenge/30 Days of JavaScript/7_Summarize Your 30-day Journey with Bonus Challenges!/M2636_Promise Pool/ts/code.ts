f12ec398aeba44673cc686028733bbdfc9cb22fd
Jin Wang <specter01wj@gmail.com>
2023-12-08 11:30:35 -0500

    v4

--- mysol/30 Days Challenge/30 Days of JavaScript/7_Summarize Your 30-day Journey with Bonus Challenges!/M2636_Promise Pool/ts/code.ts
+++ mysol/30 Days Challenge/30 Days of JavaScript/7_Summarize Your 30-day Journey with Bonus Challenges!/M2636_Promise Pool/ts/code.ts
@@ -1,15 +1,39 @@
-async function sleep(millis: number): Promise<void> {
-  await new Promise<void>(resolve => setTimeout(resolve, millis));
-}
+type F = () => Promise<any>;
 
-let input = Date.now();
-let output1 = '';
-sleep(100)
-  .then( () => {
-    output1 = (Date.now() - input);
+function promisePool(functions: F[], n: number): Promise<any> {
+	return new Promise<any>(async (resolve) => {
+        const results: any[] = [];
+        const inProgress: Promise<any>[] = [];
+        let i = 0;
+
+        while (i < functions.length || inProgress.length > 0) {
+            while (inProgress.length < n && i < functions.length) {
+                const promise = functions[i]();
+                const index = i;
+                const resultPromise = promise.then((result) => {
+                    results[index] = result;
+                    inProgress.splice(inProgress.indexOf(resultPromise), 1);
+                });
+                inProgress.push(resultPromise);
+                i++;
+            }
+
+            if (inProgress.length > 0) {
+                await Promise.race(inProgress);
+            }
+        }
+
+        resolve(results);
+    });
+};
+
+let input = '';
+const sleep = (t) => new Promise(res => setTimeout(res, t));
+promisePool([() => sleep(500), () => sleep(400)], 1)
+  .then((item) => {
+    let output1 = item.length;
     let webHeading1 = document.querySelector('#t1');
     webHeading1.textContent = 'Output: ' + output1.toString();
   });
-
 
 
