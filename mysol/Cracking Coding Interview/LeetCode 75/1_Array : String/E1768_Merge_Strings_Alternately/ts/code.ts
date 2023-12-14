f2f9d7e1f74ea812fc0bc4ccc8e5d95ffba0be44
Jin Wang <specter01wj@gmail.com>
2023-12-14 17:59:40 -0500

    v4

--- mysol/Cracking Coding Interview/LeetCode 75/1_Array : String/E1768_Merge_Strings_Alternately/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/1_Array : String/E1768_Merge_Strings_Alternately/ts/code.ts
@@ -1,31 +1,22 @@
-type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
-type OnceFn = (...args: JSONValue[]) => JSONValue | undefined;
+function mergeAlternately(word1: string, word2: string): string {
+  let n = word1.length, m = word2.length, i = 0, j = 0;
+  let res = '';
 
-function once(fn: Function): OnceFn {
-  let called = false;
-  let result: any;
+  while (i < n || j < m) {
+      if (i < n) {
+          res += word1[i++];
+      }
+      if (j < m) {
+          res += word2[j++];
+      }
+  }
 
-	return function (...args: any[]): any {
-		if (!called) {
-      result = fn(...args);
-      called = true;
-      return result;
-    } else {
-      return undefined;
-    }
-	};
+  return res;
 }
 
-let fn = (a,b,c) => (a + b + c);
-let onceFn = once(fn);
-let output1 = onceFn(1,2,3);
-let output2 = onceFn(2,3,6);
+let word1 = "abcd", word2 = "pq";
+let output1 = mergeAlternately(word1, word2);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + output1.toString();
-
-let webHeading2 = document.querySelector('#t2');
-webHeading2.textContent = 'Output: ' + output2.toString();
-
-
 
