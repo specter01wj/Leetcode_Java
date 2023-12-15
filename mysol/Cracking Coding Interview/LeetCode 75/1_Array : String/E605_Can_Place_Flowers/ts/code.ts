bbac94cddd1ec42ed24615a49b4d6d1ef26ddf01
Jin Wang <specter01wj@gmail.com>
2023-12-15 16:48:16 -0500

    v9

--- mysol/Cracking Coding Interview/LeetCode 75/1_Array : String/E605_Can_Place_Flowers/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/1_Array : String/E605_Can_Place_Flowers/ts/code.ts
@@ -1,17 +1,27 @@
-function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
-  const ans: boolean[] = [];
-  const max: number = Math.max(...candies);
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+  let count = 0;
 
-  for (let candy of candies) {
-      ans.push(candy + extraCandies >= max);
+  for (let i = 0; i < flowerbed.length; i++) {
+      if (flowerbed[i] === 0 && (i === 0 || flowerbed[i - 1] === 0) 
+          && (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)) {
+          flowerbed[i] = 1;
+          count++;
+      }
+      if (count >= n) {
+          return true;
+      }
   }
-
-  return ans;
+  return false;
 };
 
-let input = [2,3,5,1,3], extraCandies = 3;
-let output1 = kidsWithCandies(input, extraCandies);
+let input1 = [1,0,0,0,1], n1 = 1;
+let input2 = [1,0,0,0,1], n2 = 2;
+let output1 = canPlaceFlowers(input1, n1);
+let output2 = canPlaceFlowers(input2, n2);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + output1.toString();
+
+let webHeading2 = document.querySelector('#t2');
+webHeading2.textContent = 'Output: ' + output2.toString();
 
