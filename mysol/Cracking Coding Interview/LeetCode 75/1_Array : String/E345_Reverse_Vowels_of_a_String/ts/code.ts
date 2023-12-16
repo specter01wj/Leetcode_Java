53b4b537443d1bb2df82efe571c2b62c698ee9c7
Jin Wang <specter01wj@gmail.com>
2023-12-16 13:34:21 -0500

    v3

--- mysol/Cracking Coding Interview/LeetCode 75/1_Array : String/E345_Reverse_Vowels_of_a_String/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/1_Array : String/E345_Reverse_Vowels_of_a_String/ts/code.ts
@@ -1,21 +1,28 @@
-function mergeAlternately(word1: string, word2: string): string {
-  let n = word1.length, m = word2.length, i = 0, j = 0;
-  let res = '';
+function reverseVowels(s: string): string {
+  if(s === null || s.length === 0) {
+      return s;
+  }
+  const vowels: string = "aeiouAEIOU";
+  let chars: string[] = s.split("");
+  let start: number = 0, end: number = s.length - 1;
 
-  while (i < n || j < m) {
-      if (i < n) {
-          res += word1[i++];
+  while(start < end) {
+      while(start < end && !vowels.includes(chars[start])) {
+          start++;
       }
-      if (j < m) {
-          res += word2[j++];
+      while(start < end && !vowels.includes(chars[end])) {
+          end--;
       }
+      [chars[start], chars[end]] = [chars[end], chars[start]]; // Swapping
+      start++;
+      end--;
   }
 
-  return res;
-}
+  return chars.join("");
+};
 
-let word1 = "abcd", word2 = "pq";
-let output1 = mergeAlternately(word1, word2);
+let input1 = "leetcode";
+let output1 = reverseVowels(input1);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + output1.toString();
