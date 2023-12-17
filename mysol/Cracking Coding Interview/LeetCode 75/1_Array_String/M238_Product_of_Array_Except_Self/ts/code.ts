26f05b933dafd52e6fba344df9725633a1c83e93
Jin Wang <specter01wj@gmail.com>
2023-12-17 14:39:53 -0500

    v6

--- mysol/Cracking Coding Interview/LeetCode 75/1_Array_String/M238_Product_of_Array_Except_Self/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/1_Array_String/M238_Product_of_Array_Except_Self/ts/code.ts
@@ -1,28 +1,30 @@
-function reverseVowels(s: string): string {
-  if(s === null || s.length === 0) {
-      return s;
-  }
-  const vowels: string = "aeiouAEIOU";
-  let chars: string[] = s.split("");
-  let start: number = 0, end: number = s.length - 1;
+function productExceptSelf(nums: number[]): number[] {
+  let length = nums.length;
 
-  while(start < end) {
-      while(start < end && !vowels.includes(chars[start])) {
-          start++;
-      }
-      while(start < end && !vowels.includes(chars[end])) {
-          end--;
-      }
-      [chars[start], chars[end]] = [chars[end], chars[start]]; // Swapping
-      start++;
-      end--;
+  let leftProducts: number[] = new Array(length);
+  let rightProducts: number[] = new Array(length);
+
+  let answer: number[] = new Array(length);
+
+  leftProducts[0] = 1;
+  for (let i = 1; i < length; i++) {
+      leftProducts[i] = nums[i - 1] * leftProducts[i - 1];
   }
 
-  return chars.join("");
+  rightProducts[length - 1] = 1;
+  for (let i = length - 2; i >= 0; i--) {
+      rightProducts[i] = nums[i + 1] * rightProducts[i + 1];
+  }
+
+  for (let i = 0; i < length; i++) {
+      answer[i] = leftProducts[i] * rightProducts[i];
+  }
+
+  return answer;
 };
 
-let input1 = "leetcode";
-let output1 = reverseVowels(input1);
+let input1 = [1,2,3,4];
+let output1 = productExceptSelf(input1);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + output1.toString();
