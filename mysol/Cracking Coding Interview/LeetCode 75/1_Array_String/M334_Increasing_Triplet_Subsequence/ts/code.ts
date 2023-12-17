757f5581066361272cb26bacf96f1ed002c5294e
Jin Wang <specter01wj@gmail.com>
2023-12-17 15:01:20 -0500

    v13

--- mysol/Cracking Coding Interview/LeetCode 75/1_Array_String/M334_Increasing_Triplet_Subsequence/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/1_Array_String/M334_Increasing_Triplet_Subsequence/ts/code.ts
@@ -1,30 +1,26 @@
-function productExceptSelf(nums: number[]): number[] {
-  let length = nums.length;
-
-  let leftProducts: number[] = new Array(length);
-  let rightProducts: number[] = new Array(length);
-
-  let answer: number[] = new Array(length);
-
-  leftProducts[0] = 1;
-  for (let i = 1; i < length; i++) {
-      leftProducts[i] = nums[i - 1] * leftProducts[i - 1];
+function increasingTriplet(nums: number[]): boolean {
+  if (nums === null || nums.length < 3) {
+      return false;
   }
 
-  rightProducts[length - 1] = 1;
-  for (let i = length - 2; i >= 0; i--) {
-      rightProducts[i] = nums[i + 1] * rightProducts[i + 1];
+  let first = Number.MAX_SAFE_INTEGER;
+  let second = Number.MAX_SAFE_INTEGER;
+
+  for (let num of nums) {
+      if (num <= first) {
+          first = num;
+      } else if (num <= second) {
+          second = num;
+      } else {
+          return true;
+      }
   }
 
-  for (let i = 0; i < length; i++) {
-      answer[i] = leftProducts[i] * rightProducts[i];
-  }
-
-  return answer;
+  return false;
 };
 
-let input1 = [1,2,3,4];
-let output1 = productExceptSelf(input1);
+let input1 = [2,1,5,0,4,6];
+let output1 = increasingTriplet(input1);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + output1.toString();
