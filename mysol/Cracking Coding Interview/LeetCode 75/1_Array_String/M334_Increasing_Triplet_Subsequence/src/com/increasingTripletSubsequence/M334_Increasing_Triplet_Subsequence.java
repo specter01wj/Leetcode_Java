03bd2ac830a63e0b5e22cb9391acca0debb58c0e
Jin Wang <specter01wj@gmail.com>
2023-12-17 14:52:32 -0500

    v8

--- mysol/Cracking Coding Interview/LeetCode 75/1_Array_String/M334_Increasing_Triplet_Subsequence/src/com/increasingTripletSubsequence/M334_Increasing_Triplet_Subsequence.java
+++ mysol/Cracking Coding Interview/LeetCode 75/1_Array_String/M334_Increasing_Triplet_Subsequence/src/com/increasingTripletSubsequence/M334_Increasing_Triplet_Subsequence.java
@@ -1,10 +1,43 @@
 package com.increasingTripletSubsequence;
+import java.util.*;
+
+/*
+Given an integer array nums, return true if there exists a triple of 
+indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. 
+If no such indices exists, return false.
+*/
 
 public class M334_Increasing_Triplet_Subsequence {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {2,1,5,0,4,6};
+		boolean output = increasingTriplet(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public static boolean increasingTriplet(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return false;
+        }
+
+        // Initialize the first and second numbers to the maximum possible value
+        int first = Integer.MAX_VALUE, second = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= first) {
+                // Update the lowest number found so far
+                first = num;
+            } else if (num <= second) {
+                // Update the second lowest number
+                second = num;
+            } else {
+                // Found a number greater than both first and second
+                return true;
+            }
+        }
+
+        return false;
+    }
 
 }
