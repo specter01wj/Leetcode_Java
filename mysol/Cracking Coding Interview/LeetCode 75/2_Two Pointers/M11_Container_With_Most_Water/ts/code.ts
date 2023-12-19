267396ba691bee25e4b8b30de314578859baeadf
Jin Wang <specter01wj@gmail.com>
2023-12-19 11:51:49 -0500

    v6

--- mysol/Cracking Coding Interview/LeetCode 75/2_Two Pointers/M11_Container_With_Most_Water/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/2_Two Pointers/M11_Container_With_Most_Water/ts/code.ts
@@ -1,17 +1,30 @@
-function isSubsequence(s: string, t: string): boolean {
-  let i = 0, j = 0;
-  while(i < s.length && j < t.length) {
-      if(s.charCodeAt(i) === t.charCodeAt(j)) {
-          i++;
+function maxArea(height: number[]): number {
+  let maxArea = 0;
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+      // Calculate the area
+      const width = right - left;
+      const minHeight = Math.min(height[left], height[right]);
+      const area = width * minHeight;
+
+      // Update the maximum area
+      maxArea = Math.max(maxArea, area);
+
+      // Move the pointer pointing to the shorter line inward
+      if (height[left] < height[right]) {
+          left++;
+      } else {
+          right--;
       }
-      j++;
   }
-  
-  return i === s.length;
+
+  return maxArea;
 };
 
-let input1 = "abc", input2 = "ahbgdc";
-let output1 = isSubsequence(input1, input2);
+let input1 = [1,8,6,2,5,4,8,3,7];
+let output1 = maxArea(input1);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
