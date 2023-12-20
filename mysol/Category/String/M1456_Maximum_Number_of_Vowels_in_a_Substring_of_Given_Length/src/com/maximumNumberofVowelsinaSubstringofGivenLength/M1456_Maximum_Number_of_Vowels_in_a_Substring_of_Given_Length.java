17ccf63a2cf938fabeb42a8d25d15b277831880c
Jin Wang <specter01wj@gmail.com>
2023-12-20 14:18:33 -0500

    v15

---
+++ mysol/Category/String/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length/src/com/maximumNumberofVowelsinaSubstringofGivenLength/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.java
@@ -0,0 +1,63 @@
+package com.maximumNumberofVowelsinaSubstringofGivenLength;
+import java.util.*;
+
+/*
+Given a string s and an integer k, return the maximum number of vowel 
+letters in any substring of s with length k.
+
+Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+*/
+
+public class M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length {
+
+	public static void main(String[] args) {
+		String input = "abciiidef";
+		int k = 3;
+		int output = maxVowels(input, k);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    Sliding Window
+    */
+	
+	/*
+     * @param s: a string
+     * @param k: an integer
+     * @return: the maximum number of vowel letters in any substring of s with length k
+     */
+	public static int maxVowels(String s, int k) {
+        int maxVowels = 0;
+        int currentVowels = 0;
+
+        // Count vowels in the first window
+        for (int i = 0; i < k; i++) {
+            if (isVowel(s.charAt(i))) {
+                currentVowels++;
+            }
+        }
+
+        maxVowels = currentVowels;
+
+        // Slide the window and update the count
+        for (int i = k; i < s.length(); i++) {
+            if (isVowel(s.charAt(i))) {
+                currentVowels++;
+            }
+            if (isVowel(s.charAt(i - k))) {
+                currentVowels--;
+            }
+
+            maxVowels = Math.max(maxVowels, currentVowels);
+        }
+
+        return maxVowels;
+    }
+
+    // Helper method to check if a character is a vowel
+    private static boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+
+}
