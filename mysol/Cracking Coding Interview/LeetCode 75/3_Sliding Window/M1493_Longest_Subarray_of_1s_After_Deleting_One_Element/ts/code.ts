8832dfd13d73664a392e4746546f923aa1a67c09
Jin Wang <specter01wj@gmail.com>
2023-12-21 12:07:31 -0500

    v13

--- mysol/Cracking Coding Interview/LeetCode 75/3_Sliding Window/M1493_Longest_Subarray_of_1s_After_Deleting_One_Element/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/3_Sliding Window/M1493_Longest_Subarray_of_1s_After_Deleting_One_Element/ts/code.ts
@@ -1,7 +1,7 @@
-function longestOnes(nums: number[], k: number): number {
+function longestSubarray(nums: number[]): number {
   let left = 0, right = 0;
-  let maxOnes = 0;
   let zeroCount = 0;
+  let maxLength = 0;
 
   while (right < nums.length) {
       // If the current element is 0, increment the zero count
@@ -9,24 +9,26 @@ function longestOnes(nums: number[], k: number): number {
           zeroCount++;
       }
 
-      // If zero count exceeds k, shrink the window from the left
-      while (zeroCount > k) {
+      // If zero count exceeds 1, shrink the window from the left
+      while (zeroCount > 1) {
           if (nums[left] === 0) {
               zeroCount--;
           }
           left++;
       }
 
-      // Update the maximum size of the window
-      maxOnes = Math.max(maxOnes, right - left + 1);
+      // Update the maximum length of the window, minus 1 if we include a zero
+      maxLength = Math.max(maxLength, right - left);
+
       right++;
   }
 
-  return maxOnes;
+  // Return the maximum length found, minus 1 for the removed element
+  return maxLength === 0 ? 0 : maxLength;
 };
 
-let input1 = [1,1,1,0,0,0,1,1,1,1,0], k = 2;
-let output1 = longestOnes(input1, k);
+let input1 = [0,1,1,1,0,1,1,0,1];
+let output1 = longestSubarray(input1);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
