59525153d3572917c14340c6754e3ab5f5b715ec
Jin Wang <specter01wj@gmail.com>
2023-12-21 11:07:34 -0500

    v6

--- mysol/Cracking Coding Interview/LeetCode 75/3_Sliding Window/M1004_Max_Consecutive_Ones_III/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/3_Sliding Window/M1004_Max_Consecutive_Ones_III/ts/code.ts
@@ -1,19 +1,32 @@
-function findMaxAverage(nums: number[], k: number): number {
-  let sum = 0;
-  for (let i = 0; i < k; i++) {
-      sum += nums[i];
+function longestOnes(nums: number[], k: number): number {
+  let left = 0, right = 0;
+  let maxOnes = 0;
+  let zeroCount = 0;
+
+  while (right < nums.length) {
+      // If the current element is 0, increment the zero count
+      if (nums[right] === 0) {
+          zeroCount++;
+      }
+
+      // If zero count exceeds k, shrink the window from the left
+      while (zeroCount > k) {
+          if (nums[left] === 0) {
+              zeroCount--;
+          }
+          left++;
+      }
+
+      // Update the maximum size of the window
+      maxOnes = Math.max(maxOnes, right - left + 1);
+      right++;
   }
-  let max = sum;
-  
-  for (let i = k; i < nums.length; i++) {
-      sum = sum - nums[i - k] + nums[i];
-      max = Math.max(max, sum);
-  }
-  return max / k;
+
+  return maxOnes;
 };
 
-let input1 = [1,12,-5,-6,50,3], k = 4;
-let output1 = findMaxAverage(input1, 4);
+let input1 = [1,1,1,0,0,0,1,1,1,1,0], k = 2;
+let output1 = longestOnes(input1, k);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
