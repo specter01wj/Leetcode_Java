40953128d071b270ad904864b9a679aef7084b0e
Jin Wang <specter01wj@gmail.com>
2023-12-26 15:02:59 -0500

    v6

--- mysol/Cracking Coding Interview/LeetCode 75/4_Prefix Sum/E724_Find_Pivot_Index/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/4_Prefix Sum/E724_Find_Pivot_Index/ts/code.ts
@@ -1,17 +1,23 @@
-function largestAltitude(gain: number[]): number {
-  let maxAlt = 0;
-  let currAlt = 0;
+function pivotIndex(nums: number[]): number {
+  let totalSum = 0;
+  let leftSum = 0;
 
-  for (let i = 0; i < gain.length; i++) {
-      currAlt += gain[i];
-      maxAlt = Math.max(currAlt, maxAlt);
+  // Calculate the total sum of the array
+  nums.forEach(num => totalSum += num);
+
+  // Iterate through the array to find the pivot index
+  for (let i = 0; i < nums.length; i++) {
+      if (leftSum === totalSum - leftSum - nums[i]) {
+          return i; // Found the pivot index
+      }
+      leftSum += nums[i]; // Update the left sum for the next iteration
   }
 
-  return maxAlt;
+  return -1; // No pivot index found
 };
 
-let input1 = [-5,1,5,0,-7];
-let output1 = largestAltitude(input1);
+let input1 = [1,7,3,6,5,6];
+let output1 = pivotIndex(input1);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
