3fc02a308c9e6c72df7e894979e8bdfbf78b95a2
Jin Wang <specter01wj@gmail.com>
2023-12-27 12:44:50 -0500

    v7

--- mysol/Cracking Coding Interview/LeetCode 75/5_Hash Map_Set/E1207_Unique_Number_of_Occurrences/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/5_Hash Map_Set/E1207_Unique_Number_of_Occurrences/ts/code.ts
@@ -1,26 +1,13 @@
-function findDifference(nums1: number[], nums2: number[]): number[][] {
-  const set1 = new Set<number>(nums1);
-  const set2 = new Set<number>(nums2);
-
-  const result: number[][] = [[], []];
-
-  set1.forEach(num => {
-      if (!set2.has(num)) {
-          result[0].push(num);
-      }
-  });
-
-  set2.forEach(num => {
-      if (!set1.has(num)) {
-          result[1].push(num);
-      }
-  });
-
-  return result;
+function uniqueOccurrences(arr: number[]): boolean {
+  let count: Record<number, number> = {};
+  for (let a of arr) {
+      count[a] = 1 + (count[a] || 0);
+  }
+  return Object.keys(count).length === new Set(Object.values(count)).size;
 };
 
-let nums1 = [1,2,3], nums2 = [2,4,6];
-let output1 = findDifference(nums1, nums2);
+let input1 = [1,2,2,1,1,3];
+let output1 = uniqueOccurrences(input1);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
