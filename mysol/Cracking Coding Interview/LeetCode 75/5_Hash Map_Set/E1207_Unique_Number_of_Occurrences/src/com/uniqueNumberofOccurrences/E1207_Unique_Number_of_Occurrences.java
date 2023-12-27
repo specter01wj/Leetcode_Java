bb6103f46b8d520942ba7cc0e0c11b8aa9df09ec
Jin Wang <specter01wj@gmail.com>
2023-12-27 12:43:05 -0500

    v5

---
+++ mysol/Cracking Coding Interview/LeetCode 75/5_Hash Map_Set/E1207_Unique_Number_of_Occurrences/src/com/uniqueNumberofOccurrences/E1207_Unique_Number_of_Occurrences.java
@@ -0,0 +1,53 @@
+package com.uniqueNumberofOccurrences;
+import java.util.*;
+
+/*
+Given an array of integers arr, return true if the number of 
+occurrences of each value in the array is unique, or false otherwise.
+
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. 
+No two values have the same number of occurrences.
+
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+*/
+
+public class E1207_Unique_Number_of_Occurrences {
+
+	public static void main(String[] args) {
+		int[] input = {1,2,2,1,1,3};
+		boolean output = uniqueOccurrences(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    1. Count the occurrences of each char;
+	2. Compare if the numbers of distinct chars and distinct counts are equal.
+    */
+	
+	/*
+     * @param arr: a list of integer
+     * @return: true if the number of occurrences of each value in the array is unique
+     */
+	public static boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int a : arr) {
+            count.put(a, 1 + count.getOrDefault(a, 0));
+        }
+        return count.size() == new HashSet<>(count.values()).size();
+    }
+
+}
