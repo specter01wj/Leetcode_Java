6ee79c03dcf064f38d821fc4902a3595a8a81e6a
Jin Wang <specter01wj@gmail.com>
2023-12-28 12:19:55 -0500

    v5

---
+++ mysol/Cracking Coding Interview/LeetCode 75/5_Hash Map_Set/M1657_Determine_if_Two_Strings_Are_Close/ts/code.ts
@@ -0,0 +1,27 @@
+function findDifference(nums1: number[], nums2: number[]): number[][] {
+  const set1 = new Set<number>(nums1);
+  const set2 = new Set<number>(nums2);
+
+  const result: number[][] = [[], []];
+
+  set1.forEach(num => {
+      if (!set2.has(num)) {
+          result[0].push(num);
+      }
+  });
+
+  set2.forEach(num => {
+      if (!set1.has(num)) {
+          result[1].push(num);
+      }
+  });
+
+  return result;
+};
+
+let nums1 = [1,2,3], nums2 = [2,4,6];
+let output1 = findDifference(nums1, nums2);
+
+let webHeading1 = document.querySelector('#t1');
+webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
+
