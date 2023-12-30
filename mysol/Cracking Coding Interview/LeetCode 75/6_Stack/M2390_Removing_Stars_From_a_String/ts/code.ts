7038c8b13a0619a21087832a631565a44836c8a3
Jin Wang <specter01wj@gmail.com>
2023-12-30 14:06:48 -0500

    v4

--- mysol/Cracking Coding Interview/LeetCode 75/6_Stack/M2390_Removing_Stars_From_a_String/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/6_Stack/M2390_Removing_Stars_From_a_String/ts/code.ts
@@ -1,37 +1,19 @@
-function closeStrings(word1: string, word2: string): boolean {
-  if (word1.length !== word2.length) {
-      return false;
+function removeStars(s: string): string {
+  let stack: string[] = [];
+
+  for (let ch of s) {
+      if (ch === '*') {
+          stack.pop();
+      } else {
+          stack.push(ch);
+      }
   }
 
-  const freq1: number[] = new Array(26).fill(0);
-  const freq2: number[] = new Array(26).fill(0);
-  const present1: Set<string> = new Set();
-  const present2: Set<string> = new Set();
-
-  for (const char of word1) {
-      const index: number = char.charCodeAt(0) - 'a'.charCodeAt(0);
-      freq1[index]++;
-      present1.add(char);
-  }
-
-  for (const char of word2) {
-      const index: number = char.charCodeAt(0) - 'a'.charCodeAt(0);
-      freq2[index]++;
-      present2.add(char);
-  }
-
-  if (present1.size !== present2.size || ![...present1].every(char => present2.has(char))) {
-      return false;
-  }
-
-  freq1.sort((a, b) => a - b);
-  freq2.sort((a, b) => a - b);
-
-  return freq1.every((val, index) => val === freq2[index]);
+  return stack.join('');
 };
 
-let word1 = "abc", word2 = "bca";
-let output1 = closeStrings(word1, word2);
+let input1 = "leet**cod*e";
+let output1 = removeStars(input1);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
