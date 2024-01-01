e0edf9a7ec36b9c2a664ffa879b70da8ee355bda
Jin Wang <specter01wj@gmail.com>
2023-12-31 22:12:28 -0500

    v6

--- mysol/Cracking Coding Interview/LeetCode 75/6_Stack/M394_Decode_String/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/6_Stack/M394_Decode_String/ts/code.ts
@@ -1,19 +1,30 @@
-function removeStars(s: string): string {
-  let stack: string[] = [];
+function decodeString(s: string): string {
+  let counts: number[] = [];
+  let resultStack: string[][] = [];
+  resultStack.push([]);
+  let k: number = 0;
 
   for (let ch of s) {
-      if (ch === '*') {
-          stack.pop();
+      if (ch >= '0' && ch <= '9') {
+          k = k * 10 + parseInt(ch, 10);
+      } else if (ch === '[') {
+          counts.push(k);
+          resultStack.push([]);
+          k = 0;
+      } else if (ch === ']') {
+          let temp: string = resultStack.pop().join('');
+          let count: number = counts.pop();
+          resultStack[resultStack.length - 1].push(temp.repeat(count));
       } else {
-          stack.push(ch);
+          resultStack[resultStack.length - 1].push(ch);
       }
   }
 
-  return stack.join('');
+  return resultStack.pop().join('');
 };
 
-let input1 = "leet**cod*e";
-let output1 = removeStars(input1);
+let input1 = "3[a2[c]]";
+let output1 = decodeString(input1);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
