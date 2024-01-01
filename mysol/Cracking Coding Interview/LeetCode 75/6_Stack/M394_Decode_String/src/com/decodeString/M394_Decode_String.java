bbbda27fb437b8b79520e1046f19e0764f160465
Jin Wang <specter01wj@gmail.com>
2023-12-31 22:07:38 -0500

    v2

--- mysol/Cracking Coding Interview/LeetCode 75/6_Stack/M394_Decode_String/src/com/decodeString/M394_Decode_String.java
+++ mysol/Cracking Coding Interview/LeetCode 75/6_Stack/M394_Decode_String/src/com/decodeString/M394_Decode_String.java
@@ -1,10 +1,57 @@
 package com.decodeString;
+import java.util.*;
+
+/*
+Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string 
+inside the square brackets is being repeated exactly k times. 
+Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; there are 
+no extra white spaces, square brackets are well-formed, etc. 
+Furthermore, you may assume that the original data does not contain 
+any digits and that digits are only for those repeat numbers, k. 
+For example, there will not be input like 3a or 2[4].
+
+The test cases are generated so that the length of the output 
+will never exceed 105.
+*/
 
 public class M394_Decode_String {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "3[a2[c]]";
+        String output = decodeString(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	
+	public static String decodeString(String s) {
+        Stack<Integer> counts = new Stack<>();
+        Stack<StringBuilder> resultStack = new Stack<>();
+        resultStack.push(new StringBuilder());
+        int k = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                k = k * 10 + ch - '0';
+            } else if (ch == '[') {
+                counts.push(k);
+                resultStack.push(new StringBuilder());
+                k = 0;
+            } else if (ch == ']') {
+                StringBuilder temp = resultStack.pop();
+                int count = counts.pop();
+                for (int i = 0; i < count; i++) {
+                    resultStack.peek().append(temp);
+                }
+            } else {
+                resultStack.peek().append(ch);
+            }
+        }
+
+        return resultStack.pop().toString();
+    }
 
 }
