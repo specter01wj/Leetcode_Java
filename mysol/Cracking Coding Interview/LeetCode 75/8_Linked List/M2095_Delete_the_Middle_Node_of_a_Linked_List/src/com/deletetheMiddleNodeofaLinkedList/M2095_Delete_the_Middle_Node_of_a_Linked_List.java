43ac94dbc93447f4371c5c7cabc23773876ff0dd
Jin Wang <specter01wj@gmail.com>
2024-01-01 13:40:29 -0500

    v1

---
+++ mysol/Cracking Coding Interview/LeetCode 75/8_Linked List/M2095_Delete_the_Middle_Node_of_a_Linked_List/src/com/deletetheMiddleNodeofaLinkedList/M2095_Delete_the_Middle_Node_of_a_Linked_List.java
@@ -0,0 +1,10 @@
+package com.deletetheMiddleNodeofaLinkedList;
+
+public class M2095_Delete_the_Middle_Node_of_a_Linked_List {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
