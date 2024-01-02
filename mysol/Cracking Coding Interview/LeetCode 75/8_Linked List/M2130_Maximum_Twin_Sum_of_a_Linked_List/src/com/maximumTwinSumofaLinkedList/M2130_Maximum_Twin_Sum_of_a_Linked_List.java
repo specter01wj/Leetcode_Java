0c72414bf6f770fa3af95b3d1cd7bfd2106b9144
Jin Wang <specter01wj@gmail.com>
2024-01-02 11:35:26 -0500

    v7

---
+++ mysol/Cracking Coding Interview/LeetCode 75/8_Linked List/M2130_Maximum_Twin_Sum_of_a_Linked_List/src/com/maximumTwinSumofaLinkedList/M2130_Maximum_Twin_Sum_of_a_Linked_List.java
@@ -0,0 +1,10 @@
+package com.maximumTwinSumofaLinkedList;
+
+public class M2130_Maximum_Twin_Sum_of_a_Linked_List {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
