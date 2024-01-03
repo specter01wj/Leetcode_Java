5fe92b206199a68692e9295b55fd73291561bea2
Jin Wang <specter01wj@gmail.com>
2024-01-03 14:37:48 -0500

    v18

---
+++ mysol/Easy/E104_Maximum_Depth_of_Binary_Tree/ts/code.ts
@@ -0,0 +1,54 @@
+function maxDepth(root: TreeNode | null): number {
+  if (root === null) {
+      return 0;
+  }
+
+  let leftDepth = maxDepth(root.left);
+  let rightDepth = maxDepth(root.right);
+
+  return Math.max(leftDepth, rightDepth) + 1;
+};
+
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+  }
+}
+
+function arrayToTree(arr) {
+  if (arr.length === 0) return null;
+
+  let root = new TreeNode(arr[0]);
+  let queue = [root];
+
+  for (let i = 1; i < arr.length; i++) {
+      let parent = queue[0];
+      if (i % 2 === 1) {
+          if (arr[i] != null) {
+              parent.left = new TreeNode(arr[i]);
+              queue.push(parent.left);
+          }
+      } else {
+          if (arr[i] != null) {
+              parent.right = new TreeNode(arr[i]);
+              queue.push(parent.right);
+          }
+          queue.shift(); // Remove the processed node
+      }
+  }
+
+  return root;
+}
+
+let input1 = [3,9,20,null,null,15,7];
+let root1 = arrayToTree(input1);
+let output1 = maxDepth(root1);
+
+let webHeading1 = document.querySelector('#t1');
+webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
+
