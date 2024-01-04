a398b6410d4341e5c3fd21beb6b95a9a9b3659b4
Jin Wang <specter01wj@gmail.com>
2024-01-04 10:45:30 -0500

    v1

---
+++ mysol/Cracking Coding Interview/LeetCode 75/9_Binary Tree - DFS/M1448_Count_Good_Nodes_in_Binary_Tree/src/com/countGoodNodesinBinaryTree/M1448_Count_Good_Nodes_in_Binary_Tree.java
@@ -0,0 +1,10 @@
+package com.countGoodNodesinBinaryTree;
+
+public class M1448_Count_Good_Nodes_in_Binary_Tree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
