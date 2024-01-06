c4926be5585355f3a68c7d2cff36ac042a48cb30
Jin Wang <specter01wj@gmail.com>
2024-01-06 14:07:41 -0500

    v1

---
+++ mysol/Cracking Coding Interview/LeetCode 75/9_Binary Tree - DFS/M1372_Longest_ZigZag_Path_in_a_Binary_Tree/src/com/longestZigZagPathinaBinaryTree/M1372_Longest_ZigZag_Path_in_a_Binary_Tree.java
@@ -0,0 +1,10 @@
+package com.longestZigZagPathinaBinaryTree;
+
+public class M1372_Longest_ZigZag_Path_in_a_Binary_Tree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
