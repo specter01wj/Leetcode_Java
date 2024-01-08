30ff0a9e92c436fd175e5e835be0ba00ad0aac65
Jin Wang <specter01wj@gmail.com>
2024-01-07 22:15:37 -0500

    v7

---
+++ mysol/Cracking Coding Interview/LeetCode 75/10_Binary Tree - BFS/M1161_Maximum_Level_Sum_of_a_Binary_Tree/src/com/maximumLevelSumofaBinaryTree/M1161_Maximum_Level_Sum_of_a_Binary_Tree.java
@@ -0,0 +1,10 @@
+package com.maximumLevelSumofaBinaryTree;
+
+public class M1161_Maximum_Level_Sum_of_a_Binary_Tree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
