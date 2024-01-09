21883231b7bc3b3540cdf0cc9af7273653276a89
Jin Wang <specter01wj@gmail.com>
2024-01-09 14:35:17 -0500

    v11

--- mysol/Cracking Coding Interview/LeetCode 75/12_Graphs - DFS/M547_Number_of_Provinces/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/12_Graphs - DFS/M547_Number_of_Provinces/ts/code.ts
@@ -1,25 +1,29 @@
-function canVisitAllRooms(rooms: number[][]): boolean {
-  let visited: Set<number> = new Set(); // Set to keep track of visited rooms
+function findCircleNum(isConnected: number[][]): number {
+  let n: number = isConnected.length;
+  let visited: boolean[] = new Array(n).fill(false);
+  let count: number = 0;
 
-  function dfs(room: number) {
-      visited.add(room); // Mark the current room as visited
-
-      // Go through all the keys in the current room
-      rooms[room].forEach(key => {
-          if (!visited.has(key)) {
-              dfs(key); // Visit the room if it has not been visited
+  function dfs(city: number): void {
+      for (let i = 0; i < n; i++) {
+          if (isConnected[city][i] === 1 && !visited[i]) {
+              visited[i] = true;
+              dfs(i);
           }
-      });
+      }
   }
 
-  dfs(0); // Start DFS from room 0
+  for (let i = 0; i < n; i++) {
+      if (!visited[i]) {
+          dfs(i);
+          count++;
+      }
+  }
 
-  // Check if the number of visited rooms is equal to the total number of rooms
-  return visited.size === rooms.length;
+  return count;
 };
 
-let input1 = [[1],[2],[3],[]];
-let output1 = canVisitAllRooms(input1);
+let input1 = [[1,1,0],[1,1,0],[0,0,1]];
+let output1 = findCircleNum(input1);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
