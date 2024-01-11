dad63349d548c9f2b83523f5cd332336565500a5
Jin Wang <specter01wj@gmail.com>
2024-01-11 00:01:50 -0500

    v1

---
+++ mysol/Cracking Coding Interview/LeetCode 75/12_Graphs - DFS/M1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero/src/com/reorderRoutestoMakeAllPathsLeadtotheCityZero/M1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero.java
@@ -0,0 +1,10 @@
+package com.reorderRoutestoMakeAllPathsLeadtotheCityZero;
+
+public class M1466_Reorder_Routes_to_Make_All_Paths_Lead_to_the_City_Zero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
