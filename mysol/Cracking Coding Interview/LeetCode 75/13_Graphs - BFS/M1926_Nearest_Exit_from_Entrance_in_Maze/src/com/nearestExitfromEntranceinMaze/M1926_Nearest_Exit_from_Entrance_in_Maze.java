5221dc181523a2bba2f92c908358d50552b73575
Jin Wang <specter01wj@gmail.com>
2024-01-11 12:24:11 -0500

    v1

---
+++ mysol/Cracking Coding Interview/LeetCode 75/13_Graphs - BFS/M1926_Nearest_Exit_from_Entrance_in_Maze/src/com/nearestExitfromEntranceinMaze/M1926_Nearest_Exit_from_Entrance_in_Maze.java
@@ -0,0 +1,10 @@
+package com.nearestExitfromEntranceinMaze;
+
+public class M1926_Nearest_Exit_from_Entrance_in_Maze {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
