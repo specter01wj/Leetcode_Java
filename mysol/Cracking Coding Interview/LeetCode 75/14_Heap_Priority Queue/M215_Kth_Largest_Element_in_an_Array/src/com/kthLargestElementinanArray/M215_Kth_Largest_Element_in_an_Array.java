072ab6484deab8902d4d13e2204fcc36cc0412b0
Jin Wang <specter01wj@gmail.com>
2024-01-12 18:50:00 -0500

    v2

--- mysol/Cracking Coding Interview/LeetCode 75/14_Heap_Priority Queue/M215_Kth_Largest_Element_in_an_Array/src/com/kthLargestElementinanArray/M215_Kth_Largest_Element_in_an_Array.java
+++ mysol/Cracking Coding Interview/LeetCode 75/14_Heap_Priority Queue/M215_Kth_Largest_Element_in_an_Array/src/com/kthLargestElementinanArray/M215_Kth_Largest_Element_in_an_Array.java
@@ -1,10 +1,66 @@
 package com.kthLargestElementinanArray;
+import java.util.*;
+
+/*
+Given an integer array nums and an integer k, return the kth 
+largest element in the array.
+
+Note that it is the kth largest element in the sorted order, 
+not the kth distinct element.
+
+Can you solve it without sorting?
+*/
 
 public class M215_Kth_Largest_Element_in_an_Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {3,2,1,5,6,4};
+		int k = 2;
+		int output = findKthLargest(input, k);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public static int findKthLargest(int[] nums, int k) {
+        return quickSort(nums, 0, nums.length - 1, k);
+    }
+    
+    private static int quickSort(int[] nums, int left, int right, int k) {
+        if (left <= right) {
+            int p = partion(nums, left, right);
+            if (p == nums.length - k) {
+                return nums[p];
+            } else if (p > nums.length - k) {
+                return quickSort(nums, left, p - 1, k);
+            } else {
+                return quickSort(nums, p + 1, right, k);
+            }
+        }
+        return -1;
+    }
+
+    private static int partion(int[] nums, int left, int right) {
+        int i = left + 1, j = right;
+        while (true) {
+            while (i <= right && nums[i] < nums[left]) {
+                i++;
+            }
+            while (j >= left + 1 && nums[j] > nums[left]) {
+                j--;
+            }
+            if (i > j) {
+                break;
+            }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+        int temp = nums[left];
+        nums[left] = nums[j];
+        nums[j] = temp;
+        return j;
+    }
 
 }
