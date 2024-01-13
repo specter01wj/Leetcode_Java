99f80e7d159a40e97eb089793eca8d7d77c14fd1
Jin Wang <specter01wj@gmail.com>
2024-01-12 19:16:13 -0500

    v8

---
+++ mysol/Cracking Coding Interview/LeetCode 75/14_Heap_Priority Queue/M2336_Smallest_Number_in_Infinite_Set/src/com/smallestNumberinInfiniteSet/M2336_Smallest_Number_in_Infinite_Set.java
@@ -0,0 +1,10 @@
+package com.smallestNumberinInfiniteSet;
+
+public class M2336_Smallest_Number_in_Infinite_Set {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
