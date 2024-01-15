33aa27f07c618c4ed87ad7558bbcd138fe400f8e
Jin Wang <specter01wj@gmail.com>
2024-01-14 22:51:16 -0500

    v10

---
+++ mysol/Cracking Coding Interview/LeetCode 75/14_Heap_Priority Queue/M2462_Total_Cost_to_Hire_K_Workers/src/com/totalCosttoHireKWorkers/M2462_Total_Cost_to_Hire_K_Workers.java
@@ -0,0 +1,10 @@
+package com.totalCosttoHireKWorkers;
+
+public class M2462_Total_Cost_to_Hire_K_Workers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
