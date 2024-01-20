b9fb723919f1e1db475825c91c2c7891b12e829c
Jin Wang <specter01wj@gmail.com>
2024-01-20 12:32:25 -0500

    v5

--- mysol/Cracking Coding Interview/LeetCode 75/15_Binary Search/E374_Guess_Number_Higher_or_Lower/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/15_Binary Search/E374_Guess_Number_Higher_or_Lower/ts/code.ts
@@ -1,46 +1,41 @@
-function findKthLargest(nums: number[], k: number): number {
-  return quickSort(nums, 0, nums.length - 1, k);
+function guessNumber(n: number): number {
+  let left = 1;
+  let right = n;
+
+  while (left <= right) {
+      let mid = Math.floor((left + right) / 2);
+      let res = guess(mid);
+
+      if (res === 0) {
+          // Correct guess
+          return mid;
+      } else if (res === -1) {
+          // The number is lower than the guess
+          right = mid - 1;
+      } else {
+          // The number is higher than the guess
+          left = mid + 1;
+      }
+  }
+
+  // Return -1 if the number is not found, although this should not happen in this game.
+  return -1;
 };
 
-function quickSort(nums: number[], left: number, right: number, k: number): number {
-  if (left <= right) {
-      let p = partition(nums, left, right);
-      if (p == nums.length - k) {
-          return nums[p];
-      } else if (p > nums.length - k) {
-          return quickSort(nums, left, p - 1, k);
-      } else {
-          return quickSort(nums, p + 1, right, k);
-      }
-  }
-  return -1; // This line is reached only if the input array is empty
+let pickedNumber: number = 10;
+
+function guess(num: number): number {
+    if (num > pickedNumber) {
+        return -1; // num is higher than the picked number
+    } else if (num < pickedNumber) {
+        return 1; // num is lower than the picked number
+    } else {
+        return 0; // num is equal to the picked number
+    }
 }
 
-function partition(nums: number[], left: number, right: number): number {
-  let pivot = nums[left];
-  let i = left + 1, j = right;
-
-  while (true) {
-      while (i <= right && nums[i] < pivot) {
-          i++;
-      }
-      while (j >= left + 1 && nums[j] > pivot) {
-          j--;
-      }
-      if (i > j) {
-          break;
-      }
-      [nums[i], nums[j]] = [nums[j], nums[i]]; // Swap using destructuring assignment
-      i++;
-      j--;
-  }
-  [nums[left], nums[j]] = [nums[j], nums[left]]; // Swap pivot into correct position
-  return j;
-}
-
-let input1 = [3,2,1,5,6,4];
-let k = 2;
-let output1 = findKthLargest(input1, k);
+let input1 = 100;
+let output1 = guessNumber(input1);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
