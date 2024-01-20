6ab8e02f4c690cd64a1df0634ca943aeffc366f1
Jin Wang <specter01wj@gmail.com>
2024-01-20 13:24:43 -0500

    v8

---
+++ mysol/Cracking Coding Interview/LeetCode 75/15_Binary Search/M2300_Successful_Pairs_of_Spells_and_Potions/src/com/successfulPairsofSpellsandPotions/M2300_Successful_Pairs_of_Spells_and_Potions.java
@@ -0,0 +1,10 @@
+package com.successfulPairsofSpellsandPotions;
+
+public class M2300_Successful_Pairs_of_Spells_and_Potions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
