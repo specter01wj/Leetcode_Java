f03d7233ec3fd40cc4702d13566330e2c2edcc6c
Jin Wang <specter01wj@gmail.com>
2024-01-21 13:45:59 -0500

    v6

--- mysol/Cracking Coding Interview/LeetCode 75/15_Binary Search/M162_Find_Peak_Element/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/15_Binary Search/M162_Find_Peak_Element/ts/code.ts
@@ -1,33 +1,24 @@
-function successfulPairs(spells: number[], potions: number[], success: number): number[] {
-  // Sort the potions array
-  potions.sort((a, b) => a - b);
+function findPeakElement(nums: number[]): number {
+  let left = 0, right = nums.length - 1;
+  
+  while (left < right) {
+      let mid = Math.floor((left + right) / 2);
 
-  const n = spells.length;
-  let pairs = new Array(n).fill(0);
-
-  for (let i = 0; i < n; i++) {
-      let left = 0;
-      let right = potions.length - 1;
-
-      // Apply binary search for each spell
-      while (left <= right) {
-          let mid = Math.floor((left + right) / 2);
-          if (spells[i] * potions[mid] < success) {
-              left = mid + 1;
-          } else {
-              right = mid - 1;
-          }
+      if (nums[mid] > nums[mid + 1]) {
+          // Peak is in the left half or at mid
+          right = mid;
+      } else {
+          // Peak is in the right half
+          left = mid + 1;
       }
-
-      // Count the number of successful pairs for the current spell
-      pairs[i] = potions.length - left;
   }
-
-  return pairs;
+  
+  // When left equals right, we have found the peak element
+  return left;
 };
 
-let spells = [5,1,3], potions = [1,2,3,4,5], success = 7;
-let output1 = successfulPairs(spells, potions, success);
+let input1 = [1,2,1,3,5,6,4];
+let output1 = findPeakElement(input1);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
