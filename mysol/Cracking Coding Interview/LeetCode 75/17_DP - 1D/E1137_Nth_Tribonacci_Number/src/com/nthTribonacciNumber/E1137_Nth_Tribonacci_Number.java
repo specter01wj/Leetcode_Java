e42e2124116594fee753a2c684161f09ab149656
Jin Wang <specter01wj@gmail.com>
2024-01-23 23:59:23 -0500

    v2

--- mysol/Cracking Coding Interview/LeetCode 75/17_DP - 1D/E1137_Nth_Tribonacci_Number/src/com/nthTribonacciNumber/E1137_Nth_Tribonacci_Number.java
+++ mysol/Cracking Coding Interview/LeetCode 75/17_DP - 1D/E1137_Nth_Tribonacci_Number/src/com/nthTribonacciNumber/E1137_Nth_Tribonacci_Number.java
@@ -1,4 +1,13 @@
 package com.nthTribonacciNumber;
+import java.util.*;
+
+/*
+The Tribonacci sequence Tn is defined as follows: 
+
+T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+
+Given n, return the value of Tn.
+*/
 
 public class E1137_Nth_Tribonacci_Number {
 
