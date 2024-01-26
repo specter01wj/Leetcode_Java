0c587966d6ab7082253536a51e0ccf4d38ae5148
Jin Wang <specter01wj@gmail.com>
2024-01-25 23:39:26 -0500

    v12

--- mysol/Cracking Coding Interview/LeetCode 75/17_DP - 1D/M790_Domino_and_Tromino_Tiling/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/17_DP - 1D/M790_Domino_and_Tromino_Tiling/ts/code.ts
@@ -1,18 +1,21 @@
-function rob(nums: number[]): number {
-  if (nums.length === 0) return 0;
-  if (nums.length === 1) return nums[0];
+function numTilings(n: number): number {
+  const MOD: number = 1000000007;
+  if (n <= 2) return n;
 
-  let dp: number[] = [nums[0], Math.max(nums[0], nums[1])];
+  let dp: number[] = new Array(n + 1).fill(0);
+  dp[0] = 1; // base case for an empty board
+  dp[1] = 1; // only one way to tile a 2x1 board
+  dp[2] = 2; // two ways to tile a 2x2 board
 
-  for (let i = 2; i < nums.length; i++) {
-      dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+  for (let i = 3; i <= n; i++) {
+      dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD;
   }
 
-  return dp[nums.length - 1];
+  return dp[n];
 };
 
-let input1 = [2,7,9,3,1];
-let output1 = rob(input1);
+let input1 = 3;
+let output1 = numTilings(input1);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
