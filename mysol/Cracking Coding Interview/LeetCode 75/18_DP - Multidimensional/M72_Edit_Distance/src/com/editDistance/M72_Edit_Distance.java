eac4060f004786ada4a38a4a227dc45da672401e
Jin Wang <specter01wj@gmail.com>
2024-01-28 20:29:14 -0500

    v11

--- mysol/Cracking Coding Interview/LeetCode 75/18_DP - Multidimensional/M72_Edit_Distance/src/com/editDistance/M72_Edit_Distance.java
+++ mysol/Cracking Coding Interview/LeetCode 75/18_DP - Multidimensional/M72_Edit_Distance/src/com/editDistance/M72_Edit_Distance.java
@@ -1,10 +1,53 @@
 package com.editDistance;
+import java.util.*;
+
+/*
+Given two strings word1 and word2, return the minimum number of 
+operations required to convert word1 to word2.
+
+You have the following three operations permitted on a word:
+
+ .Insert a character
+ .Delete a character
+ .Replace a character
+*/
 
 public class M72_Edit_Distance {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String word1 = "horse", word2 = "ros";
+		int output = minDistance(word1, word2);
+        System.out.println("input: " + (word1) + "\noutput: " + (output));
 	}
+	
+	
+	public static int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Initialize the first row and column
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        // Compute the distances
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1]; // No operation needed
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], // Replace
+                                    Math.min(dp[i - 1][j],   // Delete
+                                            dp[i][j - 1])); // Insert
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
 
 }
