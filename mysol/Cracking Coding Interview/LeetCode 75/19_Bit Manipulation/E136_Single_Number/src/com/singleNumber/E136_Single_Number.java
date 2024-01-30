4531610dffbfccae179bc63b48d70a5ef5d3fedb
Jin Wang <specter01wj@gmail.com>
2024-01-30 02:12:00 -0500

    v8

--- mysol/Cracking Coding Interview/LeetCode 75/19_Bit Manipulation/E136_Single_Number/src/com/singleNumber/E136_Single_Number.java
+++ mysol/Cracking Coding Interview/LeetCode 75/19_Bit Manipulation/E136_Single_Number/src/com/singleNumber/E136_Single_Number.java
@@ -1,10 +1,31 @@
 package com.singleNumber;
+import java.util.*;
+
+/*
+Given a non-empty array of integers nums, every element appears 
+twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity 
+and use only constant extra space.
+*/
 
 public class E136_Single_Number {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {4,1,2,1,2};
+		int output = singleNumber(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public static int singleNumber(int[] nums) {
+        int single = 0;
+
+        for (int num : nums) {
+            single ^= num;
+        }
+
+        return single;
+    }
 
 }
