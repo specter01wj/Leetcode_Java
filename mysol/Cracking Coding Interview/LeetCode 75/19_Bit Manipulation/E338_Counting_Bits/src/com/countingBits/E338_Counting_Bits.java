63ffea966a8ebe3d62712e0c02853133fab89a69
Jin Wang <specter01wj@gmail.com>
2024-01-30 01:39:38 -0500

    v2

--- mysol/Cracking Coding Interview/LeetCode 75/19_Bit Manipulation/E338_Counting_Bits/src/com/countingBits/E338_Counting_Bits.java
+++ mysol/Cracking Coding Interview/LeetCode 75/19_Bit Manipulation/E338_Counting_Bits/src/com/countingBits/E338_Counting_Bits.java
@@ -1,10 +1,29 @@
 package com.countingBits;
+import java.util.*;
+
+/*
+Given an integer n, return an array ans of length n + 1 such that 
+for each i (0 <= i <= n), ans[i] is the number of 1's in the binary 
+representation of i.
+*/
 
 public class E338_Counting_Bits {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int input = 5;
+		int[] output = countBits(input);
+        System.out.println("input: " + (input) + "\noutput: " + Arrays.toString(output));
 	}
+	
+	
+	public static int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        
+        for (int i = 0; i <= n; i++) {
+            ans[i] = Integer.bitCount(i);
+        }
+
+        return ans;
+    }
 
 }
