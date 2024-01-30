891d3dbe4e5563625c13fff85eebf5cae27f7220
Jin Wang <specter01wj@gmail.com>
2024-01-30 02:01:52 -0500

    v7

--- mysol/Cracking Coding Interview/LeetCode 75/19_Bit Manipulation/E338_Counting_Bits/ts/code.ts
+++ mysol/Cracking Coding Interview/LeetCode 75/19_Bit Manipulation/E338_Counting_Bits/ts/code.ts
@@ -1,21 +1,21 @@
-function maxProfit(prices: number[], fee: number): number {
-  if (!prices || prices.length === 0) {
-      return 0;
+function countBits(n: number): number[] {
+  const ans: number[] = new Array(n + 1);
+
+  for (let i = 0; i <= n; i++) {
+      ans[i] = countOnes(i);
   }
 
-  let cash: number = 0;  // Maximum profit we can have if we do not hold a stock
-  let hold: number = -prices[0];  // Maximum profit we can have if we hold a stock
+  return ans;
 
-  for (let i = 1; i < prices.length; i++) {
-      cash = Math.max(cash, hold + prices[i] - fee);  // Sell the stock
-      hold = Math.max(hold, cash - prices[i]);  // Buy the stock
-  }
-
-  return cash;
+  
 };
 
-let input1 = [1,3,2,8,4,9], fee = 2;
-let output1 = maxProfit(input1, fee);
+function countOnes(num: number): number {
+    return num.toString(2).split('1').length - 1;
+}
+
+let input1 = 5;
+let output1 = countBits(input1);
 
 let webHeading1 = document.querySelector('#t1');
 webHeading1.textContent = 'Output: ' + JSON.stringify(output1);
