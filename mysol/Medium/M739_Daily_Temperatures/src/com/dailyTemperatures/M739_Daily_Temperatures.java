2f509f325db09d263c2f9da4c5755cd0dad380fc
Jin Wang <specter01wj@gmail.com>
2024-02-03 01:18:42 -0500

    v18

---
+++ mysol/Medium/M739_Daily_Temperatures/src/com/dailyTemperatures/M739_Daily_Temperatures.java
@@ -0,0 +1,43 @@
+package com.dailyTemperatures;
+import java.util.*;
+
+/*
+Given an array of integers temperatures represents the daily temperatures, 
+return an array answer such that answer[i] is the number of days you have 
+to wait after the ith day to get a warmer temperature. If there is no future 
+day for which this is possible, keep answer[i] == 0 instead.
+*/
+
+public class M739_Daily_Temperatures {
+
+	public static void main(String[] args) {
+		int[] input = {73,74,75,71,69,72,76,73};
+		int[] output = dailyTemperatures(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + Arrays.toString(output));
+	}
+	
+	/*
+    solution:
+    Monotonic Stack
+    */
+	
+	/*
+     * @param temperatures: a list of integers
+     * @return: an array answer
+     */
+	public static int[] dailyTemperatures(int[] temperatures) {
+        int[] answer = new int[temperatures.length];
+        java.util.Stack<Integer> stack = new java.util.Stack<>();
+
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int index = stack.pop();
+                answer[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        return answer;
+    }
+
+}
---
+++ mysol/Medium/M901_Online_Stock_Span/src/com/onlineStockSpan/M901_Online_Stock_Span.java
@@ -0,0 +1,69 @@
+package com.onlineStockSpan;
+import java.util.*;
+
+/*
+Design an algorithm that collects daily price quotes for some stock 
+and returns the span of that stock's price for the current day.
+
+The span of the stock's price in one day is the maximum number of 
+consecutive days (starting from that day and going backward) for 
+which the stock price was less than or equal to the price of that day.
+
+ .For example, if the prices of the stock in the last four days is [7,2,1,2] 
+  and the price of the stock today is 2, then the span of today is 4 because 
+  starting from today, the price of the stock was less than or equal 2 for 4 
+  consecutive days.
+ .Also, if the prices of the stock in the last four days is [7,34,1,2] and the 
+  price of the stock today is 8, then the span of today is 3 because starting 
+  from today, the price of the stock was less than or equal 8 for 3 consecutive days.
+
+Implement the StockSpanner class:
+
+ .StockSpanner() Initializes the object of the class.
+ .int next(int price) Returns the span of the stock's price given that today's 
+  price is price.
+*/
+
+public class M901_Online_Stock_Span {
+
+	public static void main(String[] args) {
+		StockSpanner stockSpanner = new StockSpanner();
+        System.out.println(stockSpanner.next(100)); // Output: 1
+        System.out.println(stockSpanner.next(80));  // Output: 1
+        System.out.println(stockSpanner.next(60));  // Output: 1
+        System.out.println(stockSpanner.next(70));  // Output: 2
+        System.out.println(stockSpanner.next(60));  // Output: 1
+        System.out.println(stockSpanner.next(75));  // Output: 4
+        System.out.println(stockSpanner.next(85));  // Output: 6
+	}
+	
+	/*
+    solution:
+    Monotonic Stack
+    */
+	
+	/*
+     * @param StockSpanner: StockSpanner
+     * @return: the span of that stock's price for the current day
+     */
+	static class StockSpanner {
+	    private Stack<int[]> stack;
+
+	    public StockSpanner() {
+	        stack = new Stack<>();
+	    }
+	    
+	    public int next(int price) {
+	        int span = 1; // The span of the current price is at least 1
+	        // While the stack is not empty and the current price is greater than or equal to
+	        // the price at the top of the stack, pop the stack and add the span
+	        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+	            span += stack.pop()[1];
+	        }
+	        // Push the current price and its calculated span onto the stack
+	        stack.push(new int[]{price, span});
+	        return span;
+	    }
+	}
+
+}
