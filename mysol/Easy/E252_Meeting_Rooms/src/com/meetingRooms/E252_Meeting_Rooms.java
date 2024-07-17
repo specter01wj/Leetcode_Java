e643ba2ea923ac04ac53f900c93e4bf96bbdb79e
Jin Wang <specter01wj@gmail.com>
2024-07-17 00:47:50 -0400

    v6

---
+++ mysol/Easy/E252_Meeting_Rooms/src/com/meetingRooms/E252_Meeting_Rooms.java
@@ -0,0 +1,42 @@
+package com.meetingRooms;
+import java.util.*;
+
+/*
+Given an array of meeting time intervals where 
+intervals[i] = [starti, endi], determine if a person 
+could attend all meetings.
+*/
+
+public class E252_Meeting_Rooms {
+
+	public static void main(String[] args) {
+		int[][] input = { {7, 10}, {2, 4} };
+		boolean output = canAttendMeetings(input);
+        System.out.println("input: " + Arrays.deepToString(input) + "\noutput: " + (output));
+	}
+	
+	
+	/*
+    solution:
+    Sorting
+    */
+	
+	/*
+     * @param intervals: a list of 2D integers
+     * @return: determine if a person could attend all meetings
+     */
+	public static boolean canAttendMeetings(int[][] intervals) {
+		// Sort the intervals by start time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        // Check for any overlapping intervals
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < intervals[i - 1][1]) {
+                return false; // Found overlapping intervals
+            }
+        }
+        
+        return true; // No overlapping intervals found
+    }
+
+}
---
+++ mysol/Easy/E252_Meeting_Rooms/ts/code.js
@@ -0,0 +1,15 @@
+function canAttendMeetings(intervals) {
+    // Sort the intervals by start time
+    intervals.sort((a, b) => a[0] - b[0]);
+    // Check for any overlapping intervals
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i][0] < intervals[i - 1][1]) {
+            return false; // Found overlapping intervals
+        }
+    }
+    return true; // No overlapping intervals found
+}
+let nums = [[7, 10], [2, 4]];
+let output = canAttendMeetings(nums);
+let webHeading = document.querySelector('#t1');
+webHeading.textContent = 'Output: ' + output.toString();
