5ba5b889b39aa34157ac93e2f633c6101bbd46f8
Jin Wang <specter01wj@gmail.com>
2024-07-19 11:41:36 -0400

    v1

---
+++ mysol/Category/Array/E346_Moving_Average_from_Data_Stream/src/com/movingAveragefromDataStream/E346_Moving_Average_from_Data_Stream.java
@@ -0,0 +1,10 @@
+package com.movingAveragefromDataStream;
+
+public class E346_Moving_Average_from_Data_Stream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
