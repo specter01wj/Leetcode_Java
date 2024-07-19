9953db73d3055f94e294cb957aedfe78b9382bc9
Jin Wang <specter01wj@gmail.com>
2024-07-19 12:29:00 -0400

    v6

---
+++ mysol/Category/Array/E346_Moving_Average_from_Data_Stream/ts/code.js
@@ -0,0 +1,31 @@
+class MovingAverage {
+    constructor(size) {
+        this.size = size;
+        this.queue = [];
+        this.sum = 0;
+    }
+    next(val) {
+        if (this.queue.length === this.size) {
+            this.sum -= this.queue.shift();
+        }
+        this.queue.push(val);
+        this.sum += val;
+        return this.sum / this.queue.length;
+    }
+    toString() {
+        const obj = {
+            size: this.size,
+            queue: this.queue,
+            sum: this.sum
+        };
+        return JSON.stringify(obj);
+    }
+}
+const obj = new MovingAverage(3);
+obj.next(1); // return 1.0
+obj.next(10); // return 5.5
+obj.next(3); // return 4.66667
+obj.next(5); // return 6.0
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Output: ' + obj.toString() +
+    '<br>Result = ' + obj.next(1) + ', ' + (obj.next(10)) + ', ' + obj.next(3) + ', ' + obj.next(5);
