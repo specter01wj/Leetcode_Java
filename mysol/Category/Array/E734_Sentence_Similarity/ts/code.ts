1b4191f1f054df1a45d0bf8a7c8977b62f7db5b6
Jin Wang <specter01wj@gmail.com>
2024-07-22 13:06:14 -0400

    v10

---
+++ mysol/Category/Array/E734_Sentence_Similarity/ts/code.ts
@@ -0,0 +1,60 @@
+
+class StringIterator {
+  private compressedString: string;
+  private index: number;
+  private count: number;
+  private currentChar: string;
+
+  constructor(compressedString: string) {
+      this.compressedString = compressedString;
+      this.index = 0;
+      this.count = 0;
+      this.currentChar = ' ';
+
+      // Initialize the first character and its count
+      if (this.compressedString.length > 0) {
+          this.currentChar = this.compressedString.charAt(this.index++);
+          while (this.index < this.compressedString.length && !isNaN(parseInt(this.compressedString.charAt(this.index)))) {
+              this.count = this.count * 10 + parseInt(this.compressedString.charAt(this.index++));
+          }
+      }
+  }
+
+  next(): string {
+      if (this.count === 0) {
+          if (this.index >= this.compressedString.length) {
+              return ' ';
+          }
+          this.currentChar = this.compressedString.charAt(this.index++);
+          this.count = 0;
+          while (this.index < this.compressedString.length && !isNaN(parseInt(this.compressedString.charAt(this.index)))) {
+              this.count = this.count * 10 + parseInt(this.compressedString.charAt(this.index++));
+          }
+      }
+      this.count--;
+      return this.currentChar;
+  }
+
+  hasNext(): boolean {
+      return this.count > 0 || this.index < this.compressedString.length;
+  }
+}
+
+let input = "L1e2t1C1o1d1e1";
+const obj = new StringIterator(input);
+let results: string[] = [];
+results.push('next: ' + obj.next()); // Output: "L"
+results.push('next: ' + obj.next()); // Output: "e"
+results.push('next: ' + obj.next()); // Output: "e"
+results.push('next: ' + obj.next()); // Output: "t"
+results.push('next: ' + obj.next()); // Output: "C"
+results.push('next: ' + obj.next()); // Output: "o"
+results.push('hasNext: ' + obj.hasNext()); // Output: true
+results.push('next: ' + obj.next()); // Output: "d"
+results.push('hasNext: ' + obj.hasNext()); // Output: true
+results.push('next: ' + obj.next()); // Output: "e"
+results.push('hasNext: ' + obj.hasNext()); // Output: false
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + input + '<br>Result = ' + results;
+
