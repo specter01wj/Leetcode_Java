ca46917fd0a0833f331d1ede2cc2ff45da2fd632
Jin Wang <specter01wj@gmail.com>
2024-08-02 01:28:33 -0400

    v10

---
+++ mysol/Category/Array/E1085_Sum_of_Digits_in_the_Minimum_Number/src/com/sumofDigitsintheMinimumNumber/E1085_Sum_of_Digits_in_the_Minimum_Number.java
@@ -0,0 +1,10 @@
+package com.sumofDigitsintheMinimumNumber;
+
+public class E1085_Sum_of_Digits_in_the_Minimum_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
