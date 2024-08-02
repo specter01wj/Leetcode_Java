026bda2839d065cf37e36d32f6fee2e257a336be
Jin Wang <specter01wj@gmail.com>
2024-08-02 01:32:36 -0400

    v12

--- mysol/Category/Array/E1085_Sum_of_Digits_in_the_Minimum_Number/src/com/sumofDigitsintheMinimumNumber/E1085_Sum_of_Digits_in_the_Minimum_Number.java
+++ mysol/Category/Array/E1085_Sum_of_Digits_in_the_Minimum_Number/src/com/sumofDigitsintheMinimumNumber/E1085_Sum_of_Digits_in_the_Minimum_Number.java
@@ -15,7 +15,23 @@ public class E1085_Sum_of_Digits_in_the_Minimum_Number {
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    1. Find the Minimum Element:
+		. Initialize minElement to the first element in the array.
+		. Iterate through the array and update minElement if a smaller element is found.
+	2. Sum the Digits of the Minimum Element:
+		. Use a while loop to repeatedly extract the last digit of minElement using the modulus operator (%) and add it to sumOfDigits.
+		. Remove the last digit by dividing minElement by 10.
+	3. Check if the Sum is Odd or Even:
+		. Use the modulus operator to determine if sumOfDigits is odd or even.
+		. Return 1 if sumOfDigits is even, otherwise return 0.
+    */
 	
+	/*
+     * @param nums: a list of integers
+     * @return: 0 if the sum of the digits of the minimum integer in nums is odd, or 1 otherwise
+     */
 	public int sumOfDigits(int[] nums) {
         // Step 1: Find the minimum element in the array
         int minElement = nums[0];
