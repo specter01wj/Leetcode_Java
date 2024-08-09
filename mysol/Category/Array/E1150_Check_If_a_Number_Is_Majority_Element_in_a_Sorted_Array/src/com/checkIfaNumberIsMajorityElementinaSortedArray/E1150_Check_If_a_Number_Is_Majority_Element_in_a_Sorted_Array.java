f60ec301e87970d2a7421986a4aeb73effb91c1d
Jin Wang <specter01wj@gmail.com>
2024-08-09 12:04:06 -0400

    v1

---
+++ mysol/Category/Array/E1150_Check_If_a_Number_Is_Majority_Element_in_a_Sorted_Array/src/com/checkIfaNumberIsMajorityElementinaSortedArray/E1150_Check_If_a_Number_Is_Majority_Element_in_a_Sorted_Array.java
@@ -0,0 +1,10 @@
+package com.checkIfaNumberIsMajorityElementinaSortedArray;
+
+public class E1150_Check_If_a_Number_Is_Majority_Element_in_a_Sorted_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
