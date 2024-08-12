1477cb95c2fdb00f6db63f6471b495d458e90e47
Jin Wang <specter01wj@gmail.com>
2024-08-12 12:22:35 -0400

    v1

---
+++ mysol/Category/Array/E1196_How_Many_Apples_Can_You_Put_into_the_Basket/src/com/howManyApplesCanYouPutintotheBasket/E1196_How_Many_Apples_Can_You_Put_into_the_Basket.java
@@ -0,0 +1,10 @@
+package com.howManyApplesCanYouPutintotheBasket;
+
+public class E1196_How_Many_Apples_Can_You_Put_into_the_Basket {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
