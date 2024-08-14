b6f85d424381c62f5e7fb3723e6a6e600f4eafdd
Jin Wang <specter01wj@gmail.com>
2024-08-14 12:25:56 -0400

    v1

---
+++ mysol/Category/Array/E1228_Missing_Number_In_Arithmetic_Progression/src/com/missingNumberInArithmeticProgression/E1228_Missing_Number_In_Arithmetic_Progression.java
@@ -0,0 +1,10 @@
+package com.missingNumberInArithmeticProgression;
+
+public class E1228_Missing_Number_In_Arithmetic_Progression {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
