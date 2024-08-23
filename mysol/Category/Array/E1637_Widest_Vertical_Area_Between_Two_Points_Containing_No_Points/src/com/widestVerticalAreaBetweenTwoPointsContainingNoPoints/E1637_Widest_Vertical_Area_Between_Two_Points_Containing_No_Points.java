640b399060963f719a06f67a63c946c9179c7483
Jin Wang <specter01wj@gmail.com>
2024-08-23 14:19:22 -0400

    v1

---
+++ mysol/Category/Array/E1637_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points/src/com/widestVerticalAreaBetweenTwoPointsContainingNoPoints/E1637_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points.java
@@ -0,0 +1,10 @@
+package com.widestVerticalAreaBetweenTwoPointsContainingNoPoints;
+
+public class E1637_Widest_Vertical_Area_Between_Two_Points_Containing_No_Points {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
