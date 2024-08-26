0011f0a61a8b1685742a3fff60e76349d5578c68
Jin Wang <specter01wj@gmail.com>
2024-08-26 15:17:43 -0400

    v1

---
+++ mysol/Category/Array/E2229_Check_if_an_Array_Is_Consecutive/src/com/checkifanArrayIsConsecutive/E2229_Check_if_an_Array_Is_Consecutive.java
@@ -0,0 +1,10 @@
+package com.checkifanArrayIsConsecutive;
+
+public class E2229_Check_if_an_Array_Is_Consecutive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
