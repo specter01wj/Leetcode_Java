32f789aeadd8b13ee81d742a7e7b439e90b8c6e4
Jin Wang <specter01wj@gmail.com>
2024-08-28 15:02:58 -0400

    v7

--- mysol/Category/Array/E2404_Most_Frequent_Even_Element/ts/code.ts
+++ mysol/Category/Array/E2404_Most_Frequent_Even_Element/ts/code.ts
@@ -1,32 +1,38 @@
 
-function checkDistances(s: string, distance: number[]): boolean {
-  // Array to store the first occurrence of each letter
-  const firstOccurrence: number[] = Array(26).fill(-1);
+function mostFrequentEven(nums: number[]): number {
+  // Map to store the frequency of even numbers
+  const frequencyMap: { [key: number]: number } = {};
 
-  // Loop through the string
-  for (let i = 0; i < s.length; i++) {
-      const index: number = s.charCodeAt(i) - 'a'.charCodeAt(0); // Calculate the index of the letter in the alphabet
-
-      if (firstOccurrence[index] === -1) {
-          // If this is the first occurrence of the letter, store the index
-          firstOccurrence[index] = i;
-      } else {
-          // If this is the second occurrence, calculate the distance
-          const actualDistance: number = i - firstOccurrence[index] - 1;
-          if (actualDistance !== distance[index]) {
-              // If the distance doesn't match the expected distance, return false
-              return false;
-          }
+  // Traverse through the array and count frequencies of even numbers
+  for (let num of nums) {
+      if (num % 2 === 0) {
+          frequencyMap[num] = (frequencyMap[num] || 0) + 1;
       }
   }
 
-  // If all letters satisfy the distance requirement, return true
-  return true;
+  // Variables to track the most frequent even number and its frequency
+  let mostFrequentEven = -1;
+  let maxFrequency = 0;
+
+  // Traverse through the map to find the most frequent even number
+  for (let num in frequencyMap) {
+      const freq = frequencyMap[num];
+
+      // Convert num to a number before comparison
+      const numValue = parseInt(num);
+
+      // Update the most frequent even number based on frequency and value
+      if (freq > maxFrequency || (freq === maxFrequency && numValue < mostFrequentEven)) {
+          mostFrequentEven = numValue;
+          maxFrequency = freq;
+      }
+  }
+
+  return mostFrequentEven;
 };
 
-const input: string = "abaccb";
-const distance: number[] = [1,3,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-const results = checkDistances(input, distance);
+const input: number[] = [0,1,2,2,4,4,1];
+const results = mostFrequentEven(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + input + '<br>Result = ' + results;
