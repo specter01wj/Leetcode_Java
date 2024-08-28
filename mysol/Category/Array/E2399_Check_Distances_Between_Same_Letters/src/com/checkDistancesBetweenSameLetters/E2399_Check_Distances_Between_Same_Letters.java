9ee75520e793cb524a4e39efbaa8efbb6584139d
Jin Wang <specter01wj@gmail.com>
2024-08-28 10:27:07 -0400

    v1

---
+++ mysol/Category/Array/E2399_Check_Distances_Between_Same_Letters/src/com/checkDistancesBetweenSameLetters/E2399_Check_Distances_Between_Same_Letters.java
@@ -0,0 +1,10 @@
+package com.checkDistancesBetweenSameLetters;
+
+public class E2399_Check_Distances_Between_Same_Letters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
