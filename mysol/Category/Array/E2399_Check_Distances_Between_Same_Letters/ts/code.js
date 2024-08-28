9b9ef0494599b98b8e7e9ab58f93393736f87eb2
Jin Wang <specter01wj@gmail.com>
2024-08-28 14:40:49 -0400

    v1

---
+++ mysol/Category/Array/E2399_Check_Distances_Between_Same_Letters/ts/code.js
@@ -0,0 +1,28 @@
+function checkDistances(s, distance) {
+    // Array to store the first occurrence of each letter
+    const firstOccurrence = Array(26).fill(-1);
+    // Loop through the string
+    for (let i = 0; i < s.length; i++) {
+        const index = s.charCodeAt(i) - 'a'.charCodeAt(0); // Calculate the index of the letter in the alphabet
+        if (firstOccurrence[index] === -1) {
+            // If this is the first occurrence of the letter, store the index
+            firstOccurrence[index] = i;
+        }
+        else {
+            // If this is the second occurrence, calculate the distance
+            const actualDistance = i - firstOccurrence[index] - 1;
+            if (actualDistance !== distance[index]) {
+                // If the distance doesn't match the expected distance, return false
+                return false;
+            }
+        }
+    }
+    // If all letters satisfy the distance requirement, return true
+    return true;
+}
+;
+const input = "abaccb";
+const distance = [1, 3, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+const results = checkDistances(input, distance);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + input + '<br>Result = ' + results;
---
+++ mysol/Category/Array/E2404_Most_Frequent_Even_Element/src/com/mostFrequentEvenElement/E2404_Most_Frequent_Even_Element.java
@@ -0,0 +1,10 @@
+package com.mostFrequentEvenElement;
+
+public class E2404_Most_Frequent_Even_Element {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
