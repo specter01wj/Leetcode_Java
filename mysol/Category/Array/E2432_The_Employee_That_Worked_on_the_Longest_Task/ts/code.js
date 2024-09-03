7ae6086da55a66d02a6535d7eb00bded8af34eb7
Jin Wang <specter01wj@gmail.com>
2024-09-03 19:41:59 -0400

    v7

---
+++ mysol/Category/Array/E2432_The_Employee_That_Worked_on_the_Longest_Task/ts/code.js
@@ -0,0 +1,25 @@
+function hardestWorker(n, logs) {
+    let maxDuration = 0;
+    let employeeId = Number.MAX_VALUE;
+    let previousEndTime = 0;
+    for (let i = 0; i < logs.length; i++) {
+        const id = logs[i][0];
+        const leaveTime = logs[i][1];
+        const duration = leaveTime - previousEndTime;
+        if (duration > maxDuration) {
+            maxDuration = duration;
+            employeeId = id;
+        }
+        else if (duration === maxDuration && id < employeeId) {
+            employeeId = id;
+        }
+        previousEndTime = leaveTime;
+    }
+    return employeeId;
+}
+;
+const input = [[0, 3], [2, 5], [0, 9], [1, 15]];
+const n = 10;
+const results = hardestWorker(n, input);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + input + '<br>Result = ' + results;
