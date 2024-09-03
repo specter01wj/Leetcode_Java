590286b390a96955e4fa36f4c98e1fdf06b26ced
Jin Wang <specter01wj@gmail.com>
2024-09-03 19:29:45 -0400

    v1

---
+++ mysol/Category/Array/E2432_The_Employee_That_Worked_on_the_Longest_Task/src/com/theEmployeeThatWorkedontheLongestTask/E2432_The_Employee_That_Worked_on_the_Longest_Task.java
@@ -0,0 +1,10 @@
+package com.theEmployeeThatWorkedontheLongestTask;
+
+public class E2432_The_Employee_That_Worked_on_the_Longest_Task {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
