89bef26c8982d29f5b472cd31132cdebd6714dd2
Jin Wang <specter01wj@gmail.com>
2024-09-05 12:59:26 -0400

    v1

---
+++ mysol/Category/Array/E2446_Determine_if_Two_Events_Have_Conflict/src/com/determineifTwoEventsHaveConflict/E2446_Determine_if_Two_Events_Have_Conflict.java
@@ -0,0 +1,10 @@
+package com.determineifTwoEventsHaveConflict;
+
+public class E2446_Determine_if_Two_Events_Have_Conflict {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
