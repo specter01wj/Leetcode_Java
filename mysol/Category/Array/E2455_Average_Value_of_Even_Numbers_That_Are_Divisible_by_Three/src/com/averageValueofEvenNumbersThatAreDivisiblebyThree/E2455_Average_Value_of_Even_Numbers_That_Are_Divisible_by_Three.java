8fb7a86b87229b2a1aeaf5e88879e447f1af3e3f
Jin Wang <specter01wj@gmail.com>
2024-09-11 00:59:37 -0400

    v1

---
+++ mysol/Category/Array/E2455_Average_Value_of_Even_Numbers_That_Are_Divisible_by_Three/src/com/averageValueofEvenNumbersThatAreDivisiblebyThree/E2455_Average_Value_of_Even_Numbers_That_Are_Divisible_by_Three.java
@@ -0,0 +1,10 @@
+package com.averageValueofEvenNumbersThatAreDivisiblebyThree;
+
+public class E2455_Average_Value_of_Even_Numbers_That_Are_Divisible_by_Three {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
