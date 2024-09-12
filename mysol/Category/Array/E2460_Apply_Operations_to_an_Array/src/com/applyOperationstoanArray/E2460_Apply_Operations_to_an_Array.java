6c0cef906467195674205a635edc04a41b517929
Jin Wang <specter01wj@gmail.com>
2024-09-12 11:52:16 -0400

    v1

---
+++ mysol/Category/Array/E2460_Apply_Operations_to_an_Array/src/com/applyOperationstoanArray/E2460_Apply_Operations_to_an_Array.java
@@ -0,0 +1,10 @@
+package com.applyOperationstoanArray;
+
+public class E2460_Apply_Operations_to_an_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
