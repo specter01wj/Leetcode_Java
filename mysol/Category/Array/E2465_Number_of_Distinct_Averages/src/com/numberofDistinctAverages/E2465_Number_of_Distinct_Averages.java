c5dc1a567a4aa1c07af712fdfb7bd55229330bfe
Jin Wang <specter01wj@gmail.com>
2024-09-12 19:23:41 -0400

    v2

--- mysol/Category/Array/E2465_Number_of_Distinct_Averages/src/com/numberofDistinctAverages/E2465_Number_of_Distinct_Averages.java
+++ mysol/Category/Array/E2465_Number_of_Distinct_Averages/src/com/numberofDistinctAverages/E2465_Number_of_Distinct_Averages.java
@@ -1,10 +1,50 @@
 package com.numberofDistinctAverages;
+import java.util.*;
+
+/*
+You are given a 0-indexed integer array nums of even length.
+
+As long as nums is not empty, you must repetitively:
+
+	. Find the minimum number in nums and remove it.
+	. Find the maximum number in nums and remove it.
+	. Calculate the average of the two removed numbers.
+
+The average of two numbers a and b is (a + b) / 2.
+
+	. For example, the average of 2 and 3 is (2 + 3) / 2 = 2.5.
+
+Return the number of distinct averages calculated using the above process.
+*/
 
 public class E2465_Number_of_Distinct_Averages {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E2465_Number_of_Distinct_Averages solution = new E2465_Number_of_Distinct_Averages();
+		int[] input = {4,1,4,0,3,5};
+		int output = solution.distinctAverages(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public int distinctAverages(int[] nums) {
+        // Sort the array to easily find the minimum and maximum
+        Arrays.sort(nums);
+        Set<Double> distinctAverages = new HashSet<>();
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        // Calculate the average of the minimum and maximum numbers, then remove them
+        while (left < right) {
+            double average = (nums[left] + nums[right]) / 2.0;
+            distinctAverages.add(average);
+            left++;
+            right--;
+        }
+
+        // The size of the set represents the number of distinct averages
+        return distinctAverages.size();
+    }
 
 }
