876975d6852898fd0c3e80cb31149eeb43813902
Jin Wang <specter01wj@gmail.com>
2024-09-12 19:16:12 -0400

    v1

---
+++ mysol/Category/Array/E2465_Number_of_Distinct_Averages/src/com/numberofDistinctAverages/E2465_Number_of_Distinct_Averages.java
@@ -0,0 +1,10 @@
+package com.numberofDistinctAverages;
+
+public class E2465_Number_of_Distinct_Averages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
