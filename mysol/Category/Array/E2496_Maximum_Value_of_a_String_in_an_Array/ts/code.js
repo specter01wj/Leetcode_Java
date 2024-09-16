e358a0a5b2e130fa0a5fa414123bacff2aac5b7b
Jin Wang <specter01wj@gmail.com>
2024-09-16 13:05:58 -0400

    v8

---
+++ mysol/Category/Array/E2496_Maximum_Value_of_a_String_in_an_Array/ts/code.js
@@ -0,0 +1,23 @@
+function maximumValue(strs) {
+    let maxValue = 0;
+    for (let str of strs) {
+        let currentValue;
+        // Check if the string is composed only of digits
+        if (/^\d+$/.test(str)) {
+            // Convert string to its numeric value
+            currentValue = parseInt(str, 10);
+        }
+        else {
+            // Otherwise, the value is the length of the string
+            currentValue = str.length;
+        }
+        // Update maxValue if the current value is greater
+        maxValue = Math.max(maxValue, currentValue);
+    }
+    return maxValue;
+}
+;
+const input = ["alic3", "bob", "3", "4", "00000"];
+const results = maximumValue(input);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + input + '<br>Result = ' + results;
