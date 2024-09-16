0e6a21494d6d9c341c3f8ac8c3cf274e3e7b55be
Jin Wang <specter01wj@gmail.com>
2024-09-16 11:58:34 -0400

    v1

---
+++ mysol/Category/Array/E2475_Number_of_Unequal_Triplets_in_Array/src/com/numberofUnequalTripletsinArray/E2475_Number_of_Unequal_Triplets_in_Array.java
@@ -0,0 +1,10 @@
+package com.numberofUnequalTripletsinArray;
+
+public class E2475_Number_of_Unequal_Triplets_in_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
