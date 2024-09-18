c4d74681760c2c55050b36a2593ce46e36eb3985
Jin Wang <specter01wj@gmail.com>
2024-09-18 19:11:08 -0400

    v1

---
+++ mysol/Category/Array/E2506_Count_Pairs_Of_Similar_Strings/src/com/countPairsOfSimilarStrings/E2506_Count_Pairs_Of_Similar_Strings.java
@@ -0,0 +1,10 @@
+package com.countPairsOfSimilarStrings;
+
+public class E2506_Count_Pairs_Of_Similar_Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
