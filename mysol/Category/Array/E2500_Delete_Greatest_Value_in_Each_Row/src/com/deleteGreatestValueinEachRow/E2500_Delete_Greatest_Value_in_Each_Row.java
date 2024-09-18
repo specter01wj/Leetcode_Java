d268e8f363ee3a6b9dcf691926ee1e52b11b0ea9
Jin Wang <specter01wj@gmail.com>
2024-09-18 12:40:35 -0400

    v1

---
+++ mysol/Category/Array/E2500_Delete_Greatest_Value_in_Each_Row/src/com/deleteGreatestValueinEachRow/E2500_Delete_Greatest_Value_in_Each_Row.java
@@ -0,0 +1,10 @@
+package com.deleteGreatestValueinEachRow;
+
+public class E2500_Delete_Greatest_Value_in_Each_Row {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
