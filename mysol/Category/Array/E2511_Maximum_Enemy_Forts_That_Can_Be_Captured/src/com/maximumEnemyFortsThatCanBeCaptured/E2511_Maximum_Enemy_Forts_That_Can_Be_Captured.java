bb395296e2c84e4c7d1ce1afc4c3dd507c256bfa
Jin Wang <specter01wj@gmail.com>
2024-09-23 12:20:52 -0400

    v1

---
+++ mysol/Category/Array/E2511_Maximum_Enemy_Forts_That_Can_Be_Captured/src/com/maximumEnemyFortsThatCanBeCaptured/E2511_Maximum_Enemy_Forts_That_Can_Be_Captured.java
@@ -0,0 +1,10 @@
+package com.maximumEnemyFortsThatCanBeCaptured;
+
+public class E2511_Maximum_Enemy_Forts_That_Can_Be_Captured {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
