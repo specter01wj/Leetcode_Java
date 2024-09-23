e96a187033ba4ba8f819f1a7ed456b7b8423fa0f
Jin Wang <specter01wj@gmail.com>
2024-09-23 13:47:53 -0400

    v5

--- mysol/Category/Array/E2515_Shortest_Distance_to_Target_String_in_a_Circular_Array/src/com/shortestDistancetoTargetStringinaCircularArray/E2515_Shortest_Distance_to_Target_String_in_a_Circular_Array.java
+++ mysol/Category/Array/E2515_Shortest_Distance_to_Target_String_in_a_Circular_Array/src/com/shortestDistancetoTargetStringinaCircularArray/E2515_Shortest_Distance_to_Target_String_in_a_Circular_Array.java
@@ -27,7 +27,36 @@ public class E2515_Shortest_Distance_to_Target_String_in_a_Circular_Array {
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    1. Initialization:
+		. n is the length of the words array.
+		. minDistance is initialized to the maximum integer value to keep track of the minimum distance found.
+	2. Iterating Through the Array:
+		. We loop through each index i in the words array.
+		. If words[i] equals the target, we proceed to calculate the distance.
+	3. Calculating Distances:
+		. Forward Distance:
+			. Calculated using (i - startIndex + n) % n.
+			. This formula accounts for the circular nature of the array and ensures a non-negative result.
+		. Backward Distance:
+			. Calculated using (startIndex - i + n) % n.
+			. Similarly adjusted for the circular array.
+		. We take the minimum of the forward and backward distances as the shortest path to the current target.
+	4. Updating Minimum Distance:
+		. We compare the calculated distance with minDistance and update minDistance if the new distance is smaller.
+	5. Final Result:
+		. After checking all occurrences of the target, if minDistance remains Integer.MAX_VALUE, it means the target 
+			was not found in the array, so we return -1.
+		. Otherwise, we return the minDistance, which is the shortest distance to reach the target from the startIndex.
+    */
 	
+	/*
+     * @param words: a list of strings
+     * @param target: a string
+     * @param startIndex: an integer
+     * @return: the shortest distance needed to reach the string target
+     */
 	public int closetTarget(String[] words, String target, int startIndex) {
         int n = words.length;
         int minDistance = Integer.MAX_VALUE;
