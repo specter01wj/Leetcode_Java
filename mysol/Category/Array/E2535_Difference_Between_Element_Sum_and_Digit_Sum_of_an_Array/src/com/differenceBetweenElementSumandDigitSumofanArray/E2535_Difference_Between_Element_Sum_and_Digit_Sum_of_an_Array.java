38b3394af88dbe38f00705842bd929833935ded8
Jin Wang <specter01wj@gmail.com>
2024-09-25 13:37:01 -0400

    v1

---
+++ mysol/Category/Array/E2535_Difference_Between_Element_Sum_and_Digit_Sum_of_an_Array/src/com/differenceBetweenElementSumandDigitSumofanArray/E2535_Difference_Between_Element_Sum_and_Digit_Sum_of_an_Array.java
@@ -0,0 +1,10 @@
+package com.differenceBetweenElementSumandDigitSumofanArray;
+
+public class E2535_Difference_Between_Element_Sum_and_Digit_Sum_of_an_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
