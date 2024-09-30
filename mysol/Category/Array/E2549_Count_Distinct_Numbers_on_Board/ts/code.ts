10f43c0122d87b50232839277f57c4f1da3e72ac
Jin Wang <specter01wj@gmail.com>
2024-09-30 13:03:32 -0400

    v15

--- mysol/Category/Array/E2549_Count_Distinct_Numbers_on_Board/ts/code.ts
+++ mysol/Category/Array/E2549_Count_Distinct_Numbers_on_Board/ts/code.ts
@@ -1,25 +1,16 @@
-function getCommon(nums1: number[], nums2: number[]): number {
-  let i = 0, j = 0;
-  
-  // Use two pointers to traverse through both arrays
-  while (i < nums1.length && j < nums2.length) {
-      if (nums1[i] === nums2[j]) {
-          return nums1[i]; // Common element found
-      } else if (nums1[i] < nums2[j]) {
-          i++; // Move pointer of nums1 forward
-      } else {
-          j++; // Move pointer of nums2 forward
-      }
+function distinctIntegers(n: number): number {
+  // If n is 1, return 1 since only 1 remains on the board
+  if (n === 1) {
+      return 1;
   }
   
-  return -1; // No common element found
+  // For any n > 1, the result is n - 1 distinct integers
+  return n - 1;
 };
 
-const nums1: number[] = [1,2,3];
-const nums2: number[] = [2,4];
-const results = getCommon(nums1, nums2);
+const input: number = 5;
+const results = distinctIntegers(input);
 
 let webHeading = document.querySelector('#t1');
-webHeading.innerHTML = 'Input1: ' + JSON.stringify(nums1, null, 2) + 
-  '; Input2: ' + JSON.stringify(nums2, null, 2) + '<br>Result = ' + results;
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + results;
 
