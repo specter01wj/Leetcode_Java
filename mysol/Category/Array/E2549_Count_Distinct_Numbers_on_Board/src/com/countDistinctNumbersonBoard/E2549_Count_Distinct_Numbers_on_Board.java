a221dd7a08e05ddb9af7ab2b465371bd93397c2a
Jin Wang <specter01wj@gmail.com>
2024-09-30 12:43:24 -0400

    v10

---
+++ mysol/Category/Array/E2549_Count_Distinct_Numbers_on_Board/src/com/countDistinctNumbersonBoard/E2549_Count_Distinct_Numbers_on_Board.java
@@ -0,0 +1,10 @@
+package com.countDistinctNumbersonBoard;
+
+public class E2549_Count_Distinct_Numbers_on_Board {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
