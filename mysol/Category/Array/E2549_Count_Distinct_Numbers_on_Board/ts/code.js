026a89ddea5e6297554cf04001d3dc8118cbc5a9
Jin Wang <specter01wj@gmail.com>
2024-09-30 13:03:54 -0400

    v16

---
+++ mysol/Category/Array/E2549_Count_Distinct_Numbers_on_Board/ts/code.js
@@ -0,0 +1,13 @@
+function distinctIntegers(n) {
+    // If n is 1, return 1 since only 1 remains on the board
+    if (n === 1) {
+        return 1;
+    }
+    // For any n > 1, the result is n - 1 distinct integers
+    return n - 1;
+}
+;
+const input = 5;
+const results = distinctIntegers(input);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + results;
