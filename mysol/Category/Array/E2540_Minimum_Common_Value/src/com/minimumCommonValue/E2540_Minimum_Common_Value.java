7b2181f0a001d53c855d298765e466fdeba6a4ee
Jin Wang <specter01wj@gmail.com>
2024-09-30 12:02:31 -0400

    v2

--- mysol/Category/Array/E2540_Minimum_Common_Value/src/com/minimumCommonValue/E2540_Minimum_Common_Value.java
+++ mysol/Category/Array/E2540_Minimum_Common_Value/src/com/minimumCommonValue/E2540_Minimum_Common_Value.java
@@ -37,18 +37,19 @@ public class E2540_Minimum_Common_Value {
 	
 	public int getCommon(int[] nums1, int[] nums2) {
 		int i = 0, j = 0;
-		
-		while (i < nums1.length && j < nums2.length) {
-			if (nums1[i] == nums2[j]) {
-				return nums1[i];
-			} else if (nums1[i] < nums2[j]) {
-				i++;
-			} else {
-				j++;
-			}
-		}
-		
-		return -1;
+        
+        // Use two pointers to traverse through both arrays
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i]; // Common element found
+            } else if (nums1[i] < nums2[j]) {
+                i++; // Move pointer of nums1 forward
+            } else {
+                j++; // Move pointer of nums2 forward
+            }
+        }
+        
+        return -1; // No common element found
 	}
 
 }
