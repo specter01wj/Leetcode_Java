a487e18063fc33cd3ffd28d4d5ad866ad385cc75
Jin Wang <specter01wj@gmail.com>
2024-10-03 00:05:16 -0400

    v1

---
+++ mysol/Category/Array/E2553_Separate_the_Digits_in_an_Array/src/com/separatetheDigitsinanArray/E2553_Separate_the_Digits_in_an_Array.java
@@ -0,0 +1,10 @@
+package com.separatetheDigitsinanArray;
+
+public class E2553_Separate_the_Digits_in_an_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
