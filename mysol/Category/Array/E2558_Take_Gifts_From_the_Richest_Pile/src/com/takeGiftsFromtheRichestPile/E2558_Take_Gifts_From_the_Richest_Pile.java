298a2704fc24102851bfa2d742329ce3654a95a6
Jin Wang <specter01wj@gmail.com>
2024-10-03 01:13:43 -0400

    v1

---
+++ mysol/Category/Array/E2558_Take_Gifts_From_the_Richest_Pile/src/com/takeGiftsFromtheRichestPile/E2558_Take_Gifts_From_the_Richest_Pile.java
@@ -0,0 +1,10 @@
+package com.takeGiftsFromtheRichestPile;
+
+public class E2558_Take_Gifts_From_the_Richest_Pile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
