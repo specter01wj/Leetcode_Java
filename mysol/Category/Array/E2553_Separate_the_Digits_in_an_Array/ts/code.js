ba7a368ea3c9ef549e8f7a352c3ca6c8c864f801
Jin Wang <specter01wj@gmail.com>
2024-10-03 00:42:52 -0400

    v5

---
+++ mysol/Category/Array/E2553_Separate_the_Digits_in_an_Array/ts/code.js
@@ -0,0 +1,18 @@
+function separateDigits(nums) {
+    const result = [];
+    // Iterate through each number in the input array
+    for (const num of nums) {
+        // Convert the number to a string to separate its digits
+        const numStr = num.toString();
+        // Convert each character back to a number and add to the result array
+        for (const digit of numStr) {
+            result.push(Number(digit));
+        }
+    }
+    return result;
+}
+;
+const input = [13, 25, 83, 77];
+const results = separateDigits(input);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + results;
