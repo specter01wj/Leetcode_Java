7e1780e79e38b426b6e99467df47197ea4e9b37b
Jin Wang <specter01wj@gmail.com>
2024-10-08 02:01:04 -0400

    v4

---
+++ mysol/Category/Array/E2570_Merge_Two_2D_Arrays_by_Summing_Values/ts/code.ts
@@ -0,0 +1,26 @@
+function findTheArrayConcVal(nums: number[]): number {
+  let concatenationValue = 0;
+  let left = 0;
+  let right = nums.length - 1;
+  
+  while (left <= right) {
+      if (left === right) {
+          concatenationValue += nums[left];
+          break;
+      } else {
+          let concat = nums[left].toString() + nums[right].toString();
+          concatenationValue += parseInt(concat);
+      }
+      left++;
+      right--;
+  }
+  
+  return concatenationValue;
+};
+
+const input: number[] = [7,52,2,4];
+const results = findTheArrayConcVal(input);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + results;
+
