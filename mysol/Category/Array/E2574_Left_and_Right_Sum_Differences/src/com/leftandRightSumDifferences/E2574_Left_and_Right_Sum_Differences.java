db7b90adf5c3945716faae7e87c0bce64b378841
Jin Wang <specter01wj@gmail.com>
2024-10-09 17:59:06 -0400

    v1

---
+++ mysol/Category/Array/E2574_Left_and_Right_Sum_Differences/src/com/leftandRightSumDifferences/E2574_Left_and_Right_Sum_Differences.java
@@ -0,0 +1,10 @@
+package com.leftandRightSumDifferences;
+
+public class E2574_Left_and_Right_Sum_Differences {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
