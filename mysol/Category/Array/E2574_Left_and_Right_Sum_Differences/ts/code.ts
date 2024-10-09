fd6ef1dbce2a3cc2f254b1a711e4dcb2ec1bc2c5
Jin Wang <specter01wj@gmail.com>
2024-10-09 19:07:28 -0400

    v5

--- mysol/Category/Array/E2574_Left_and_Right_Sum_Differences/ts/code.ts
+++ mysol/Category/Array/E2574_Left_and_Right_Sum_Differences/ts/code.ts
@@ -1,46 +1,30 @@
-function mergeArrays(nums1: number[][], nums2: number[][]): number[][] {
-  let result: number[][] = [];
-  let i = 0, j = 0;
+function leftRightDifference(nums: number[]): number[] {
+  const n = nums.length;
+  const leftSum: number[] = new Array(n).fill(0);
+  const rightSum: number[] = new Array(n).fill(0);
+  const answer: number[] = new Array(n).fill(0);
 
-  // Traverse both arrays
-  while (i < nums1.length && j < nums2.length) {
-      if (nums1[i][0] === nums2[j][0]) {
-          // If the ids are the same, sum their values
-          result.push([nums1[i][0], nums1[i][1] + nums2[j][1]]);
-          i++;
-          j++;
-      } else if (nums1[i][0] < nums2[j][0]) {
-          // Add the id and value from nums1 if its id is smaller
-          result.push([nums1[i][0], nums1[i][1]]);
-          i++;
-      } else {
-          // Add the id and value from nums2 if its id is smaller
-          result.push([nums2[j][0], nums2[j][1]]);
-          j++;
-      }
+  // Calculate left sums
+  for (let i = 1; i < n; i++) {
+      leftSum[i] = leftSum[i - 1] + nums[i - 1];
   }
 
-  // Add remaining elements from nums1, if any
-  while (i < nums1.length) {
-      result.push([nums1[i][0], nums1[i][1]]);
-      i++;
+  // Calculate right sums
+  for (let i = n - 2; i >= 0; i--) {
+      rightSum[i] = rightSum[i + 1] + nums[i + 1];
   }
 
-  // Add remaining elements from nums2, if any
-  while (j < nums2.length) {
-      result.push([nums2[j][0], nums2[j][1]]);
-      j++;
+  // Calculate the answer array
+  for (let i = 0; i < n; i++) {
+      answer[i] = Math.abs(leftSum[i] - rightSum[i]);
   }
 
-  return result;
+  return answer;
 };
 
-const nums1: number[][] = [[1,2],[2,3],[4,5]];
-const nums2: number[][] = [[1,4],[3,2],[4,1]];
-const results = mergeArrays(nums1, nums2);
+const input: number[] = [10,4,8,3];
+const results = leftRightDifference(input);
 
 let webHeading = document.querySelector('#t1');
-webHeading.innerHTML = 'Input1: ' + JSON.stringify(nums1, null, 2) + 
-  '; Input2: ' + JSON.stringify(nums2, null, 2) + 
-  '<br>Result = ' + JSON.stringify(results, null, 2);
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
 
