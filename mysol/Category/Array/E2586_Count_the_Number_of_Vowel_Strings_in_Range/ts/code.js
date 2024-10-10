229bfd579259eb0c21350c99b668440305f5ccb0
Jin Wang <specter01wj@gmail.com>
2024-10-10 00:48:34 -0400

    v7

---
+++ mysol/Category/Array/E2586_Count_the_Number_of_Vowel_Strings_in_Range/ts/code.js
@@ -0,0 +1,17 @@
+function vowelStrings(words, left, right) {
+    let count = 0;
+    const vowels = "aeiou";
+    for (let i = left; i <= right; i++) {
+        const word = words[i];
+        if (vowels.includes(word.charAt(0)) && vowels.includes(word.charAt(word.length - 1))) {
+            count++;
+        }
+    }
+    return count;
+}
+;
+const input = ["hey", "aeo", "mu", "ooo", "artro"];
+const left = 1, right = 4;
+const results = vowelStrings(input, left, right);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
