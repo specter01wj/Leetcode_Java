a684d137b8299ef81da9d7312fcda2e1cab37d8a
Jin Wang <specter01wj@gmail.com>
2024-10-10 00:00:03 -0400

    v1

---
+++ mysol/Category/Array/E2586_Count_the_Number_of_Vowel_Strings_in_Range/src/com/counttheNumberofVowelStringsinRange/E2586_Count_the_Number_of_Vowel_Strings_in_Range.java
@@ -0,0 +1,10 @@
+package com.counttheNumberofVowelStringsinRange;
+
+public class E2586_Count_the_Number_of_Vowel_Strings_in_Range {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
