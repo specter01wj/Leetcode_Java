1b418d2e6b1b72f5cfdd739cff798f249f76ce96
Jin Wang <specter01wj@gmail.com>
2024-10-17 01:30:56 -0400

    v4

--- mysol/Category/Array/E2614_Prime_In_Diagonal/src/com/primeInDiagonal/E2614_Prime_In_Diagonal.java
+++ mysol/Category/Array/E2614_Prime_In_Diagonal/src/com/primeInDiagonal/E2614_Prime_In_Diagonal.java
@@ -35,31 +35,31 @@ public class E2614_Prime_In_Diagonal {
      * @return: the largest prime number
      */
 	public int diagonalPrime(int[][] nums) {
-	       int maxPrime = 0;
-	        int n = nums.length;
+       int maxPrime = 0;
+        int n = nums.length;
 
-	        for (int i = 0; i < n; i++) {
-	            // Check the first diagonal (nums[i][i])
-	            if (isPrime(nums[i][i])) {
-	                maxPrime = Math.max(maxPrime, nums[i][i]);
-	            }
-	            
-	            // Check the second diagonal (nums[i][n - i - 1])
-	            if (isPrime(nums[i][n - i - 1])) {
-	                maxPrime = Math.max(maxPrime, nums[i][n - i - 1]);
-	            }
-	        }
+        for (int i = 0; i < n; i++) {
+            // Check the first diagonal (nums[i][i])
+            if (isPrime(nums[i][i])) {
+                maxPrime = Math.max(maxPrime, nums[i][i]);
+            }
+            
+            // Check the second diagonal (nums[i][n - i - 1])
+            if (isPrime(nums[i][n - i - 1])) {
+                maxPrime = Math.max(maxPrime, nums[i][n - i - 1]);
+            }
+        }
 
-	        return maxPrime; 
-	    }
+        return maxPrime; 
+    }
 
-	    // Helper function to check if a number is prime
-	    private boolean isPrime(int num) {
-	        if (num <= 1) return false;
-	        for (int i = 2; i * i <= num; i++) {
-	            if (num % i == 0) return false;
-	        }
-	        return true;
-	    }
+    // Helper function to check if a number is prime
+    private boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
 
 }
