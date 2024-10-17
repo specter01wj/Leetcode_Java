93bfe071e3bb2d68ec8e23c4d9e3b82f71916145
Jin Wang <specter01wj@gmail.com>
2024-10-17 00:00:25 -0400

    v1

---
+++ mysol/Category/Array/E2605_Form_Smallest_Number_From_Two_Digit_Arrays/src/com/formSmallestNumberFromTwoDigitArrays/E2605_Form_Smallest_Number_From_Two_Digit_Arrays.java
@@ -0,0 +1,10 @@
+package com.formSmallestNumberFromTwoDigitArrays;
+
+public class E2605_Form_Smallest_Number_From_Two_Digit_Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
