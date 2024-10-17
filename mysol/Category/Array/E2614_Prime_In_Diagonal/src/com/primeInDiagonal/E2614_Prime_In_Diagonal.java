3baa6218e5b7f54f991750989b0bb095af2bd92e
Jin Wang <specter01wj@gmail.com>
2024-10-17 01:30:26 -0400

    v3

--- mysol/Category/Array/E2614_Prime_In_Diagonal/src/com/primeInDiagonal/E2614_Prime_In_Diagonal.java
+++ mysol/Category/Array/E2614_Prime_In_Diagonal/src/com/primeInDiagonal/E2614_Prime_In_Diagonal.java
@@ -18,7 +18,22 @@ public class E2614_Prime_In_Diagonal {
         System.out.println("input: " + Arrays.deepToString(input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    1. isPrime function: This function checks if a number is prime. 
+    	If the number is less than or equal to 1, it is not prime. 
+    	Otherwise, we check divisibility from 2 to the square root of the number.
+	2. Loop through diagonals: We iterate through both diagonals 
+		(nums[i][i] and nums[i][nums.length - i - 1]). For each number 
+		on the diagonals, we check if it's prime using the isPrime function.
+	3. Return the result: We keep track of the maximum prime number found 
+		on the diagonals and return it. If no prime number is found, we return 0.
+    */
 	
+	/*
+     * @param nums: a list of 2D integers
+     * @return: the largest prime number
+     */
 	public int diagonalPrime(int[][] nums) {
 	       int maxPrime = 0;
 	        int n = nums.length;
