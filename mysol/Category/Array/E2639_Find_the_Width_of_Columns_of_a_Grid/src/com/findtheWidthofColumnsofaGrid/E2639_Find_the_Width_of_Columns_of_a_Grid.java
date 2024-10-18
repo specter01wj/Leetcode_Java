2ed869bf9ade36f699765bde33087d23161d0dba
Jin Wang <specter01wj@gmail.com>
2024-10-17 23:53:15 -0400

    v1

---
+++ mysol/Category/Array/E2639_Find_the_Width_of_Columns_of_a_Grid/src/com/findtheWidthofColumnsofaGrid/E2639_Find_the_Width_of_Columns_of_a_Grid.java
@@ -0,0 +1,10 @@
+package com.findtheWidthofColumnsofaGrid;
+
+public class E2639_Find_the_Width_of_Columns_of_a_Grid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
