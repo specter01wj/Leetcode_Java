d5026aee01aa327a34bb0b8c3c5dfbeaeddf7b4e
Jin Wang <specter01wj@gmail.com>
2024-10-18 01:17:43 -0400

    v5

--- mysol/Category/Array/E2643_Row_With_Maximum_Ones/src/com/rowWithMaximumOnes/E2643_Row_With_Maximum_Ones.java
+++ mysol/Category/Array/E2643_Row_With_Maximum_Ones/src/com/rowWithMaximumOnes/E2643_Row_With_Maximum_Ones.java
@@ -21,7 +21,26 @@ public class E2643_Row_With_Maximum_Ones {
         System.out.println("input: " + Arrays.deepToString(input) + "\noutput: " + Arrays.toString(output));
 	}
 	
+	/*
+    solution:
+    1. Tracking the maximum ones: initialize two variables: maxOnes to 
+    	store the maximum count of 1s, and rowIndex to store the index of 
+    	the row with that count.
+	2. Counting ones in each row: loop through each row and count the number 
+		of 1s in that row.
+	3. Updating the result: If the count of 1s in the current row is greater 
+		than maxOnes, update both maxOnes and rowIndex. 
+		If multiple rows have the same number of ones, the row with the smaller 
+		index is automatically selected since only update when a row has a 
+		strictly greater count.
+	4. Return the result: return the index of the row with the maximum ones 
+		and the count of ones in that row.
+    */
 	
+	/*
+     * @param mat: a list of 2D integers
+     * @return: an array containing the index of the row, and the number of ones in it
+     */
 	public int[] rowAndMaximumOnes(int[][] mat) {
         int maxOnes = 0; // Maximum number of ones in any row
         int rowIndex = -1; // Row index with maximum number of ones
