df9006023238badd823f2b7ce0ef4e9e4c023b57
Jin Wang <specter01wj@gmail.com>
2024-10-21 12:08:56 -0400

    v6

--- mysol/Category/Array/E2660_Determine_the_Winner_of_a_Bowling_Game/ts/code.ts
+++ mysol/Category/Array/E2660_Determine_the_Winner_of_a_Bowling_Game/ts/code.ts
@@ -1,22 +1,38 @@
-function maximizeSum(nums: number[], k: number): number {
-  // Find the maximum element in nums
-  let maxNum = Math.max(...nums);
+function isWinner(player1: number[], player2: number[]): number {
+  const score1 = calculateScore(player1);
+  const score2 = calculateScore(player2);
   
-  // Initialize the sum
-  let sum = 0;
-  
-  // Perform k operations to maximize the sum
-  for (let i = 0; i < k; i++) {
-      sum += maxNum;  // Add the current largest number to the sum
-      maxNum++;       // Increment the number by 1 for the next step
+  if (score1 > score2) {
+      return 1;
+  } else if (score2 > score1) {
+      return 2;
+  } else {
+      return 0;
   }
-  
-  return sum;
 };
 
-const input: number[] = [1,2,3,4,5], k: number = 3;
-const results = maximizeSum(input, k);
+function calculateScore(player: number[]): number {
+  let totalScore = 0;
+
+  for (let i = 0; i < player.length; i++) {
+      let currentTurnScore = player[i];
+      
+      // Check if the previous or two-turns-ago hit exactly 10 pins
+      if ((i > 0 && player[i - 1] === 10) || (i > 1 && player[i - 2] === 10)) {
+          currentTurnScore *= 2;
+      }
+      
+      totalScore += currentTurnScore;
+  }
+  
+  return totalScore;
+}
+
+const player1: number[] = [5,10,3,2], player2: number[] = [6,5,7,3];
+const results = isWinner(player1, player2);
 
 let webHeading = document.querySelector('#t1');
-webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+webHeading.innerHTML = 'Input1: ' + JSON.stringify(player1, null, 2) + 
+    '; Input2: ' + JSON.stringify(player2, null, 2) + 
+    '<br>Result = ' + JSON.stringify(results, null, 2);
 
