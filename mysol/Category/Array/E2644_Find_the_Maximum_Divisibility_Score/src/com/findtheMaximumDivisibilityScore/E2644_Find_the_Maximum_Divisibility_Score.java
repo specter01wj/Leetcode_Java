6d8520a0fab938136f78e7599f189dd8c13179dd
Jin Wang <specter01wj@gmail.com>
2024-10-20 22:48:22 -0400

    v1

---
+++ mysol/Category/Array/E2644_Find_the_Maximum_Divisibility_Score/src/com/findtheMaximumDivisibilityScore/E2644_Find_the_Maximum_Divisibility_Score.java
@@ -0,0 +1,10 @@
+package com.findtheMaximumDivisibilityScore;
+
+public class E2644_Find_the_Maximum_Divisibility_Score {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
