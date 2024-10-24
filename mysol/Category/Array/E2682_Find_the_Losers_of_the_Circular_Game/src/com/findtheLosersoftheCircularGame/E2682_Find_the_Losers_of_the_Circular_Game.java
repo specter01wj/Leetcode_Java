a07ebe339cbeecf07fd997152de51ed24da78c91
Jin Wang <specter01wj@gmail.com>
2024-10-23 23:35:51 -0400

    v1

---
+++ mysol/Category/Array/E2682_Find_the_Losers_of_the_Circular_Game/src/com/findtheLosersoftheCircularGame/E2682_Find_the_Losers_of_the_Circular_Game.java
@@ -0,0 +1,10 @@
+package com.findtheLosersoftheCircularGame;
+
+public class E2682_Find_the_Losers_of_the_Circular_Game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
