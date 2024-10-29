18341cdbfa33306855ffcce0cec832b490bb9550
Jin Wang <specter01wj@gmail.com>
2024-10-29 14:27:48 -0400

    v8

---
+++ mysol/Category/Array/E2706_Buy_Two_Chocolates/ts/code.js
@@ -0,0 +1,18 @@
+function buyChoco(prices, money) {
+    // Sort the prices to get the two cheapest chocolates
+    prices.sort((a, b) => a - b);
+    // Calculate the sum of the two cheapest chocolates
+    const cost = prices[0] + prices[1];
+    // Check if the total cost is within the available money
+    if (cost <= money) {
+        return money - cost; // Return the remaining money
+    }
+    else {
+        return money; // Not enough money, return original amount
+    }
+}
+;
+const input = [3, 2, 3], money = 2;
+const results = buyChoco(input, money);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
