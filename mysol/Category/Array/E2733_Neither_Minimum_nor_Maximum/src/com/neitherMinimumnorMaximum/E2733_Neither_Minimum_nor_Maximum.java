5cdfab51e6e0845b0841e5f2f570fd762a50e76c
Jin Wang <specter01wj@gmail.com>
2024-10-31 01:42:48 -0400

    v1

---
+++ mysol/Category/Array/E2733_Neither_Minimum_nor_Maximum/src/com/neitherMinimumnorMaximum/E2733_Neither_Minimum_nor_Maximum.java
@@ -0,0 +1,10 @@
+package com.neitherMinimumnorMaximum;
+
+public class E2733_Neither_Minimum_nor_Maximum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
