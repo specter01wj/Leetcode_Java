1480b15ae1b09d2abedde457518def6c0548b3ad
Jin Wang <specter01wj@gmail.com>
2024-10-31 01:09:02 -0400

    v2

--- mysol/Category/Array/E2728_Count_Houses_in_a_Circular_Street/src/com/countHousesinaCircularStreet/E2728_Count_Houses_in_a_Circular_Street.java
+++ mysol/Category/Array/E2728_Count_Houses_in_a_Circular_Street/src/com/countHousesinaCircularStreet/E2728_Count_Houses_in_a_Circular_Street.java
@@ -1,10 +1,77 @@
 package com.countHousesinaCircularStreet;
+import java.util.*;
+
+
+
+
+class Street {
+    private boolean[] doors;
+    private int currentPosition;
+
+    public Street(int[] doorStates) {
+        doors = new boolean[doorStates.length];
+        for (int i = 0; i < doorStates.length; i++) {
+            doors[i] = doorStates[i] == 1;
+        }
+        currentPosition = 0; // Start at the first house
+    }
+
+    public void openDoor() {
+        doors[currentPosition] = true;
+    }
+
+    public void closeDoor() {
+        doors[currentPosition] = false;
+    }
+
+    public boolean isDoorOpen() {
+        return doors[currentPosition];
+    }
+
+    public void moveRight() {
+        currentPosition = (currentPosition + 1) % doors.length;
+    }
+
+    public void moveLeft() {
+        currentPosition = (currentPosition - 1 + doors.length) % doors.length;
+    }
+}
 
 public class E2728_Count_Houses_in_a_Circular_Street {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E2728_Count_Houses_in_a_Circular_Street solution = new E2728_Count_Houses_in_a_Circular_Street();
+		int[] input = {1,0,1,1,0};
+		int k = 5;
+		Street street = new Street(input);
+		int output = solution.houseCount(street, k);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public int houseCount(Street street, int k) {
+        // Prepare a consistent state by closing doors and moving k steps
+        for (int i = 0; i < k; i++) {
+            if (street.isDoorOpen()) {
+                street.closeDoor();
+            }
+            street.moveRight();
+        }
+
+        // Mark the starting point by opening the current door and moving to the next house
+        street.openDoor();
+        street.moveRight();
+
+        int houseCount = 0;
+
+        // Count houses until we encounter the open door again
+        while (!street.isDoorOpen()) {
+            houseCount += 1;
+            street.moveRight();
+        }
+
+        // Include the starting house in the count
+        return houseCount + 1;
+    }
 
 }
