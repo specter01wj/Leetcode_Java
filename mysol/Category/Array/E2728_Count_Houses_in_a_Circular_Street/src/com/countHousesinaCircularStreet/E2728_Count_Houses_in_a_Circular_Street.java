b68e4803c7a249350809e0069924f09d7234146f
Jin Wang <specter01wj@gmail.com>
2024-10-31 01:12:06 -0400

    v3

--- mysol/Category/Array/E2728_Count_Houses_in_a_Circular_Street/src/com/countHousesinaCircularStreet/E2728_Count_Houses_in_a_Circular_Street.java
+++ mysol/Category/Array/E2728_Count_Houses_in_a_Circular_Street/src/com/countHousesinaCircularStreet/E2728_Count_Houses_in_a_Circular_Street.java
@@ -1,8 +1,26 @@
 package com.countHousesinaCircularStreet;
 import java.util.*;
 
+/*
+You are given an object street of class Street that represents a 
+circular street and a positive integer k which represents a maximum 
+bound for the number of houses in that street (in other words, 
+the number of houses is less than or equal to k). Houses' doors 
+could be open or closed initially.
 
+Initially, you are standing in front of a door to a house on this street. 
+Your task is to count the number of houses in the street.
 
+The class Street contains the following functions which may help you:
+
+	. void openDoor(): Open the door of the house you are in front of.
+	. void closeDoor(): Close the door of the house you are in front of.
+	. boolean isDoorOpen(): Returns true if the door of the current house is open and false otherwise.
+	. void moveRight(): Move to the right house.
+	. void moveLeft(): Move to the left house.
+
+Return ans which represents the number of houses on this street.
+*/
 
 class Street {
     private boolean[] doors;
@@ -48,7 +66,29 @@ public class E2728_Count_Houses_in_a_Circular_Street {
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    1. First Loop (Setup Phase):
+		. We iterate k times, moving around the circular street to ensure 
+			that if there are fewer than k houses, we have fully circled around.
+		. For each house, if the door is open, we close it. This ensures a 
+			consistent starting state for counting.
+	2. Marking the Starting Point:
+		. After the initial loop, we open the current door and move to the 
+			next house. This marks the starting point with an open door, 
+			making it easy to detect when we have completed a full loop.
+	3. Counting Houses:
+		. We initialize ans to 0 and then move right, counting each house 
+			until we encounter the marked starting house with an open door.
+		. When we reach the open door, it signifies a complete loop, and we 
+			return ans + 1 (to include the starting house).
+    */
 	
+	/*
+     * @param arr: a list of integers
+     * @param k: an integer
+     * @return: ans which represents the number of houses on this street
+     */
 	public int houseCount(Street street, int k) {
         // Prepare a consistent state by closing doors and moving k steps
         for (int i = 0; i < k; i++) {
