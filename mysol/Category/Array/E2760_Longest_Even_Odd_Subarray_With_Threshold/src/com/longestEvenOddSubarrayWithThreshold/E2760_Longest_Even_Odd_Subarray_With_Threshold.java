babd1b6e7367edc817250082cbf1928cba705709
Jin Wang <specter01wj@gmail.com>
2024-11-07 00:00:54 -0500

    v3

--- mysol/Category/Array/E2760_Longest_Even_Odd_Subarray_With_Threshold/src/com/longestEvenOddSubarrayWithThreshold/E2760_Longest_Even_Odd_Subarray_With_Threshold.java
+++ mysol/Category/Array/E2760_Longest_Even_Odd_Subarray_With_Threshold/src/com/longestEvenOddSubarrayWithThreshold/E2760_Longest_Even_Odd_Subarray_With_Threshold.java
@@ -20,12 +20,38 @@ Note: A subarray is a contiguous non-empty sequence of elements within an array.
 public class E2760_Longest_Even_Odd_Subarray_With_Threshold {
 
 	public static void main(String[] args) {
-		int[] input = {3,0,1,1,9,7};
-		int output = countGoodTriplets(input, 7, 2, 3);
+		int[] input = {3,2,5,4};
+		int threshold = 5;
+		int output = longestAlternatingSubarray(input, threshold);
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
 	
 	
-	
+	public int longestAlternatingSubarray(int[] nums, int threshold) {
+        int maxLength = 0;
+        int i = 0;
+
+        while (i < nums.length) {
+            // Only start counting if we find an even number within the threshold
+            if (nums[i] % 2 == 0 && nums[i] <= threshold) {
+                int currentLength = 1;
+                boolean expectOdd = true;
+
+                // Expand the subarray by checking alternating parity and threshold condition
+                for (int j = i + 1; j < nums.length && nums[j] <= threshold; j++) {
+                    if ((nums[j] % 2 == 0) == expectOdd) {
+                        break;
+                    }
+                    currentLength++;
+                    expectOdd = !expectOdd;  // Alternate between even and odd expectation
+                }
+
+                maxLength = Math.max(maxLength, currentLength);
+            }
+            i++;
+        }
+
+        return maxLength;
+    }
 
 }
