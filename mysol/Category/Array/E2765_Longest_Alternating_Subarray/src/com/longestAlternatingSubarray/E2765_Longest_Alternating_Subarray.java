cd78b14b5509ec9b7be2cd5069079fb64f0051f0
Jin Wang <specter01wj@gmail.com>
2024-11-07 00:57:47 -0500

    v1

---
+++ mysol/Category/Array/E2765_Longest_Alternating_Subarray/src/com/longestAlternatingSubarray/E2765_Longest_Alternating_Subarray.java
@@ -0,0 +1,10 @@
+package com.longestAlternatingSubarray;
+
+public class E2765_Longest_Alternating_Subarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
