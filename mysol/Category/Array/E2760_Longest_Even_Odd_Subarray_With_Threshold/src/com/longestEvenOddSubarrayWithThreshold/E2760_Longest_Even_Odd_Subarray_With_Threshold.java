55214904d7eec18cbd09fe2f98f69926c9f5284d
Jin Wang <specter01wj@gmail.com>
2024-11-06 23:55:35 -0500

    v1

---
+++ mysol/Category/Array/E2760_Longest_Even_Odd_Subarray_With_Threshold/src/com/longestEvenOddSubarrayWithThreshold/E2760_Longest_Even_Odd_Subarray_With_Threshold.java
@@ -0,0 +1,10 @@
+package com.longestEvenOddSubarrayWithThreshold;
+
+public class E2760_Longest_Even_Odd_Subarray_With_Threshold {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
