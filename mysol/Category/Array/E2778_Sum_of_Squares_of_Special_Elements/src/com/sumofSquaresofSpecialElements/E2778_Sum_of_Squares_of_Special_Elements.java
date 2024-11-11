435e7c7b54fa40fdf9dc2b6c7cd130f2e114f9e7
Jin Wang <specter01wj@gmail.com>
2024-11-11 12:36:34 -0500

    v1

---
+++ mysol/Category/Array/E2778_Sum_of_Squares_of_Special_Elements/src/com/sumofSquaresofSpecialElements/E2778_Sum_of_Squares_of_Special_Elements.java
@@ -0,0 +1,10 @@
+package com.sumofSquaresofSpecialElements;
+
+public class E2778_Sum_of_Squares_of_Special_Elements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
