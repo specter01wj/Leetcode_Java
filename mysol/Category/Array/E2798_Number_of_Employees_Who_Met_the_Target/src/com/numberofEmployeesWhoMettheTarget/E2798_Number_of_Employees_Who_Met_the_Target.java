fe68c4d16f748da8d88344ef3f67bcbecd3b3b44
Jin Wang <specter01wj@gmail.com>
2024-11-14 15:16:48 -0500

    v1

---
+++ mysol/Category/Array/E2798_Number_of_Employees_Who_Met_the_Target/src/com/numberofEmployeesWhoMettheTarget/E2798_Number_of_Employees_Who_Met_the_Target.java
@@ -0,0 +1,10 @@
+package com.numberofEmployeesWhoMettheTarget;
+
+public class E2798_Number_of_Employees_Who_Met_the_Target {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
