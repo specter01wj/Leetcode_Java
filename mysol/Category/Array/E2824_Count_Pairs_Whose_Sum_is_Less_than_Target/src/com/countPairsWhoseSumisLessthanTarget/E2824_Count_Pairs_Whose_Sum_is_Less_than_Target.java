98eeefe34e1c653045bc7b6fe6005d9dc0b88513
Jin Wang <specter01wj@gmail.com>
2024-11-18 13:11:05 -0500

    v1

---
+++ mysol/Category/Array/E2824_Count_Pairs_Whose_Sum_is_Less_than_Target/src/com/countPairsWhoseSumisLessthanTarget/E2824_Count_Pairs_Whose_Sum_is_Less_than_Target.java
@@ -0,0 +1,10 @@
+package com.countPairsWhoseSumisLessthanTarget;
+
+public class E2824_Count_Pairs_Whose_Sum_is_Less_than_Target {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
