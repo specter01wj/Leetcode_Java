daf89aeddc4a0650c2d6756636aeb01b5a107aaf
Jin Wang <specter01wj@gmail.com>
2024-11-20 13:17:31 -0500

    v1

---
+++ mysol/Category/Array/E2828_Check_if_a_String_Is_an_Acronym_of_Words/src/com/checkifaStringIsanAcronymofWords/E2828_Check_if_a_String_Is_an_Acronym_of_Words.java
@@ -0,0 +1,10 @@
+package com.checkifaStringIsanAcronymofWords;
+
+public class E2828_Check_if_a_String_Is_an_Acronym_of_Words {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
