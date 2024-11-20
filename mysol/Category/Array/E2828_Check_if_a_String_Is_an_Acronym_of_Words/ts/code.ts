92721d80a6df33bcff4b07526ec1a502e3f27f03
Jin Wang <specter01wj@gmail.com>
2024-11-20 14:09:17 -0500

    v5

--- mysol/Category/Array/E2828_Check_if_a_String_Is_an_Acronym_of_Words/ts/code.ts
+++ mysol/Category/Array/E2828_Check_if_a_String_Is_an_Acronym_of_Words/ts/code.ts
@@ -1,21 +1,22 @@
-function countPairs(nums: number[], target: number): number {
-  let count = 0;
-
-  // Iterate over all pairs (i, j) with 0 <= i < j < nums.length
-  for (let i = 0; i < nums.length; i++) {
-      for (let j = i + 1; j < nums.length; j++) {
-          if (nums[i] + nums[j] < target) {
-              count++;
-          }
-      }
+function isAcronym(words: string[], s: string): boolean {
+  // If the lengths do not match, it cannot be an acronym
+  if (words.length !== s.length) {
+      return false;
   }
 
-  return count;
+  // Build the acronym from the first characters of the words
+  let acronym = "";
+  for (const word of words) {
+      acronym += word[0]; // Add the first character of each word
+  }
+
+  // Compare the built acronym with the given string s
+  return acronym === s;
 };
 
-const input: number[] = [-6,2,5,-2,-7,-1,3];
-const target: number = -2;
-const results = countPairs(input, target);
+const input: string[] = ["never","gonna","give","up","on","you"];
+const s: string = "ngguoy";
+const results = isAcronym(input, s);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
