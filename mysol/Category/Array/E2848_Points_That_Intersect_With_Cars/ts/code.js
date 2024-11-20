9ba842afc4c310f6edc372cb29d73d7416334a98
Jin Wang <specter01wj@gmail.com>
2024-11-20 15:27:57 -0500

    v6

---
+++ mysol/Category/Array/E2848_Points_That_Intersect_With_Cars/ts/code.js
@@ -0,0 +1,17 @@
+function numberOfPoints(nums) {
+    const coveredPoints = new Set();
+    // Iterate through each car's range
+    for (const [start, end] of nums) {
+        // Add all points within the range [start, end] to the set
+        for (let i = start; i <= end; i++) {
+            coveredPoints.add(i);
+        }
+    }
+    // Return the total number of unique points
+    return coveredPoints.size;
+}
+;
+const input = [[3, 6], [1, 5], [4, 7]];
+const results = numberOfPoints(input);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
