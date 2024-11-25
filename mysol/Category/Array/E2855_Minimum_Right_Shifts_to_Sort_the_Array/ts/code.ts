5a5bc27db544c9e08c0c963a392977bc00652dcf
Jin Wang <specter01wj@gmail.com>
2024-11-25 13:10:13 -0500

    v5

--- mysol/Category/Array/E2855_Minimum_Right_Shifts_to_Sort_the_Array/ts/code.ts
+++ mysol/Category/Array/E2855_Minimum_Right_Shifts_to_Sort_the_Array/ts/code.ts
@@ -1,20 +1,44 @@
-function numberOfPoints(nums: number[][]): number {
-  const coveredPoints: Set<number> = new Set();
+function minimumRightShifts(nums: number[]): number {
+  const n = nums.length;
 
-  // Iterate through each car's range
-  for (const [start, end] of nums) {
-      // Add all points within the range [start, end] to the set
-      for (let i = start; i <= end; i++) {
-          coveredPoints.add(i);
+  // Check if the array is already sorted
+  let alreadySorted = true;
+  for (let i = 1; i < n; i++) {
+      if (nums[i] < nums[i - 1]) {
+          alreadySorted = false;
+          break;
+      }
+  }
+  if (alreadySorted) return 0;
+
+  // Find the rotation point where the sequence breaks
+  let rotationPoint = -1;
+  for (let i = 1; i < n; i++) {
+      if (nums[i] < nums[i - 1]) {
+          if (rotationPoint !== -1) {
+              // More than one rotation point means it's not sortable
+              return -1;
+          }
+          rotationPoint = i;
       }
   }
 
-  // Return the total number of unique points
-  return coveredPoints.size;
+  // Check if the array is sorted after applying the rotation
+  for (let i = rotationPoint; i < n - 1; i++) {
+      if (nums[i] > nums[i + 1]) {
+          return -1;
+      }
+  }
+  if (nums[n - 1] > nums[0]) {
+      return -1;
+  }
+
+  // Return the number of shifts needed
+  return n - rotationPoint;
 };
 
-const input: number[][] = [[3,6],[1,5],[4,7]];
-const results = numberOfPoints(input);
+const input: number[] = [3,4,5,1,2];
+const results = minimumRightShifts(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
