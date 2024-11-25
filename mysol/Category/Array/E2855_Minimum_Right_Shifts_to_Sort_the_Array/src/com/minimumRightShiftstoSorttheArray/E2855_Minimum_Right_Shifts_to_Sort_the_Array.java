96ea902dbf2df8d433d7d88ddb140c05b4d1e77e
Jin Wang <specter01wj@gmail.com>
2024-11-25 12:28:28 -0500

    v1

---
+++ mysol/Category/Array/E2855_Minimum_Right_Shifts_to_Sort_the_Array/src/com/minimumRightShiftstoSorttheArray/E2855_Minimum_Right_Shifts_to_Sort_the_Array.java
@@ -0,0 +1,10 @@
+package com.minimumRightShiftstoSorttheArray;
+
+public class E2855_Minimum_Right_Shifts_to_Sort_the_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
