d827ae05db6d5d9a9d69162d550eceeed0eaa112
Jin Wang <specter01wj@gmail.com>
2024-11-28 22:47:37 -0500

    v1

---
+++ mysol/Category/Array/E2869_Minimum_Operations_to_Collect_Elements/src/com/minimumOperationstoCollectElements/E2869_Minimum_Operations_to_Collect_Elements.java
@@ -0,0 +1,10 @@
+package com.minimumOperationstoCollectElements;
+
+public class E2869_Minimum_Operations_to_Collect_Elements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
