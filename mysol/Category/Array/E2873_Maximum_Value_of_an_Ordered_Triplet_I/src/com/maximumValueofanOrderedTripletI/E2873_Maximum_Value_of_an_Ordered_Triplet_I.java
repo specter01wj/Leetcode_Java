6a893dc133dc5f3fc890b080323426608b81b4de
Jin Wang <specter01wj@gmail.com>
2024-11-28 23:09:16 -0500

    v1

---
+++ mysol/Category/Array/E2873_Maximum_Value_of_an_Ordered_Triplet_I/src/com/maximumValueofanOrderedTripletI/E2873_Maximum_Value_of_an_Ordered_Triplet_I.java
@@ -0,0 +1,10 @@
+package com.maximumValueofanOrderedTripletI;
+
+public class E2873_Maximum_Value_of_an_Ordered_Triplet_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
