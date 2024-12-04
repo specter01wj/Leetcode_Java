ed390677a1b17065f2440a3d118ef98ba03f8ad0
Jin Wang <specter01wj@gmail.com>
2024-12-04 00:31:52 -0500

    v1

---
+++ mysol/Category/Array/E2900_Longest_Unequal_Adjacent_Groups_Subsequence_I/src/com/longestUnequalAdjacentGroupsSubsequenceI/E2900_Longest_Unequal_Adjacent_Groups_Subsequence_I.java
@@ -0,0 +1,10 @@
+package com.longestUnequalAdjacentGroupsSubsequenceI;
+
+public class E2900_Longest_Unequal_Adjacent_Groups_Subsequence_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
