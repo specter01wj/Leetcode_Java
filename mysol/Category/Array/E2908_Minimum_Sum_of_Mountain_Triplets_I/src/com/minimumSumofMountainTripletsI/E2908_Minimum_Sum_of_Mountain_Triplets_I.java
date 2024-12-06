b5d09e1daec269ca9af3d7cc73033339fc1cd36a
Jin Wang <specter01wj@gmail.com>
2024-12-06 12:47:43 -0500

    v1

---
+++ mysol/Category/Array/E2908_Minimum_Sum_of_Mountain_Triplets_I/src/com/minimumSumofMountainTripletsI/E2908_Minimum_Sum_of_Mountain_Triplets_I.java
@@ -0,0 +1,10 @@
+package com.minimumSumofMountainTripletsI;
+
+public class E2908_Minimum_Sum_of_Mountain_Triplets_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
