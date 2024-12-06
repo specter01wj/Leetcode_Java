576ae35779f68e1466a3523f5cd86a94a2675fdc
Jin Wang <specter01wj@gmail.com>
2024-12-06 12:22:41 -0500

    v7

---
+++ mysol/Category/Array/E2903_Find_Indices_With_Index_and_Value_Difference_I/ts/code.js
@@ -0,0 +1,20 @@
+function findIndices(nums, indexDifference, valueDifference) {
+    const n = nums.length;
+    // Iterate through all pairs of indices (i, j)
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            // Check if the conditions are satisfied
+            if (Math.abs(i - j) >= indexDifference && Math.abs(nums[i] - nums[j]) >= valueDifference) {
+                return [i, j];
+            }
+        }
+    }
+    // If no such indices are found, return [-1, -1]
+    return [-1, -1];
+}
+;
+const input = [5, 1, 4, 1];
+const indexDifference = 2, valueDifference = 4;
+const results = findIndices(input, indexDifference, valueDifference);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
