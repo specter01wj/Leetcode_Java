8791d52320f8e585471d141d820ffb584fb5e903
Jin Wang <specter01wj@gmail.com>
2024-12-10 11:37:54 -0500

    v1

---
+++ mysol/Category/Array/E2913_Subarrays_Distinct_Element_Sum_of_Squares_I/src/com/subarraysDistinctElementSumofSquaresI/E2913_Subarrays_Distinct_Element_Sum_of_Squares_I.java
@@ -0,0 +1,10 @@
+package com.subarraysDistinctElementSumofSquaresI;
+
+public class E2913_Subarrays_Distinct_Element_Sum_of_Squares_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
