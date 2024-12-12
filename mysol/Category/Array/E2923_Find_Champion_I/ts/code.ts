962f31d0ce46ec9ca5ecf23c3616a744a26d2840
Jin Wang <specter01wj@gmail.com>
2024-12-12 11:46:28 -0500

    v5

---
+++ mysol/Category/Array/E2923_Find_Champion_I/ts/code.ts
@@ -0,0 +1,30 @@
+function findKOr(nums: number[], k: number): number {
+  let result = 0;
+
+  // Iterate over each bit position (0 to 31 for a 32-bit integer)
+  for (let bit = 0; bit < 32; bit++) {
+      let count = 0;
+
+      // Count how many numbers have the current bit set
+      for (let num of nums) {
+          if ((num & (1 << bit)) !== 0) {
+              count++;
+          }
+      }
+
+      // If the bit is set in at least k numbers, set it in the result
+      if (count >= k) {
+          result |= (1 << bit);
+      }
+  }
+
+  return result;
+};
+
+const input: number[] = [7,12,9,8,9,15];
+const k: number = 4;
+const results = findKOr(input, k);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
