e4da1544e259bded7195f5c40b4b81514fe27078
Jin Wang <specter01wj@gmail.com>
2024-12-16 18:59:48 -0500

    v2

--- mysol/Category/Array/E2942_Find_Words_Containing_Character/src/com/findWordsContainingCharacter/E2942_Find_Words_Containing_Character.java
+++ mysol/Category/Array/E2942_Find_Words_Containing_Character/src/com/findWordsContainingCharacter/E2942_Find_Words_Containing_Character.java
@@ -1,10 +1,35 @@
 package com.findWordsContainingCharacter;
+import java.util.*;
+
+/*
+You are given a 0-indexed array of strings words and a character x.
+
+Return an array of indices representing the words that contain the character x.
+
+Note that the returned array may be in any order.
+*/
 
 public class E2942_Find_Words_Containing_Character {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E2942_Find_Words_Containing_Character solution = new E2942_Find_Words_Containing_Character();
+		String[] input = {"abc","bcd","aaaa","cbc"};
+		char x = 'a';
+		List<Integer> output = solution.findWordsContaining(input, x);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) { // Check if the character x exists in the current word
+                result.add(i); // Add the index to the result list
+            }
+        }
+        
+        return result; // Return the list of indices
+    }
 
 }
