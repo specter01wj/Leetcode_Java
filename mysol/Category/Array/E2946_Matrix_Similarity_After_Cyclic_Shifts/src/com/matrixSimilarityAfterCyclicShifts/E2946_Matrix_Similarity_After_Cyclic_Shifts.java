be0d9da6a46cc80993bf8b29efd36c37cdc848e8
Jin Wang <specter01wj@gmail.com>
2024-12-17 03:04:16 -0500

    v1

---
+++ mysol/Category/Array/E2946_Matrix_Similarity_After_Cyclic_Shifts/src/com/matrixSimilarityAfterCyclicShifts/E2946_Matrix_Similarity_After_Cyclic_Shifts.java
@@ -0,0 +1,10 @@
+package com.matrixSimilarityAfterCyclicShifts;
+
+public class E2946_Matrix_Similarity_After_Cyclic_Shifts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
