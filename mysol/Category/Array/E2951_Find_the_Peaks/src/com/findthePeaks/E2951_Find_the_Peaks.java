4f642b363b9e10ee6907acef335b996e393b28aa
Jin Wang <specter01wj@gmail.com>
2024-12-18 20:36:01 -0500

    v3

--- mysol/Category/Array/E2951_Find_the_Peaks/src/com/findthePeaks/E2951_Find_the_Peaks.java
+++ mysol/Category/Array/E2951_Find_the_Peaks/src/com/findthePeaks/E2951_Find_the_Peaks.java
@@ -17,7 +17,23 @@ public class E2951_Find_the_Peaks {
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    1. Initialization:
+		. Create a List<Integer> to store the indices of peaks.
+	2. Iterate Through the Array:
+		. Start from the second element (i = 1) and end at the second last element (mountain.length - 2).
+		. For each element, check if it is strictly greater than its left neighbor (mountain[i - 1]) and its right neighbor (mountain[i + 1]).
+	3. Add Index to List:
+		. If the element satisfies the condition, add the current index i to the peaks list.
+	4. Return Peaks:
+		. After completing the loop, return the list of peak indices.
+    */
 	
+	/*
+     * @param mountain: a list of integers
+     * @return: an array that consists of indices of peaks in the given array in any order
+     */
 	public List<Integer> findPeaks(int[] mountain) {
         List<Integer> peaks = new ArrayList<>();
         
