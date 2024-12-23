071b0264045935fb5384ffa19edaf1c5d92f6b25
Jin Wang <specter01wj@gmail.com>
2024-12-23 12:43:12 -0500

    v8

--- mysol/Category/Array/E2960_Count_Tested_Devices_After_Test_Operations/ts/code.ts
+++ mysol/Category/Array/E2960_Count_Tested_Devices_After_Test_Operations/ts/code.ts
@@ -1,30 +1,24 @@
-function findIntersectionValues(nums1: number[], nums2: number[]): number[] {
-  // Create sets to store the elements of each array
-  const set1: Set<number> = new Set(nums1);
-  const set2: Set<number> = new Set(nums2);
+function countTestedDevices(batteryPercentages: number[]): number {
+  let testedDevices = 0;
 
-  // Count indices in nums1 that exist in nums2
-  let answer1: number = 0;
-  for (const num of nums1) {
-      if (set2.has(num)) {
-          answer1++;
+  for (let i = 0; i < batteryPercentages.length; i++) {
+      // Check if the current device can be tested
+      if (batteryPercentages[i] > 0) {
+          testedDevices++;
+          // Decrease the battery percentage for devices in range [i + 1, n - 1]
+          for (let j = i + 1; j < batteryPercentages.length; j++) {
+              batteryPercentages[j] = Math.max(0, batteryPercentages[j] - 1);
+          }
       }
   }
 
-  // Count indices in nums2 that exist in nums1
-  let answer2: number = 0;
-  for (const num of nums2) {
-      if (set1.has(num)) {
-          answer2++;
-      }
-  }
-
-  return [answer1, answer2];
+  return testedDevices;
 };
 
-const input1: number[] = [4,3,2,3,1], input2: number[] = [2,2,5,2,3,6];
-const results = findIntersectionValues(input1, input2);
+const input: number[] = [1,1,2,1,3];
+const inputCopy: number[] = [...input];
+const results = countTestedDevices(inputCopy);
 
 let webHeading = document.querySelector('#t1');
-webHeading.innerHTML = 'Input: ' + JSON.stringify(input1, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
 
