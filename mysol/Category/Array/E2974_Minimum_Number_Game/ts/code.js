2e2258497ad743b464dec9b9571f55293aaa44ac
Jin Wang <specter01wj@gmail.com>
2024-12-24 12:30:24 -0500

    v8

---
+++ mysol/Category/Array/E2974_Minimum_Number_Game/ts/code.js
@@ -0,0 +1,22 @@
+function numberGame(nums) {
+    // Sort the array to handle minimum elements
+    nums.sort((a, b) => a - b);
+    // Result array to store the output
+    const arr = [];
+    // Simulate the game
+    while (nums.length > 0) {
+        // Alice removes the minimum element
+        const alice = nums.shift(); // Use `!` to indicate that shift won't return undefined
+        // Bob removes the minimum element
+        const bob = nums.shift(); // Use `!` to indicate that shift won't return undefined
+        // Bob appends to the array first, then Alice
+        arr.push(bob);
+        arr.push(alice);
+    }
+    return arr;
+}
+;
+const input = [5, 4, 2, 3];
+const results = numberGame(input);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
