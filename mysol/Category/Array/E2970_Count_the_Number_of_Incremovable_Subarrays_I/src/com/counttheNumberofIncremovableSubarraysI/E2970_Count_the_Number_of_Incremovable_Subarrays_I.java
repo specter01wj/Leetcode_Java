d699003c76982f2494e0c1aeec1c3fb7641c9e24
Jin Wang <specter01wj@gmail.com>
2024-12-24 11:49:22 -0500

    v1

---
+++ mysol/Category/Array/E2970_Count_the_Number_of_Incremovable_Subarrays_I/src/com/counttheNumberofIncremovableSubarraysI/E2970_Count_the_Number_of_Incremovable_Subarrays_I.java
@@ -0,0 +1,10 @@
+package com.counttheNumberofIncremovableSubarraysI;
+
+public class E2970_Count_the_Number_of_Incremovable_Subarrays_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
