a0436cb1f36a29b033ec9a58dc078380dfc33aaa
Jin Wang <specter01wj@gmail.com>
2024-12-30 12:09:41 -0500

    v1

---
+++ mysol/Category/Array/E2980_Check_if_Bitwise_OR_Has_Trailing_Zeros/src/com/checkifBitwiseORHasTrailingZeros/E2980_Check_if_Bitwise_OR_Has_Trailing_Zeros.java
@@ -0,0 +1,10 @@
+package com.checkifBitwiseORHasTrailingZeros;
+
+public class E2980_Check_if_Bitwise_OR_Has_Trailing_Zeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
