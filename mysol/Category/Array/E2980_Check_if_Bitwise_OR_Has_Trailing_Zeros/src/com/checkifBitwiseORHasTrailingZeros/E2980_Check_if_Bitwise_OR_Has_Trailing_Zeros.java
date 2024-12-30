992d3f59da0a8dcbcb03259ef18c1e71f3bc12be
Jin Wang <specter01wj@gmail.com>
2024-12-30 12:32:48 -0500

    v3

--- mysol/Category/Array/E2980_Check_if_Bitwise_OR_Has_Trailing_Zeros/src/com/checkifBitwiseORHasTrailingZeros/E2980_Check_if_Bitwise_OR_Has_Trailing_Zeros.java
+++ mysol/Category/Array/E2980_Check_if_Bitwise_OR_Has_Trailing_Zeros/src/com/checkifBitwiseORHasTrailingZeros/E2980_Check_if_Bitwise_OR_Has_Trailing_Zeros.java
@@ -25,7 +25,20 @@ public class E2980_Check_if_Bitwise_OR_Has_Trailing_Zeros {
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    1. Outer Loop: Iterates through each element in the array.
+	2. Inner Loop: Iterates through the subsequent elements to form all pairs.
+	3. Bitwise OR: Calculates the OR of two selected numbers.
+	4. Trailing Zero Check: (bitwiseOr & 1) == 0 checks if the least significant bit is zero, indicating at least one trailing zero.
+	5. Return True: If a valid pair is found, return true immediately.
+	6. Return False: If no pairs satisfy the condition, return false.
+    */
 	
+	/*
+     * @param nums: a list of integers
+     * @return: true if it is possible to select two or more elements whose bitwise OR has trailing zeros
+     */
 	public boolean hasTrailingZeros(int[] nums) {
         // Iterate through all pairs of elements in the array
         for (int i = 0; i < nums.length; i++) {
