73a519808b81db8d3e52d0ca9090212376b7d9a1
Jin Wang <specter01wj@gmail.com>
2024-12-30 13:03:04 -0500

    v1

---
+++ mysol/Category/Array/E2996_Smallest_Missing_Integer_Greater_Than_Sequential_Prefix_Sum/src/com/smallestMissingIntegerGreaterThanSequentialPrefixSum/E2996_Smallest_Missing_Integer_Greater_Than_Sequential_Prefix_Sum.java
@@ -0,0 +1,10 @@
+package com.smallestMissingIntegerGreaterThanSequentialPrefixSum;
+
+public class E2996_Smallest_Missing_Integer_Greater_Than_Sequential_Prefix_Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
