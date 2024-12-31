4039de6955f0c3588cf726d8e8eb978e4369720b
Jin Wang <specter01wj@gmail.com>
2024-12-31 12:06:14 -0500

    v1

---
+++ mysol/Category/Array/E3000_Maximum_Area_of_Longest_Diagonal_Rectangle/src/com/maximumAreaofLongestDiagonalRectangle/E3000_Maximum_Area_of_Longest_Diagonal_Rectangle.java
@@ -0,0 +1,10 @@
+package com.maximumAreaofLongestDiagonalRectangle;
+
+public class E3000_Maximum_Area_of_Longest_Diagonal_Rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
