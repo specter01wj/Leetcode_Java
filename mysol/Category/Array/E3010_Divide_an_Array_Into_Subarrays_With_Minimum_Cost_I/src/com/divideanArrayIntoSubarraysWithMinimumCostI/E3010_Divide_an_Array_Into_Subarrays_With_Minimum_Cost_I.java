3362ea1c90eb63fd1425bdfa658d9414e1f8d292
Jin Wang <specter01wj@gmail.com>
2025-01-06 11:54:06 -0500

    v1

---
+++ mysol/Category/Array/E3010_Divide_an_Array_Into_Subarrays_With_Minimum_Cost_I/src/com/divideanArrayIntoSubarraysWithMinimumCostI/E3010_Divide_an_Array_Into_Subarrays_With_Minimum_Cost_I.java
@@ -0,0 +1,10 @@
+package com.divideanArrayIntoSubarraysWithMinimumCostI;
+
+public class E3010_Divide_an_Array_Into_Subarrays_With_Minimum_Cost_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
