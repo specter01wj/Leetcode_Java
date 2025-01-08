3edc5d2009f1098894d9e6ff09e3cd21c23e6303
Jin Wang <specter01wj@gmail.com>
2025-01-08 11:52:34 -0500

    v2

--- mysol/Category/Array/E3028_Ant_on_the_Boundary/src/com/antontheBoundary/E3028_Ant_on_the_Boundary.java
+++ mysol/Category/Array/E3028_Ant_on_the_Boundary/src/com/antontheBoundary/E3028_Ant_on_the_Boundary.java
@@ -1,10 +1,48 @@
 package com.antontheBoundary;
+import java.util.*;
+
+/*
+An ant is on a boundary. It sometimes goes left and sometimes right.
+
+You are given an array of non-zero integers nums. The ant starts 
+reading nums from the first element of it to its end. At each step, 
+it moves according to the value of the current element:
+
+	. If nums[i] < 0, it moves left by -nums[i] units.
+	. If nums[i] > 0, it moves right by nums[i] units.
+
+Return the number of times the ant returns to the boundary.
+*/
 
 public class E3028_Ant_on_the_Boundary {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E3028_Ant_on_the_Boundary solution = new E3028_Ant_on_the_Boundary();
+		int[] input = {2,3,-5};
+		int output = solution.returnToBoundaryCount(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public int returnToBoundaryCount(int[] nums) {
+        // Variable to track the current position of the ant
+        int position = 0;
+        // Variable to count the number of times the ant returns to the boundary
+        int boundaryCount = 0;
+
+        // Iterate through the given array
+        for (int num : nums) {
+            // Update the position of the ant based on the current step
+            position += num;
+
+            // Check if the ant is back to the boundary
+            if (position == 0) {
+                boundaryCount++;
+            }
+        }
+
+        // Return the total count
+        return boundaryCount;
+    }
 
 }
