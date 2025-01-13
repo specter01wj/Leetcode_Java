c945fba4986f5c13ee5cfcb0341f8a3c8c245e6f
Jin Wang <specter01wj@gmail.com>
2025-01-13 12:17:56 -0500

    v1

---
+++ mysol/Category/Array/E3038_Maximum_Number_of_Operations_With_the_Same_Score_I/src/com/maximumNumberofOperationsWiththeSameScoreI/E3038_Maximum_Number_of_Operations_With_the_Same_Score_I.java
@@ -0,0 +1,10 @@
+package com.maximumNumberofOperationsWiththeSameScoreI;
+
+public class E3038_Maximum_Number_of_Operations_With_the_Same_Score_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
