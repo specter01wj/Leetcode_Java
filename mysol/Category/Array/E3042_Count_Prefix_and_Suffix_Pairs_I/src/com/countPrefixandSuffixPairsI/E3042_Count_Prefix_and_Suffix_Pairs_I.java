4aedb4d98df198d6962aaa777368a98b5eb60d94
Jin Wang <specter01wj@gmail.com>
2025-01-14 01:02:42 -0500

    v1

---
+++ mysol/Category/Array/E3042_Count_Prefix_and_Suffix_Pairs_I/src/com/countPrefixandSuffixPairsI/E3042_Count_Prefix_and_Suffix_Pairs_I.java
@@ -0,0 +1,10 @@
+package com.countPrefixandSuffixPairsI;
+
+public class E3042_Count_Prefix_and_Suffix_Pairs_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
