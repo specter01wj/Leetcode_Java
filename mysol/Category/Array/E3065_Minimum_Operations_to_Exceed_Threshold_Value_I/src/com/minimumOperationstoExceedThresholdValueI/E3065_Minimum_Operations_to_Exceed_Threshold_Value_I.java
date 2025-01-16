f85d31fea2292ac0bc88c79fabe5d646d4d4f4d8
Jin Wang <specter01wj@gmail.com>
2025-01-16 02:31:17 -0500

    v1

---
+++ mysol/Category/Array/E3065_Minimum_Operations_to_Exceed_Threshold_Value_I/src/com/minimumOperationstoExceedThresholdValueI/E3065_Minimum_Operations_to_Exceed_Threshold_Value_I.java
@@ -0,0 +1,10 @@
+package com.minimumOperationstoExceedThresholdValueI;
+
+public class E3065_Minimum_Operations_to_Exceed_Threshold_Value_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
