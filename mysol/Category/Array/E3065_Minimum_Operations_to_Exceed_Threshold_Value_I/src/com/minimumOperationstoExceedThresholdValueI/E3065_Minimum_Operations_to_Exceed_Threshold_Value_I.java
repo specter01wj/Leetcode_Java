5dcfa088afc16565945ae5fbc20799a093adcc22
Jin Wang <specter01wj@gmail.com>
2025-01-16 02:39:32 -0500

    v3

--- mysol/Category/Array/E3065_Minimum_Operations_to_Exceed_Threshold_Value_I/src/com/minimumOperationstoExceedThresholdValueI/E3065_Minimum_Operations_to_Exceed_Threshold_Value_I.java
+++ mysol/Category/Array/E3065_Minimum_Operations_to_Exceed_Threshold_Value_I/src/com/minimumOperationstoExceedThresholdValueI/E3065_Minimum_Operations_to_Exceed_Threshold_Value_I.java
@@ -16,11 +16,27 @@ public class E3065_Minimum_Operations_to_Exceed_Threshold_Value_I {
 		E3065_Minimum_Operations_to_Exceed_Threshold_Value_I solution = new E3065_Minimum_Operations_to_Exceed_Threshold_Value_I();
 		int[] input = {2,11,10,1,3};
 		int k = 10;
+		int[] originalArray = Arrays.copyOf(input, input.length);
 		int output = solution.minOperations(input, k);
-        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+        System.out.println("input: " + Arrays.toString(originalArray) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    1. Sorting:
+		. The array is sorted to ensure that the smallest elements are processed first.
+	2. Iterate and Count:
+		. Start from the smallest element (index 0) and check if it is less than k.
+		. If it is, increment the operations counter and move to the next element.
+	3. Termination:
+		. Stop when all elements are greater than or equal to k.
+    */
 	
+	/*
+     * @param nums: a list of integers
+     * @param k: an integer
+     * @return: the minimum number of operations
+     */
 	public int minOperations(int[] nums, int k) {
         // Sort the array to make it easier to remove the smallest elements
         Arrays.sort(nums);
