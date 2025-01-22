0ea98540ab8c1f586eef5e15fbf40a57ab70e8df
Jin Wang <specter01wj@gmail.com>
2025-01-22 02:33:13 -0500

    v1

---
+++ mysol/Category/Array/E3069_Distribute_Elements_Into_Two_Arrays_I/src/com/distributeElementsIntoTwoArraysI/E3069_Distribute_Elements_Into_Two_Arrays_I.java
@@ -0,0 +1,10 @@
+package com.distributeElementsIntoTwoArraysI;
+
+public class E3069_Distribute_Elements_Into_Two_Arrays_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
