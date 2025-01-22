ede7f8f3d38b5123d80e1b40ad7220f77d5d7369
Jin Wang <specter01wj@gmail.com>
2025-01-22 03:10:42 -0500

    v6

--- mysol/Category/Array/E3074_Apple_Redistribution_into_Boxes/ts/code.ts
+++ mysol/Category/Array/E3074_Apple_Redistribution_into_Boxes/ts/code.ts
@@ -1,26 +1,27 @@
-function resultArray(nums: number[]): number[] {
-  let arr1: number[] = [];
-  let arr2: number[] = [];
+function minimumBoxes(apple: number[], capacity: number[]): number {
+  // Calculate the total number of apples
+  const totalApples = apple.reduce((sum, count) => sum + count, 0);
 
-  // Add the first element to arr1 and the second element to arr2
-  arr1.push(nums[0]);
-  arr2.push(nums[1]);
+  // Sort the capacity array in descending order
+  capacity.sort((a, b) => b - a);
 
-  // Start from the third element
-  for (let i = 2; i < nums.length; i++) {
-      if (arr1[arr1.length - 1] > arr2[arr2.length - 1]) {
-          arr1.push(nums[i]);
-      } else {
-          arr2.push(nums[i]);
+  let boxesUsed = 0;
+  let currentCapacity = 0;
+
+  // Select boxes until the total capacity meets or exceeds the total apples
+  for (const box of capacity) {
+      currentCapacity += box;
+      boxesUsed++;
+      if (currentCapacity >= totalApples) {
+          break;
       }
   }
 
-  // Combine arr1 and arr2 into the result array
-  return [...arr1, ...arr2];
+  return boxesUsed;
 };
 
-const input: number[] = [5,4,3,8];
-const results = resultArray(input);
+const input: number[] = [1,3,2], capacity: number[] = [4,3,1,5,2];
+const results = minimumBoxes(input, capacity);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
