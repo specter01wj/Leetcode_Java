2d7be296ca8f0a7f467f7306b632f8e8e6747dfb
Jin Wang <specter01wj@gmail.com>
2025-01-22 02:39:57 -0500

    v3

---
+++ mysol/Category/Array/E3069_Distribute_Elements_Into_Two_Arrays_I/ts/code.ts
@@ -0,0 +1,25 @@
+function minOperations(nums: number[], k: number): number {
+  // Sort the array in ascending order
+  nums.sort((a, b) => a - b);
+  
+  let operations = 0;
+  let i = 0;
+
+  // Iterate through the array until all elements are >= k
+  while (i < nums.length && nums[i] < k) {
+      operations++;
+      i++;
+  }
+
+  // Return the operations count
+  return operations;
+};
+
+const input: number[] = [2,11,10,1,3];
+const k: number = 10;
+const originalArray = [...input];
+const results = minOperations(input, k);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(originalArray, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
