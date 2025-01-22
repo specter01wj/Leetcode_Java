d2b360327d40b5c5186bf6f9d52e98b999545698
Jin Wang <specter01wj@gmail.com>
2025-01-22 02:58:03 -0500

    v1

---
+++ mysol/Category/Array/E3074_Apple_Redistribution_into_Boxes/src/com/appleRedistributionintoBoxes/E3074_Apple_Redistribution_into_Boxes.java
@@ -0,0 +1,10 @@
+package com.appleRedistributionintoBoxes;
+
+public class E3074_Apple_Redistribution_into_Boxes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
