c88953f580a6041ca6133be791729ddfd52d47ec
Jin Wang <specter01wj@gmail.com>
2025-01-24 20:51:44 -0500

    v1

---
+++ mysol/Category/Array/E3095_Shortest_Subarray_With_OR_at_Least_K_I/src/com/shortestSubarrayWithORatLeastKI/E3095_Shortest_Subarray_With_OR_at_Least_K_I.java
@@ -0,0 +1,10 @@
+package com.shortestSubarrayWithORatLeastKI;
+
+public class E3095_Shortest_Subarray_With_OR_at_Least_K_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
