4f3d95acdb0231a035a54ca18a0bbd326ccf1c68
Jin Wang <specter01wj@gmail.com>
2025-01-27 18:11:40 -0500

    v1

---
+++ mysol/Category/Array/E3105_Longest_Strictly_Increasing_or_Strictly_Decreasing_Subarray/src/com/longestStrictlyIncreasingorStrictlyDecreasingSubarray/E3105_Longest_Strictly_Increasing_or_Strictly_Decreasing_Subarray.java
@@ -0,0 +1,10 @@
+package com.longestStrictlyIncreasingorStrictlyDecreasingSubarray;
+
+public class E3105_Longest_Strictly_Increasing_or_Strictly_Decreasing_Subarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
