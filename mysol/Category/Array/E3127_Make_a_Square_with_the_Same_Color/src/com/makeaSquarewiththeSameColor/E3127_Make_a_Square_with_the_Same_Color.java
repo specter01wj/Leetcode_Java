b8a20f2a27c595d44650ae9308a2205b7770352e
Jin Wang <specter01wj@gmail.com>
2025-01-28 02:02:33 -0500

    v1

---
+++ mysol/Category/Array/E3127_Make_a_Square_with_the_Same_Color/src/com/makeaSquarewiththeSameColor/E3127_Make_a_Square_with_the_Same_Color.java
@@ -0,0 +1,10 @@
+package com.makeaSquarewiththeSameColor;
+
+public class E3127_Make_a_Square_with_the_Same_Color {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
