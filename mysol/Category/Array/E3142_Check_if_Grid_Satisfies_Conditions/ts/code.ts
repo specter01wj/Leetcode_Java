5224cd161f5bdcad38f3e1cfb0ff4f7245a2a8d6
Jin Wang <specter01wj@gmail.com>
2025-01-29 12:15:33 -0500

    v6

--- mysol/Category/Array/E3142_Check_if_Grid_Satisfies_Conditions/ts/code.ts
+++ mysol/Category/Array/E3142_Check_if_Grid_Satisfies_Conditions/ts/code.ts
@@ -1,33 +1,25 @@
-function canMakeSquare(grid: string[][]): boolean {
-  // Loop through all possible 2x2 squares in the 3x3 grid
-  for (let i = 0; i < 2; i++) {
-      for (let j = 0; j < 2; j++) {
-          // Count the occurrences of 'B' and 'W' in the current 2x2 square
-          let countB = 0;
-          let countW = 0;
+function satisfiesConditions(grid: number[][]): boolean {
+  const m = grid.length;
+  const n = grid[0].length;
 
-          for (let x = i; x < i + 2; x++) {
-              for (let y = j; y < j + 2; y++) {
-                  if (grid[x][y] === 'B') {
-                      countB++;
-                  } else {
-                      countW++;
-                  }
-              }
+  for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+          // Check if grid[i][j] is equal to the cell below it
+          if (i < m - 1 && grid[i][j] !== grid[i + 1][j]) {
+              return false;
           }
-
-          // Check if the current 2x2 square can be made uniform by changing at most one cell
-          if (countB >= 3 || countW >= 3) {
-              return true;
+          // Check if grid[i][j] is different from the cell to its right
+          if (j < n - 1 && grid[i][j] === grid[i][j + 1]) {
+              return false;
           }
       }
   }
 
-  return false;
+  return true;
 };
 
-const input: string[][] = [["B","W","B"],["B","W","W"],["B","W","B"]];
-const results = canMakeSquare(input);
+const input: int[][] = [[1,0,2],[1,0,2]];
+const results = satisfiesConditions(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
