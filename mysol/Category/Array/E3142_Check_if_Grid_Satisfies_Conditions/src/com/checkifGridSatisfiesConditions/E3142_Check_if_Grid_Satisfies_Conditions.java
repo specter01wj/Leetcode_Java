d5d71c960166b56765018d3a8a059189f914ee47
Jin Wang <specter01wj@gmail.com>
2025-01-29 12:04:12 -0500

    v1

---
+++ mysol/Category/Array/E3142_Check_if_Grid_Satisfies_Conditions/src/com/checkifGridSatisfiesConditions/E3142_Check_if_Grid_Satisfies_Conditions.java
@@ -0,0 +1,10 @@
+package com.checkifGridSatisfiesConditions;
+
+public class E3142_Check_if_Grid_Satisfies_Conditions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
