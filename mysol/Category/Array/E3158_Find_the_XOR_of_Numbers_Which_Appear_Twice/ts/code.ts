2219b75373e6ba2d9c674a1b4a8844ab968bbc02
Jin Wang <specter01wj@gmail.com>
2025-02-11 01:53:11 -0500

    v5

---
+++ mysol/Category/Array/E3158_Find_the_XOR_of_Numbers_Which_Appear_Twice/ts/code.ts
@@ -0,0 +1,15 @@
+function isArraySpecial(nums: number[]): boolean {
+  for (let i = 1; i < nums.length; i++) {
+      if ((nums[i] % 2) === (nums[i - 1] % 2)) {
+          return false; // Adjacent elements have the same parity
+      }
+  }
+  return true; // All adjacent pairs have different parity
+};
+
+const input: number[] = [2,1,4];
+const results = isArraySpecial(input);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
