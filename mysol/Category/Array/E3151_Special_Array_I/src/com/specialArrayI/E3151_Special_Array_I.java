247f8e5442ffa0c4d49c040f0c1714ea50d4b4f1
Jin Wang <specter01wj@gmail.com>
2025-02-11 01:20:25 -0500

    v2

--- mysol/Category/Array/E3151_Special_Array_I/src/com/specialArrayI/E3151_Special_Array_I.java
+++ mysol/Category/Array/E3151_Special_Array_I/src/com/specialArrayI/E3151_Special_Array_I.java
@@ -1,10 +1,32 @@
 package com.specialArrayI;
+import java.util.*;
+
+/*
+An array is considered special if every pair of its adjacent 
+elements contains two numbers with different parity.
+
+You are given an array of integers nums. Return true if nums 
+is a special array, otherwise, return false.
+*/
 
 public class E3151_Special_Array_I {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E3151_Special_Array_I solution = new E3151_Special_Array_I();
+		int[] input = {2,1,4};
+		boolean output = solution.isArraySpecial(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	
+	public boolean isArraySpecial(int[] nums) {
+		for (int i = 1; i < nums.length; i++) {
+			if ((nums[i] % 2) == (nums[i - 1] % 2)) {
+				return false; // Adjacent elements have the same parity
+			}
+		}
+		
+		return true; // All adjacent pairs have different parity
 	}
 
 }
