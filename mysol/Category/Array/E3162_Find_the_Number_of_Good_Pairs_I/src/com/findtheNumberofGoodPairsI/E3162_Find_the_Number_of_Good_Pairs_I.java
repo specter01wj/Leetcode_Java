2142e544c496966e4f250e2fb3565e4103c20a64
Jin Wang <specter01wj@gmail.com>
2025-02-12 18:12:35 -0500

    v1

---
+++ mysol/Category/Array/E3162_Find_the_Number_of_Good_Pairs_I/src/com/findtheNumberofGoodPairsI/E3162_Find_the_Number_of_Good_Pairs_I.java
@@ -0,0 +1,10 @@
+package com.findtheNumberofGoodPairsI;
+
+public class E3162_Find_the_Number_of_Good_Pairs_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
