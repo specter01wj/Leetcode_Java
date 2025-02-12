23db537151b921fcbe1ff19e619891714d887aab
Jin Wang <specter01wj@gmail.com>
2025-02-12 18:17:10 -0500

    v2

--- mysol/Category/Array/E3162_Find_the_Number_of_Good_Pairs_I/src/com/findtheNumberofGoodPairsI/E3162_Find_the_Number_of_Good_Pairs_I.java
+++ mysol/Category/Array/E3162_Find_the_Number_of_Good_Pairs_I/src/com/findtheNumberofGoodPairsI/E3162_Find_the_Number_of_Good_Pairs_I.java
@@ -1,10 +1,39 @@
 package com.findtheNumberofGoodPairsI;
+import java.util.*;
+
+/*
+You are given 2 integer arrays nums1 and nums2 of lengths n 
+and m respectively. You are also given a positive integer k.
+
+A pair (i, j) is called good if nums1[i] is divisible by 
+nums2[j] * k (0 <= i <= n - 1, 0 <= j <= m - 1).
+
+Return the total number of good pairs.
+*/
 
 public class E3162_Find_the_Number_of_Good_Pairs_I {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E3162_Find_the_Number_of_Good_Pairs_I solution = new E3162_Find_the_Number_of_Good_Pairs_I();
+		int[] nums1 = {1,2,4,12}, nums2 = {2,4};
+		int k = 3;
+		int output = solution.numberOfPairs(nums1, nums2, k);
+        System.out.println("input: " + Arrays.toString(nums1) + "\noutput: " + (output));
+	}
+	
+	
+	public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+		int count = 0;
+		
+		for (int i = 0; i < nums1.length; i++) {
+			for (int j = 0; j < nums2.length; j++) {
+				if (nums1[i] % (nums2[j] * k) == 0) {
+					count++;
+				}
+			}
+		}
+		
+		return count;
 	}
 
 }
