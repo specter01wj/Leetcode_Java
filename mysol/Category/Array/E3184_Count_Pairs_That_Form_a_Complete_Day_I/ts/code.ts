534d17c931e45d6144d50bcdafdce1bdf652557f
Jin Wang <specter01wj@gmail.com>
2025-02-20 00:16:25 -0500

    v8

--- mysol/Category/Array/E3184_Count_Pairs_That_Form_a_Complete_Day_I/ts/code.ts
+++ mysol/Category/Array/E3184_Count_Pairs_That_Form_a_Complete_Day_I/ts/code.ts
@@ -1,16 +1,20 @@
-function orArray(nums: number[]): number[] {
-  const n = nums.length;
-  const answer: number[] = new Array(n - 1);
-  
-  for (let i = 0; i < n - 1; i++) {
-      answer[i] = nums[i] | nums[i + 1];
+function countCompleteDayPairs(hours: number[]): number {
+  let remainderCount: Map<number, number> = new Map();
+  let count: number = 0;
+
+  for (let hour of hours) {
+      let remainder: number = hour % 24;
+      let complement: number = (24 - remainder) % 24;
+      
+      count += remainderCount.get(complement) || 0;
+      remainderCount.set(remainder, (remainderCount.get(remainder) || 0) + 1);
   }
-  
-  return answer;
+
+  return count;
 };
 
-const input: number[] = [1,3,7,15];
-const results = orArray(input);
+const input: number[] = [12,12,30,24,24];
+const results = countCompleteDayPairs(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
