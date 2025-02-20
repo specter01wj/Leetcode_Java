fbf2acb55a26edf60c9bc68411d0baa9546f9573
Jin Wang <specter01wj@gmail.com>
2025-02-20 00:56:49 -0500

    v8

--- mysol/Category/Array/E3190_Find_Minimum_Operations_to_Make_All_Elements_Divisible_by_Three/ts/code.ts
+++ mysol/Category/Array/E3190_Find_Minimum_Operations_to_Make_All_Elements_Divisible_by_Three/ts/code.ts
@@ -1,20 +1,17 @@
-function countCompleteDayPairs(hours: number[]): number {
-  let remainderCount: Map<number, number> = new Map();
-  let count: number = 0;
+function minimumOperations(nums: number[]): number {
+  let count = 0;
 
-  for (let hour of hours) {
-      let remainder: number = hour % 24;
-      let complement: number = (24 - remainder) % 24;
-      
-      count += remainderCount.get(complement) || 0;
-      remainderCount.set(remainder, (remainderCount.get(remainder) || 0) + 1);
+  for (let num of nums) {
+      if (num % 3 !== 0) {
+          count++;
+      }
   }
-
+  
   return count;
 };
 
-const input: number[] = [12,12,30,24,24];
-const results = countCompleteDayPairs(input);
+const input: number[] = [1,2,3,4];
+const results = minimumOperations(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
