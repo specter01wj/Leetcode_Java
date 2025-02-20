34782424a25874fd656662c6b00e54659c6cad2b
Jin Wang <specter01wj@gmail.com>
2025-02-20 00:46:19 -0500

    v1

---
+++ mysol/Category/Array/E3190_Find_Minimum_Operations_to_Make_All_Elements_Divisible_by_Three/src/com/findMinimumOperationstoMakeAllElementsDivisiblebyThree/E3190_Find_Minimum_Operations_to_Make_All_Elements_Divisible_by_Three.java
@@ -0,0 +1,10 @@
+package com.findMinimumOperationstoMakeAllElementsDivisiblebyThree;
+
+public class E3190_Find_Minimum_Operations_to_Make_All_Elements_Divisible_by_Three {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
