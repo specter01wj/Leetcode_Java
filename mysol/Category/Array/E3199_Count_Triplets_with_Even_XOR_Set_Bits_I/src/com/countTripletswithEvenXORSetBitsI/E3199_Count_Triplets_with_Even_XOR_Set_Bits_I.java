2540658b7e60461a131293e34af510916b55123a
Jin Wang <specter01wj@gmail.com>
2025-02-21 02:32:23 -0500

    v5

---
+++ mysol/Category/Array/E3199_Count_Triplets_with_Even_XOR_Set_Bits_I/src/com/countTripletswithEvenXORSetBitsI/E3199_Count_Triplets_with_Even_XOR_Set_Bits_I.java
@@ -0,0 +1,10 @@
+package com.countTripletswithEvenXORSetBitsI;
+
+public class E3199_Count_Triplets_with_Even_XOR_Set_Bits_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
