3278b736736b5e85087518565b7400adc715b1be
Jin Wang <specter01wj@gmail.com>
2025-02-20 20:37:14 -0500

    v3

--- mysol/Category/Array/E3194_Minimum_Average_of_Smallest_and_Largest_Elements/src/com/minimumAverageofSmallestandLargestElements/E3194_Minimum_Average_of_Smallest_and_Largest_Elements.java
+++ mysol/Category/Array/E3194_Minimum_Average_of_Smallest_and_Largest_Elements/src/com/minimumAverageofSmallestandLargestElements/E3194_Minimum_Average_of_Smallest_and_Largest_Elements.java
@@ -1,10 +1,41 @@
 package com.minimumAverageofSmallestandLargestElements;
+import java.util.*;
+
+/*
+You have an array of floating point numbers averages which is initially empty. 
+You are given an array nums of n integers where n is even.
+
+You repeat the following procedure n / 2 times:
+
+	. Remove the smallest element, minElement, and the largest element maxElement, from nums.
+	. Add (minElement + maxElement) / 2 to averages.
+
+Return the minimum element in averages.
+*/
 
 public class E3194_Minimum_Average_of_Smallest_and_Largest_Elements {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E3194_Minimum_Average_of_Smallest_and_Largest_Elements solution = new E3194_Minimum_Average_of_Smallest_and_Largest_Elements();
+		int[] input = {7,8,3,4,15,13,4,1};
+		double output = solution.minimumAverage(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public double minimumAverage(int[] nums) {
+        Arrays.sort(nums);
+        List<Double> averages = new ArrayList<>();
+        int left = 0, right = nums.length - 1;
+        
+        while (left < right) {
+            double avg = (nums[left] + nums[right]) / 2.0;
+            averages.add(avg);
+            left++;
+            right--;
+        }
+        
+        return Collections.min(averages);
+    }
 
 }
