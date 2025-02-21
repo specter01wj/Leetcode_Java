5b6827e0e13294c7f116ac3bd23400159d1819e0
Jin Wang <specter01wj@gmail.com>
2025-02-21 02:24:09 -0500

    v4

--- mysol/Category/Array/E3194_Minimum_Average_of_Smallest_and_Largest_Elements/ts/code.ts
+++ mysol/Category/Array/E3194_Minimum_Average_of_Smallest_and_Largest_Elements/ts/code.ts
@@ -1,17 +1,20 @@
-function minimumOperations(nums: number[]): number {
-  let count = 0;
+function minimumAverage(nums: number[]): number {
+  nums.sort((a, b) => a - b);
+  let averages: number[] = [];
+  let left = 0, right = nums.length - 1;
 
-  for (let num of nums) {
-      if (num % 3 !== 0) {
-          count++;
-      }
+  while (left < right) {
+      let avg = (nums[left] + nums[right]) / 2;
+      averages.push(avg);
+      left++;
+      right--;
   }
-  
-  return count;
+
+  return Math.min(...averages);
 };
 
-const input: number[] = [1,2,3,4];
-const results = minimumOperations(input);
+const input: number[] = [7,8,3,4,15,13,4,1];
+const results = minimumAverage([...input]);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
