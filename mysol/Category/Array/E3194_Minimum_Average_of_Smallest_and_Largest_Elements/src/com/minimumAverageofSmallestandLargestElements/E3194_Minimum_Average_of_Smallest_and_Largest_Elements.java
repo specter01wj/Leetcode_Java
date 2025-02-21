9435830623f40dc3f9901b51b1e2224613df4fe9
Jin Wang <specter01wj@gmail.com>
2025-02-20 20:33:05 -0500

    v1

---
+++ mysol/Category/Array/E3194_Minimum_Average_of_Smallest_and_Largest_Elements/src/com/minimumAverageofSmallestandLargestElements/E3194_Minimum_Average_of_Smallest_and_Largest_Elements.java
@@ -0,0 +1,10 @@
+package com.minimumAverageofSmallestandLargestElements;
+
+public class E3194_Minimum_Average_of_Smallest_and_Largest_Elements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
