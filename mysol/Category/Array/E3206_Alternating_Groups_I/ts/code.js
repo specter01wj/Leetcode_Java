447c2dec8dfef8246dd9612681a49de4e501c34c
Jin Wang <specter01wj@gmail.com>
2025-02-25 03:03:48 -0500

    v6

---
+++ mysol/Category/Array/E3206_Alternating_Groups_I/ts/code.js
@@ -0,0 +1,18 @@
+function numberOfAlternatingGroups(colors) {
+    let n = colors.length;
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+        let prev = colors[(i - 1 + n) % n];
+        let curr = colors[i];
+        let next = colors[(i + 1) % n];
+        if (prev !== curr && curr !== next) {
+            count++;
+        }
+    }
+    return count;
+}
+;
+const input = [0, 1, 0, 0, 1];
+const results = numberOfAlternatingGroups(input);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
