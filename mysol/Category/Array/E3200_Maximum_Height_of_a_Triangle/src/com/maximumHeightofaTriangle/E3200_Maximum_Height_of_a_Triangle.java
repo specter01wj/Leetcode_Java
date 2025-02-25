f13a7c91a042089a0bbedddb6b5e731ce16e136c
Jin Wang <specter01wj@gmail.com>
2025-02-25 02:24:31 -0500

    v1

--- mysol/Category/Array/E3200_Maximum_Height_of_a_Triangle/src/com/maximumHeightofaTriangle/E3200_Maximum_Height_of_a_Triangle.java
+++ mysol/Category/Array/E3200_Maximum_Height_of_a_Triangle/src/com/maximumHeightofaTriangle/E3200_Maximum_Height_of_a_Triangle.java
@@ -1,4 +1,18 @@
 package com.maximumHeightofaTriangle;
+import java.util.*;
+
+/*
+You are given two integers red and blue representing the count 
+of red and blue colored balls. You have to arrange these balls 
+to form a triangle such that the 1st row will have 1 ball, 
+the 2nd row will have 2 balls, the 3rd row will have 3 balls, 
+and so on.
+
+All the balls in a particular row should be the same color, 
+and adjacent rows should have different colors.
+
+Return the maximum height of the triangle that can be achieved.
+*/
 
 public class E3200_Maximum_Height_of_a_Triangle {
 
