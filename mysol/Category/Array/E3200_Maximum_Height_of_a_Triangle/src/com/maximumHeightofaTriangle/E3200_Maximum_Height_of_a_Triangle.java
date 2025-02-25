e3bc6b77b5b401990fc602faee77b74ce5b46e57
Jin Wang <specter01wj@gmail.com>
2025-02-25 02:16:09 -0500

    v1

---
+++ mysol/Category/Array/E3200_Maximum_Height_of_a_Triangle/src/com/maximumHeightofaTriangle/E3200_Maximum_Height_of_a_Triangle.java
@@ -0,0 +1,10 @@
+package com.maximumHeightofaTriangle;
+
+public class E3200_Maximum_Height_of_a_Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
