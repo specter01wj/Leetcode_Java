52c41b9fa45f1096de5e8145bc884b8174b1c545
Jin Wang <specter01wj@gmail.com>
2025-02-25 03:03:23 -0500

    v5

--- mysol/Category/Array/E3206_Alternating_Groups_I/ts/code.ts
+++ mysol/Category/Array/E3206_Alternating_Groups_I/ts/code.ts
@@ -1,32 +1,23 @@
-function tripletCount(a: number[], b: number[], c: number[]): number {
+function numberOfAlternatingGroups(colors: number[]): number {
+  let n = colors.length;
   let count = 0;
 
-  for (let numA of a) {
-      for (let numB of b) {
-          for (let numC of c) {
-              let xorValue = numA ^ numB ^ numC;
-              if (countSetBits(xorValue) % 2 === 0) {
-                  count++;
-              }
-          }
+  for (let i = 0; i < n; i++) {
+      let prev = colors[(i - 1 + n) % n];
+      let curr = colors[i];
+      let next = colors[(i + 1) % n];
+
+      if (prev !== curr && curr !== next) {
+          count++;
       }
   }
 
   return count;
 };
 
-function countSetBits(n: number): number {
-  let count = 0;
-  while (n > 0) {
-      count += n & 1;
-      n >>= 1;
-  }
-  return count;
-}
-
-const a: number[] = [1,1], b: number[] = [2,3], c: number[] = [1,5];
-const results = tripletCount(a, b, c);
+const input: number[] = [0,1,0,0,1];
+const results = numberOfAlternatingGroups(input);
 
 let webHeading = document.querySelector('#t1');
-webHeading.innerHTML = 'Input: ' + JSON.stringify(a, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
 
