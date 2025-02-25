2d3ec1e20b9680c431d7fdad4102d63e052947bf
Jin Wang <specter01wj@gmail.com>
2025-02-25 02:25:35 -0500

    v2

--- mysol/Category/Array/E3200_Maximum_Height_of_a_Triangle/src/com/maximumHeightofaTriangle/E3200_Maximum_Height_of_a_Triangle.java
+++ mysol/Category/Array/E3200_Maximum_Height_of_a_Triangle/src/com/maximumHeightofaTriangle/E3200_Maximum_Height_of_a_Triangle.java
@@ -17,8 +17,40 @@ Return the maximum height of the triangle that can be achieved.
 public class E3200_Maximum_Height_of_a_Triangle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E3200_Maximum_Height_of_a_Triangle solution = new E3200_Maximum_Height_of_a_Triangle();
+		int red = 2, blue = 1;
+		int output = solution.maxHeightOfTriangle(red, blue);
+        System.out.println("red: " + (red) + "; blue: " + (blue) + "\noutput: " + (output));
 	}
+	
+	
+	public int maxHeightOfTriangle(int red, int blue) {
+        return Math.max(buildTriangle(red, blue), buildTriangle(blue, red));
+    }
+
+    private int buildTriangle(int primary, int secondary) {
+        int height = 0;
+        int i = 1; // The number of balls required for the current row
+
+        while (true) {
+            if (i % 2 == 1) { // Odd rows use the primary color
+                if (primary >= i) {
+                    primary -= i;
+                } else {
+                    break;
+                }
+            } else { // Even rows use the secondary color
+                if (secondary >= i) {
+                    secondary -= i;
+                } else {
+                    break;
+                }
+            }
+            height++; // Successfully added another row
+            i++; // Move to the next row
+        }
+
+        return height;
+    }
 
 }
