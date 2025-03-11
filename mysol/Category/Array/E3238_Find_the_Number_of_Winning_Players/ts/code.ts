208ac8eb1937cc227af002c4eb0e7b4594580fc7
Jin Wang <specter01wj@gmail.com>
2025-03-11 12:20:41 -0400

    v6

--- mysol/Category/Array/E3238_Find_the_Number_of_Winning_Players/ts/code.ts
+++ mysol/Category/Array/E3238_Find_the_Number_of_Winning_Players/ts/code.ts
@@ -1,25 +1,33 @@
-function canAliceWin(nums: number[]): boolean {
-  let singleDigitSum = 0, doubleDigitSum = 0;
+function winningPlayerCount(n: number, pick: number[][]): number {
+  let playerMap = new Map<number, Map<number, number>>();
 
-  // Calculate the sum of single-digit and double-digit numbers
-  for (let num of nums) {
-      if (num < 10) {
-          singleDigitSum += num;
-      } else if (num < 100) {
-          doubleDigitSum += num;
+  // Count the number of balls of each color picked by each player
+  for (let [player, color] of pick) {
+      if (!playerMap.has(player)) {
+          playerMap.set(player, new Map<number, number>());
+      }
+      let colorMap = playerMap.get(player)!;
+      colorMap.set(color, (colorMap.get(color) || 0) + 1);
+  }
+
+  let count = 0;
+
+  // Check which players meet the winning condition
+  for (let [player, colorMap] of playerMap) {
+      for (let ballCount of colorMap.values()) {
+          if (ballCount > player) {
+              count++;
+              break; // No need to check further colors for this player
+          }
       }
   }
 
-  // Calculate Bob's sums in each case
-  let bobSumWhenAliceTakesSingles = doubleDigitSum;
-  let bobSumWhenAliceTakesDoubles = singleDigitSum;
-
-  // Check if Alice can win in either case
-  return singleDigitSum > bobSumWhenAliceTakesSingles || doubleDigitSum > bobSumWhenAliceTakesDoubles;
+  return count;
 };
 
-const input: number[] = [1,2,3,4,5,14];
-const results = canAliceWin(input);
+const input: number[][] = [[0,0],[1,0],[1,0],[2,1],[2,1],[2,0]];
+const n: number = 4;
+const results = winningPlayerCount(n, input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
