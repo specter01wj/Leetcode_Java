3f92ee5fc2c51b4928c61f0a4eeff04112ddb32d
Jin Wang <specter01wj@gmail.com>
2025-03-18 11:39:55 -0400

    v2

--- mysol/Category/Array/E3248_Snake_in_Matrix/src/com/snakeinMatrix/E3248_Snake_in_Matrix.java
+++ mysol/Category/Array/E3248_Snake_in_Matrix/src/com/snakeinMatrix/E3248_Snake_in_Matrix.java
@@ -1,10 +1,55 @@
 package com.snakeinMatrix;
+import java.util.*;
+
+/*
+There is a snake in an n x n matrix grid and can move in four 
+possible directions. Each cell in the grid is identified by 
+the position: grid[i][j] = (i * n) + j.
+
+The snake starts at cell 0 and follows a sequence of commands.
+
+You are given an integer n representing the size of the grid and 
+an array of strings commands where each command[i] is either "UP", 
+"RIGHT", "DOWN", and "LEFT". It's guaranteed that the snake will 
+remain within the grid boundaries throughout its movement.
+
+Return the position of the final cell where the snake ends up after 
+executing commands.
+*/
 
 public class E3248_Snake_in_Matrix {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E3248_Snake_in_Matrix solution = new E3248_Snake_in_Matrix();
+		int input = 2;
+		List<String> commands = List.of("RIGHT","DOWN");
+		int output = solution.finalPositionOfSnake(input, commands);
+        System.out.println("input: " + (commands) + "\noutput: " + (output));
 	}
+	
+	
+	public int finalPositionOfSnake(int n, List<String> commands) {
+        int row = 0, col = 0; // The snake starts at position (0,0)
+        
+        for (String command : commands) {
+            switch (command) {
+                case "UP":
+                    row -= 1;
+                    break;
+                case "DOWN":
+                    row += 1;
+                    break;
+                case "LEFT":
+                    col -= 1;
+                    break;
+                case "RIGHT":
+                    col += 1;
+                    break;
+            }
+        }
+        
+        // Convert (row, col) position to 1D index
+        return (row * n) + col;
+    }
 
 }
