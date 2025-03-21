f58086aa771a4e915b34c5cee773b06e0d500ac6
Jin Wang <specter01wj@gmail.com>
2025-03-21 00:56:09 -0400

    v1

---
+++ mysol/Category/Array/E3264_Final_Array_State_After_K_Multiplication_Operations_I/src/com/finalArrayStateAfterKMultiplicationOperationsI/E3264_Final_Array_State_After_K_Multiplication_Operations_I.java
@@ -0,0 +1,10 @@
+package com.finalArrayStateAfterKMultiplicationOperationsI;
+
+public class E3264_Final_Array_State_After_K_Multiplication_Operations_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
