8895a0d6d039fd37f5e9b709307b11b74ee1f4f9
Jin Wang <specter01wj@gmail.com>
2025-03-24 15:08:40 -0400

    v5

--- mysol/Category/Array/E3289_The_Two_Sneaky_Numbers_of_Digitville/ts/code.ts
+++ mysol/Category/Array/E3289_The_Two_Sneaky_Numbers_of_Digitville/ts/code.ts
@@ -1,18 +1,21 @@
-function stableMountains(height: number[], threshold: number): number[] {
+function getSneakyNumbers(nums: number[]): number[] {
+  const seen = new Set<number>();
   const result: number[] = [];
 
-  for (let i = 1; i < height.length; i++) {
-      if (height[i - 1] > threshold) {
-          result.push(i);
+  for (const num of nums) {
+      if (seen.has(num)) {
+          result.push(num);
+          if (result.length === 2) break;
+      } else {
+          seen.add(num);
       }
   }
 
   return result;
 };
 
-const input: number[] = [1,2,3,4,5];
-const threshold: number = 2;
-const results = stableMountains(input, threshold);
+const input: number[] = [7,1,5,4,3,4,6,0,9,5,8,2];
+const results = getSneakyNumbers(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
