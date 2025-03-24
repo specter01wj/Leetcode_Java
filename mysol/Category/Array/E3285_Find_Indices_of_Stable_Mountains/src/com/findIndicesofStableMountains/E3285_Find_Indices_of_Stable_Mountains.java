b0e4755fbb10e041084dd64e5831c23a9f847eae
Jin Wang <specter01wj@gmail.com>
2025-03-24 14:19:13 -0400

    v1

---
+++ mysol/Category/Array/E3285_Find_Indices_of_Stable_Mountains/src/com/findIndicesofStableMountains/E3285_Find_Indices_of_Stable_Mountains.java
@@ -0,0 +1,10 @@
+package com.findIndicesofStableMountains;
+
+public class E3285_Find_Indices_of_Stable_Mountains {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
