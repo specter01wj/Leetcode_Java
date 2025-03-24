2f7cd80ad2963bd2c9df5badf6aeed0a149d8385
Jin Wang <specter01wj@gmail.com>
2025-03-24 14:38:32 -0400

    v4

--- mysol/Category/Array/E3285_Find_Indices_of_Stable_Mountains/ts/code.ts
+++ mysol/Category/Array/E3285_Find_Indices_of_Stable_Mountains/ts/code.ts
@@ -1,20 +1,18 @@
-function getFinalState(nums: number[], k: number, multiplier: number): number[] {
-  for (let i = 0; i < k; i++) {
-      let minIndex = 0;
-      for (let j = 1; j < nums.length; j++) {
-          if (nums[j] < nums[minIndex]) {
-              minIndex = j;
-          }
+function stableMountains(height: number[], threshold: number): number[] {
+  const result: number[] = [];
+
+  for (let i = 1; i < height.length; i++) {
+      if (height[i - 1] > threshold) {
+          result.push(i);
       }
-      nums[minIndex] *= multiplier;
   }
-  return nums;
+
+  return result;
 };
 
-const input: number[] = [2,1,3,5,6];
-const k: number = 2, multiplier: number = 2;
-const inputCopy: number[] = [...input];
-const results = getFinalState(inputCopy, k, multiplier);
+const input: number[] = [1,2,3,4,5];
+const threshold: number = 2;
+const results = stableMountains(input, threshold);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
