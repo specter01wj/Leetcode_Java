0807d16ea7f9218e76ea8831b94c34761c736e40
Jin Wang <specter01wj@gmail.com>
2025-03-26 13:25:59 -0400

    v2

--- mysol/Category/Array/E3300_Minimum_Element_After_Replacement_With_Digit_Sum/src/com/minimumElementAfterReplacementWithDigitSum/E3300_Minimum_Element_After_Replacement_With_Digit_Sum.java
+++ mysol/Category/Array/E3300_Minimum_Element_After_Replacement_With_Digit_Sum/src/com/minimumElementAfterReplacementWithDigitSum/E3300_Minimum_Element_After_Replacement_With_Digit_Sum.java
@@ -1,10 +1,40 @@
 package com.minimumElementAfterReplacementWithDigitSum;
+import java.util.*;
+
+/*
+You are given an integer array nums.
+
+You replace each element in nums with the sum of its digits.
+
+Return the minimum element in nums after all replacements.
+*/
 
 public class E3300_Minimum_Element_After_Replacement_With_Digit_Sum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E3300_Minimum_Element_After_Replacement_With_Digit_Sum solution = new E3300_Minimum_Element_After_Replacement_With_Digit_Sum();
+		int[] input = {10,12,13,14};
+		int output = solution.minElement(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public int minElement(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int num : nums) {
+            int digitSum = getDigitSum(num);
+            min = Math.min(min, digitSum);
+        }
+        return min;
+    }
+
+    private int getDigitSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
 
 }
