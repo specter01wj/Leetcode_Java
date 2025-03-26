2281ac8340d58b77933126bfab3ee27d1283ce85
Jin Wang <specter01wj@gmail.com>
2025-03-26 15:18:23 -0400

    v6

--- mysol/Category/Array/E3314_Construct_the_Minimum_Bitwise_Array_I/ts/code.ts
+++ mysol/Category/Array/E3314_Construct_the_Minimum_Bitwise_Array_I/ts/code.ts
@@ -1,22 +1,28 @@
-function minElement(nums: number[]): number {
-  const digitSum = (num: number): number => {
-      let sum = 0;
-      while (num > 0) {
-          sum += num % 10;
-          num = Math.floor(num / 10);
-      }
-      return sum;
-  };
+function minBitwiseArray(nums: number[]): number[] {
+  const ans: number[] = [];
 
-  let min = Infinity;
-  for (const num of nums) {
-      min = Math.min(min, digitSum(num));
+  for (let i = 0; i < nums.length; i++) {
+      const num = nums[i];
+      let found = false;
+
+      for (let x = 0; x <= num; x++) {
+          if ((x | (x + 1)) === num) {
+              ans.push(x);
+              found = true;
+              break;
+          }
+      }
+
+      if (!found) {
+          ans.push(-1);
+      }
   }
-  return min;
+
+  return ans;
 };
 
-const input: number[] = [10,12,13,14];
-const results = minElement(input);
+const input: number[] = [2,3,5,7];
+const results = minBitwiseArray(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
