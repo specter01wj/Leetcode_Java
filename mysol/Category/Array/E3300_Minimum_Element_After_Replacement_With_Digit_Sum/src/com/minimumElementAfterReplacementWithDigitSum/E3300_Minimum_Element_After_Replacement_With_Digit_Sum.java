f7998172ecf4dd8d3635c80a9f0bb8fdee0c6227
Jin Wang <specter01wj@gmail.com>
2025-03-26 13:23:27 -0400

    v1

---
+++ mysol/Category/Array/E3300_Minimum_Element_After_Replacement_With_Digit_Sum/src/com/minimumElementAfterReplacementWithDigitSum/E3300_Minimum_Element_After_Replacement_With_Digit_Sum.java
@@ -0,0 +1,10 @@
+package com.minimumElementAfterReplacementWithDigitSum;
+
+public class E3300_Minimum_Element_After_Replacement_With_Digit_Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
