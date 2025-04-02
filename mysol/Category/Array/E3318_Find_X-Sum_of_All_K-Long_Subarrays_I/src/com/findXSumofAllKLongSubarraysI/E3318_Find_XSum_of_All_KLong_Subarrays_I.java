07cdc600a061621584d40989c63fed7028dcbd48
Jin Wang <specter01wj@gmail.com>
2025-04-02 02:29:03 -0400

    v1

---
+++ mysol/Category/Array/E3318_Find_X-Sum_of_All_K-Long_Subarrays_I/src/com/findXSumofAllKLongSubarraysI/E3318_Find_XSum_of_All_KLong_Subarrays_I.java
@@ -0,0 +1,10 @@
+package com.findXSumofAllKLongSubarraysI;
+
+public class E3318_Find_XSum_of_All_KLong_Subarrays_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
