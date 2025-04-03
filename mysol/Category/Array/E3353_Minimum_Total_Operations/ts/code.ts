b13ac95bf1507df431aa7eaee13ed91d0f60aa5d
Jin Wang <specter01wj@gmail.com>
2025-04-03 13:43:46 -0400

    v4

---
+++ mysol/Category/Array/E3353_Minimum_Total_Operations/ts/code.ts
@@ -0,0 +1,29 @@
+function hasIncreasingSubarrays(nums: number[], k: number): boolean {
+  const n = nums.length;
+  if (n < 2 * k) return false;
+
+  for (let i = 0; i <= n - 2 * k; i++) {
+      if (isIncreasing(nums, i, k) && isIncreasing(nums, i + k, k)) {
+          return true;
+      }
+  }
+
+  return false;
+};
+
+function isIncreasing(nums: number[], start: number, k: number): boolean {
+  for (let i = start + 1; i < start + k; i++) {
+      if (nums[i] <= nums[i - 1]) {
+          return false;
+      }
+  }
+  return true;
+}
+
+const input: number[] = [2,5,7,8,9,2,3,4,3,1];
+const k: number = 3;
+const results = hasIncreasingSubarrays(input, k);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
