c2ca64dc9d7e51f4d7d2429c356ad4003c4d1e68
Jin Wang <specter01wj@gmail.com>
2025-04-03 13:48:57 -0400

    v6

--- mysol/Category/Array/E3353_Minimum_Total_Operations/ts/code.ts
+++ mysol/Category/Array/E3353_Minimum_Total_Operations/ts/code.ts
@@ -1,28 +1,15 @@
-function hasIncreasingSubarrays(nums: number[], k: number): boolean {
-  const n = nums.length;
-  if (n < 2 * k) return false;
-
-  for (let i = 0; i <= n - 2 * k; i++) {
-      if (isIncreasing(nums, i, k) && isIncreasing(nums, i + k, k)) {
-          return true;
+function minOperations(nums: number[]): number {
+  let operations = 0;
+  for (let i = nums.length - 2; i >= 0; i--) {
+      if (nums[i] !== nums[i + 1]) {
+          operations++;
       }
   }
-
-  return false;
+  return operations;
 };
 
-function isIncreasing(nums: number[], start: number, k: number): boolean {
-  for (let i = start + 1; i < start + k; i++) {
-      if (nums[i] <= nums[i - 1]) {
-          return false;
-      }
-  }
-  return true;
-}
-
-const input: number[] = [2,5,7,8,9,2,3,4,3,1];
-const k: number = 3;
-const results = hasIncreasingSubarrays(input, k);
+const input: number[] = [1,4,2];
+const results = minOperations(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
