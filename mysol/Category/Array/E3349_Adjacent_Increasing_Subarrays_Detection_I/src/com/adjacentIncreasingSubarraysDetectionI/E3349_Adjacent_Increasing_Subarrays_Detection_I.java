8114e71a1786c002f73520d57d1c05c120975d45
Jin Wang <specter01wj@gmail.com>
2025-04-02 20:20:10 -0400

    v1

---
+++ mysol/Category/Array/E3349_Adjacent_Increasing_Subarrays_Detection_I/src/com/adjacentIncreasingSubarraysDetectionI/E3349_Adjacent_Increasing_Subarrays_Detection_I.java
@@ -0,0 +1,10 @@
+package com.adjacentIncreasingSubarraysDetectionI;
+
+public class E3349_Adjacent_Increasing_Subarrays_Detection_I {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
