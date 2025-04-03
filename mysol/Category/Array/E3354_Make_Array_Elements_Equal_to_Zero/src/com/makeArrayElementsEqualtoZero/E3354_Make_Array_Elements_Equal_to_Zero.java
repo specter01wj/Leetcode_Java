32ee1444f43096c966ee47ce287ebd654e1cb2de
Jin Wang <specter01wj@gmail.com>
2025-04-03 13:58:46 -0400

    v1

---
+++ mysol/Category/Array/E3354_Make_Array_Elements_Equal_to_Zero/src/com/makeArrayElementsEqualtoZero/E3354_Make_Array_Elements_Equal_to_Zero.java
@@ -0,0 +1,10 @@
+package com.makeArrayElementsEqualtoZero;
+
+public class E3354_Make_Array_Elements_Equal_to_Zero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
