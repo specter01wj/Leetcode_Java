0f6570212f1201d4f2733e5151e37e03777a25fe
Jin Wang <specter01wj@gmail.com>
2025-04-03 14:11:22 -0400

    v6

--- mysol/Category/Array/E3354_Make_Array_Elements_Equal_to_Zero/ts/code.ts
+++ mysol/Category/Array/E3354_Make_Array_Elements_Equal_to_Zero/ts/code.ts
@@ -1,15 +1,34 @@
-function minOperations(nums: number[]): number {
-  let operations = 0;
-  for (let i = nums.length - 2; i >= 0; i--) {
-      if (nums[i] !== nums[i + 1]) {
-          operations++;
+function countValidSelections(nums: number[]): number {
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === 0) {
+          if (simulate([...nums], i, -1)) count++;
+          if (simulate([...nums], i, 1)) count++;
       }
   }
-  return operations;
+
+  return count;
 };
 
-const input: number[] = [1,4,2];
-const results = minOperations(input);
+function simulate(arr: number[], curr: number, direction: number): boolean {
+  const n = arr.length;
+
+  while (curr >= 0 && curr < n) {
+      if (arr[curr] === 0) {
+          curr += direction;
+      } else {
+          arr[curr]--;
+          direction *= -1;
+          curr += direction;
+      }
+  }
+
+  return arr.every(val => val === 0);
+}
+
+const input: number[] = [1,0,2,0,3];
+const results = countValidSelections(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
