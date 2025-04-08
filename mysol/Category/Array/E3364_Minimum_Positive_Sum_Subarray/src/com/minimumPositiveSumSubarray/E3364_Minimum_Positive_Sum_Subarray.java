fe33c8e4b76d4f9722787fede25b1b1e950a71a3
Jin Wang <specter01wj@gmail.com>
2025-04-07 20:05:35 -0400

    v4

--- mysol/Category/Array/E3364_Minimum_Positive_Sum_Subarray/src/com/minimumPositiveSumSubarray/E3364_Minimum_Positive_Sum_Subarray.java
+++ mysol/Category/Array/E3364_Minimum_Positive_Sum_Subarray/src/com/minimumPositiveSumSubarray/E3364_Minimum_Positive_Sum_Subarray.java
@@ -21,7 +21,20 @@ public class E3364_Minimum_Positive_Sum_Subarray {
         System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    1. We create a prefix sum array to calculate subarray sums in constant time.
+	2. For each length between l and r, we slide a window and check the sum.
+	3. If the sum is positive and less than the current minimum, we update the result.
+	4. At the end, if we found at least one valid subarray, return the minimum positive sum; otherwise, return -1.
+    */
 	
+	/*
+     * @param nums: a list of integers
+     * @param l: an integer
+     * @param r: an integer
+     * @return: the minimum sum of such a subarray
+     */
 	public int minimumSumSubarray(List<Integer> nums, int l, int r) {
         int n = nums.size();
         int[] prefix = new int[n + 1];
