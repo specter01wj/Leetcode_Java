ca842623d47cb9de395402e49b018e106a2be6fc
Jin Wang <specter01wj@gmail.com>
2025-04-07 20:11:20 -0400

    v6

--- mysol/Category/Array/E3364_Minimum_Positive_Sum_Subarray/ts/code.ts
+++ mysol/Category/Array/E3364_Minimum_Positive_Sum_Subarray/ts/code.ts
@@ -1,34 +1,30 @@
-function countValidSelections(nums: number[]): number {
-  let count = 0;
+function minimumSumSubarray(nums: number[], l: number, r: number): number {
+  const n = nums.length;
+  const prefix: number[] = new Array(n + 1).fill(0);
 
-  for (let i = 0; i < nums.length; i++) {
-      if (nums[i] === 0) {
-          if (simulate([...nums], i, -1)) count++;
-          if (simulate([...nums], i, 1)) count++;
+  // Build prefix sum array
+  for (let i = 0; i < n; i++) {
+      prefix[i + 1] = prefix[i] + nums[i];
+  }
+
+  let minPositiveSum = Infinity;
+
+  // Try all subarray lengths from l to r
+  for (let len = l; len <= r; len++) {
+      for (let i = 0; i + len <= n; i++) {
+          const sum = prefix[i + len] - prefix[i];
+          if (sum > 0) {
+              minPositiveSum = Math.min(minPositiveSum, sum);
+          }
       }
   }
 
-  return count;
+  return minPositiveSum === Infinity ? -1 : minPositiveSum;
 };
 
-function simulate(arr: number[], curr: number, direction: number): boolean {
-  const n = arr.length;
-
-  while (curr >= 0 && curr < n) {
-      if (arr[curr] === 0) {
-          curr += direction;
-      } else {
-          arr[curr]--;
-          direction *= -1;
-          curr += direction;
-      }
-  }
-
-  return arr.every(val => val === 0);
-}
-
-const input: number[] = [1,0,2,0,3];
-const results = countValidSelections(input);
+const input: number[] = [3, -2, 1, 4];
+const l: number = 2, r: number = 3;
+const results = minimumSumSubarray(input, l, r);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
