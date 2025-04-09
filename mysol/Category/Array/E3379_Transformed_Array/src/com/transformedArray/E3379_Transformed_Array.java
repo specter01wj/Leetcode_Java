d08d65de687a467ea357049b2bb574512e32e556
Jin Wang <specter01wj@gmail.com>
2025-04-09 18:24:51 -0400

    v2

--- mysol/Category/Array/E3379_Transformed_Array/src/com/transformedArray/E3379_Transformed_Array.java
+++ mysol/Category/Array/E3379_Transformed_Array/src/com/transformedArray/E3379_Transformed_Array.java
@@ -1,10 +1,46 @@
 package com.transformedArray;
+import java.util.*;
+
+/*
+You are given an integer array nums that represents a circular array. 
+Your task is to create a new array result of the same size, following these rules:
+
+For each index i (where 0 <= i < nums.length), perform the following independent actions:
+If nums[i] > 0: Start at index i and move nums[i] steps to the right in the circular array. Set result[i] to the value of the index where you land.
+If nums[i] < 0: Start at index i and move abs(nums[i]) steps to the left in the circular array. Set result[i] to the value of the index where you land.
+If nums[i] == 0: Set result[i] to nums[i].
+Return the new array result.
+*/
 
 public class E3379_Transformed_Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E3379_Transformed_Array solution = new E3379_Transformed_Array();
+		int[] input = {3,-2,1,1};
+		int[] output = solution.constructTransformedArray(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + Arrays.toString(output));
 	}
+	
+	
+	public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            int steps = nums[i];
+            if (steps == 0) {
+                result[i] = nums[i];
+            } else {
+                int newIndex = (i + steps) % n;
+                // Ensure newIndex is within bounds for negative steps
+                if (newIndex < 0) {
+                    newIndex += n;
+                }
+                result[i] = nums[newIndex];
+            }
+        }
+        
+        return result;
+    }
 
 }
