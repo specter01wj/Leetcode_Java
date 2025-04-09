ac3e77468afdb1a8073f8aaa9808a8a067e9b72a
Jin Wang <specter01wj@gmail.com>
2025-04-09 19:02:58 -0400

    v1

---
+++ mysol/Category/Array/E3386_Button_with_Longest_Push_Time/src/com/buttonwithLongestPushTime/E3386_Button_with_Longest_Push_Time.java
@@ -0,0 +1,10 @@
+package com.buttonwithLongestPushTime;
+
+public class E3386_Button_with_Longest_Push_Time {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
