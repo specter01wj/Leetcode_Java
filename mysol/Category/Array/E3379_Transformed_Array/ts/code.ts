8f733cded36e08519164b6804ec2e55708da903d
Jin Wang <specter01wj@gmail.com>
2025-04-09 18:47:45 -0400

    v5

--- mysol/Category/Array/E3379_Transformed_Array/ts/code.ts
+++ mysol/Category/Array/E3379_Transformed_Array/ts/code.ts
@@ -1,20 +1,25 @@
-function minOperations(nums: number[], k: number): number {
-  const set = new Set<number>();
-
-  for (const num of nums) {
-      if (num < k) {
-          return -1; // Step 1: Impossible if any number is less than k
-      } else if (num > k) {
-          set.add(num); // Step 2: Track unique values greater than k
+function constructTransformedArray(nums: number[]): number[] {
+  const n: number = nums.length;
+  const result: number[] = new Array(n);
+  
+  for (let i = 0; i < n; i++) {
+      const steps: number = nums[i];
+      if (steps === 0) {
+          result[i] = nums[i];
+      } else {
+          let newIndex: number = (i + steps) % n;
+          if (newIndex < 0) {
+              newIndex += n;
+          }
+          result[i] = nums[newIndex];
       }
   }
 
-  return set.size; // Step 3: Number of operations = number of distinct values > k
+  return result;
 };
 
-const input: number[] = [5,2,5,4,5];
-const k: number = 2;
-const results = minOperations(input, k);
+const input: number[] = [3,-2,1,1];
+const results = constructTransformedArray(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
