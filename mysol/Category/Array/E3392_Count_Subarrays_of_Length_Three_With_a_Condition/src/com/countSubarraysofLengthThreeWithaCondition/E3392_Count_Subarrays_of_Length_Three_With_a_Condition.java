0dd7c0ed09d2b2f7c13226a9e48a7bc3a22eec4d
Jin Wang <specter01wj@gmail.com>
2025-04-14 20:40:00 -0400

    v1

---
+++ mysol/Category/Array/E3392_Count_Subarrays_of_Length_Three_With_a_Condition/src/com/countSubarraysofLengthThreeWithaCondition/E3392_Count_Subarrays_of_Length_Three_With_a_Condition.java
@@ -0,0 +1,10 @@
+package com.countSubarraysofLengthThreeWithaCondition;
+
+public class E3392_Count_Subarrays_of_Length_Three_With_a_Condition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
