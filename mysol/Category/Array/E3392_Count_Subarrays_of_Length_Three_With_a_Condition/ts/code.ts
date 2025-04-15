544752c63955ebec3ab19846ef214cc0c73077e1
Jin Wang <specter01wj@gmail.com>
2025-04-14 20:54:56 -0400

    v5

--- mysol/Category/Array/E3392_Count_Subarrays_of_Length_Three_With_a_Condition/ts/code.ts
+++ mysol/Category/Array/E3392_Count_Subarrays_of_Length_Three_With_a_Condition/ts/code.ts
@@ -1,24 +1,21 @@
-function buttonWithLongestTime(events: number[][]): number {
-  let longestDuration: number = events[0][1];     // First button duration
-  let buttonIndex: number = events[0][0];         // First button index
+function countSubarrays(nums: number[]): number {
+  let count = 0;
 
-  for (let i = 1; i < events.length; i++) {
-      const duration: number = events[i][1] - events[i - 1][1];
-      const currentButton: number = events[i][0];
+  for (let i = 1; i < nums.length - 1; i++) {
+      const first = nums[i - 1];
+      const mid = nums[i];
+      const third = nums[i + 1];
 
-      if (duration > longestDuration) {
-          longestDuration = duration;
-          buttonIndex = currentButton;
-      } else if (duration === longestDuration && currentButton < buttonIndex) {
-          buttonIndex = currentButton;
+      if ((first + third) * 2 === mid) {
+          count++;
       }
   }
 
-  return buttonIndex;
+  return count;
 };
 
-const input: number[][] = [[1,2],[2,5],[3,9],[1,15]];
-const results = buttonWithLongestTime(input);
+const input: number[] = [1,2,1,4,1];
+const results = countSubarrays(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
