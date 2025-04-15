2eb2d36cfd07fd636b694d7fc760f7cd2e30a5f0
Jin Wang <specter01wj@gmail.com>
2025-04-14 23:51:29 -0400

    v1

---
+++ mysol/Category/Array/E3396_Minimum_Number_of_Operations_to_Make_Elements_in_Array_Distinct/src/com/minimumNumberofOperationstoMakeElementsinArrayDistinct/E3396_Minimum_Number_of_Operations_to_Make_Elements_in_Array_Distinct.java
@@ -0,0 +1,10 @@
+package com.minimumNumberofOperationstoMakeElementsinArrayDistinct;
+
+public class E3396_Minimum_Number_of_Operations_to_Make_Elements_in_Array_Distinct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
