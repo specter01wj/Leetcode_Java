83de37101e63715d2d233f3640046f630d9f4837
Jin Wang <specter01wj@gmail.com>
2025-04-14 20:47:21 -0400

    v2

--- mysol/Category/Array/E3392_Count_Subarrays_of_Length_Three_With_a_Condition/src/com/countSubarraysofLengthThreeWithaCondition/E3392_Count_Subarrays_of_Length_Three_With_a_Condition.java
+++ mysol/Category/Array/E3392_Count_Subarrays_of_Length_Three_With_a_Condition/src/com/countSubarraysofLengthThreeWithaCondition/E3392_Count_Subarrays_of_Length_Three_With_a_Condition.java
@@ -1,10 +1,36 @@
 package com.countSubarraysofLengthThreeWithaCondition;
+import java.util.*;
+
+/*
+Given an integer array nums, return the number of subarrays of length 3 
+such that the sum of the first and third numbers equals exactly half of 
+the second number.
+*/
 
 public class E3392_Count_Subarrays_of_Length_Three_With_a_Condition {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {3,0,1,1,9,7};
+		int output = countGoodTriplets(input, 7, 2, 3);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	
+	public int countSubarrays(int[] nums) {
+		int count = 0;
+		
+		for (int i = 1; i < nums.length - 1; i++) {
+			int first = nums[i - 1];
+			int mid = nums[i];
+			int third = nums[i + 1];
+			
+			// Check if first + third == mid / 2 (make sure mid is even)
+			if ((first + third) * 2 == mid) {
+				count++;
+			}
+		}
+		
+		return count;
 	}
 
 }
