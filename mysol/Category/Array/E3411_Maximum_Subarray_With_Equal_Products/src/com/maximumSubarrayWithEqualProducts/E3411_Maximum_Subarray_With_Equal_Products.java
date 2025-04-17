12aaedcb89707d5347bcf812a3f9346351ba4222
Jin Wang <specter01wj@gmail.com>
2025-04-17 02:42:25 -0400

    v1

---
+++ mysol/Category/Array/E3411_Maximum_Subarray_With_Equal_Products/src/com/maximumSubarrayWithEqualProducts/E3411_Maximum_Subarray_With_Equal_Products.java
@@ -0,0 +1,10 @@
+package com.maximumSubarrayWithEqualProducts;
+
+public class E3411_Maximum_Subarray_With_Equal_Products {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
