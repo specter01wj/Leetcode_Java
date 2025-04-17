bbc78f875d08583eb60c206f2475e8208f82ce0c
Jin Wang <specter01wj@gmail.com>
2025-04-17 02:58:41 -0400

    v6

--- mysol/Category/Array/E3411_Maximum_Subarray_With_Equal_Products/ts/code.ts
+++ mysol/Category/Array/E3411_Maximum_Subarray_With_Equal_Products/ts/code.ts
@@ -1,33 +1,44 @@
-function minimumOperations(nums: number[]): number {
-  let ops = 0;
-  let i = 0;
+function maxLength(nums: number[]): number {
+  const n = nums.length;
+  let maxLen = 0;
 
-  while (i < nums.length) {
-      const seen = new Set<number>();
-      let hasDuplicate = false;
+  for (let i = 0; i < n; i++) {
+      let product = 1n;
+      let gcdVal = 0n;
+      let lcmVal = 1n;
 
-      for (let j = i; j < nums.length; j++) {
-          if (seen.has(nums[j])) {
-              hasDuplicate = true;
-              break;
+      for (let j = i; j < n; j++) {
+          const current = BigInt(nums[j]);
+          product *= current;
+          gcdVal = gcdVal === 0n ? current : gcd(gcdVal, current);
+          lcmVal = lcm(current, lcmVal);
+
+          if (product > 10n ** 18n || lcmVal > 10n ** 18n) break;
+
+          if (product === gcdVal * lcmVal) {
+              maxLen = Math.max(maxLen, j - i + 1);
           }
-          seen.add(nums[j]);
       }
-
-      if (!hasDuplicate) {
-          return ops;
-      }
-
-      // Simulate removing first 3 elements
-      i += 3;
-      ops++;
   }
 
-  return ops;
+  return maxLen;
 };
 
-const input: number[] = [1,2,3,4,2,3,3,5,7];
-const results = minimumOperations(input);
+function gcd(a: bigint, b: bigint): bigint {
+  while (b !== 0n) {
+      const temp = a % b;
+      a = b;
+      b = temp;
+  }
+  return a;
+}
+
+function lcm(a: bigint, b: bigint): bigint {
+  return (a * b) / gcd(a, b);
+}
+
+const input: number[] = [1,2,1,2,1,1,1];
+const results = maxLength(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
