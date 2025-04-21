f6f37256bada0b6ad8adbeac9d767a27c187c614
Jin Wang <specter01wj@gmail.com>
2025-04-21 11:12:51 -0400

    v1

---
+++ mysol/Category/Array/E3417_Zigzag_Grid_Traversal_With_Skip/src/com/zigzagGridTraversalWithSkip/E3417_Zigzag_Grid_Traversal_With_Skip.java
@@ -0,0 +1,10 @@
+package com.zigzagGridTraversalWithSkip;
+
+public class E3417_Zigzag_Grid_Traversal_With_Skip {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
