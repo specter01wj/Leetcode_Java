6a1654a3e02d3be6081909c58a4d1c8ee3cea2eb
Jin Wang <specter01wj@gmail.com>
2025-04-21 12:25:24 -0400

    v7

---
+++ mysol/Category/Array/E3417_Zigzag_Grid_Traversal_With_Skip/ts/code.js
@@ -0,0 +1,31 @@
+function zigzagTraversal(grid) {
+    const rows = grid.length;
+    const cols = grid[0].length;
+    const traversal = [];
+    // Step 1: Traverse in zigzag order
+    for (let i = 0; i < rows; i++) {
+        if (i % 2 === 0) {
+            // left to right
+            for (let j = 0; j < cols; j++) {
+                traversal.push(grid[i][j]);
+            }
+        }
+        else {
+            // right to left
+            for (let j = cols - 1; j >= 0; j--) {
+                traversal.push(grid[i][j]);
+            }
+        }
+    }
+    // Step 2: Skip every alternate cell globally
+    const result = [];
+    for (let i = 0; i < traversal.length; i += 2) {
+        result.push(traversal[i]);
+    }
+    return result;
+}
+;
+const input = [[1, 2], [3, 4]];
+const results = zigzagTraversal(input);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
