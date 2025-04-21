ea786f5514efba251cd6bcfc3c2d7b2b1cc16eb3
Jin Wang <specter01wj@gmail.com>
2025-04-21 12:24:59 -0400

    v6

--- mysol/Category/Array/E3417_Zigzag_Grid_Traversal_With_Skip/ts/code.ts
+++ mysol/Category/Array/E3417_Zigzag_Grid_Traversal_With_Skip/ts/code.ts
@@ -1,44 +1,34 @@
-function maxLength(nums: number[]): number {
-  const n = nums.length;
-  let maxLen = 0;
+function zigzagTraversal(grid: number[][]): number[] {
+  const rows: number = grid.length;
+  const cols: number = grid[0].length;
+  const traversal: number[] = [];
 
-  for (let i = 0; i < n; i++) {
-      let product = 1n;
-      let gcdVal = 0n;
-      let lcmVal = 1n;
-
-      for (let j = i; j < n; j++) {
-          const current = BigInt(nums[j]);
-          product *= current;
-          gcdVal = gcdVal === 0n ? current : gcd(gcdVal, current);
-          lcmVal = lcm(current, lcmVal);
-
-          if (product > 10n ** 18n || lcmVal > 10n ** 18n) break;
-
-          if (product === gcdVal * lcmVal) {
-              maxLen = Math.max(maxLen, j - i + 1);
+  // Step 1: Traverse in zigzag order
+  for (let i = 0; i < rows; i++) {
+      if (i % 2 === 0) {
+          // left to right
+          for (let j = 0; j < cols; j++) {
+              traversal.push(grid[i][j]);
+          }
+      } else {
+          // right to left
+          for (let j = cols - 1; j >= 0; j--) {
+              traversal.push(grid[i][j]);
           }
       }
   }
 
-  return maxLen;
+  // Step 2: Skip every alternate cell globally
+  const result: number[] = [];
+  for (let i = 0; i < traversal.length; i += 2) {
+      result.push(traversal[i]);
+  }
+
+  return result;
 };
 
-function gcd(a: bigint, b: bigint): bigint {
-  while (b !== 0n) {
-      const temp = a % b;
-      a = b;
-      b = temp;
-  }
-  return a;
-}
-
-function lcm(a: bigint, b: bigint): bigint {
-  return (a * b) / gcd(a, b);
-}
-
-const input: number[] = [1,2,1,2,1,1,1];
-const results = maxLength(input);
+const input: number[][] = [[1,2],[3,4]];
+const results = zigzagTraversal(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
