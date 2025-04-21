2127e9a406d2f8435a338b6c4e2e09b960f51140
Jin Wang <specter01wj@gmail.com>
2025-04-21 12:35:35 -0400

    v1

---
+++ mysol/Category/Array/E3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array/src/com/maximumDifferenceBetweenAdjacentElementsinaCircularArray/E3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array.java
@@ -0,0 +1,10 @@
+package com.maximumDifferenceBetweenAdjacentElementsinaCircularArray;
+
+public class E3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
