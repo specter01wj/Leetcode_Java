0653989038c74d98d96e68f08a0c5bdb2b5af5db
Jin Wang <specter01wj@gmail.com>
2025-04-21 13:18:17 -0400

    v2

--- mysol/Category/Array/E3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array/src/com/maximumDifferenceBetweenAdjacentElementsinaCircularArray/E3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array.java
+++ mysol/Category/Array/E3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array/src/com/maximumDifferenceBetweenAdjacentElementsinaCircularArray/E3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array.java
@@ -1,10 +1,32 @@
 package com.maximumDifferenceBetweenAdjacentElementsinaCircularArray;
+import java.util.*;
+
+/*
+Given a circular array nums, find the maximum absolute difference between 
+adjacent elements.
+*/
 
 public class E3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array solution = new E3423_Maximum_Difference_Between_Adjacent_Elements_in_a_Circular_Array();
+		int[] input = {1,2,4};
+		int output = solution.maxAdjacentDistance(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	
+	public int maxAdjacentDistance(int[] nums) {
+		int maxDiff = 0;
+		int n = nums.length;
+		
+		for (int i = 0; i < n; i++) {
+			int next = (i + 1) % n; // Circular wrap
+			int diff = Math.abs(nums[i] - nums[next]);
+			maxDiff = Math.max(maxDiff, diff);
+		}
+		
+		return maxDiff;
 	}
 
 }
