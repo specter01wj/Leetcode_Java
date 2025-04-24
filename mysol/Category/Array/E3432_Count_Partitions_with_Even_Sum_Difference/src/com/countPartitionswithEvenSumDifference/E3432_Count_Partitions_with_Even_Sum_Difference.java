2c93a4edb2a92c1595966d47a648a42bb413d371
Jin Wang <specter01wj@gmail.com>
2025-04-24 13:59:53 -0400

    v1

---
+++ mysol/Category/Array/E3432_Count_Partitions_with_Even_Sum_Difference/src/com/countPartitionswithEvenSumDifference/E3432_Count_Partitions_with_Even_Sum_Difference.java
@@ -0,0 +1,10 @@
+package com.countPartitionswithEvenSumDifference;
+
+public class E3432_Count_Partitions_with_Even_Sum_Difference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
