d1ba7f466897fb0ed8a4c0f2b82a559de1548f75
Jin Wang <specter01wj@gmail.com>
2025-04-24 03:07:36 -0400

    v4

--- mysol/Category/Array/E3427_Sum_of_Variable_Length_Subarrays/ts/code.ts
+++ mysol/Category/Array/E3427_Sum_of_Variable_Length_Subarrays/ts/code.ts
@@ -1,18 +1,18 @@
-function maxAdjacentDistance(nums: number[]): number {
-  let maxDiff = 0;
-  const n = nums.length;
+function subarraySum(nums: number[]): number {
+  let totalSum = 0;
 
-  for (let i = 0; i < n; i++) {
-      const next = (i + 1) % n; // circular index
-      const diff = Math.abs(nums[i] - nums[next]);
-      maxDiff = Math.max(maxDiff, diff);
+  for (let i = 0; i < nums.length; i++) {
+      const start = Math.max(0, i - nums[i]);
+      for (let j = start; j <= i; j++) {
+          totalSum += nums[j];
+      }
   }
 
-  return maxDiff;
+  return totalSum;
 };
 
-const input: number[] = [1,2,4];
-const results = maxAdjacentDistance(input);
+const input: number[] = [2,3,1];
+const results = subarraySum(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
