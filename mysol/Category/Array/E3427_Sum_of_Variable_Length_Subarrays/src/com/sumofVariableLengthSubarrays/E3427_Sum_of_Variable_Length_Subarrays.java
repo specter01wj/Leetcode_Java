c8d0917d0b076e3c910e46ae42faae81982cc49f
Jin Wang <specter01wj@gmail.com>
2025-04-24 02:53:00 -0400

    v1

---
+++ mysol/Category/Array/E3427_Sum_of_Variable_Length_Subarrays/src/com/sumofVariableLengthSubarrays/E3427_Sum_of_Variable_Length_Subarrays.java
@@ -0,0 +1,10 @@
+package com.sumofVariableLengthSubarrays;
+
+public class E3427_Sum_of_Variable_Length_Subarrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
