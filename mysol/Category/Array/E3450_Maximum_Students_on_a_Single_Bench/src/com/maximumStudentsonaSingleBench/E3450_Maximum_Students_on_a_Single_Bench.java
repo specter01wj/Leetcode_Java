8b66f127ad84e6b257d72a9230bdd8adb09d1386
Jin Wang <specter01wj@gmail.com>
2025-04-29 02:45:09 -0400

    v1

---
+++ mysol/Category/Array/E3450_Maximum_Students_on_a_Single_Bench/src/com/maximumStudentsonaSingleBench/E3450_Maximum_Students_on_a_Single_Bench.java
@@ -0,0 +1,10 @@
+package com.maximumStudentsonaSingleBench;
+
+public class E3450_Maximum_Students_on_a_Single_Bench {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
