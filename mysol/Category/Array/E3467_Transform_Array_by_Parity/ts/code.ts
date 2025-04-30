d58620d843eb7e4598929c4165862aaea3f2b63d
Jin Wang <specter01wj@gmail.com>
2025-04-30 12:39:00 -0400

    v7

--- mysol/Category/Array/E3467_Transform_Array_by_Parity/ts/code.ts
+++ mysol/Category/Array/E3467_Transform_Array_by_Parity/ts/code.ts
@@ -1,31 +1,17 @@
-function sumOfGoodNumbers(nums: number[], k: number): number {
-  let sum: number = 0;
-
+function transformArray(nums: number[]): number[] {
+  // Step 1: Replace even with 0 and odd with 1
   for (let i = 0; i < nums.length; i++) {
-      let isGood: boolean = true;
-
-      // Check left neighbor at index i - k
-      if (i - k >= 0 && nums[i] <= nums[i - k]) {
-          isGood = false;
-      }
-
-      // Check right neighbor at index i + k
-      if (i + k < nums.length && nums[i] <= nums[i + k]) {
-          isGood = false;
-      }
-
-      // If number is good, add to sum
-      if (isGood) {
-          sum += nums[i];
-      }
+      nums[i] = nums[i] % 2;
   }
 
-  return sum;
+  // Step 2: Sort the transformed array
+  nums.sort((a, b) => a - b);
+
+  return nums;
 };
 
-const input: number[] = [1,3,2,1,5,4];
-const k: number = 2;
-const results = sumOfGoodNumbers(input, k);
+const input: number[] = [4,3,2,1];
+const results = transformArray(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
