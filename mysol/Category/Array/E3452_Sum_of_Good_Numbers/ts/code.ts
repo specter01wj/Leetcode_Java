2df3b5b77309433371065d68cbdc65f1d9b7fa96
Jin Wang <specter01wj@gmail.com>
2025-04-30 00:08:32 -0400

    v7

--- mysol/Category/Array/E3452_Sum_of_Good_Numbers/ts/code.ts
+++ mysol/Category/Array/E3452_Sum_of_Good_Numbers/ts/code.ts
@@ -1,22 +1,31 @@
-function countPartitions(nums: number[]): number {
-  const totalSum = nums.reduce((sum, num) => sum + num, 0);
-  let leftSum = 0;
-  let count = 0;
+function sumOfGoodNumbers(nums: number[], k: number): number {
+  let sum: number = 0;
 
-  for (let i = 0; i < nums.length - 1; i++) {
-      leftSum += nums[i];
-      const rightSum = totalSum - leftSum;
+  for (let i = 0; i < nums.length; i++) {
+      let isGood: boolean = true;
 
-      if (Math.abs(leftSum - rightSum) % 2 === 0) {
-          count++;
+      // Check left neighbor at index i - k
+      if (i - k >= 0 && nums[i] <= nums[i - k]) {
+          isGood = false;
+      }
+
+      // Check right neighbor at index i + k
+      if (i + k < nums.length && nums[i] <= nums[i + k]) {
+          isGood = false;
+      }
+
+      // If number is good, add to sum
+      if (isGood) {
+          sum += nums[i];
       }
   }
 
-  return count;
+  return sum;
 };
 
-const input: number[] = [10,10,3,7,6];
-const results = countPartitions(input);
+const input: number[] = [1,3,2,1,5,4];
+const k: number = 2;
+const results = sumOfGoodNumbers(input, k);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
