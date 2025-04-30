55e0dc2b98da71370f043990c779147e4caa722c
Jin Wang <specter01wj@gmail.com>
2025-04-30 15:41:47 -0400

    v5

--- mysol/Category/Array/E3471_Find_the_Largest_Almost_Missing_Integer/ts/code.ts
+++ mysol/Category/Array/E3471_Find_the_Largest_Almost_Missing_Integer/ts/code.ts
@@ -1,18 +1,30 @@
-function transformArray(nums: number[]): number[] {
-  // Step 1: Replace even with 0 and odd with 1
-  for (let i = 0; i < nums.length; i++) {
-      nums[i] = nums[i] % 2;
+function largestInteger(nums: number[], k: number): number {
+  const freqMap: Map<number, number> = new Map();
+  const n = nums.length;
+
+  for (let i = 0; i <= n - k; i++) {
+      const windowSet = new Set<number>();
+      for (let j = i; j < i + k; j++) {
+          windowSet.add(nums[j]);
+      }
+      for (const num of windowSet) {
+          freqMap.set(num, (freqMap.get(num) || 0) + 1);
+      }
   }
 
-  // Step 2: Sort the transformed array
-  nums.sort((a, b) => a - b);
+  let max = -1;
+  for (const [num, count] of freqMap.entries()) {
+      if (count === 1) {
+          max = Math.max(max, num);
+      }
+  }
 
-  return nums;
+  return max;
 };
 
-const input: number[] = [4,3,2,1];
-const inputCopy: number[] = [...input];
-const results = transformArray(inputCopy);
+const input: number[] = [3,9,2,1,7];
+const k: number = 3;
+const results = transformArray(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
