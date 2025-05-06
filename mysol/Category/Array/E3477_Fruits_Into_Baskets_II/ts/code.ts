200c2e10901cf72f1e3d2ad635c19aa75673bc7e
Jin Wang <specter01wj@gmail.com>
2025-05-06 03:12:14 -0400

    v6

---
+++ mysol/Category/Array/E3477_Fruits_Into_Baskets_II/ts/code.ts
@@ -0,0 +1,31 @@
+function largestInteger(nums: number[], k: number): number {
+  const freqMap: Map<number, number> = new Map();
+  const n = nums.length;
+
+  for (let i = 0; i <= n - k; i++) {
+      const windowSet = new Set<number>();
+      for (let j = i; j < i + k; j++) {
+          windowSet.add(nums[j]);
+      }
+      for (const num of windowSet) {
+          freqMap.set(num, (freqMap.get(num) || 0) + 1);
+      }
+  }
+
+  let max = -1;
+  for (const [num, count] of freqMap.entries()) {
+      if (count === 1) {
+          max = Math.max(max, num);
+      }
+  }
+
+  return max;
+};
+
+const input: number[] = [3,9,2,1,7];
+const k: number = 3;
+const results = largestInteger(input, k);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
