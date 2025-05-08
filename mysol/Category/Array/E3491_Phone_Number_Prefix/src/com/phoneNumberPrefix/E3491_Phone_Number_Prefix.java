363a83692cdedfcd5e8681b1dc36000683bfd856
Jin Wang <specter01wj@gmail.com>
2025-05-08 00:02:09 -0400

    v3

--- mysol/Category/Array/E3491_Phone_Number_Prefix/src/com/phoneNumberPrefix/E3491_Phone_Number_Prefix.java
+++ mysol/Category/Array/E3491_Phone_Number_Prefix/src/com/phoneNumberPrefix/E3491_Phone_Number_Prefix.java
@@ -16,7 +16,25 @@ public class E3491_Phone_Number_Prefix {
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    1. Why Sorting Helps:
+		. Sorting places strings with common prefixes next to each other.
+		. For example, ["001", "00153"] will be adjacent after sorting.
+	2. Iterate Through Sorted Array:
+		. Only need to compare numbers[i] with numbers[i+1] since any prefix relationship would now be next to each other.
+	3. Use startsWith():
+		. This built-in method checks if one string is a prefix of another.
+	4. Return Early:
+		. As soon as a prefix is found, return false.
+	5. Final Check:
+		. If loop finishes without finding any prefix, return true.
+    */
 	
+	/*
+     * @param numbers: a list of strings
+     * @return: true if no phone number is a prefix of any other phone number
+     */
 	public boolean phonePrefix(String[] numbers) {
         // Step 1: Sort the array to bring prefix-close strings adjacent
         Arrays.sort(numbers);
