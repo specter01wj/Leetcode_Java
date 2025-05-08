47f64e0863b65d1b7a275b0ed051c01fcbafd1ca
Jin Wang <specter01wj@gmail.com>
2025-05-08 02:02:49 -0400

    v3

---
+++ mysol/Category/Array/E3491_Phone_Number_Prefix/ts/code.ts
@@ -0,0 +1,26 @@
+function maxSum(nums: number[]): number {
+  const unique = new Set<number>();
+  let sum = 0;
+  let maxNegative = -Infinity;
+
+  for (const num of nums) {
+      if (num > 0) {
+          unique.add(num);
+      } else {
+          maxNegative = Math.max(maxNegative, num);
+      }
+  }
+
+  for (const val of unique) {
+      sum += val;
+  }
+
+  return unique.size > 0 ? sum : maxNegative;
+};
+
+const input: number[] = [1,2,-1,-2,1,0,-1];
+const results = maxSum(input);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
