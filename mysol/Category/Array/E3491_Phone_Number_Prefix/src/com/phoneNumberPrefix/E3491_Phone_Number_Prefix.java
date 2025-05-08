0cb69ec0837fa1a5fd5ab12118819d32b78efebe
Jin Wang <specter01wj@gmail.com>
2025-05-07 23:53:42 -0400

    v2

--- mysol/Category/Array/E3491_Phone_Number_Prefix/src/com/phoneNumberPrefix/E3491_Phone_Number_Prefix.java
+++ mysol/Category/Array/E3491_Phone_Number_Prefix/src/com/phoneNumberPrefix/E3491_Phone_Number_Prefix.java
@@ -1,10 +1,33 @@
 package com.phoneNumberPrefix;
+import java.util.*;
+
+/*
+You are given a string array numbers that represents phone numbers. 
+Return true if no phone number is a prefix of any other phone number; 
+otherwise, return false.
+*/
 
 public class E3491_Phone_Number_Prefix {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String[] input = {"001","007","15","00153"};
+		int output = phonePrefix(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public boolean phonePrefix(String[] numbers) {
+        // Step 1: Sort the array to bring prefix-close strings adjacent
+        Arrays.sort(numbers);
+
+        // Step 2: Compare each number with the next one in the sorted array
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i + 1].startsWith(numbers[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
 }
