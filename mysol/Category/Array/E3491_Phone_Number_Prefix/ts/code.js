2078254d92201856b09eaa320f6ab996330e6a13
Jin Wang <specter01wj@gmail.com>
2025-05-08 02:20:59 -0400

    v8

---
+++ mysol/Category/Array/E3491_Phone_Number_Prefix/ts/code.js
@@ -0,0 +1,16 @@
+function phonePrefix(numbers) {
+    // Step 1: Sort the array lexicographically
+    numbers.sort();
+    // Step 2: Compare each number with its next one
+    for (let i = 0; i < numbers.length - 1; i++) {
+        if (numbers[i + 1].startsWith(numbers[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+;
+const input = ["001", "007", "15", "00153"];
+const results = phonePrefix(input);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
