ef8ef673f9502f4baff0c15cdd087942c0b548e4
Jin Wang <specter01wj@gmail.com>
2025-05-14 19:49:34 -0400

    v7

--- mysol/Category/Array/E3512_Minimum_Operations_to_Make_Array_Sum_Divisible_by_K/ts/code.ts
+++ mysol/Category/Array/E3512_Minimum_Operations_to_Make_Array_Sum_Divisible_by_K/ts/code.ts
@@ -1,41 +1,18 @@
-function minimumPairRemoval(nums: number[]): number {
-  let operations = 0;
+function minOperations(nums: number[], k: number): number {
+  let sum = 0;
 
-  while (!isSorted(nums)) {
-      let minSum = Infinity;
-      let index = -1;
-
-      // Find the leftmost adjacent pair with the minimum sum
-      for (let i = 0; i < nums.length - 1; i++) {
-          const sum = nums[i] + nums[i + 1];
-          if (sum < minSum) {
-              minSum = sum;
-              index = i;
-          }
-      }
-
-      // Merge the pair at index and index + 1
-      const merged = nums[index] + nums[index + 1];
-      nums.splice(index, 2, merged); // Remove 2 elements and insert the sum
-
-      operations++;
+  for (let num of nums) {
+      sum += num;
   }
 
-  return operations;
+  const remainder = sum % k;
+
+  return remainder === 0 ? 0 : remainder;
 };
 
-function isSorted(arr: number[]): boolean {
-  for (let i = 1; i < arr.length; i++) {
-      if (arr[i] < arr[i - 1]) {
-          return false;
-      }
-  }
-  return true;
-}
-
-const input: number[] = [5,2,3,1];
-const inputCopy = [...input];
-const results = minimumPairRemoval(inputCopy);
+const input: number[] = [3,9,7];
+const k = 5;
+const results = minOperations(input, k);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
