624c9e89bc1c623e850f34f0e3b00939f4a83a23
Jin Wang <specter01wj@gmail.com>
2025-05-14 19:22:40 -0400

    v1

---
+++ mysol/Category/Array/E3512_Minimum_Operations_to_Make_Array_Sum_Divisible_by_K/src/com/minimumOperationstoMakeArraySumDivisiblebyK/E3512_Minimum_Operations_to_Make_Array_Sum_Divisible_by_K.java
@@ -0,0 +1,10 @@
+package com.minimumOperationstoMakeArraySumDivisiblebyK;
+
+public class E3512_Minimum_Operations_to_Make_Array_Sum_Divisible_by_K {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
