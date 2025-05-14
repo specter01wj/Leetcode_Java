f4d2f6f35d52b96eaa8f3178b8a183157c78b071
Jin Wang <specter01wj@gmail.com>
2025-05-13 20:33:19 -0400

    v5

---
+++ mysol/Category/Array/E3502_Minimum_Cost_to_Reach_Every_Position/ts/code.ts
@@ -0,0 +1,21 @@
+function phonePrefix(numbers: string[]): boolean {
+  // Step 1: Sort the array lexicographically
+  numbers.sort();
+
+  // Step 2: Compare each number with its next one
+  for (let i = 0; i < numbers.length - 1; i++) {
+      if (numbers[i + 1].startsWith(numbers[i])) {
+          return false;
+      }
+  }
+
+  return true;
+};
+
+const input: string[] = ["001","007","15","00153"];
+const inputCopy = [...input];
+const results = phonePrefix(inputCopy);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
