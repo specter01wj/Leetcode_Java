9edc5113082d3d187f795bda22f12ea96e1069f9
Jin Wang <specter01wj@gmail.com>
2025-05-15 01:45:46 -0400

    v3

--- mysol/Featured/The LeetCode Beginners Guide/1_Solving Your First Problem/E2235_Add_Two_Integers/src/com/addTwoIntegers/E2235_Add_Two_Integers.java
+++ mysol/Featured/The LeetCode Beginners Guide/1_Solving Your First Problem/E2235_Add_Two_Integers/src/com/addTwoIntegers/E2235_Add_Two_Integers.java
@@ -1,10 +1,22 @@
 package com.addTwoIntegers;
+import java.util.*;
+
+/*
+Given two integers num1 and num2, return the sum of the two integers.
+*/
 
 public class E2235_Add_Two_Integers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E2235_Add_Two_Integers solution = new E2235_Add_Two_Integers();
+		int num1 = 12, num2 = 5;
+		int output = solution.sum(num1, num2);
+        System.out.println("input: " + (num1) + "; " + (num2) + "\noutput: " + (output));
 	}
+	
+	
+	public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
 
 }
