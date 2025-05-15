a23b8ca4b4e1b758097542e39b02d74ca8045ac8
Jin Wang <specter01wj@gmail.com>
2025-05-15 01:51:32 -0400

    v5

---
+++ mysol/Featured/The LeetCode Beginners Guide/1_Solving Your First Problem/E2235_Add_Two_Integers/ts/code.ts
@@ -0,0 +1,19 @@
+function minOperations(nums: number[], k: number): number {
+  let sum = 0;
+
+  for (let num of nums) {
+      sum += num;
+  }
+
+  const remainder = sum % k;
+
+  return remainder === 0 ? 0 : remainder;
+};
+
+const input: number[] = [3,9,7];
+const k = 5;
+const results = minOperations(input, k);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
