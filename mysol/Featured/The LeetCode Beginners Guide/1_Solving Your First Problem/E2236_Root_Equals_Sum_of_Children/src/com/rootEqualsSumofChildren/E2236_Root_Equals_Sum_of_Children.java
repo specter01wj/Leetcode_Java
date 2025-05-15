807d677502a62ba7c6c58a624d4b476ac086bee2
Jin Wang <specter01wj@gmail.com>
2025-05-15 02:37:52 -0400

    v1

---
+++ mysol/Featured/The LeetCode Beginners Guide/1_Solving Your First Problem/E2236_Root_Equals_Sum_of_Children/src/com/rootEqualsSumofChildren/E2236_Root_Equals_Sum_of_Children.java
@@ -0,0 +1,10 @@
+package com.rootEqualsSumofChildren;
+
+public class E2236_Root_Equals_Sum_of_Children {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
