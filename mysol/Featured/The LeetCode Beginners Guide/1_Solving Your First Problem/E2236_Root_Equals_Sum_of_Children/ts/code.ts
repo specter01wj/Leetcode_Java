588e2acaee8240c83766a62985e8769afaa5af97
Jin Wang <specter01wj@gmail.com>
2025-05-15 02:57:21 -0400

    v7

--- mysol/Featured/The LeetCode Beginners Guide/1_Solving Your First Problem/E2236_Root_Equals_Sum_of_Children/ts/code.ts
+++ mysol/Featured/The LeetCode Beginners Guide/1_Solving Your First Problem/E2236_Root_Equals_Sum_of_Children/ts/code.ts
@@ -1,10 +1,24 @@
-function sum(num1: number, num2: number): number {
-  return num1 + num2;
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+      this.val = val === undefined ? 0 : val;
+      this.left = left === undefined ? null : left;
+      this.right = right === undefined ? null : right;
+  }
+}
+
+function checkTree(root: TreeNode | null): boolean {
+  return root.val === root.left.val + root.right.val;
 };
 
-const num1: number = 12, num2: number = 5;
-const results = sum(num1, num2);
+const left = new TreeNode(4);
+const right = new TreeNode(6);
+const root = new TreeNode(10, left, right);
+const results = checkTree(root);
 
 let webHeading = document.querySelector('#t1');
-webHeading.innerHTML = 'Input: ' + JSON.stringify(num1, null, 2) + '; ' + JSON.stringify(num2, null, 2)  + '<br>Result = ' + JSON.stringify(results, null, 2);
+webHeading.innerHTML = 'Input: ' + JSON.stringify(root, null, 2) + '; ' + JSON.stringify(num2, null, 2)  + '<br>Result = ' + JSON.stringify(results, null, 2);
 
