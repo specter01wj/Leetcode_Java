0786a96f60cb57ad19fa259f03b27b03a6ce54f2
Jin Wang <specter01wj@gmail.com>
2025-05-16 01:27:47 -0400

    v2

--- mysol/Featured/The LeetCode Beginners Guide/1_Solving Your First Problem/E226_Invert_Binary_Tree/src/com/invertBinaryTree/E226_Invert_Binary_Tree.java
+++ mysol/Featured/The LeetCode Beginners Guide/1_Solving Your First Problem/E226_Invert_Binary_Tree/src/com/invertBinaryTree/E226_Invert_Binary_Tree.java
@@ -1,10 +1,62 @@
 package com.invertBinaryTree;
+import java.util.*;
+
+/*
+Given the root of a binary tree, invert the tree, and return its root.
+*/
+
+//TreeNode class
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	
+	TreeNode() {}
+	
+	TreeNode(int val) { this.val = val; }
+	
+	TreeNode(int val, TreeNode left, TreeNode right) {
+	   this.val = val;
+	   this.left = left;
+	   this.right = right;
+	}
+	
+	@Override
+	public String toString() {
+	   return "[" + val + ", " + left.val + ", " + right.val + "]";
+	}
+}
 
 public class E226_Invert_Binary_Tree {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E226_Invert_Binary_Tree solution = new E226_Invert_Binary_Tree();
+		TreeNode root = new TreeNode(4,
+            new TreeNode(2, new TreeNode(1), new TreeNode(3)),
+            new TreeNode(7, new TreeNode(6), new TreeNode(9))
+        );
+		TreeNode invertedRoot = solution.invertTree(root);
+		System.out.println("input: " + (root) + "\noutput: " + (invertedRoot));
 	}
+	
+	/*
+    solution:
+    binary tree
+    */
+	
+	/*
+     * @param root: a binary tree node
+     * @return: its root
+     */
+	public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        // Swap left and right subtrees
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+
+        return root;
+    }
 
 }
