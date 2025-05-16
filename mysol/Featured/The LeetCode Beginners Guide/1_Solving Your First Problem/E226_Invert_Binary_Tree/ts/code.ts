0f32132c4e2553329b8b9b9f8074c008879a107b
Jin Wang <specter01wj@gmail.com>
2025-05-16 01:39:02 -0400

    v5

---
+++ mysol/Featured/The LeetCode Beginners Guide/1_Solving Your First Problem/E226_Invert_Binary_Tree/ts/code.ts
@@ -0,0 +1,24 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+      this.val = val === undefined ? 0 : val;
+      this.left = left === undefined ? null : left;
+      this.right = right === undefined ? null : right;
+  }
+}
+
+function checkTree(root: TreeNode | null): boolean {
+  return root.val === root.left.val + root.right.val;
+};
+
+const left = new TreeNode(4);
+const right = new TreeNode(6);
+const root = new TreeNode(10, left, right);
+const results = checkTree(root);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: <pre>' + JSON.stringify(root, null, 2) + 
+    '</pre>Result = <pre>' + JSON.stringify(results, null, 2)  + '</pre>';
