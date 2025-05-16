644172ff57d6659193f12635082875742cc9180b
Jin Wang <specter01wj@gmail.com>
2025-05-16 14:46:05 -0400

    v2

--- mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E1480_Running_Sum_of_1d_Array/src/com/runningSumof1dArray/E1480_Running_Sum_of_1d_Array.java
+++ mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E1480_Running_Sum_of_1d_Array/src/com/runningSumof1dArray/E1480_Running_Sum_of_1d_Array.java
@@ -1,10 +1,28 @@
 package com.runningSumof1dArray;
+import java.util.*;
+
+/*
+Given an array nums. We define a running sum of an array as 
+runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+*/
 
 public class E1480_Running_Sum_of_1d_Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {1,2,3,4};
+		int output = runningSum(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+
+        return nums;
+    }
 
 }
