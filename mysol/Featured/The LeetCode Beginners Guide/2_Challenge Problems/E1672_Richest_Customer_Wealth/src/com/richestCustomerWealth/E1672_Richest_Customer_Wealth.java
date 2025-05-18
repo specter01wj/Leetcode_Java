b0bc6c55c366408a628244ec87b33ba8a0c9c378
Jin Wang <specter01wj@gmail.com>
2025-05-18 12:14:37 -0400

    v2

--- mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E1672_Richest_Customer_Wealth/src/com/richestCustomerWealth/E1672_Richest_Customer_Wealth.java
+++ mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E1672_Richest_Customer_Wealth/src/com/richestCustomerWealth/E1672_Richest_Customer_Wealth.java
@@ -1,10 +1,37 @@
 package com.richestCustomerWealth;
+import java.util.*;
+
+/*
+You are given an m x n integer grid accounts where accounts[i][j] 
+is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. 
+Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. 
+The richest customer is the customer that has the maximum wealth.
+*/
 
 public class E1672_Richest_Customer_Wealth {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[][] input = {{1,5},{7,3},{3,5}};
+		int output = maximumWealth(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
+
+        for (int[] customer : accounts) {
+            int sum = 0;
+
+            for (int money : customer) {
+                sum += money;
+            }
+            maxWealth = Math.max(maxWealth, sum);
+        }
+
+        return maxWealth;
+    }
 
 }
