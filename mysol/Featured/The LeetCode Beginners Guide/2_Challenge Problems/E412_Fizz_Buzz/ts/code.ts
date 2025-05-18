80a89deacfd401f6248c0cc4a1b3d287857a6050
Jin Wang <specter01wj@gmail.com>
2025-05-18 12:56:39 -0400

    v11

---
+++ mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E412_Fizz_Buzz/ts/code.ts
@@ -0,0 +1,15 @@
+function maximumWealth(accounts: number[][]): number {
+    let maxWealth = 0;
+    for (let customer of accounts) {
+        const sum = customer.reduce((a, b) => a + b, 0);
+        maxWealth = Math.max(maxWealth, sum);
+    }
+    return maxWealth;
+};
+
+const input: number[][] = [[1,5],[7,3],[3,5]];
+const results = maximumWealth(input);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
