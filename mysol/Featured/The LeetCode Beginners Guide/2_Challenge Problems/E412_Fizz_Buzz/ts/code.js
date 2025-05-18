68c43dca2f6605851329d5e63545a2fa0e18fd1c
Jin Wang <specter01wj@gmail.com>
2025-05-18 13:02:27 -0400

    v14

---
+++ mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E412_Fizz_Buzz/ts/code.js
@@ -0,0 +1,23 @@
+function fizzBuzz(n) {
+    const result = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+        }
+        else if (i % 3 === 0) {
+            result.push("Fizz");
+        }
+        else if (i % 5 === 0) {
+            result.push("Buzz");
+        }
+        else {
+            result.push(i.toString());
+        }
+    }
+    return result;
+}
+;
+const input = 5;
+const results = fizzBuzz(input);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
