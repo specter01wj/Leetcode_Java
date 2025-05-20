868bcb9e44ad00253e512ac198e14dbb9fd0df1a
Jin Wang <specter01wj@gmail.com>
2025-05-20 02:41:06 -0400

    v1

---
+++ mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E1342_Number_of_Steps_to_Reduce_a_Number_to_Zero/src/com/numberofStepstoReduceaNumbertoZero/E1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.java
@@ -0,0 +1,10 @@
+package com.numberofStepstoReduceaNumbertoZero;
+
+public class E1342_Number_of_Steps_to_Reduce_a_Number_to_Zero {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
