4a3169956856e5e3594f6e96333920dc96c8bd23
Jin Wang <specter01wj@gmail.com>
2025-05-20 02:54:46 -0400

    v7

--- mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E1342_Number_of_Steps_to_Reduce_a_Number_to_Zero/ts/code.ts
+++ mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E1342_Number_of_Steps_to_Reduce_a_Number_to_Zero/ts/code.ts
@@ -1,13 +1,18 @@
-function runningSum(nums: number[]): number[] {
-    for (let i = 1; i < nums.length; i++) {
-        nums[i] += nums[i - 1];
+function numberOfSteps(num: number): number {
+    let steps = 0;
+    while (num > 0) {
+        if (num % 2 === 0) {
+            num /= 2;
+        } else {
+            num -= 1;
+        }
+        steps++;
     }
-    return nums;
+    return steps;
 };
 
-const input: number[] = [1,2,3,4];
-const inputCopy: number[] = [...input];
-const results = runningSum(inputCopy);
+const input: number = 14;
+const results = numberOfSteps(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
