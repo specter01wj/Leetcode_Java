e0bce98d9d567c9b34798f206856745ca969ad68
Jin Wang <specter01wj@gmail.com>
2025-05-20 02:42:24 -0400

    v2

--- mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E1342_Number_of_Steps_to_Reduce_a_Number_to_Zero/src/com/numberofStepstoReduceaNumbertoZero/E1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.java
+++ mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E1342_Number_of_Steps_to_Reduce_a_Number_to_Zero/src/com/numberofStepstoReduceaNumbertoZero/E1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.java
@@ -1,10 +1,36 @@
 package com.numberofStepstoReduceaNumbertoZero;
+import java.util.*;
+
+/*
+Given an integer num, return the number of steps to reduce it to zero.
+
+In one step, if the current number is even, you have to divide it by 2, 
+otherwise, you have to subtract 1 from it.
+*/
 
 public class E1342_Number_of_Steps_to_Reduce_a_Number_to_Zero {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int input = 14;
+		int output = numberOfSteps(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	
+	public int numberOfSteps(int num) {
+        int steps = 0;
+
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+
+            steps++;
+        }
+
+        return steps;
+    }
 
 }
