11b3e88f23fc3e96609d021fef7ed5098c3ad2aa
Jin Wang <specter01wj@gmail.com>
2025-05-21 00:16:24 -0400

    v2

--- mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E383_Ransom_Note/src/com/ransomNote/E383_Ransom_Note.java
+++ mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E383_Ransom_Note/src/com/ransomNote/E383_Ransom_Note.java
@@ -1,10 +1,39 @@
 package com.ransomNote;
+import java.util.*;
+
+/*
+Given two strings ransomNote and magazine, return true if ransomNote 
+can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+*/
 
 public class E383_Ransom_Note {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String input = "aa";
+		String magazine = "aab";
+		boolean output = canConstruct(input, magazine);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	
+	public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26]; // For each letter 'a' to 'z'
+
+        // Count letters in magazine
+        for (char c : magazine.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        // Check if ransomNote can be constructed
+        for (char c : ransomNote.toCharArray()) {
+            if (--count[c - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
 }
