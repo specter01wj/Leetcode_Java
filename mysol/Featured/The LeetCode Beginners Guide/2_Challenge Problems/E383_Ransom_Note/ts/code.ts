1eac0507774a5fed4852ba7e3b125063e4757f24
Jin Wang <specter01wj@gmail.com>
2025-05-21 00:33:17 -0400

    v7

--- mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E383_Ransom_Note/ts/code.ts
+++ mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E383_Ransom_Note/ts/code.ts
@@ -1,21 +1,23 @@
-function fizzBuzz(n: number): string[] {
-    const result: string[] = [];
-    for (let i = 1; i <= n; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            result.push("FizzBuzz");
-        } else if (i % 3 === 0) {
-            result.push("Fizz");
-        } else if (i % 5 === 0) {
-            result.push("Buzz");
-        } else {
-            result.push(i.toString());
+function canConstruct(ransomNote: string, magazine: string): boolean {
+    const count: number[] = new Array(26).fill(0);
+
+    for (const char of magazine) {
+        count[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+
+    for (const char of ransomNote) {
+        const index = char.charCodeAt(0) - 'a'.charCodeAt(0);
+        if (--count[index] < 0) {
+            return false;
         }
     }
-    return result;
+
+    return true;
 };
 
-const input: number = 5;
-const results = fizzBuzz(input);
+const input: string = "aa";
+const magazine: string = "aab";
+const results = canConstruct(input, magazine);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
