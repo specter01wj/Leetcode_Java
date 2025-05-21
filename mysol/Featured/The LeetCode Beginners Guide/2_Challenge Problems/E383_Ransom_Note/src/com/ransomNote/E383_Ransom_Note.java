0bc4a05f1994ed8a07c9ac0fd95f52b9a2b425cb
Jin Wang <specter01wj@gmail.com>
2025-05-21 00:24:01 -0400

    v4

--- mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E383_Ransom_Note/src/com/ransomNote/E383_Ransom_Note.java
+++ mysol/Featured/The LeetCode Beginners Guide/2_Challenge Problems/E383_Ransom_Note/src/com/ransomNote/E383_Ransom_Note.java
@@ -18,7 +18,18 @@ public class E383_Ransom_Note {
         System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    1. Use an array count of size 26 to keep track of the frequency of each lowercase letter in magazine.
+	2. For each character in ransomNote, we decrement the corresponding count.
+	3. If any count goes below 0, it means there aren’t enough letters in magazine to form ransomNote.
+    */
 	
+	/*
+     * @param ransomNote: a string
+     * @param magazine: a string
+     * @return: true if ransomNote can be constructed by using the letters from magazine
+     */
 	public boolean canConstruct(String ransomNote, String magazine) {
         int[] count = new int[26]; // For each letter 'a' to 'z'
 
