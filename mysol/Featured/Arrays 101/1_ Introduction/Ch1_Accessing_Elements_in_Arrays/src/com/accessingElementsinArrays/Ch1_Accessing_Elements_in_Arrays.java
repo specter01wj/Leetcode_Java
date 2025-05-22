a76f5554782037214e3ea2f191571ea1c05fe694
Jin Wang <specter01wj@gmail.com>
2025-05-22 01:15:44 -0400

    v9

--- mysol/Featured/Arrays 101/1_ Introduction/Ch1_Accessing_Elements_in_Arrays/src/com/accessingElementsinArrays/Ch1_Accessing_Elements_in_Arrays.java
+++ mysol/Featured/Arrays 101/1_ Introduction/Ch1_Accessing_Elements_in_Arrays/src/com/accessingElementsinArrays/Ch1_Accessing_Elements_in_Arrays.java
@@ -27,6 +27,24 @@ public class Ch1_Accessing_Elements_in_Arrays {
 		System.out.println(dvdCollection[7]);
 		System.out.println(dvdCollection[10]);
 		System.out.println(dvdCollection[3]);
+		
+		
+		int[] squareNumbers = new int[10];
+
+		// Go through each of the Array indexes, from 0 to 9.
+		for (int i = 0; i < 10; i++) {
+		    // We need to be careful with the 0-indexing. The next square number
+		    // is given by (i + 1) * (i + 1).
+		    // Calculate it and insert it into the Array at index i.
+		    int square = (i + 1) * (i + 1);
+		    squareNumbers[i] = square;
+		}
+		
+		// Go through each of the Array indexes, from 0 to 9.
+		for (int i = 0; i < 10; i++) {
+		    // Access and print what's at the i'th index.
+		    System.out.println(squareNumbers[i]);
+		}
 	}
 	
 	public static class DVD {
