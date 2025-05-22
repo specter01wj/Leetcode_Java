2fe9f11867c52bf014a2535564254d477bd74d40
Jin Wang <specter01wj@gmail.com>
2025-05-22 00:19:29 -0400

    v5

--- mysol/Featured/Arrays 101/1_ Introduction/Ch1_What_Is_an_Array/src/com/whatIsanArray/Ch1_What_Is_an_Array.java
+++ mysol/Featured/Arrays 101/1_ Introduction/Ch1_What_Is_an_Array/src/com/whatIsanArray/Ch1_What_Is_an_Array.java
@@ -1,10 +1,32 @@
 package com.whatIsanArray;
+import java.util.*;
 
 public class Ch1_What_Is_an_Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Ch1_What_Is_an_Array solution = new Ch1_What_Is_an_Array();
+		DVD[] dvdCollection = new DVD[15];
+		dvdCollection[0] = solution.new DVD("The Matrix", 1999, "The Wachowskis");
+	    dvdCollection[1] = solution.new DVD("Inception", 2010, "Christopher Nolan");
+	    System.out.println("Output: " + Arrays.toString(dvdCollection));
+	}
+	
+	
+	// A simple definition for a DVD.
+	public class DVD {
+	    public String name;
+	    public int releaseYear;
+	    public String director;
 
+	    public DVD(String name, int releaseYear, String director) {
+	        this.name = name;
+	        this.releaseYear = releaseYear;
+	        this.director = director;
+	    }
+
+	    public String toString() {
+	        return this.name + ", directed by " + this.director + ", released in " + this.releaseYear;
+	    }
 	}
 
 }
