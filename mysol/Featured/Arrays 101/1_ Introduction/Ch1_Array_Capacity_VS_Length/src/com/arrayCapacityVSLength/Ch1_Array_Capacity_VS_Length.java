fc64ec5e19a0b1fb4effb90ea1852d4482e82b8e
Jin Wang <specter01wj@gmail.com>
2025-05-23 01:30:07 -0400

    v2

--- mysol/Featured/Arrays 101/1_ Introduction/Ch1_Array_Capacity_VS_Length/src/com/arrayCapacityVSLength/Ch1_Array_Capacity_VS_Length.java
+++ mysol/Featured/Arrays 101/1_ Introduction/Ch1_Array_Capacity_VS_Length/src/com/arrayCapacityVSLength/Ch1_Array_Capacity_VS_Length.java
@@ -1,10 +1,40 @@
 package com.arrayCapacityVSLength;
+import java.util.*;
 
 public class Ch1_Array_Capacity_VS_Length {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Create a new array with a capacity of 6.
+		int[] array = new int[6];
 
+		// Current length is 0, because it has 0 elements.
+		int length = 0;
+
+		// Add 3 items into it.
+		for (int i = 0; i < 3; i++) {
+		    array[i] = i * i;
+		    // Each time we add an element, the length goes up by one.
+		    length++;
+		}
+
+		System.out.println("The Array has a capacity of " + array.length);
+		System.out.println("The Array has a length of " + length);
+	}
+	
+	public static class DVD {
+	    public String name;
+	    public int releaseYear;
+	    public String director;
+
+	    public DVD(String name, int releaseYear, String director) {
+	        this.name = name;
+	        this.releaseYear = releaseYear;
+	        this.director = director;
+	    }
+
+	    public String toString() {
+	        return this.name + ", directed by " + this.director + ", released in " + this.releaseYear;
+	    }
 	}
 
 }
