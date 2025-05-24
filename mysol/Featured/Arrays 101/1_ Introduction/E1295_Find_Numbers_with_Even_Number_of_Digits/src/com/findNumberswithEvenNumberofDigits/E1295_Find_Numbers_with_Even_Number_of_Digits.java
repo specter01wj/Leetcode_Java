fbbfeeda748fb1e01f2feb6c1dfe9fdf46c91f58
Jin Wang <specter01wj@gmail.com>
2025-05-24 19:14:14 -0400

    v2

--- mysol/Featured/Arrays 101/1_ Introduction/E1295_Find_Numbers_with_Even_Number_of_Digits/src/com/findNumberswithEvenNumberofDigits/E1295_Find_Numbers_with_Even_Number_of_Digits.java
+++ mysol/Featured/Arrays 101/1_ Introduction/E1295_Find_Numbers_with_Even_Number_of_Digits/src/com/findNumberswithEvenNumberofDigits/E1295_Find_Numbers_with_Even_Number_of_Digits.java
@@ -1,10 +1,29 @@
 package com.findNumberswithEvenNumberofDigits;
+import java.util.*;
+
+/*
+Given an array nums of integers, return how many of them contain an even number of digits.
+*/
 
 public class E1295_Find_Numbers_with_Even_Number_of_Digits {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {12,345,2,6,7896};
+		int output = findNumbers(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public int findNumbers(int[] nums) {
+        int count = 0;
+
+        for (int num : nums) {
+            if (String.valueOf(num).length() % 2 == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 
 }
