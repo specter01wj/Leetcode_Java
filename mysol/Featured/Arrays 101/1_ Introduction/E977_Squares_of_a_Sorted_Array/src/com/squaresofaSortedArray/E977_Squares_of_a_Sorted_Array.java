501b07e7fe4a580f81c7b556d6eeab544244ce70
Jin Wang <specter01wj@gmail.com>
2025-05-24 19:36:42 -0400

    v3

--- mysol/Featured/Arrays 101/1_ Introduction/E977_Squares_of_a_Sorted_Array/src/com/squaresofaSortedArray/E977_Squares_of_a_Sorted_Array.java
+++ mysol/Featured/Arrays 101/1_ Introduction/E977_Squares_of_a_Sorted_Array/src/com/squaresofaSortedArray/E977_Squares_of_a_Sorted_Array.java
@@ -1,10 +1,40 @@
 package com.squaresofaSortedArray;
+import java.util.*;
+
+/*
+Given an integer array nums sorted in non-decreasing order, 
+return an array of the squares of each number sorted in non-decreasing order.
+*/
 
 public class E977_Squares_of_a_Sorted_Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E977_Squares_of_a_Sorted_Array solution = new E977_Squares_of_a_Sorted_Array();
+		int[] input = {-4,-1,0,3,10};
+		int[] output = solution.sortedSquares(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + Arrays.toString(output));
 	}
+	
+	
+	public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0, right = n - 1;
+        int pos = n - 1;
+
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+            if (leftSquare > rightSquare) {
+                result[pos--] = leftSquare;
+                left++;
+            } else {
+                result[pos--] = rightSquare;
+                right--;
+            }
+        }
+
+        return result;
+    }
 
 }
