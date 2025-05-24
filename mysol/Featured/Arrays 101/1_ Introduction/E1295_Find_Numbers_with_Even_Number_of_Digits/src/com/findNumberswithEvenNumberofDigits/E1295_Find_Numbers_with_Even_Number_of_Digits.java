590101fe8ed88456409dad48ac986b152e204f51
Jin Wang <specter01wj@gmail.com>
2025-05-24 19:12:33 -0400

    v1

---
+++ mysol/Featured/Arrays 101/1_ Introduction/E1295_Find_Numbers_with_Even_Number_of_Digits/src/com/findNumberswithEvenNumberofDigits/E1295_Find_Numbers_with_Even_Number_of_Digits.java
@@ -0,0 +1,10 @@
+package com.findNumberswithEvenNumberofDigits;
+
+public class E1295_Find_Numbers_with_Even_Number_of_Digits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
