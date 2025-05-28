b9539dcdaba0b1c691a9186216f707b20b34519a
Jin Wang <specter01wj@gmail.com>
2025-05-28 02:07:44 -0400

    v3

--- mysol/Featured/Arrays 101/2_Inserting Items Into an Array/Ch2_Array_Insertions/src/com/arrayInsertions/Ch2_Array_Insertions.java
+++ mysol/Featured/Arrays 101/2_Inserting Items Into an Array/Ch2_Array_Insertions/src/com/arrayInsertions/Ch2_Array_Insertions.java
@@ -21,6 +21,18 @@ public class Ch2_Array_Insertions {
 		intArray[length] = 10;
 		length++;
 		
+		// First, we will have to create space for a new element.
+		// We do that by shifting each element one index to the right.
+		// This will firstly move the element at index 3, then 2, then 1, then finally 0.
+		// We need to go backwards to avoid overwriting any elements.
+		for (int i = 3; i >= 0; i--) {
+		    intArray[i + 1] = intArray[i];
+		}
+
+		// Now that we have created space for the new element,
+		// we can insert it at the beginning.
+		intArray[0] = 20;
+		
 		for (int i = 0; i < intArray.length; i++) {
 		    System.out.println("Index " + i + " contains " + intArray[i]);
 		}
