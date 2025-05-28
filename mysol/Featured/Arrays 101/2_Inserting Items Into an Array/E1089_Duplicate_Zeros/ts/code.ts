67a48880816081fc8b8fe001da1d01f234553859
Jin Wang <specter01wj@gmail.com>
2025-05-28 03:28:05 -0400

    v9

--- mysol/Featured/Arrays 101/2_Inserting Items Into an Array/E1089_Duplicate_Zeros/ts/code.ts
+++ mysol/Featured/Arrays 101/2_Inserting Items Into an Array/E1089_Duplicate_Zeros/ts/code.ts
@@ -1,17 +1,37 @@
-function findNumbers(nums: number[]): number {
-    let count = 0;
+function duplicateZeros(arr: number[]): void {
+    let n = arr.length;
+    let possibleDups = 0;
+    let last = n - 1;
 
-    for (let num of nums) {
-        if (num.toString().length % 2 === 0) {
-            count++;
+    // Count the number of zeros to be duplicated
+    for (let i = 0; i <= last - possibleDups; i++) {
+        if (arr[i] === 0) {
+            // Edge case: zero at the boundary
+            if (i === last - possibleDups) {
+                arr[last] = 0;
+                last--;
+                break;
+            }
+            possibleDups++;
         }
     }
 
-    return count;
+    // Start from the end and move elements accordingly
+    for (let i = last - possibleDups; i >= 0; i--) {
+        if (arr[i] === 0) {
+            arr[i + possibleDups] = 0;
+            possibleDups--;
+            arr[i + possibleDups] = 0;
+        } else {
+            arr[i + possibleDups] = arr[i];
+        }
+    }
 };
 
-const input: number[] = [12,345,2,6,7896];
-const results = findNumbers(input);
+const input: number[] = [1,0,2,3,0,4,5,0];
+const inputCopy = [...input];
+duplicateZeros(inputCopy);
+const results = inputCopy;
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
