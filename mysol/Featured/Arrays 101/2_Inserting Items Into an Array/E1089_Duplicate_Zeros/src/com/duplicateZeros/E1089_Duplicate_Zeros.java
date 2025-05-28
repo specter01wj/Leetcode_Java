8692baf618dfa7d8a953829868232edf744c4b3d
Jin Wang <specter01wj@gmail.com>
2025-05-28 03:16:37 -0400

    v4

---
+++ mysol/Featured/Arrays 101/2_Inserting Items Into an Array/E1089_Duplicate_Zeros/src/com/duplicateZeros/E1089_Duplicate_Zeros.java
@@ -0,0 +1,51 @@
+package com.duplicateZeros;
+import java.util.*;
+
+/*
+Given a fixed-length integer array arr, duplicate each occurrence of zero, 
+shifting the remaining elements to the right.
+
+Note that elements beyond the length of the original array are not written. 
+Do the above modifications to the input array in place and do not return anything.
+*/
+
+public class E1089_Duplicate_Zeros {
+
+	public static void main(String[] args) {
+		int[] input = {1,0,2,3,0,4,5,0};
+		int[] output = duplicateZeros(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + Arrays.toString(output));
+	}
+	
+	
+	public void duplicateZeros(int[] arr) {
+        int n = arr.length;
+        int possibleDups = 0;
+        int last = n - 1;
+
+        // Count the number of zeros to be duplicated
+        for (int i = 0; i <= last - possibleDups; i++) {
+            if (arr[i] == 0) {
+                // Edge case: zero at the boundary
+                if (i == last - possibleDups) {
+                    arr[last] = 0;
+                    last--;
+                    break;
+                }
+                possibleDups++;
+            }
+        }
+
+        // Start from the end and shift
+        for (int i = last - possibleDups; i >= 0; i--) {
+            if (arr[i] == 0) {
+                arr[i + possibleDups] = 0;
+                possibleDups--;
+                arr[i + possibleDups] = 0;
+            } else {
+                arr[i + possibleDups] = arr[i];
+            }
+        }
+    }
+
+}
