2812d1e348264a30d1fbe9fc6437628d587f0d3d
Jin Wang <specter01wj@gmail.com>
2025-05-28 01:56:13 -0400

    v2

--- mysol/Featured/Arrays 101/2_Inserting Items Into an Array/Ch2_Array_Insertions/src/com/arrayInsertions/Ch2_Array_Insertions.java
+++ mysol/Featured/Arrays 101/2_Inserting Items Into an Array/Ch2_Array_Insertions/src/com/arrayInsertions/Ch2_Array_Insertions.java
@@ -1,10 +1,22 @@
 package com.arrayInsertions;
+import java.util.*;
 
 public class Ch2_Array_Insertions {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Declare an integer array of 6 elements
+		int[] intArray = new int[6];
+		int length = 0;
 
+		// Add 3 elements to the Array
+		for (int i = 0; i < 3; i++) {
+		    intArray[length] = i;
+		    length++;
+		}
+		
+		for (int i = 0; i < intArray.length; i++) {
+		    System.out.println("Index " + i + " contains " + intArray[i]);
+		}
 	}
 
 }
