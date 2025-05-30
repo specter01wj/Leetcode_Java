b7ca75d51e0c3edc6e66d75065df87f1974781bb
Jin Wang <specter01wj@gmail.com>
2025-05-29 20:10:58 -0400

    v2

--- mysol/Featured/Arrays 101/3_Deleting Items From an Array/Ch3_Array_Deletions/src/com/arrayDeletions/Ch3_Array_Deletions.java
+++ mysol/Featured/Arrays 101/3_Deleting Items From an Array/Ch3_Array_Deletions/src/com/arrayDeletions/Ch3_Array_Deletions.java
@@ -5,6 +5,8 @@ public class Ch3_Array_Deletions {
 
 	public static void main(String[] args) {
 		
+		// Deleting From the End of an Array
+		
 		// Declare an integer array of 10 elements.
 		int[] intArray = new int[10];
 		
@@ -26,11 +28,38 @@ public class Ch3_Array_Deletions {
 		    System.out.println("Index " + i + " contains " + intArray[i]);
 		}
 		
+		System.out.println("==================");
+		
 		
 		for (int i = 0; i < length; i++) {
 		    System.out.println("Index " + i + " contains " + intArray[i]);
 		}
+		
+		System.out.println("==================");
+		
+		
+		// Deleting From the Start of an Array
+		
+		// Starting at index 1, we shift each element one position
+		// to the left.
+		for (int i = 1; i < length; i++) {
+		    // Shift each element one position to the left
+			intArray[i - 1] = intArray[i];
+		}
 
+		// Note that it's important to reduce the length of the array by 1.
+		// Otherwise, we'll lose consistency of the size. This length
+		// variable is the only thing controlling where new elements might
+		// get added.
+		length--;
+		
+		
+		for (int i = 0; i < length; i++) {
+		    System.out.println("Index " + i + " contains " + intArray[i]);
+		}
+		
+		
+		
 	}
 
 }
