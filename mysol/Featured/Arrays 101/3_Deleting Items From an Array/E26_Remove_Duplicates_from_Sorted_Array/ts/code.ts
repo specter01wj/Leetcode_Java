71b820e1b8c03abd374adbf5eab8fcc3c575bc7c
Jin Wang <specter01wj@gmail.com>
2025-05-29 22:59:17 -0400

    v4

---
+++ mysol/Featured/Arrays 101/3_Deleting Items From an Array/E26_Remove_Duplicates_from_Sorted_Array/ts/code.ts
@@ -0,0 +1,19 @@
+function removeElement(nums: number[], val: number): number {
+    let k = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+};
+
+const input: number[] = [0,1,2,2,3,0,4,2];
+const val: number = 2;
+const inputCopy = [...input];
+const results = removeElement(inputCopy, val);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
