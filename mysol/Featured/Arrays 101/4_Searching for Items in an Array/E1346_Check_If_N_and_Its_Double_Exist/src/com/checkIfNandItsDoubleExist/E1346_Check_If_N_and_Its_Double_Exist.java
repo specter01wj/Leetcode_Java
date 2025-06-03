aacb0409e773b27ed615d5f8d3d4e40a180ecde4
Jin Wang <specter01wj@gmail.com>
2025-06-02 23:38:12 -0400

    v1

---
+++ mysol/Featured/Arrays 101/4_Searching for Items in an Array/E1346_Check_If_N_and_Its_Double_Exist/src/com/checkIfNandItsDoubleExist/E1346_Check_If_N_and_Its_Double_Exist.java
@@ -0,0 +1,10 @@
+package com.checkIfNandItsDoubleExist;
+
+public class E1346_Check_If_N_and_Its_Double_Exist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
