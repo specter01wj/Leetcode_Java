a981e26889575d4c83080cd4e57423ed39520482
Jin Wang <specter01wj@gmail.com>
2025-06-03 02:35:39 -0400

    v1

--- mysol/Featured/Arrays 101/4_Searching for Items in an Array/E1346_Check_If_N_and_Its_Double_Exist/src/com/checkIfNandItsDoubleExist/E1346_Check_If_N_and_Its_Double_Exist.java
+++ mysol/Featured/Arrays 101/4_Searching for Items in an Array/E1346_Check_If_N_and_Its_Double_Exist/src/com/checkIfNandItsDoubleExist/E1346_Check_If_N_and_Its_Double_Exist.java
@@ -1,10 +1,32 @@
 package com.checkIfNandItsDoubleExist;
+import java.util.*;
+
+/*
+Given an array arr of integers, check if there exist two indices i and j such that :
+
+  . i != j
+  . 0 <= i, j < arr.length
+  . arr[i] == 2 * arr[j]
+*/
 
 public class E1346_Check_If_N_and_Its_Double_Exist {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {10,2,5,3};
+		int output = checkIfExist(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public boolean checkIfExist(int[] arr) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : arr) {
+            if (seen.contains(2 * num) || (num % 2 == 0 && seen.contains(num / 2))) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
 
 }
