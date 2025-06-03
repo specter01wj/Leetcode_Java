2df2ebcfbdaedf785c76657d6afc6ca54f2e9f9c
Jin Wang <specter01wj@gmail.com>
2025-06-03 03:13:48 -0400

    v7

--- mysol/Featured/Arrays 101/4_Searching for Items in an Array/E941_Valid_Mountain_Array/ts/code.ts
+++ mysol/Featured/Arrays 101/4_Searching for Items in an Array/E941_Valid_Mountain_Array/ts/code.ts
@@ -1,16 +1,27 @@
-function checkIfExist(arr: number[]): boolean {
-    const seen = new Set<number>();
-    for (const num of arr) {
-        if (seen.has(2 * num) || (num % 2 === 0 && seen.has(num / 2))) {
-            return true;
-        }
-        seen.add(num);
+function validMountainArray(arr: number[]): boolean {
+    const n = arr.length;
+    if (n < 3) return false;
+
+    let i = 0;
+
+    // walk up
+    while (i + 1 < n && arr[i] < arr[i + 1]) {
+        i++;
     }
-    return false;
+
+    // peak can't be first or last
+    if (i === 0 || i === n - 1) return false;
+
+    // walk down
+    while (i + 1 < n && arr[i] > arr[i + 1]) {
+        i++;
+    }
+
+    return i === n - 1;
 };
 
-const input: number[] = [10,2,5,3];
-const results = checkIfExist(input);
+const input: number[] = [0,3,2,1];
+const results = validMountainArray(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
