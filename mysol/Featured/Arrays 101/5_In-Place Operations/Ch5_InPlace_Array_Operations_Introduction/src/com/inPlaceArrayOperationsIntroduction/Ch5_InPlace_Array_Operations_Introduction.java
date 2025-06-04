51cabe393a680883d2d21ccd5be1b856eec03564
Jin Wang <specter01wj@gmail.com>
2025-06-04 18:22:44 -0400

    v2

--- mysol/Featured/Arrays 101/5_In-Place Operations/Ch5_InPlace_Array_Operations_Introduction/src/com/inPlaceArrayOperationsIntroduction/Ch5_InPlace_Array_Operations_Introduction.java
+++ mysol/Featured/Arrays 101/5_In-Place Operations/Ch5_InPlace_Array_Operations_Introduction/src/com/inPlaceArrayOperationsIntroduction/Ch5_InPlace_Array_Operations_Introduction.java
@@ -1,10 +1,44 @@
 package com.inPlaceArrayOperationsIntroduction;
+import java.util.*;
 
 public class Ch5_InPlace_Array_Operations_Introduction {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] array = {9, -2, -9, 11, 56, -12, -3};
+	    int length = array.length;
 
+	    int[] result = Ch5_InPlace_Array_Operations_Introduction.squareEven1(array, length);
+
+	    System.out.println("Output: " + Arrays.toString(result));
+	}
+	
+	
+	public static int[] squareEven1(int[] array, int length) {
+	    // Check for edge cases.
+		if (array == null) {
+		    return null;
+		}
+
+		// Create a resultant Array which would hold the result.
+		int result[] = new int[length];
+
+		// Iterate through the original Array.
+		for (int i = 0; i < length; i++) {
+
+			// Get the element from slot i of the input Array.
+			int element = array[i];
+
+		    // If the index is an even number, then we need to square element.
+		    if (i % 2 == 0) {
+		    	element *= element;
+		    }
+
+		    // Write element into the result Array.
+		    result[i] = element;
+		}
+
+		// Return the result Array.
+		return result;
 	}
 
 }
