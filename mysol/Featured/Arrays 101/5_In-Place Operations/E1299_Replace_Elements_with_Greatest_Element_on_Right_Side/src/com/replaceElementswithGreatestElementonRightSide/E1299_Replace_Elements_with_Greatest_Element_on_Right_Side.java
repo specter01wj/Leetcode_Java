268f6c20f7671ed7b487a8320ad35867d3d10be3
Jin Wang <specter01wj@gmail.com>
2025-06-05 00:29:44 -0400

    v2

--- mysol/Featured/Arrays 101/5_In-Place Operations/E1299_Replace_Elements_with_Greatest_Element_on_Right_Side/src/com/replaceElementswithGreatestElementonRightSide/E1299_Replace_Elements_with_Greatest_Element_on_Right_Side.java
+++ mysol/Featured/Arrays 101/5_In-Place Operations/E1299_Replace_Elements_with_Greatest_Element_on_Right_Side/src/com/replaceElementswithGreatestElementonRightSide/E1299_Replace_Elements_with_Greatest_Element_on_Right_Side.java
@@ -1,10 +1,37 @@
 package com.replaceElementswithGreatestElementonRightSide;
+import java.util.*;
+
+/*
+Given an array arr, replace every element in that array with the greatest 
+element among the elements to its right, and replace the last element with -1.
+
+After doing so, return the array.
+*/
 
 public class E1299_Replace_Elements_with_Greatest_Element_on_Right_Side {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E1299_Replace_Elements_with_Greatest_Element_on_Right_Side solution = new E1299_Replace_Elements_with_Greatest_Element_on_Right_Side();
+		int[] input = {17,18,5,4,6,1};
+		int[] output = solution.replaceElements(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + Arrays.toString(output));
 	}
+	
+	
+	public int[] replaceElements(int[] arr) {
+        int n = arr.length;
+        int maxRight = -1;
+
+        // Traverse from right to left
+        for (int i = n - 1; i >= 0; i--) {
+            int current = arr[i];
+            arr[i] = maxRight;
+            if (current > maxRight) {
+                maxRight = current;
+            }
+        }
+
+        return arr;
+    }
 
 }
