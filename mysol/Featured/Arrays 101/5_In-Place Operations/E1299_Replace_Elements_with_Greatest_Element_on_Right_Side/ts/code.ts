7d44a90f9cd57cab3cbeb893abfc14338a019c68
Jin Wang <specter01wj@gmail.com>
2025-06-05 00:44:07 -0400

    v6

--- mysol/Featured/Arrays 101/5_In-Place Operations/E1299_Replace_Elements_with_Greatest_Element_on_Right_Side/ts/code.ts
+++ mysol/Featured/Arrays 101/5_In-Place Operations/E1299_Replace_Elements_with_Greatest_Element_on_Right_Side/ts/code.ts
@@ -1,16 +1,20 @@
-function checkIfExist(arr: number[]): boolean {
-    const seen = new Set<number>();
-    for (const num of arr) {
-        if (seen.has(2 * num) || (num % 2 === 0 && seen.has(num / 2))) {
-            return true;
+function replaceElements(arr: number[]): number[] {
+    let maxRight = -1;
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        const current = arr[i];
+        arr[i] = maxRight;
+        if (current > maxRight) {
+            maxRight = current;
         }
-        seen.add(num);
     }
-    return false;
+
+    return arr;
 };
 
-const input: number[] = [10,2,5,3];
-const results = checkIfExist(input);
+const input: number[] = [17,18,5,4,6,1];
+const inputCopy: number[] = [...input];
+const results = replaceElements(inputCopy);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
