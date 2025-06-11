0e0eed111e66ec4356a8ffea8d39add76523be15
Jin Wang <specter01wj@gmail.com>
2025-06-11 01:05:49 -0400

    v1

---
+++ mysol/Featured/Arrays 101/5_In-Place Operations/E905_Sort_Array_By_Parity/src/com/sortArrayByParity/E905_Sort_Array_By_Parity.java
@@ -0,0 +1,39 @@
+package com.sortArrayByParity;
+import java.util.*;
+
+/*
+Given an integer array nums, move all the even integers at the beginning of 
+the array followed by all the odd integers.
+
+Return any array that satisfies this condition.
+*/
+
+public class E905_Sort_Array_By_Parity {
+
+	public static void main(String[] args) {
+		E905_Sort_Array_By_Parity solution = new E905_Sort_Array_By_Parity();
+		int[] input = {3,1,2,4};
+		int[] output = solution.sortArrayByParity(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + Arrays.toString(output));
+	}
+	
+	
+	public int[] sortArrayByParity(int[] A) {
+        int left = 0, right = A.length - 1;
+
+        while (left < right) {
+            if (A[left] % 2 > A[right] % 2) {
+                // Swap A[left] and A[right]
+                int temp = A[left];
+                A[left] = A[right];
+                A[right] = temp;
+            }
+
+            if (A[left] % 2 == 0) left++;
+            if (A[right] % 2 == 1) right--;
+        }
+
+        return A;
+    }
+
+}
