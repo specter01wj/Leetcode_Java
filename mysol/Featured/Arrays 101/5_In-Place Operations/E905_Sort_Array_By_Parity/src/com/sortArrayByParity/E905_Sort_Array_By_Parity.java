9ed0956a324995ad96083906f4daaccc2d94a9e2
Jin Wang <specter01wj@gmail.com>
2025-06-11 01:06:13 -0400

    v2

--- mysol/Featured/Arrays 101/5_In-Place Operations/E905_Sort_Array_By_Parity/src/com/sortArrayByParity/E905_Sort_Array_By_Parity.java
+++ mysol/Featured/Arrays 101/5_In-Place Operations/E905_Sort_Array_By_Parity/src/com/sortArrayByParity/E905_Sort_Array_By_Parity.java
@@ -13,7 +13,8 @@ public class E905_Sort_Array_By_Parity {
 	public static void main(String[] args) {
 		E905_Sort_Array_By_Parity solution = new E905_Sort_Array_By_Parity();
 		int[] input = {3,1,2,4};
-		int[] output = solution.sortArrayByParity(input);
+		int[] inputCopy = Arrays.copyOf(input, input.length);
+		int[] output = solution.sortArrayByParity(inputCopy);
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + Arrays.toString(output));
 	}
 	
