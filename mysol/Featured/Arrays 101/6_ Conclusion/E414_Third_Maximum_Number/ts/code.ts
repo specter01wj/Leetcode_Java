26a609a90fcb5edf8ef6f9c64a5777d6ac1b8861
Jin Wang <specter01wj@gmail.com>
2025-06-14 23:37:15 -0400

    v7

--- mysol/Featured/Arrays 101/6_ Conclusion/E414_Third_Maximum_Number/ts/code.ts
+++ mysol/Featured/Arrays 101/6_ Conclusion/E414_Third_Maximum_Number/ts/code.ts
@@ -1,18 +1,28 @@
-function heightChecker(heights: number[]): number {
-    const expected: number[] = [...heights].sort((a, b) => a - b);
-    let count = 0;
+function thirdMax(nums: number[]): number {
+    let first: number | null = null;
+    let second: number | null = null;
+    let third: number | null = null;
 
-    for (let i = 0; i < heights.length; i++) {
-        if (heights[i] !== expected[i]) {
-            count++;
+    for (const num of nums) {
+        if (num === first || num === second || num === third) continue;
+
+        if (first === null || num > first) {
+            third = second;
+            second = first;
+            first = num;
+        } else if (second === null || num > second) {
+            third = second;
+            second = num;
+        } else if (third === null || num > third) {
+            third = num;
         }
     }
 
-    return count;
+    return third !== null ? third : first!;
 };
 
-const input: number[] = [1,1,4,2,1,3];
-const results = heightChecker(input);
+const input: number[] = [2,2,3,1];
+const results = thirdMax(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
