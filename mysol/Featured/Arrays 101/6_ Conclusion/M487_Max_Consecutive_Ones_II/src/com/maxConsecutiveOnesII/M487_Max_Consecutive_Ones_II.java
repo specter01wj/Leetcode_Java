457c05dcc7f2b1b2850980148fd69f85563cf69e
Jin Wang <specter01wj@gmail.com>
2025-06-14 23:07:54 -0400

    v2

--- mysol/Featured/Arrays 101/6_ Conclusion/M487_Max_Consecutive_Ones_II/src/com/maxConsecutiveOnesII/M487_Max_Consecutive_Ones_II.java
+++ mysol/Featured/Arrays 101/6_ Conclusion/M487_Max_Consecutive_Ones_II/src/com/maxConsecutiveOnesII/M487_Max_Consecutive_Ones_II.java
@@ -1,10 +1,41 @@
 package com.maxConsecutiveOnesII;
+import java.util.*;
+
+/*
+Given a binary array nums, return the maximum number of consecutive 1's 
+in the array if you can flip at most one 0.
+*/
 
 public class M487_Max_Consecutive_Ones_II {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {1,0,1,1,0};
+		int output = findMaxConsecutiveOnes(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int left = 0;
+        int zeroCount = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+
+            while (zeroCount > 1) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+
+            max = Math.max(max, right - left + 1);
+        }
+
+        return max;
+    }
 
 }
