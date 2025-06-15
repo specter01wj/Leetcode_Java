c0ba9099e3bab65a8db38fff86d439d5eacb238c
Jin Wang <specter01wj@gmail.com>
2025-06-14 23:09:41 -0400

    v3

--- mysol/Featured/Arrays 101/6_ Conclusion/M487_Max_Consecutive_Ones_II/src/com/maxConsecutiveOnesII/M487_Max_Consecutive_Ones_II.java
+++ mysol/Featured/Arrays 101/6_ Conclusion/M487_Max_Consecutive_Ones_II/src/com/maxConsecutiveOnesII/M487_Max_Consecutive_Ones_II.java
@@ -15,7 +15,19 @@ public class M487_Max_Consecutive_Ones_II {
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    1. Use two pointers: left and right to form a window.
+	2. Allow at most one 0 in the window (zeroCount <= 1).
+	3. If more than one 0 is in the window:
+		. Shrink the window from the left until zeroCount <= 1.
+	4. Update the maximum window size during each step.
+    */
 	
+	/*
+     * @param nums: a list of integers
+     * @return: the maximum number of consecutive 1's in the array
+     */
 	public int findMaxConsecutiveOnes(int[] nums) {
         int max = 0;
         int left = 0;
