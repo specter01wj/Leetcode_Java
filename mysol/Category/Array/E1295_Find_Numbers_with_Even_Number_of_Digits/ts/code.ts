908efa9021c026eedef28821954bb83597995ca5
Jin Wang <specter01wj@gmail.com>
2025-06-16 12:49:28 -0400

    v8

---
+++ mysol/Category/Array/E1295_Find_Numbers_with_Even_Number_of_Digits/ts/code.ts
@@ -0,0 +1,18 @@
+function findNumbers(nums: number[]): number {
+    let count = 0;
+
+    for (let num of nums) {
+        if (num.toString().length % 2 === 0) {
+            count++;
+        }
+    }
+
+    return count;
+};
+
+const input: number[] = [12,345,2,6,7896];
+const results = findNumbers(input);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
