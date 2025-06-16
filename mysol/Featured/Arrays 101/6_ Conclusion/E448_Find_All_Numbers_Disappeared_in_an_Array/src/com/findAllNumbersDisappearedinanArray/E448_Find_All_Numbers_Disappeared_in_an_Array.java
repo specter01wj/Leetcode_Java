5ac791932ab67467f6c688267e93c669d60e8724
Jin Wang <specter01wj@gmail.com>
2025-06-16 12:04:57 -0400

    v3

--- mysol/Featured/Arrays 101/6_ Conclusion/E448_Find_All_Numbers_Disappeared_in_an_Array/src/com/findAllNumbersDisappearedinanArray/E448_Find_All_Numbers_Disappeared_in_an_Array.java
+++ mysol/Featured/Arrays 101/6_ Conclusion/E448_Find_All_Numbers_Disappeared_in_an_Array/src/com/findAllNumbersDisappearedinanArray/E448_Find_All_Numbers_Disappeared_in_an_Array.java
@@ -11,11 +11,24 @@ public class E448_Find_All_Numbers_Disappeared_in_an_Array {
 	public static void main(String[] args) {
 		E448_Find_All_Numbers_Disappeared_in_an_Array solution = new E448_Find_All_Numbers_Disappeared_in_an_Array();
 		int[] input = {4,3,2,7,8,2,3,1};
-		List<Integer> output = solution.findDisappearedNumbers(input);
+		int[] inputCopy = Arrays.copyOf(input, input.length);
+		List<Integer> output = solution.findDisappearedNumbers(inputCopy);
         System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    1. In-place marking:
+		. Treat the number x as a marker to mark index x - 1 negative.
+		. This shows that number x was found.
+	2. Second pass:
+		. Any index i with a positive value indicates that i + 1 is missing.
+    */
 	
+	/*
+     * @param nums: a list of integers
+     * @return: an array of all the integers
+     */
 	public List<Integer> findDisappearedNumbers(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             int index = Math.abs(nums[i]) - 1;
