459c402da52196f4f3d5380783a6df4aa4ed8060
Jin Wang <specter01wj@gmail.com>
2025-07-05 19:53:08 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Ch2_checkForTarget/src/com/checkForTarget/Ch2_checkForTarget.java
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Ch2_checkForTarget/src/com/checkForTarget/Ch2_checkForTarget.java
@@ -1,10 +1,45 @@
 package com.checkForTarget;
+import java.util.*;
+
+/*
+Example 2: Given a sorted array of unique integers and a target integer, 
+return true if there exists a pair of numbers that sum to target, false otherwise. 
+This problem is similar to Two Sum. (In Two Sum, the input is not sorted).
+
+For example, given nums = [1, 2, 4, 6, 8, 9, 14, 15] and target = 13, 
+return true because 4 + 9 = 13.
+*/
 
 public class Ch2_checkForTarget {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Ch2_checkForTarget solution = new Ch2_checkForTarget();
+		int[] input = {1, 2, 4, 6, 8, 9, 14, 15};
+		int target = 13;
+		boolean output = solution.checkForTarget(input, target);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	
+	public boolean checkForTarget(int[] nums, int target) {
+	    int left = 0;
+	    int right = nums.length - 1;
 
+	    while (left < right) {
+	        // curr is the current sum
+	        int curr = nums[left] + nums[right];
+	        if (curr == target) {
+	            return true;
+	        }
+
+	        if (curr > target) {
+	            right--;
+	        } else {
+	            left++;
+	        }
+	    }
+
+	    return false;
 	}
 
 }
