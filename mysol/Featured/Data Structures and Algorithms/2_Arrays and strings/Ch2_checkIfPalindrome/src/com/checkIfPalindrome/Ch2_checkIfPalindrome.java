35fbafb6471cd253c9cfa4c998859f3b616bdeb1
Jin Wang <specter01wj@gmail.com>
2025-07-05 13:22:32 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Ch2_checkIfPalindrome/src/com/checkIfPalindrome/Ch2_checkIfPalindrome.java
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Ch2_checkIfPalindrome/src/com/checkIfPalindrome/Ch2_checkIfPalindrome.java
@@ -1,10 +1,38 @@
 package com.checkIfPalindrome;
+import java.util.*;
+
+/*
+Example 1: Given a string s, return true if it is a palindrome, false otherwise.
+
+A string is a palindrome if it reads the same forward as backward. 
+That means, after reversing it, it is still the same string. 
+For example: "abcdcba", or "racecar".
+*/
 
 public class Ch2_checkIfPalindrome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Ch2_checkIfPalindrome solution = new Ch2_checkIfPalindrome();
+		String input = "racecar";
+		boolean output = solution.checkIfPalindrome(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
+	}
+	
+	
+	public boolean checkIfPalindrome(String s) {
+	    int left = 0;
+	    int right = s.length() - 1;
 
+	    while (left < right) {
+	        if (s.charAt(left) != s.charAt(right)) {
+	            return false;
+	        }
+
+	        left++;
+	        right--;
+	    }
+
+	    return true;
 	}
 
 }
