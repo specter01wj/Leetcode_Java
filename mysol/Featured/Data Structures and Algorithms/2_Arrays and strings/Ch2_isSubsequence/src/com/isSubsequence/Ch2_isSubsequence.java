01c8823130d3758eafb69c40bbbbacd107b119b6
Jin Wang <specter01wj@gmail.com>
2025-07-06 19:24:37 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Ch2_isSubsequence/src/com/isSubsequence/Ch2_isSubsequence.java
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Ch2_isSubsequence/src/com/isSubsequence/Ch2_isSubsequence.java
@@ -1,10 +1,41 @@
 package com.isSubsequence;
+import java.util.*;
+
+/*
+Example 4: 392. Is Subsequence.
+
+Given two strings s and t, return true if s is a subsequence of t, 
+or false otherwise.
+
+A subsequence of a string is a sequence of characters that can be 
+obtained by deleting some (or none) of the characters from the 
+original string, while maintaining the relative order of the 
+remaining characters. For example, "ace" is a subsequence 
+of "abcde" while "aec" is not.
+*/
 
 public class Ch2_isSubsequence {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Ch2_isSubsequence solution = new Ch2_isSubsequence();
+		String input = "ace", t = "abcde";
+		boolean output = solution.isSubsequence(input, t);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	
+	public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+        
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        
+        return i == s.length();
+    }
 
 }
