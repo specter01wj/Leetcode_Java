8d86aba2ba7362456e6964204f123f4f779257e4
Jin Wang <specter01wj@gmail.com>
2025-07-05 22:05:02 -0400

    v8

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Ch2_combine/ts/code.js
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Ch2_combine/ts/code.js
@@ -1,36 +1,28 @@
-function combine(arr1: number[], arr2: number[]): number[] {
-    const ans: number[] = [];
+function combine(arr1, arr2) {
+    const ans = [];
     let i = 0, j = 0;
-
     while (i < arr1.length && j < arr2.length) {
         if (arr1[i] < arr2[j]) {
             ans.push(arr1[i]);
             i++;
-        } else {
+        }
+        else {
             ans.push(arr2[j]);
             j++;
         }
     }
-
     while (i < arr1.length) {
         ans.push(arr1[i]);
         i++;
     }
-
     while (j < arr2.length) {
         ans.push(arr2[j]);
         j++;
     }
-
     return ans;
 }
-
-
-
-const input1: number[] = [1, 4, 7, 20];
-const input2: number[] = [3, 5, 6];
+const input1 = [1, 4, 7, 20];
+const input2 = [3, 5, 6];
 const results = combine(input1, input2);
-
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input1: ' + JSON.stringify(input1, null, 2) + '; Input2: ' + JSON.stringify(input2, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
-
