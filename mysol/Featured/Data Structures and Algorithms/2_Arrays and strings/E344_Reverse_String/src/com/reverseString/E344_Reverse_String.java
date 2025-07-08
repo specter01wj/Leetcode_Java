f40a92f5d9979443cea149e920347e91c7f31169
Jin Wang <specter01wj@gmail.com>
2025-07-08 19:45:09 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/E344_Reverse_String/src/com/reverseString/E344_Reverse_String.java
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/E344_Reverse_String/src/com/reverseString/E344_Reverse_String.java
@@ -1,10 +1,35 @@
 package com.reverseString;
+import java.util.*;
+
+/*
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+*/
 
 public class E344_Reverse_String {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E344_Reverse_String solution = new E344_Reverse_String();
+		char[] input = {'h','e','l','l','o'};
+		char[] inputCopy = Arrays.copyOf(input, input.length);
+		solution.reverseString(input);
+        System.out.println("input: " + Arrays.toString(inputCopy) + "\noutput: " + Arrays.toString(input));
 	}
+	
+	
+	public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
 
 }
