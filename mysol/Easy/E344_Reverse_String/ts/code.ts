128edd168c5cc810b97ad7a56cc96362cb9c1dc2
Jin Wang <specter01wj@gmail.com>
2025-07-09 01:51:36 -0400

    v6

---
+++ mysol/Easy/E344_Reverse_String/ts/code.ts
@@ -0,0 +1,18 @@
+function reverseString(s: string[]): void {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        [s[left], s[right]] = [s[right], s[left]];
+        left++;
+        right--;
+    }
+};
+
+const input: string[] = ["h","e","l","l","o"];
+const inputCopy: string[] = [...input];
+reverseString(inputCopy);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(inputCopy, null, 2);
+
