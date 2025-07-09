34af6ad617ad8b0ac0caf6281ae3f79048ac535b
Jin Wang <specter01wj@gmail.com>
2025-07-09 01:14:15 -0400

    v4

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/E344_Reverse_String/ts/code.js
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/E344_Reverse_String/ts/code.js
@@ -1,18 +1,15 @@
-function reverseString(s: string[]): void {
+function reverseString(s) {
     let left = 0;
     let right = s.length - 1;
-
     while (left < right) {
         [s[left], s[right]] = [s[right], s[left]];
         left++;
         right--;
     }
-};
-
-const input: string[] = ["h","e","l","l","o"];
-const inputCopy: string[] = [...input];
+}
+;
+const input = ["h", "e", "l", "l", "o"];
+const inputCopy = [...input];
 reverseString(inputCopy);
-
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(inputCopy, null, 2);
-
