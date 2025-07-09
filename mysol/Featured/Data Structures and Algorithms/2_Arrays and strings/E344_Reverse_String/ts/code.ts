fa75d386de63c13162d90d8201a159109038042f
Jin Wang <specter01wj@gmail.com>
2025-07-09 01:04:03 -0400

    v1

---
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/E344_Reverse_String/ts/code.ts
@@ -0,0 +1,20 @@
+function isSubsequence(s: string, t: string): boolean {
+    let i = 0;
+    let j = 0;
+
+    while (i < s.length && j < t.length) {
+        if (s[i] === t[j]) {
+            i++;
+        }
+        j++;
+    }
+
+    return i === s.length;
+}
+
+const input1: string = "ace", input2: string = "abcde";
+const results = isSubsequence(input1, input2);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input1, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
