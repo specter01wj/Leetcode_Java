f1b7d02b8e83f5d595c219c5a0ffe5ac5ed44231
Jin Wang <specter01wj@gmail.com>
2025-07-14 14:59:48 -0400

    v4

---
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/M1004_Max_Consecutive_Ones_III/ts/code.ts
@@ -0,0 +1,23 @@
+function findMaxAverage(nums: number[], k: number): number {
+    let curr = 0;
+    for (let i = 0; i < k; i++) {
+        curr += nums[i];
+    }
+
+    let maxSum = curr;
+    for (let i = k; i < nums.length; i++) {
+        curr += nums[i] - nums[i - k];
+        maxSum = Math.max(maxSum, curr);
+    }
+
+    return maxSum / k;
+};
+
+
+const input: number[] = [1,12,-5,-6,50,3];
+const k: number = 4;
+const results = findMaxAverage(input, k);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
