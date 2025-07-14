0b9f141d23921f6480cc9aea410305a95628fc13
Jin Wang <specter01wj@gmail.com>
2025-07-14 12:45:30 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/E643_Maximum_Average_Subarray_I/src/com/maximumAverageSubarrayI/E643_Maximum_Average_Subarray_I.java
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/E643_Maximum_Average_Subarray_I/src/com/maximumAverageSubarrayI/E643_Maximum_Average_Subarray_I.java
@@ -1,10 +1,50 @@
 package com.maximumAverageSubarrayI;
+import java.util.*;
+
+/*
+You are given an integer array nums consisting of n elements, and an integer k.
+
+Find a contiguous subarray whose length is equal to k that has the maximum 
+average value and return this value. Any answer with a calculation error 
+less than 10-5 will be accepted.
+*/
 
 public class E643_Maximum_Average_Subarray_I {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {1,12,-5,-6,50,3};
+		int k = 4;
+		int output = findMaxAverage(input, k);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	/*
+    solution:
+    Sliding window
+    */
+	
+	/*
+     * @param nums: a list of integers
+     * @param k: an integer
+     * @return: find a contiguous subarray
+     */
+	public double findMaxAverage(int[] nums, int k) {
+        double curr = 0;
+
+        // Sum of first k elements
+        for (int i = 0; i < k; i++) {
+            curr += nums[i];
+        }
+
+        double maxSum = curr;
+
+        // Sliding window
+        for (int i = k; i < nums.length; i++) {
+            curr += nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, curr);
+        }
+
+        return maxSum / k;
+    }
 
 }
