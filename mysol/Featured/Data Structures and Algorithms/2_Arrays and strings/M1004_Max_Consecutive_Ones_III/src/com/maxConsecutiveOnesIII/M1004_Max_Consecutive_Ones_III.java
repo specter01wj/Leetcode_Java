b6059cb54ed5777b7553f644bed7a667a45ac66f
Jin Wang <specter01wj@gmail.com>
2025-07-14 14:57:48 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/M1004_Max_Consecutive_Ones_III/src/com/maxConsecutiveOnesIII/M1004_Max_Consecutive_Ones_III.java
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/M1004_Max_Consecutive_Ones_III/src/com/maxConsecutiveOnesIII/M1004_Max_Consecutive_Ones_III.java
@@ -1,10 +1,43 @@
 package com.maxConsecutiveOnesIII;
+import java.util.*;
+
+/*
+Given a binary array nums and an integer k, return the maximum number 
+of consecutive 1's in the array if you can flip at most k 0's.
+*/
 
 public class M1004_Max_Consecutive_Ones_III {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		M1004_Max_Consecutive_Ones_III solution = new M1004_Max_Consecutive_Ones_III();
+		int[] input = {1,1,1,0,0,0,1,1,1,1,0};
+		int k = 2;
+		int output = solution.longestOnes(input, k);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public int longestOnes(int[] nums, int k) {
+        int left = 0;
+        int zeroCount = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+
+            while (zeroCount > k) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
 
 }
