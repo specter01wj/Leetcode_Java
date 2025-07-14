d62f765bd1cecdc2ef9ea5b62ee984e6f8399d5c
Jin Wang <specter01wj@gmail.com>
2025-07-14 15:03:05 -0400

    v7

---
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/M1004_Max_Consecutive_Ones_III/ts/code.js
@@ -0,0 +1,24 @@
+function longestOnes(nums, k) {
+    let left = 0;
+    let zeroCount = 0;
+    let maxLength = 0;
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] === 0) {
+            zeroCount++;
+        }
+        while (zeroCount > k) {
+            if (nums[left] === 0) {
+                zeroCount--;
+            }
+            left++;
+        }
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+    return maxLength;
+}
+;
+const input = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0];
+const k = 2;
+const results = longestOnes(input, k);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
