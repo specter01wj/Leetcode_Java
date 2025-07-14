f95413f22998cd51ae7cf78d8136e58a35fb1839
Jin Wang <specter01wj@gmail.com>
2025-07-13 22:58:20 -0400

    v6

---
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Ch2_numSubarrayProductLessThanK/ts/code.js
@@ -0,0 +1,21 @@
+function numSubarrayProductLessThanK(nums, k) {
+    if (k <= 1)
+        return 0;
+    let ans = 0;
+    let left = 0;
+    let curr = 1;
+    for (let right = 0; right < nums.length; right++) {
+        curr *= nums[right];
+        while (curr >= k) {
+            curr /= nums[left];
+            left++;
+        }
+        ans += right - left + 1;
+    }
+    return ans;
+}
+const input = [10, 5, 2, 6];
+const k = 100;
+const results = numSubarrayProductLessThanK(input, k);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
