28ee2c30d2b11e929ff9ab193f7180c5145a9776
Jin Wang <specter01wj@gmail.com>
2025-07-13 22:57:57 -0400

    v5

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Ch2_numSubarrayProductLessThanK/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Ch2_numSubarrayProductLessThanK/ts/code.ts
@@ -1,29 +1,28 @@
-function findLengthStr(s: string): number {
-    let left = 0;
-    let curr = 0;
+function numSubarrayProductLessThanK(nums: number[], k: number): number {
+    if (k <= 1) return 0;
+
     let ans = 0;
+    let left = 0;
+    let curr = 1;
 
-    for (let right = 0; right < s.length; right++) {
-        if (s[right] === '0') {
-            curr++;
-        }
+    for (let right = 0; right < nums.length; right++) {
+        curr *= nums[right];
 
-        while (curr > 1) {
-            if (s[left] === '0') {
-                curr--;
-            }
+        while (curr >= k) {
+            curr /= nums[left];
             left++;
         }
 
-        ans = Math.max(ans, right - left + 1);
+        ans += right - left + 1;
     }
 
     return ans;
 }
 
 
-const input: string = "1101100111";
-const results = findLengthStr(input);
+const input: number[] = [10, 5, 2, 6];
+const k: number = 100;
+const results = numSubarrayProductLessThanK(input, k);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
