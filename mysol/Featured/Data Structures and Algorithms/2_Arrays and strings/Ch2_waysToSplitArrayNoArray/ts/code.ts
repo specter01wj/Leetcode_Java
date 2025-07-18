adc4389024d3dc0845886dcbbc827e04a10ef209
Jin Wang <specter01wj@gmail.com>
2025-07-18 00:16:46 -0400

    v5

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Ch2_waysToSplitArrayNoArray/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Ch2_waysToSplitArrayNoArray/ts/code.ts
@@ -1,16 +1,15 @@
-function waysToSplitArray(nums: number[]): number {
-    const n = nums.length;
-    const prefix: number[] = new Array(n);
-    prefix[0] = nums[0];
+function waysToSplitArrayNoArray(nums: number[]): number {
+    let ans = 0;
+    let leftSection = 0;
+    let total = 0;
 
-    for (let i = 1; i < n; i++) {
-        prefix[i] = nums[i] + prefix[i - 1];
+    for (let num of nums) {
+        total += num;
     }
 
-    let ans = 0;
-    for (let i = 0; i < n - 1; i++) {
-        const leftSection = prefix[i];
-        const rightSection = prefix[n - 1] - prefix[i];
+    for (let i = 0; i < nums.length - 1; i++) {
+        leftSection += nums[i];
+        let rightSection = total - leftSection;
         if (leftSection >= rightSection) {
             ans++;
         }
@@ -20,7 +19,7 @@ function waysToSplitArray(nums: number[]): number {
 }
 
 const input: number[] = [10,4,-8,7];
-const results = waysToSplitArray(input);
+const results = waysToSplitArrayNoArray(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
