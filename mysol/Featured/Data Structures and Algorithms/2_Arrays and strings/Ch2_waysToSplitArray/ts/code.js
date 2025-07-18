554425a5de087d9d6ed925685f7b34f77f82ae41
Jin Wang <specter01wj@gmail.com>
2025-07-17 23:58:55 -0400

    v8

---
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Ch2_waysToSplitArray/ts/code.js
@@ -0,0 +1,21 @@
+function waysToSplitArray(nums) {
+    const n = nums.length;
+    const prefix = new Array(n);
+    prefix[0] = nums[0];
+    for (let i = 1; i < n; i++) {
+        prefix[i] = nums[i] + prefix[i - 1];
+    }
+    let ans = 0;
+    for (let i = 0; i < n - 1; i++) {
+        const leftSection = prefix[i];
+        const rightSection = prefix[n - 1] - prefix[i];
+        if (leftSection >= rightSection) {
+            ans++;
+        }
+    }
+    return ans;
+}
+const input = [10, 4, -8, 7];
+const results = waysToSplitArray(input);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
