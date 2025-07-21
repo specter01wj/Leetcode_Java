d6a83174e7f4c926da2fe805ff486527d8a4b8d9
Jin Wang <specter01wj@gmail.com>
2025-07-21 01:21:59 -0400

    v10

---
+++ mysol/Easy/E1413_Minimum_Value_to_Get_Positive_Step_by_Step_Sum/ts/code.js
@@ -0,0 +1,14 @@
+function minStartValue(nums) {
+    let sum = 0;
+    let minSum = 0;
+    for (let num of nums) {
+        sum += num;
+        minSum = Math.min(minSum, sum);
+    }
+    return 1 - minSum;
+}
+;
+const input = [-3, 2, -3, 4, 2];
+const results = minStartValue(input);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
