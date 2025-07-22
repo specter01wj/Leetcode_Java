f4ac7c65fde9fcecbd7d2b16206aa7d6b5dcd752
Jin Wang <specter01wj@gmail.com>
2025-07-22 02:21:33 -0400

    v7

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/M2090_K_Radius_Subarray_Averages/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/M2090_K_Radius_Subarray_Averages/ts/code.ts
@@ -1,17 +1,28 @@
-function minStartValue(nums: number[]): number {
-    let sum = 0;
-    let minSum = 0;
+function getAverages(nums: number[], k: number): number[] {
+    const n = nums.length;
+    const ans: number[] = new Array(n).fill(-1);
+    if (k === 0) return nums;
+    if (2 * k + 1 > n) return ans;
 
-    for (let num of nums) {
-        sum += num;
-        minSum = Math.min(minSum, sum);
+    let windowSum = 0;
+    const windowSize = 2 * k + 1;
+
+    for (let i = 0; i < n; i++) {
+        windowSum += nums[i];
+
+        if (i >= windowSize - 1) {
+            const center = i - k;
+            ans[center] = Math.floor(windowSum / windowSize);
+            windowSum -= nums[i - windowSize + 1];
+        }
     }
 
-    return 1 - minSum;
+    return ans;
 };
 
-const input: number[] = [-3,2,-3,4,2];
-const results = minStartValue(input);
+const input: number[] = [7,4,3,9,1,8,5,2,6];
+const k: number = 3;
+const results = getAverages(input, k);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
