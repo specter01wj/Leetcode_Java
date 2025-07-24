abbd5cdfb401e537840f36ffe87fb0246641e621
Jin Wang <specter01wj@gmail.com>
2025-07-24 12:29:02 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/E557_Reverse_Words_in_a_String_III/src/com/reverseWordsinaStringIII/E557_Reverse_Words_in_a_String_III.java
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/E557_Reverse_Words_in_a_String_III/src/com/reverseWordsinaStringIII/E557_Reverse_Words_in_a_String_III.java
@@ -1,10 +1,28 @@
 package com.reverseWordsinaStringIII;
+import java.util.*;
+
+/*
+Given a string s, reverse the order of characters in each word within 
+a sentence while still preserving whitespace and initial word order.
+*/
 
 public class E557_Reverse_Words_in_a_String_III {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E557_Reverse_Words_in_a_String_III solution = new E557_Reverse_Words_in_a_String_III();
+		String input = "Let's take LeetCode contest";
+		String output = solution.reverseWords(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	
+	public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (String word : words) {
+            result.append(new StringBuilder(word).reverse()).append(" ");
+        }
+        return result.toString().trim();
+    }
 
 }
