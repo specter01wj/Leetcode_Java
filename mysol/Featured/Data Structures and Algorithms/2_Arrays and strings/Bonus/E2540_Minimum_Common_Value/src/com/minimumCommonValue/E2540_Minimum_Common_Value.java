a52aaa74aa579f3477bf5365ac35046db183900d
Jin Wang <specter01wj@gmail.com>
2025-07-29 00:43:48 -0400

    v1

---
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/E2540_Minimum_Common_Value/src/com/minimumCommonValue/E2540_Minimum_Common_Value.java
@@ -0,0 +1,66 @@
+package com.minimumCommonValue;
+import java.util.*;
+
+/*
+Given two integer arrays nums1 and nums2, sorted in non-decreasing order, 
+return the minimum integer common to both arrays. If there is no common 
+integer amongst nums1 and nums2, return -1.
+
+Note that an integer is said to be common to nums1 and nums2 if both arrays 
+have at least one occurrence of that integer.
+
+
+Example 1:
+
+Input: nums1 = [1,2,3], nums2 = [2,4]
+Output: 2
+Explanation: The smallest element common to both arrays is 2, so we return 2.
+
+Example 2:
+
+Input: nums1 = [1,2,3,6], nums2 = [2,3,4,5]
+Output: 2
+Explanation: There are two common elements in the array 2 and 3 out of which 2 
+is the smallest, so 2 is returned.
+*/
+
+public class E2540_Minimum_Common_Value {
+
+	public static void main(String[] args) {
+		E2540_Minimum_Common_Value solution = new E2540_Minimum_Common_Value();
+		int[] nums1 = {1,2,3}, nums2 = {2,4};
+		int output = solution.getCommon(nums1, nums2);
+        System.out.println("input1: " + Arrays.toString(nums1) + 
+        		"; input2: " + Arrays.toString(nums2) + "\noutput: " + (output));
+	}
+	
+	/*
+    solution:
+    1. The code uses two pointers to traverse both sorted arrays (nums1 and nums2).
+	2. It compares elements at the current pointers and moves the pointer with the smaller element.
+	3. When both elements match, the common value is returned. If no common element is found, -1 is returned.
+    */
+	
+	/*
+     * @param nums1: a list of integers
+     * @param nums2: a list of integers
+     * @return: the minimum integer common to both arrays
+     */
+	public int getCommon(int[] nums1, int[] nums2) {
+		int i = 0, j = 0;
+        
+        // Use two pointers to traverse through both arrays
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i]; // Common element found
+            } else if (nums1[i] < nums2[j]) {
+                i++; // Move pointer of nums1 forward
+            } else {
+                j++; // Move pointer of nums2 forward
+            }
+        }
+        
+        return -1; // No common element found
+	}
+
+}
---
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/E2540_Minimum_Common_Value/ts/code.ts
@@ -0,0 +1,25 @@
+function getCommon(nums1: number[], nums2: number[]): number {
+  let i = 0, j = 0;
+  
+  // Use two pointers to traverse through both arrays
+  while (i < nums1.length && j < nums2.length) {
+      if (nums1[i] === nums2[j]) {
+          return nums1[i]; // Common element found
+      } else if (nums1[i] < nums2[j]) {
+          i++; // Move pointer of nums1 forward
+      } else {
+          j++; // Move pointer of nums2 forward
+      }
+  }
+  
+  return -1; // No common element found
+};
+
+const nums1: number[] = [1,2,3];
+const nums2: number[] = [2,4];
+const results = getCommon(nums1, nums2);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input1: ' + JSON.stringify(nums1, null, 2) + 
+  '; Input2: ' + JSON.stringify(nums2, null, 2) + '<br>Result = ' + results;
+
