cd1dff27dc1fac9ae8093bebc210cd2fb578c6f0
Jin Wang <specter01wj@gmail.com>
2025-07-29 00:55:16 -0400

    v2

---
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/E283_Move_Zeroes/ts/code.ts
@@ -0,0 +1,23 @@
+function moveZeroes(nums: number[]): void {
+    let insertPos = 0;
+
+    // Move all non-zero elements to the front
+    for (let num of nums) {
+        if (num !== 0) {
+            nums[insertPos++] = num;
+        }
+    }
+
+    // Fill the remaining positions with zeroes
+    while (insertPos < nums.length) {
+        nums[insertPos++] = 0;
+    }
+};
+
+const input: number[] = [0,1,0,3,12];
+const inputCopy = [...input];
+moveZeroes(inputCopy);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(inputCopy, null, 2);
+
