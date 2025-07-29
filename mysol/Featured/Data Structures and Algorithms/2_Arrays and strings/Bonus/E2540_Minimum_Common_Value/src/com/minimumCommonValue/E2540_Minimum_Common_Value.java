36ace62eb9e8bf90defb79f17d56422f1e1c2ccd
Jin Wang <specter01wj@gmail.com>
2025-07-29 00:56:53 -0400

    v3

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/E2540_Minimum_Common_Value/src/com/minimumCommonValue/E2540_Minimum_Common_Value.java
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/E2540_Minimum_Common_Value/src/com/minimumCommonValue/E2540_Minimum_Common_Value.java
@@ -8,20 +8,6 @@ integer amongst nums1 and nums2, return -1.
 
 Note that an integer is said to be common to nums1 and nums2 if both arrays 
 have at least one occurrence of that integer.
-
-
-Example 1:
-
-Input: nums1 = [1,2,3], nums2 = [2,4]
-Output: 2
-Explanation: The smallest element common to both arrays is 2, so we return 2.
-
-Example 2:
-
-Input: nums1 = [1,2,3,6], nums2 = [2,3,4,5]
-Output: 2
-Explanation: There are two common elements in the array 2 and 3 out of which 2 
-is the smallest, so 2 is returned.
 */
 
 public class E2540_Minimum_Common_Value {
@@ -36,9 +22,7 @@ public class E2540_Minimum_Common_Value {
 	
 	/*
     solution:
-    1. The code uses two pointers to traverse both sorted arrays (nums1 and nums2).
-	2. It compares elements at the current pointers and moves the pointer with the smaller element.
-	3. When both elements match, the common value is returned. If no common element is found, -1 is returned.
+    Two pointers
     */
 	
 	/*
