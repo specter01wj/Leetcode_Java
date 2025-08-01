bc31d977736cdb1e9d437bd83e1a25ea47681250
Jin Wang <specter01wj@gmail.com>
2025-07-31 23:44:19 -0400

    v1

---
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length/src/com/maximumNumberofVowelsinaSubstringofGivenLength/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.java
@@ -0,0 +1,10 @@
+package com.maximumNumberofVowelsinaSubstringofGivenLength;
+
+public class M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
