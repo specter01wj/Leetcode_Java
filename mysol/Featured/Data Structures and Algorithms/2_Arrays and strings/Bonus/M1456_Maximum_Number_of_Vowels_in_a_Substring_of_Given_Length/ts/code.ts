4e71f2e8961bceb8db0e379ddbe4d4255c7e110f
Jin Wang <specter01wj@gmail.com>
2025-08-01 00:06:07 -0400

    v6

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length/ts/code.ts
@@ -1,21 +1,19 @@
-function minSubArrayLen(target: number, nums: number[]): number {
-    let left = 0, sum = 0, minLen = Infinity;
+function maxVowels(s: string, k: number): number {
+    let count = 0, max = 0;
+    const isVowel = (ch: string): boolean => "aeiou".includes(ch);
 
-    for (let right = 0; right < nums.length; right++) {
-        sum += nums[right];
-
-        while (sum >= target) {
-            minLen = Math.min(minLen, right - left + 1);
-            sum -= nums[left++];
-        }
+    for (let i = 0; i < s.length; i++) {
+        if (isVowel(s[i])) count++;
+        if (i >= k && isVowel(s[i - k])) count--;
+        max = Math.max(max, count);
     }
 
-    return minLen === Infinity ? 0 : minLen;
+    return max;
 };
 
-const input: number[] = [2,3,1,2,4,3];
-const target: number = 7;
-const results = minSubArrayLen(target, input);
+const input: string = "abciiidef";
+const k: number = 7;
+const results = maxVowels(input, k);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
