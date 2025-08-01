f120c2445a9465f17d6a1ea5d87915742bfe7ba4
Jin Wang <specter01wj@gmail.com>
2025-07-31 23:52:03 -0400

    v3

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length/src/com/maximumNumberofVowelsinaSubstringofGivenLength/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.java
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length/src/com/maximumNumberofVowelsinaSubstringofGivenLength/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.java
@@ -18,7 +18,16 @@ public class M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length {
         System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
 	
+	/*
+    solution:
+    Sliding window
+    */
 	
+	/*
+     * @param s: a string
+     * @param k: an integer
+     * @return: the maximum number of vowel letters
+     */
 	public int maxVowels(String s, int k) {
         int max = 0, count = 0;
         for (int i = 0; i < s.length(); i++) {
