22656d634872ecf2b4dcec51813e672bda469159
Jin Wang <specter01wj@gmail.com>
2025-07-31 23:50:56 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length/src/com/maximumNumberofVowelsinaSubstringofGivenLength/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.java
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length/src/com/maximumNumberofVowelsinaSubstringofGivenLength/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.java
@@ -1,10 +1,36 @@
 package com.maximumNumberofVowelsinaSubstringofGivenLength;
+import java.util.*;
+
+/*
+Given a string s and an integer k, return the maximum number of 
+vowel letters in any substring of s with length k.
+
+Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+*/
 
 public class M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length solution = new M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length();
+		String input = "abciiidef";
+		int k = 3;
+		int output = solution.maxVowels(input, k);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	
+	public int maxVowels(String s, int k) {
+        int max = 0, count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (isVowel(s.charAt(i))) count++;
+            if (i >= k && isVowel(s.charAt(i - k))) count--;
+            max = Math.max(max, count);
+        }
+        return max;
+    }
+
+    private boolean isVowel(char c) {
+        return "aeiou".indexOf(c) != -1;
+    }
 
 }
