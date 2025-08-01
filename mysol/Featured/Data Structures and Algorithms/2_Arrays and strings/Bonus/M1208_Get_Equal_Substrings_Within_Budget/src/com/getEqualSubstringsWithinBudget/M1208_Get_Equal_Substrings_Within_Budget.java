8aab2faee8ccedaa2d4ad096ed71f63d39ad85ef
Jin Wang <specter01wj@gmail.com>
2025-08-01 01:41:55 -0400

    v1

---
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/M1208_Get_Equal_Substrings_Within_Budget/src/com/getEqualSubstringsWithinBudget/M1208_Get_Equal_Substrings_Within_Budget.java
@@ -0,0 +1,10 @@
+package com.getEqualSubstringsWithinBudget;
+
+public class M1208_Get_Equal_Substrings_Within_Budget {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
