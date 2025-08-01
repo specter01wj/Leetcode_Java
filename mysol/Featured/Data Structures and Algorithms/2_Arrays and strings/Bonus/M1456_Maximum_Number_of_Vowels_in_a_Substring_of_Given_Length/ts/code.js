dfaedc00245b835631e92f064eae107dee72020e
Jin Wang <specter01wj@gmail.com>
2025-08-01 00:06:50 -0400

    v7

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length/ts/code.js
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/M1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length/ts/code.js
@@ -1,20 +1,18 @@
-function maxVowels(s: string, k: number): number {
+function maxVowels(s, k) {
     let count = 0, max = 0;
-    const isVowel = (ch: string): boolean => "aeiou".includes(ch);
-
+    const isVowel = (ch) => "aeiou".includes(ch);
     for (let i = 0; i < s.length; i++) {
-        if (isVowel(s[i])) count++;
-        if (i >= k && isVowel(s[i - k])) count--;
+        if (isVowel(s[i]))
+            count++;
+        if (i >= k && isVowel(s[i - k]))
+            count--;
         max = Math.max(max, count);
     }
-
     return max;
-};
-
-const input: string = "abciiidef";
-const k: number = 4;
+}
+;
+const input = "abciiidef";
+const k = 4;
 const results = maxVowels(input, k);
-
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
-
