f916b4ef810b79bb84aba49d7fc6250ab40d8e82
Jin Wang <specter01wj@gmail.com>
2025-08-01 01:58:13 -0400

    v8

---
+++ mysol/Category/String/M1208_Get_Equal_Substrings_Within_Budget/ts/code.ts
@@ -0,0 +1,24 @@
+function equalSubstring(s: string, t: string, maxCost: number): number {
+    let left = 0, cost = 0, maxLen = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        cost += Math.abs(s.charCodeAt(right) - t.charCodeAt(right));
+
+        while (cost > maxCost) {
+            cost -= Math.abs(s.charCodeAt(left) - t.charCodeAt(left));
+            left++;
+        }
+
+        maxLen = Math.max(maxLen, right - left + 1);
+    }
+
+    return maxLen;
+};
+
+const s: string = "abcd", t: string = "bcdf";
+const maxCost: number = 3;
+const results = equalSubstring(s, t, maxCost);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input s: ' + JSON.stringify(s, null, 2) + '; Input t: ' + JSON.stringify(t, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
