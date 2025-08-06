7aba95a2b3b7a89ef879ad1fa35033238316ce7d
Jin Wang <specter01wj@gmail.com>
2025-08-06 00:01:30 -0400

    v8

---
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/E1732_Find_the_Highest_Altitude/ts/code.js
@@ -0,0 +1,14 @@
+function largestAltitude(gain) {
+    let maxAltitude = 0;
+    let current = 0;
+    for (let g of gain) {
+        current += g;
+        maxAltitude = Math.max(maxAltitude, current);
+    }
+    return maxAltitude;
+}
+;
+const input = [-5, 1, 5, 0, -7];
+const results = largestAltitude(input);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
