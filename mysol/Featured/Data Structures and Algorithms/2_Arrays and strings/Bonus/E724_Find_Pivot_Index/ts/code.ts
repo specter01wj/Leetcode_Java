e69124c246ff3fe6fac77dfe7c63a4825e12f329
Jin Wang <specter01wj@gmail.com>
2025-08-06 00:47:56 -0400

    v6

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/E724_Find_Pivot_Index/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/E724_Find_Pivot_Index/ts/code.ts
@@ -1,17 +1,19 @@
-function largestAltitude(gain: number[]): number {
-    let maxAltitude = 0;
-    let current = 0;
+function pivotIndex(nums: number[]): number {
+    const total = nums.reduce((a, b) => a + b, 0);
+    let leftSum = 0;
 
-    for (let g of gain) {
-        current += g;
-        maxAltitude = Math.max(maxAltitude, current);
+    for (let i = 0; i < nums.length; i++) {
+        if (leftSum === total - leftSum - nums[i]) {
+            return i;
+        }
+        leftSum += nums[i];
     }
 
-    return maxAltitude;
+    return -1;
 };
 
-const input: number[] = [-5,1,5,0,-7];
-const results = largestAltitude(input);
+const input: number[] = [1,7,3,6,5,6];
+const results = pivotIndex(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
