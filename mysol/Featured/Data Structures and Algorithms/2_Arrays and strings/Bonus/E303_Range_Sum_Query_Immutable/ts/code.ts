2896d2a0b3cef0cd7bc26bfb68da091b7930d59d
Jin Wang <specter01wj@gmail.com>
2025-08-11 23:57:24 -0400

    v3

---
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/E303_Range_Sum_Query_Immutable/ts/code.ts
@@ -0,0 +1,20 @@
+function pivotIndex(nums: number[]): number {
+    const total = nums.reduce((a, b) => a + b, 0);
+    let leftSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (leftSum === total - leftSum - nums[i]) {
+            return i;
+        }
+        leftSum += nums[i];
+    }
+
+    return -1;
+};
+
+const input: number[] = [1,7,3,6,5,6];
+const results = pivotIndex(input);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
