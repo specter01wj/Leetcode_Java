0ceb2199b8a3686113678ffc67406c23ebc5d907
Jin Wang <specter01wj@gmail.com>
2025-08-12 00:07:50 -0400

    v6

--- mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/E303_Range_Sum_Query_Immutable/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/2_Arrays and strings/Bonus/E303_Range_Sum_Query_Immutable/ts/code.ts
@@ -1,19 +1,21 @@
-function pivotIndex(nums: number[]): number {
-    const total = nums.reduce((a, b) => a + b, 0);
-    let leftSum = 0;
+class NumArray {
+    private pre: number[];
 
-    for (let i = 0; i < nums.length; i++) {
-        if (leftSum === total - leftSum - nums[i]) {
-            return i;
+    constructor(nums: number[]) {
+        this.pre = new Array(nums.length + 1).fill(0);
+        for (let i = 0; i < nums.length; i++) {
+            this.pre[i + 1] = this.pre[i] + nums[i];
         }
-        leftSum += nums[i];
     }
 
-    return -1;
-};
+    sumRange(left: number, right: number): number {
+        return this.pre[right + 1] - this.pre[left];
+    }
+}
 
-const input: number[] = [1,7,3,6,5,6];
-const results = pivotIndex(input);
+const input: number[] = [-2, 0, 3, -5, 2, -1];
+const numArray = new NumArray(input);
+const results = numArray.sumRange(0, 2);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
