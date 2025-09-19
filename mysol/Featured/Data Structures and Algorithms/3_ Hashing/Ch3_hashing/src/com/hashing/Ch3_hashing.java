7f2bb6324ec895db12650654c5de70d69962c548
Jin Wang <specter01wj@gmail.com>
2025-09-19 03:18:14 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_hashing/src/com/hashing/Ch3_hashing.java
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_hashing/src/com/hashing/Ch3_hashing.java
@@ -1,10 +1,55 @@
 package com.hashing;
+import java.util.*;
 
 public class Ch3_hashing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Ch3_hashing solution = new Ch3_hashing();
+		solution.interfaceGuide();
+	}
+	
+	public void interfaceGuide() {
+		// Declaration: Java supports multiple implementations, but we will using
+		// the Map interface with the HashMap implementation. Specify the data type
+		// of the keys and values
+//		Map<Integer, Integer> hashMap = new HashMap<>();
 
+		// If you want to initialize it with some key value pairs, use the following syntax:
+		Map<Integer, Integer> hashMap = new HashMap<>() {{
+		    put(1, 2);
+		    put(5, 3);
+		    put(7, 2);
+		}};
+
+		// Checking if a key exists: use .containsKey()
+		hashMap.containsKey(1); // true
+		hashMap.containsKey(9); // false
+
+		// Accessing a value given a key: use .get()
+		hashMap.get(5); // 3
+
+		// Adding or updating a key: use .put()
+		// If the key already exists, the value will be updated
+		hashMap.put(5, 6);
+
+		// If the key doesn't exist yet, the key value pair will be inserted
+		hashMap.put(9, 15);
+
+		// Deleting a key: use .remove()
+		hashMap.remove(9);
+
+		// Get size
+		hashMap.size(); // 3
+
+		// Iterate over keys: use .keySet()
+		for (int key: hashMap.keySet()) {
+		    System.out.println(key);
+		}
+
+		// Iterate over values: use .values()
+		for (int val: hashMap.values()) {
+		    System.out.println(val);
+		}
 	}
 
 }
