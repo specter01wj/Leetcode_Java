54fea995afa5674d955d4f4fb99793e45926bdc4
Jin Wang <specter01wj@gmail.com>
2025-09-21 22:52:19 -0400

    v1

---
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E2351_First_Letter_to_Appear_Twice/src/com/firstLettertoAppearTwice/E2351_First_Letter_to_Appear_Twice.java
@@ -0,0 +1,10 @@
+package com.firstLettertoAppearTwice;
+
+public class E2351_First_Letter_to_Appear_Twice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
