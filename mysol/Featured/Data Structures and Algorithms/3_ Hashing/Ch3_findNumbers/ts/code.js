f3ecbbca56a0d1ec8d653a25d10fc18789633b82
Jin Wang <specter01wj@gmail.com>
2025-09-22 01:22:58 -0400

    v10

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_findNumbers/ts/code.js
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_findNumbers/ts/code.js
@@ -1,19 +1,14 @@
-function findNumbers(nums: number[]): number[] {
-    const numSet = new Set<number>(nums);
-    const result: number[] = [];
-
+function findNumbers(nums) {
+    const numSet = new Set(nums);
+    const result = [];
     for (const num of numSet) {
         if (!numSet.has(num - 1) && !numSet.has(num + 1)) {
             result.push(num);
         }
     }
-
     return result;
 }
-
-const input: number[] = [1, 2, 4, 5, 7, 7, 9];
+const input = [1, 2, 4, 5, 7, 7, 9];
 const results = findNumbers(input);
-
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
-
