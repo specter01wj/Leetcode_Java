3e16e7ac92fc469f34404adfcdc197e173c08380
Jin Wang <specter01wj@gmail.com>
2025-09-22 14:01:27 -0400

    v6

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1832_Check_if_the_Sentence_Is_Pangram/ts/code.js
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1832_Check_if_the_Sentence_Is_Pangram/ts/code.js
@@ -1,16 +1,11 @@
-function checkIfPangram(sentence: string): boolean {
-    const seen: Set<string> = new Set();
-
+function checkIfPangram(sentence) {
+    const seen = new Set();
     for (const char of sentence) {
         seen.add(char);
     }
-
     return seen.size === 26;
 }
-
-const input: string = "thequickbrownfoxjumpsoverthelazydog";
+const input = "thequickbrownfoxjumpsoverthelazydog";
 const results = checkIfPangram(input);
-
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
-
