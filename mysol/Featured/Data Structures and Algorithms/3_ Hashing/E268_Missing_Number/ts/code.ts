1593a9c5cbccd42d14f9326cf391bb15ccbbbd09
Jin Wang <specter01wj@gmail.com>
2025-09-22 23:36:34 -0400

    v5

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/E268_Missing_Number/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E268_Missing_Number/ts/code.ts
@@ -1,15 +1,18 @@
-function checkIfPangram(sentence: string): boolean {
-    const seen: Set<string> = new Set();
+function missingNumber(nums: number[]): number {
+    const numSet = new Set<number>(nums);
+    const n = nums.length;
 
-    for (const char of sentence) {
-        seen.add(char);
+    for (let i = 0; i <= n; i++) {
+        if (!numSet.has(i)) {
+            return i;
+        }
     }
 
-    return seen.size === 26;
-}
+    return -1;
+};
 
-const input: string = "thequickbrownfoxjumpsoverthelazydog";
-const results = checkIfPangram(input);
+const input: number[] = [9,6,4,2,3,5,7,0,1];
+const results = missingNumber(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
