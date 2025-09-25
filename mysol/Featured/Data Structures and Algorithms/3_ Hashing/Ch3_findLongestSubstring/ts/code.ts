56b05f5e02df47d09708c7b980386cbcf77afbb9
Jin Wang <specter01wj@gmail.com>
2025-09-25 01:38:36 -0400

    v5

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_findLongestSubstring/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_findLongestSubstring/ts/code.ts
@@ -1,18 +1,32 @@
-function findNumbers(nums: number[]): number[] {
-    const numSet = new Set<number>(nums);
-    const result: number[] = [];
+function findLongestSubstring(s: string, k: number): number {
+    if (k === 0 || s.length === 0) return 0;
 
-    for (const num of numSet) {
-        if (!numSet.has(num - 1) && !numSet.has(num + 1)) {
-            result.push(num);
+    const map: Map<string, number> = new Map();
+    let left = 0;
+    let maxLen = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        const char = s[right];
+        map.set(char, (map.get(char) || 0) + 1);
+
+        while (map.size > k) {
+            const leftChar = s[left];
+            map.set(leftChar, map.get(leftChar)! - 1);
+            if (map.get(leftChar)! === 0) {
+                map.delete(leftChar);
+            }
+            left++;
         }
+
+        maxLen = Math.max(maxLen, right - left + 1);
     }
 
-    return result;
+    return maxLen;
 }
 
-const input: number[] = [1, 2, 4, 5, 7, 7, 9];
-const results = findNumbers(input);
+const input: string = "eceba";
+const k: number = 2;
+const results = findLongestSubstring(input, k);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
