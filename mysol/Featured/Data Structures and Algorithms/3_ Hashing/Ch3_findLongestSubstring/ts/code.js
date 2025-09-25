36c4f696d8ceb349c5d4e3162c174dc91dcc1178
Jin Wang <specter01wj@gmail.com>
2025-09-25 01:39:12 -0400

    v6

---
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_findLongestSubstring/ts/code.js
@@ -0,0 +1,26 @@
+function findLongestSubstring(s, k) {
+    if (k === 0 || s.length === 0)
+        return 0;
+    const map = new Map();
+    let left = 0;
+    let maxLen = 0;
+    for (let right = 0; right < s.length; right++) {
+        const char = s[right];
+        map.set(char, (map.get(char) || 0) + 1);
+        while (map.size > k) {
+            const leftChar = s[left];
+            map.set(leftChar, map.get(leftChar) - 1);
+            if (map.get(leftChar) === 0) {
+                map.delete(leftChar);
+            }
+            left++;
+        }
+        maxLen = Math.max(maxLen, right - left + 1);
+    }
+    return maxLen;
+}
+const input = "eceba";
+const k = 2;
+const results = findLongestSubstring(input, k);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
