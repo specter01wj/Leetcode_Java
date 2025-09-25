45a1b54739b48f91983b2e20c2ef7addee9159d7
Jin Wang <specter01wj@gmail.com>
2025-09-24 23:48:31 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1426_Counting_Elements/src/com/countingElements/E1426_Counting_Elements.java
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1426_Counting_Elements/src/com/countingElements/E1426_Counting_Elements.java
@@ -1,10 +1,36 @@
 package com.countingElements;
+import java.util.*;
+
+/*
+Given an integer array arr, count how many elements x there are, 
+such that x + 1 is also in arr. If there are duplicates in arr, 
+count them separately.
+*/
 
 public class E1426_Counting_Elements {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] input = {1,2,3};
+		int output = countElements(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	
+	public int countElements(int[] arr) {
+		Set<Integer> set = new HashSet<>();
+		
+		for (int num : arr) {
+			set.add(num);
+		}
+		
+		int count = 0;
+		for (int num : arr) {
+			if (set.contains(num + 1)) {
+				count++;
+			}
+		}
+		
+		return count;
 	}
 
 }
