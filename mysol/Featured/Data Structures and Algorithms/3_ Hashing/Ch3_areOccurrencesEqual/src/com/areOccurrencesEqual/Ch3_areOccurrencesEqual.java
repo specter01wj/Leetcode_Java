7bfe2a8ebd39f8e58bdb04f458e9e57db6ab9bcb
Jin Wang <specter01wj@gmail.com>
2025-09-29 20:43:47 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_areOccurrencesEqual/src/com/areOccurrencesEqual/Ch3_areOccurrencesEqual.java
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_areOccurrencesEqual/src/com/areOccurrencesEqual/Ch3_areOccurrencesEqual.java
@@ -1,10 +1,33 @@
 package com.areOccurrencesEqual;
+import java.util.*;
+
+/*
+Given a string s, return true if s is a good string, or false otherwise.
+
+A string s is good if all the characters that appear in s have the same 
+number of occurrences (i.e., the same frequency).
+*/
 
 public class Ch3_areOccurrencesEqual {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Ch3_areOccurrencesEqual solution = new Ch3_areOccurrencesEqual();
+		String input = "abacbc";
+		boolean output = solution.areOccurrencesEqual(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	
+	public boolean areOccurrencesEqual(String s) {
+        Map<Character, Integer> freqMap = new HashMap<>();
+
+        // Count frequencies
+        for (char c : s.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+
+        Set<Integer> frequencies = new HashSet<>(freqMap.values());
+        return frequencies.size() == 1;
+    }
 
 }
