e3af60adc7b9d145e458eeb9d19c9b7edca98f3a
Jin Wang <specter01wj@gmail.com>
2025-09-29 20:48:19 -0400

    v4

---
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_areOccurrencesEqual/ts/code.ts
@@ -0,0 +1,27 @@
+function intersection(nums: number[][]): number[] {
+    const countMap: Map<number, number> = new Map();
+    const totalArrays = nums.length;
+
+    for (const arr of nums) {
+        const unique = new Set<number>(arr);
+        for (const num of unique) {
+            countMap.set(num, (countMap.get(num) || 0) + 1);
+        }
+    }
+
+    const result: number[] = [];
+    for (const [num, count] of countMap) {
+        if (count === totalArrays) {
+            result.push(num);
+        }
+    }
+
+    return result.sort((a, b) => a - b);
+};
+
+const input: number[][] = [[3,1,2,4,5],[1,2,3,4],[3,4,5,6]];
+const results = intersection(input);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
