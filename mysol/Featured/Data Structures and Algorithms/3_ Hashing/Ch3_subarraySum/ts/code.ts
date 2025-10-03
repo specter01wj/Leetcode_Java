4a4880b1862bc9218a517b85d6bdc612c85cc875
Jin Wang <specter01wj@gmail.com>
2025-10-03 01:49:06 -0400

    v6

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_subarraySum/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_subarraySum/ts/code.ts
@@ -1,16 +1,26 @@
-function areOccurrencesEqual(s: string): boolean {
-    const counts: Map<string, number> = new Map();
+function subarraySum(nums: number[], k: number): number {
+    const prefixSumCount: Map<number, number> = new Map();
+    prefixSumCount.set(0, 1);
 
-    for (const char of s) {
-        counts.set(char, (counts.get(char) || 0) + 1);
+    let prefixSum = 0;
+    let count = 0;
+
+    for (const num of nums) {
+        prefixSum += num;
+
+        if (prefixSumCount.has(prefixSum - k)) {
+            count += prefixSumCount.get(prefixSum - k)!;
+        }
+
+        prefixSumCount.set(prefixSum, (prefixSumCount.get(prefixSum) || 0) + 1);
     }
 
-    const frequencies: Set<number> = new Set(counts.values());
-    return frequencies.size === 1;
+    return count;
 };
 
-const input: string = "abacbc";
-const results = areOccurrencesEqual(input);
+const input: number[] = [1,2,3];
+const k: number = 3;
+const results = subarraySum(input, k);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
