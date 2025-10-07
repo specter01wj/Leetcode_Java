dd706dd44577afb89b998659ce4bfdd9683300a3
Jin Wang <specter01wj@gmail.com>
2025-10-07 00:07:03 -0400

    v12

---
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1133_Largest_Unique_Number/ts/code.ts
@@ -0,0 +1,34 @@
+function findWinners(matches: number[][]): number[][] {
+    const lossCount: Map<number, number> = new Map();
+    const allPlayers: Set<number> = new Set();
+
+    for (const [winner, loser] of matches) {
+        lossCount.set(loser, (lossCount.get(loser) || 0) + 1);
+        allPlayers.add(winner);
+        allPlayers.add(loser);
+    }
+
+    const noLoss: number[] = [];
+    const oneLoss: number[] = [];
+
+    for (const player of allPlayers) {
+        const losses = lossCount.get(player) || 0;
+        if (losses === 0) {
+            noLoss.push(player);
+        } else if (losses === 1) {
+            oneLoss.push(player);
+        }
+    }
+
+    noLoss.sort((a, b) => a - b);
+    oneLoss.sort((a, b) => a - b);
+
+    return [noLoss, oneLoss];
+};
+
+const input: nubmer[][] = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]];
+const results = findWinners(input);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
