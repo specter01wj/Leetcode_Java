916631efcf1a994d6bceb629a770b3e77a30f2f2
Jin Wang <specter01wj@gmail.com>
2025-10-06 23:59:40 -0400

    v11

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1133_Largest_Unique_Number/src/com/largestUniqueNumber/E1133_Largest_Unique_Number.java
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1133_Largest_Unique_Number/src/com/largestUniqueNumber/E1133_Largest_Unique_Number.java
@@ -1,10 +1,38 @@
 package com.largestUniqueNumber;
+import java.util.*;
+
+/*
+Given an integer array nums, return the largest integer that only occurs once. 
+If no integer occurs once, return -1.
+*/
 
 public class E1133_Largest_Unique_Number {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E1133_Largest_Unique_Number solution = new E1133_Largest_Unique_Number();
+		int[] input = {5,7,3,9,4,9,8,3,1};
+		int output = solution.largestUniqueNumber(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public int largestUniqueNumber(int[] nums) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        // Step 1: Count frequency of each number
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Step 2: Find the largest number with frequency == 1
+        int maxUnique = -1;
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                maxUnique = Math.max(maxUnique, entry.getKey());
+            }
+        }
+
+        return maxUnique;
+    }
 
 }
