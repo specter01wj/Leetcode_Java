b25077573b1aa09fe2996ae5a0c1cf9cfa2d94db
Jin Wang <specter01wj@gmail.com>
2025-10-09 00:28:30 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/M525_Contiguous_Array/src/com/contiguousArray/M525_Contiguous_Array.java
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/M525_Contiguous_Array/src/com/contiguousArray/M525_Contiguous_Array.java
@@ -1,10 +1,40 @@
 package com.contiguousArray;
+import java.util.*;
+
+/*
+Given a binary array nums, return the maximum length of a contiguous 
+subarray with an equal number of 0 and 1.
+*/
 
 public class M525_Contiguous_Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		M525_Contiguous_Array solution = new M525_Contiguous_Array();
+		int[] input = {0,1,1,1,1,1,0,0,0};
+		int output = solution.findMaxLength(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> sumToIndex = new HashMap<>();
+        sumToIndex.put(0, -1); // base case
+
+        int sum = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            // Treat 0 as -1, 1 as +1
+            sum += (nums[i] == 0) ? -1 : 1;
+
+            if (sumToIndex.containsKey(sum)) {
+                maxLen = Math.max(maxLen, i - sumToIndex.get(sum));
+            } else {
+                sumToIndex.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
 
 }
