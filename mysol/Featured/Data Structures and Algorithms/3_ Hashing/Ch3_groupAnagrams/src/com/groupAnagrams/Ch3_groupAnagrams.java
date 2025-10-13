ee801c286473b555116ebbbeeb0ef99e8b64a6dc
Jin Wang <specter01wj@gmail.com>
2025-10-13 11:54:06 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_groupAnagrams/src/com/groupAnagrams/Ch3_groupAnagrams.java
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_groupAnagrams/src/com/groupAnagrams/Ch3_groupAnagrams.java
@@ -1,10 +1,33 @@
 package com.groupAnagrams;
+import java.util.*;
+
+/*
+Given an array of strings strs, group the anagrams together. 
+You can return the answer in any order.
+*/
 
 public class Ch3_groupAnagrams {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Ch3_groupAnagrams solution = new Ch3_groupAnagrams();
+		String[] input = {"eat","tea","tan","ate","nat","bat"};
+		List<List<String>> output = solution.groupAnagrams(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagramGroups = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars); // Sorting is the key to identifying anagram groups
+            String key = new String(chars);
+
+            anagramGroups.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(anagramGroups.values());
+    }
 
 }
