d1c872023c221eb49b8eae3609fa7b7cfe2393c8
Jin Wang <specter01wj@gmail.com>
2025-10-13 12:16:03 -0400

    v6

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_groupAnagrams/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_groupAnagrams/ts/code.ts
@@ -1,32 +1,19 @@
-function findLongestSubstring(s: string, k: number): number {
-    if (k === 0 || s.length === 0) return 0;
+function groupAnagrams(strs: string[]): string[][] {
+    const map: Map<string, string[]> = new Map();
 
-    const map: Map<string, number> = new Map();
-    let left = 0;
-    let maxLen = 0;
-
-    for (let right = 0; right < s.length; right++) {
-        const char = s[right];
-        map.set(char, (map.get(char) || 0) + 1);
-
-        while (map.size > k) {
-            const leftChar = s[left];
-            map.set(leftChar, map.get(leftChar)! - 1);
-            if (map.get(leftChar)! === 0) {
-                map.delete(leftChar);
-            }
-            left++;
+    for (const str of strs) {
+        const key = str.split('').sort().join('');
+        if (!map.has(key)) {
+            map.set(key, []);
         }
-
-        maxLen = Math.max(maxLen, right - left + 1);
+        map.get(key)!.push(str);
     }
 
-    return maxLen;
-}
+    return Array.from(map.values());
+};
 
-const input: string = "eceba";
-const k: number = 2;
-const results = findLongestSubstring(input, k);
+const input: string[] = ["eat","tea","tan","ate","nat","bat"];
+const results = groupAnagrams(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
