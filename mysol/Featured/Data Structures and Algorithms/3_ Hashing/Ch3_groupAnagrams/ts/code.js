26bc7f5c2a5e94fdadacf93a3307fd38aefbefa1
Jin Wang <specter01wj@gmail.com>
2025-10-13 12:16:55 -0400

    v7

---
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_groupAnagrams/ts/code.js
@@ -0,0 +1,16 @@
+function groupAnagrams(strs) {
+    const map = new Map();
+    for (const str of strs) {
+        const key = str.split('').sort().join('');
+        if (!map.has(key)) {
+            map.set(key, []);
+        }
+        map.get(key).push(str);
+    }
+    return Array.from(map.values());
+}
+;
+const input = ["eat", "tea", "tan", "ate", "nat", "bat"];
+const results = groupAnagrams(input);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
