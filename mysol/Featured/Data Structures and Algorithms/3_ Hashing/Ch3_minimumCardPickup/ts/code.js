92d1afc76b6ceaaabb79dda5001d38cb1e412211
Jin Wang <specter01wj@gmail.com>
2025-10-13 22:21:29 -0400

    v6

---
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_minimumCardPickup/ts/code.js
@@ -0,0 +1,17 @@
+function minimumCardPickup(cards) {
+    const lastSeen = new Map();
+    let minLength = Infinity;
+    for (let i = 0; i < cards.length; i++) {
+        const card = cards[i];
+        if (lastSeen.has(card)) {
+            minLength = Math.min(minLength, i - lastSeen.get(card) + 1);
+        }
+        lastSeen.set(card, i);
+    }
+    return minLength === Infinity ? -1 : minLength;
+}
+;
+const input = [3, 4, 2, 3, 4, 7];
+const results = minimumCardPickup(input);
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
