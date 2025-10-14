d3add2205b1c5fc1d179e4648aae6c9e08427fc2
Jin Wang <specter01wj@gmail.com>
2025-10-13 22:15:01 -0400

    v3

---
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_minimumCardPickup/ts/code.ts
@@ -0,0 +1,20 @@
+function groupAnagrams(strs: string[]): string[][] {
+    const map: Map<string, string[]> = new Map();
+
+    for (const str of strs) {
+        const key = str.split('').sort().join('');
+        if (!map.has(key)) {
+            map.set(key, []);
+        }
+        map.get(key)!.push(str);
+    }
+
+    return Array.from(map.values());
+};
+
+const input: string[] = ["eat","tea","tan","ate","nat","bat"];
+const results = groupAnagrams(input);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
