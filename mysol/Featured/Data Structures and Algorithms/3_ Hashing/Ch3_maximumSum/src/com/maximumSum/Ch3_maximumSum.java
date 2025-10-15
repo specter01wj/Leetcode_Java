002f45671796095ee5faa8af1752742297806717
Jin Wang <specter01wj@gmail.com>
2025-10-15 11:34:10 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_maximumSum/src/com/maximumSum/Ch3_maximumSum.java
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_maximumSum/src/com/maximumSum/Ch3_maximumSum.java
@@ -1,10 +1,51 @@
 package com.maximumSum;
+import java.util.*;
+
+/*
+You are given a 0-indexed array nums consisting of positive integers. 
+You can choose two indices i and j, such that i != j, and the sum of 
+digits of the number nums[i] is equal to that of nums[j].
+
+Return the maximum value of nums[i] + nums[j] that you can obtain over 
+all possible indices i and j that satisfy the conditions. 
+If no such pair of indices exists, return -1.
+*/
 
 public class Ch3_maximumSum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Ch3_maximumSum solution = new Ch3_maximumSum();
+		int[] input = {18,43,36,13,7};
+		int output = solution.maximumSum(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
 	}
+	
+	
+	public int maximumSum(int[] nums) {
+        Map<Integer, Integer> digitSumToMax = new HashMap<>();
+        int maxSum = -1;
+
+        for (int num : nums) {
+            int digitSum = getDigitSum(num);
+            if (digitSumToMax.containsKey(digitSum)) {
+                int prevMax = digitSumToMax.get(digitSum);
+                maxSum = Math.max(maxSum, prevMax + num);
+                digitSumToMax.put(digitSum, Math.max(prevMax, num));
+            } else {
+                digitSumToMax.put(digitSum, num);
+            }
+        }
+
+        return maxSum;
+    }
+
+    private int getDigitSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
 
 }
