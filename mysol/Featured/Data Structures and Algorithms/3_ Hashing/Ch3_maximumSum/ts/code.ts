331676a580fff39ec0c21d7285b9f05ece5b8083
Jin Wang <specter01wj@gmail.com>
2025-10-15 11:46:33 -0400

    v5

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_maximumSum/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_maximumSum/ts/code.ts
@@ -1,20 +1,32 @@
-function minimumCardPickup(cards: number[]): number {
-    const lastSeen = new Map<number, number>();
-    let minLength = Infinity;
+function maximumSum(nums: number[]): number {
+    const digitSumToMax = new Map<number, number>();
+    let maxSum = -1;
 
-    for (let i = 0; i < cards.length; i++) {
-        const card = cards[i];
-        if (lastSeen.has(card)) {
-            minLength = Math.min(minLength, i - lastSeen.get(card)! + 1);
+    for (const num of nums) {
+        const digitSum = getDigitSum(num);
+        if (digitSumToMax.has(digitSum)) {
+            const prevMax = digitSumToMax.get(digitSum)!;
+            maxSum = Math.max(maxSum, prevMax + num);
+            digitSumToMax.set(digitSum, Math.max(prevMax, num));
+        } else {
+            digitSumToMax.set(digitSum, num);
         }
-        lastSeen.set(card, i);
     }
 
-    return minLength === Infinity ? -1 : minLength;
+    return maxSum;
 };
 
-const input: number[] = [3,4,2,3,4,7];
-const results = minimumCardPickup(input);
+const getDigitSum = (num: number): number => {
+    let sum = 0;
+    while (num > 0) {
+        sum += num % 10;
+        num = Math.floor(num / 10);
+    }
+    return sum;
+};
+
+const input: number[] = [18,43,36,13,7];
+const results = maximumSum(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
