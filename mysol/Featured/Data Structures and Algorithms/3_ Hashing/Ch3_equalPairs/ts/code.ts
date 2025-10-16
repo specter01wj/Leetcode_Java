9fb0dc69fbdb60a22956086a8be98b39cfb139c3
Jin Wang <specter01wj@gmail.com>
2025-10-16 00:20:57 -0400

    v3

---
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/Ch3_equalPairs/ts/code.ts
@@ -0,0 +1,33 @@
+function maximumSum(nums: number[]): number {
+    const digitSumToMax = new Map<number, number>();
+    let maxSum = -1;
+
+    for (const num of nums) {
+        const digitSum = getDigitSum(num);
+        if (digitSumToMax.has(digitSum)) {
+            const prevMax = digitSumToMax.get(digitSum)!;
+            maxSum = Math.max(maxSum, prevMax + num);
+            digitSumToMax.set(digitSum, Math.max(prevMax, num));
+        } else {
+            digitSumToMax.set(digitSum, num);
+        }
+    }
+
+    return maxSum;
+};
+
+const getDigitSum = (num: number): number => {
+    let sum = 0;
+    while (num > 0) {
+        sum += num % 10;
+        num = Math.floor(num / 10);
+    }
+    return sum;
+};
+
+const input: number[] = [18,43,36,13,7];
+const results = maximumSum(input);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
