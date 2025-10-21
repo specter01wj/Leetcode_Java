ea2ca5e54f5b276271b54c8789bbe65a32c709d0
Jin Wang <specter01wj@gmail.com>
2025-10-20 22:32:53 -0400

    v1

---
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E383_Ransom_Note/src/com/ransomNote/E383_Ransom_Note.java
@@ -0,0 +1,37 @@
+package com.ransomNote;
+import java.util.*;
+
+/*
+Given two strings ransomNote and magazine, return true if ransomNote 
+can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+*/
+
+public class E383_Ransom_Note {
+
+	public static void main(String[] args) {
+		int[] input = {3,0,1,1,9,7};
+		int output = countGoodTriplets(input, 7, 2, 3);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	
+	public boolean canConstruct(String ransomNote, String magazine) {
+		Map<Character, Integer> freq = new HashMap<>();
+		
+		for (char c : magazine.toCharArray()) {
+			freq.put(c, freq.getOrDefault(c, 0) + 1);
+		}
+		
+		for (char c : ransomNote.toCharArray()) {
+			if (!freq.containsKey(c) || freq.get(c) == 0) {
+				return false;
+			}
+			freq.put(c, freq.get(c) - 1);
+		}
+		
+		return true;
+	}
+
+}
