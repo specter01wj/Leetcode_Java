2b75b7b4e2f8d83fd25c077fac7c5c073ca3c6db
Jin Wang <specter01wj@gmail.com>
2025-10-20 23:30:19 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/E771_Jewels_and_Stones/src/com/jewelsandStones/E771_Jewels_and_Stones.java
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E771_Jewels_and_Stones/src/com/jewelsandStones/E771_Jewels_and_Stones.java
@@ -1,10 +1,40 @@
 package com.jewelsandStones;
+import java.util.*;
+
+/*
+You're given strings jewels representing the types of stones 
+that are jewels, and stones representing the stones you have. 
+Each character in stones is a type of stone you have. 
+You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+*/
 
 public class E771_Jewels_and_Stones {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E771_Jewels_and_Stones solution = new E771_Jewels_and_Stones();
+		String input = "aA", stones = "aAAbbbb";
+		int output = solution.numJewelsInStones(input, stones);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
+	}
+	
+	
+	public int numJewelsInStones(String jewels, String stones) {
+		Set<Character> jewelSet = new HashSet<>();
+		
+		for (char c : jewels.toCharArray()) {
+			jewelSet.add(c);
+		}
+		
+		int count = 0;
+		for (char c : stones.toCharArray()) {
+			if (jewelSet.contains(c)) {
+				count++;
+			}
+		}
+		
+		return count;
 	}
 
 }
