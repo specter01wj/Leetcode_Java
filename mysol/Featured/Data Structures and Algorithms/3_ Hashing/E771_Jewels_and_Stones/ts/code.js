6537c41368df48131239d8d338f8a77bb64b39ae
Jin Wang <specter01wj@gmail.com>
2025-10-20 23:45:06 -0400

    v7

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/E771_Jewels_and_Stones/ts/code.js
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E771_Jewels_and_Stones/ts/code.js
@@ -1,20 +1,16 @@
-function numJewelsInStones(jewels: string, stones: string): number {
-    const jewelSet: Set<string> = new Set(jewels);
-    let count: number = 0;
-
+function numJewelsInStones(jewels, stones) {
+    const jewelSet = new Set(jewels);
+    let count = 0;
     for (const stone of stones) {
         if (jewelSet.has(stone)) {
             count++;
         }
     }
-
     return count;
-};
-
-const input: string = "aA";
-const stones: string = "aAAbbbb";
+}
+;
+const input = "aA";
+const stones = "aAAbbbb";
 const results = numJewelsInStones(input, stones);
-
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
-
