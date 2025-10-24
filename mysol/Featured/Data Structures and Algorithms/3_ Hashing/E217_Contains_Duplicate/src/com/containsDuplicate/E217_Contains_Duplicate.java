8fcf2f6f245e4fc78b3422008450891b73400096
Jin Wang <specter01wj@gmail.com>
2025-10-23 23:35:43 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/E217_Contains_Duplicate/src/com/containsDuplicate/E217_Contains_Duplicate.java
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E217_Contains_Duplicate/src/com/containsDuplicate/E217_Contains_Duplicate.java
@@ -1,10 +1,32 @@
 package com.containsDuplicate;
+import java.util.*;
+
+/*
+Given an integer array nums, return true if any value appears at least 
+twice in the array, and return false if every element is distinct.
+*/
 
 public class E217_Contains_Duplicate {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E217_Contains_Duplicate solution = new E217_Contains_Duplicate();
+		int[] input = {1,1,1,3,3,4,3,2,4,2};
+		boolean output = solution.containsDuplicate(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	
+	public boolean containsDuplicate(int[] nums) {
+		Set<Integer> seen = new HashSet<>();
+		
+		for (int num : nums) {
+			if (seen.contains(num)) {
+				return true; // Found a duplicate
+			}
+			seen.add(num);
+		}
+		
+		return false; // All elements are distinct
 	}
 
 }
