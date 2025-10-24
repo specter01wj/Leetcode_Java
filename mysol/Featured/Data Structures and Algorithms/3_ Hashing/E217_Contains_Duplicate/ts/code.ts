17473a6b24bcdf6efe82b13b6499fa0fdcd380f7
Jin Wang <specter01wj@gmail.com>
2025-10-23 23:41:16 -0400

    v6

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/E217_Contains_Duplicate/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E217_Contains_Duplicate/ts/code.ts
@@ -1,23 +1,18 @@
-function canConstruct(ransomNote: string, magazine: string): boolean {
-    const freq: Map<string, number> = new Map();
+function containsDuplicate(nums: number[]): boolean {
+    const seen = new Set<number>();
 
-    for (const char of magazine) {
-        freq.set(char, (freq.get(char) || 0) + 1);
-    }
-
-    for (const char of ransomNote) {
-        if (!freq.has(char) || freq.get(char)! === 0) {
-            return false;
+    for (const num of nums) {
+        if (seen.has(num)) {
+            return true; // Duplicate found
         }
-        freq.set(char, freq.get(char)! - 1);
+        seen.add(num);
     }
 
-    return true;
+    return false; // All elements are unique
 };
 
-const input: string = "aa";
-const magazine: string = "aab";
-const results = canConstruct(input, magazine);
+const input: number[] = [1,1,1,3,3,4,3,2,4,2];
+const results = containsDuplicate(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
