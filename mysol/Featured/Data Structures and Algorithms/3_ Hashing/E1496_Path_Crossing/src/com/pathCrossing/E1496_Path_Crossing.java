071c4e1855682d22845eb908abaa87c930561ed1
Jin Wang <specter01wj@gmail.com>
2025-10-28 23:49:36 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1496_Path_Crossing/src/com/pathCrossing/E1496_Path_Crossing.java
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1496_Path_Crossing/src/com/pathCrossing/E1496_Path_Crossing.java
@@ -1,10 +1,50 @@
 package com.pathCrossing;
+import java.util.*;
+
+/*
+Given a string path, where path[i] = 'N', 'S', 'E' or 'W', 
+each representing moving one unit north, south, east, or west, 
+respectively. You start at the origin (0, 0) on a 2D plane 
+and walk on the path specified by path.
+
+Return true if the path crosses itself at any point, that is, 
+if at any time you are on a location you have previously visited. 
+Return false otherwise.
+*/
 
 public class E1496_Path_Crossing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E1496_Path_Crossing solution = new E1496_Path_Crossing();
+		String input = "NESWW";
+		boolean output = solution.isPathCrossing(input);
+        System.out.println("input: " + (input) + "\noutput: " + (output));
 	}
+	
+	
+	public boolean isPathCrossing(String path) {
+        // Set to store visited coordinates as string "x,y"
+        Set<String> visited = new HashSet<>();
+        int x = 0, y = 0;
+
+        visited.add("0,0"); // starting point
+
+        for (char dir : path.toCharArray()) {
+            switch (dir) {
+                case 'N': y++; break;
+                case 'S': y--; break;
+                case 'E': x++; break;
+                case 'W': x--; break;
+            }
+
+            String pos = x + "," + y;
+            if (visited.contains(pos)) {
+                return true;
+            }
+            visited.add(pos);
+        }
+
+        return false;
+    }
 
 }
