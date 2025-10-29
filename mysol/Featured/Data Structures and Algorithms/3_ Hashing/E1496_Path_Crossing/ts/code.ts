b5e7511d7492d151d39da44351a068f0d5dccfc6
Jin Wang <specter01wj@gmail.com>
2025-10-28 23:56:01 -0400

    v5

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1496_Path_Crossing/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1496_Path_Crossing/ts/code.ts
@@ -1,21 +1,25 @@
-function destCity(paths: string[][]): string {
-    const startingCities = new Set<string>();
+function isPathCrossing(path: string): boolean {
+    const visited = new Set<string>();
+    let x = 0, y = 0;
 
-    for (const [from, _] of paths) {
-        startingCities.add(from);
+    visited.add("0,0");
+
+    for (const dir of path) {
+        if (dir === 'N') y++;
+        else if (dir === 'S') y--;
+        else if (dir === 'E') x++;
+        else if (dir === 'W') x--;
+
+        const key = `${x},${y}`;
+        if (visited.has(key)) return true;
+        visited.add(key);
     }
 
-    for (const [_, to] of paths) {
-        if (!startingCities.has(to)) {
-            return to;
-        }
-    }
-
-    return "";
+    return false;
 };
 
-const input: string[][] = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]];
-const results = destCity(input);
+const input: string = "NESWW";
+const results = isPathCrossing(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
