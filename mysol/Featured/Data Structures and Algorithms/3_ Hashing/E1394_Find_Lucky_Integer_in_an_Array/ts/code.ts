294ca600188e328806fb7af131951baddfcb2d19
Jin Wang <specter01wj@gmail.com>
2025-11-05 01:06:53 -0500

    v4

---
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1394_Find_Lucky_Integer_in_an_Array/ts/code.ts
@@ -0,0 +1,28 @@
+function maxFrequencyElements(nums: number[]): number {
+    const freqMap: Map<number, number> = new Map();
+    let maxFreq = 0;
+    let sum = 0;
+
+    for (const num of nums) {
+        const freq = (freqMap.get(num) || 0) + 1;
+        freqMap.set(num, freq);
+        if (freq > maxFreq) {
+            maxFreq = freq;
+        }
+    }
+
+    for (const freq of freqMap.values()) {
+        if (freq === maxFreq) {
+            sum += freq;
+        }
+    }
+
+    return sum;
+};
+
+const input: number[] = [1,2,2,3,1,4];
+const results = maxFrequencyElements(input);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
