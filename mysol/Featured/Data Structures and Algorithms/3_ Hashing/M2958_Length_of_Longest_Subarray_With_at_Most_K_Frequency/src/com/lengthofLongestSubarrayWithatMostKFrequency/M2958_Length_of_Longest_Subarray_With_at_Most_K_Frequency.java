687b78f8ef4621a4822770c0336b8e75aecb5d20
Jin Wang <specter01wj@gmail.com>
2025-11-06 16:03:42 -0500

    v1

---
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/M2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency/src/com/lengthofLongestSubarrayWithatMostKFrequency/M2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency.java
@@ -0,0 +1,10 @@
+package com.lengthofLongestSubarrayWithatMostKFrequency;
+
+public class M2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
