219d90e972dec3197a89ad60a79980b776c6b584
Jin Wang <specter01wj@gmail.com>
2025-11-06 12:29:58 -0500

    v3

---
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/M451_Sort_Characters_By_Frequency/ts/code.ts
@@ -0,0 +1,25 @@
+function uniqueOccurrences(arr: number[]): boolean {
+    const countMap: Map<number, number> = new Map();
+
+    // Count frequency of each number
+    for (const num of arr) {
+        countMap.set(num, (countMap.get(num) || 0) + 1);
+    }
+
+    const seen: Set<number> = new Set();
+    for (const freq of countMap.values()) {
+        if (seen.has(freq)) {
+            return false;
+        }
+        seen.add(freq);
+    }
+
+    return true;
+};
+
+const input: number[] = [1,2,2,1,1,3];
+const results = uniqueOccurrences(input);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
