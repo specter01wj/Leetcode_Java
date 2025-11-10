b34ecc557c7fc7fc990ff9d7524f08aca95351a9
Jin Wang <specter01wj@gmail.com>
2025-11-10 15:47:07 -0500

    v1

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1512_Number_of_Good_Pairs/src/com/numberofGoodPairs/E1512_Number_of_Good_Pairs.java
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1512_Number_of_Good_Pairs/src/com/numberofGoodPairs/E1512_Number_of_Good_Pairs.java
@@ -1,10 +1,34 @@
 package com.numberofGoodPairs;
+import java.util.*;
+
+/*
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+*/
 
 public class E1512_Number_of_Good_Pairs {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		E1512_Number_of_Good_Pairs solution = new E1512_Number_of_Good_Pairs();
+		int[] input = {1,2,3,1,1,3};
+		int output = solution.numIdenticalPairs(input);
+        System.out.println("input: " + Arrays.toString(input) + "\noutput: " + (output));
+	}
+	
+	
+	public int numIdenticalPairs(int[] nums) {
+		Map<Integer, Integer> freqMap = new HashMap<>();
+		int count = 0;
+		
+		for (int num : nums) {
+			if (freqMap.containsKey(num)) {
+				count += freqMap.get(num); // all previous occurrences can pair with this
+			}
+			freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+		}
+		
+		return count;
 	}
 
 }
