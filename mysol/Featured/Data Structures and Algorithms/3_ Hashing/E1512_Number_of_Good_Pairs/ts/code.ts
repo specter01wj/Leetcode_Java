50d34b3a1b45775ab42f856d71aeca1d56b045d3
Jin Wang <specter01wj@gmail.com>
2025-11-10 15:56:37 -0500

    v7

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1512_Number_of_Good_Pairs/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/E1512_Number_of_Good_Pairs/ts/code.ts
@@ -1,27 +1,21 @@
-function maxFrequencyElements(nums: number[]): number {
-    const freqMap: Map<number, number> = new Map();
-    let maxFreq = 0;
-    let sum = 0;
+function numIdenticalPairs(nums: number[]): number {
+    const freqMap: Record<number, number> = {};
+    let count = 0;
 
     for (const num of nums) {
-        const freq = (freqMap.get(num) || 0) + 1;
-        freqMap.set(num, freq);
-        if (freq > maxFreq) {
-            maxFreq = freq;
+        if (freqMap[num]) {
+        count += freqMap[num]; // count how many good pairs can be formed
+        freqMap[num]++;
+        } else {
+        freqMap[num] = 1;
         }
     }
 
-    for (const freq of freqMap.values()) {
-        if (freq === maxFreq) {
-            sum += freq;
-        }
-    }
-
-    return sum;
+    return count;
 };
 
-const input: number[] = [1,2,2,3,1,4];
-const results = maxFrequencyElements(input);
+const input: number[] = [1,2,3,1,1,3];
+const results = numIdenticalPairs(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
