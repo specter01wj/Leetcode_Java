1058249659233115cf9b297784f61a7c0afd2c67
Jin Wang <specter01wj@gmail.com>
2025-11-11 00:35:22 -0500

    v2

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/M930_Binary_Subarrays_With_Sum/ts/code.js
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/M930_Binary_Subarrays_With_Sum/ts/code.js
@@ -1,25 +1,20 @@
-function numSubarraysWithSum(nums: number[], goal: number): number {
-    const prefixMap: Map<number, number> = new Map();
+function numSubarraysWithSum(nums, goal) {
+    const prefixMap = new Map();
     prefixMap.set(0, 1);
-
     let sum = 0;
     let count = 0;
-
     for (let num of nums) {
         sum += num;
         if (prefixMap.has(sum - goal)) {
-            count += prefixMap.get(sum - goal)!;
+            count += prefixMap.get(sum - goal);
         }
         prefixMap.set(sum, (prefixMap.get(sum) || 0) + 1);
     }
-
     return count;
-};
-
-const input: number[] = [1,0,1,0,1];
-const goal: number = 2;
+}
+;
+const input = [1, 0, 1, 0, 1];
+const goal = 2;
 const results = numSubarraysWithSum(input, goal);
-
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
-
