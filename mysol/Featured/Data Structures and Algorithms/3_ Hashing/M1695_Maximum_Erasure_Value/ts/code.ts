52e5ef4f7bea69fa9392642f17e4d4afb4effc5c
Jin Wang <specter01wj@gmail.com>
2025-11-12 12:24:43 -0500

    v3

---
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/M1695_Maximum_Erasure_Value/ts/code.ts
@@ -0,0 +1,25 @@
+function numSubarraysWithSum(nums: number[], goal: number): number {
+    const prefixMap: Map<number, number> = new Map();
+    prefixMap.set(0, 1);
+
+    let sum = 0;
+    let count = 0;
+
+    for (let num of nums) {
+        sum += num;
+        if (prefixMap.has(sum - goal)) {
+            count += prefixMap.get(sum - goal)!;
+        }
+        prefixMap.set(sum, (prefixMap.get(sum) || 0) + 1);
+    }
+
+    return count;
+};
+
+const input: number[] = [1,0,1,0,1];
+const goal: number = 2;
+const results = numSubarraysWithSum(input, goal);
+
+let webHeading = document.querySelector('#t1');
+webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
+
