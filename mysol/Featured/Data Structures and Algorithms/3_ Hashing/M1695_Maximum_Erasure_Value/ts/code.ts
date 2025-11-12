c121b11f1d8a9481974438f5c317770ebdd60588
Jin Wang <specter01wj@gmail.com>
2025-11-12 12:29:11 -0500

    v4

--- mysol/Featured/Data Structures and Algorithms/3_ Hashing/M1695_Maximum_Erasure_Value/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/3_ Hashing/M1695_Maximum_Erasure_Value/ts/code.ts
@@ -1,24 +1,23 @@
-function numSubarraysWithSum(nums: number[], goal: number): number {
-    const prefixMap: Map<number, number> = new Map();
-    prefixMap.set(0, 1);
+function maximumUniqueSubarray(nums: number[]): number {
+    const seen: Set<number> = new Set();
+    let left = 0, currentSum = 0, maxSum = 0;
 
-    let sum = 0;
-    let count = 0;
-
-    for (let num of nums) {
-        sum += num;
-        if (prefixMap.has(sum - goal)) {
-            count += prefixMap.get(sum - goal)!;
+    for (let right = 0; right < nums.length; right++) {
+        while (seen.has(nums[right])) {
+            seen.delete(nums[left]);
+            currentSum -= nums[left];
+            left++;
         }
-        prefixMap.set(sum, (prefixMap.get(sum) || 0) + 1);
+        seen.add(nums[right]);
+        currentSum += nums[right];
+        maxSum = Math.max(maxSum, currentSum);
     }
 
-    return count;
+    return maxSum;
 };
 
-const input: number[] = [1,0,1,0,1];
-const goal: number = 2;
-const results = numSubarraysWithSum(input, goal);
+const input: number[] = [4,2,4,5,6];
+const results = maximumUniqueSubarray(input);
 
 let webHeading = document.querySelector('#t1');
 webHeading.innerHTML = 'Input: ' + JSON.stringify(input, null, 2) + '<br>Result = ' + JSON.stringify(results, null, 2);
