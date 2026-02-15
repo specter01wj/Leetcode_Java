97c04d35e02392f70bd27d5c93c69dfb01b0c6ed
Jin Wang <specter01wj@gmail.com>
2026-02-14 22:10:32 -0500

    v14

--- mysol/Featured/Data Structures and Algorithms/4_Linked lists/Linked_lists/Ch4_exp6/src/com/jinwang/Ch4_exp6.java
+++ mysol/Featured/Data Structures and Algorithms/4_Linked lists/Linked_lists/Ch4_exp6/src/com/jinwang/Ch4_exp6.java
@@ -2,9 +2,55 @@ package com.jinwang;
 
 public class Ch4_exp6 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode prev;
 
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+	
+	public static void main(String[] args) {
+		// Create nodes
+        ListNode one = new ListNode(1);
+        ListNode two = new ListNode(2);
+        ListNode three = new ListNode(3);
+
+        // Link forward
+        one.next = two;
+        two.next = three;
+
+        // Link backward
+        two.prev = one;
+        three.prev = two;
+
+        ListNode head = one;
+
+        System.out.println("Created Doubly Linked List:");
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val);
+            if (current.next != null) {
+                System.out.print(" <-> ");
+            }
+            current = current.next;
+        }
+        System.out.println();
+
+        // Traverse using dummy pointer (sum)
+        int ans = 0;
+        ListNode dummy = head;
+        while (dummy != null) {
+            ans += dummy.val;
+            dummy = dummy.next;
+        }
+
+        System.out.println("Sum using dummy pointer: " + ans);
+
+        // Verify head not lost
+        System.out.println("Head still at: " + head.val);
 	}
 
 }
