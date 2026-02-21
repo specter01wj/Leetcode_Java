b96346f3ba5a06b9a139492891468509a9ba6115
Jin Wang <specter01wj@gmail.com>
2026-02-21 01:24:44 -0500

    v15

--- mysol/Featured/Data Structures and Algorithms/4_Linked lists/Reversing_a_linked_list/M24_Swap_Nodes_in_Pairs/js_ts/js/code.js
+++ mysol/Featured/Data Structures and Algorithms/4_Linked lists/Reversing_a_linked_list/M24_Swap_Nodes_in_Pairs/js_ts/js/code.js
@@ -1,38 +1,51 @@
 class ListNode {
-  constructor(val) {
+  constructor(val, next = null) {
     this.val = val;
-    this.next = null;
+    this.next = next;
   }
 }
 
-function middleNode(head) {
-  let slow = head;
-  let fast = head;
+function swapPairs(head) {
 
-  while (fast !== null && fast.next !== null) {
-    slow = slow.next;
-    fast = fast.next.next;
+  if (head === null || head.next === null) {
+    return head;
   }
 
-  return slow;
+  let dummy = head.next;
+  let prev = null;
+
+  while (head !== null && head.next !== null) {
+
+    if (prev !== null) {
+      prev.next = head.next;
+    }
+
+    prev = head;
+
+    const nextNode = head.next.next;
+    head.next.next = head;
+
+    head.next = nextNode;
+    head = nextNode;
+  }
+
+  return dummy;
 }
 
 document.getElementById("title").innerText =
-  "876. Middle of the Linked List (JS)";
+  "24. Swap Nodes in Pairs (JS)";
 
 let output = "";
 
-// Example 1: head = [1,2,3,4,5]
+// Example 1: head = [1,2,3,4]
 const one = new ListNode(1);
 const two = new ListNode(2);
 const three = new ListNode(3);
 const four = new ListNode(4);
-const five = new ListNode(5);
 
 one.next = two;
 two.next = three;
 three.next = four;
-four.next = five;
 
 const head1 = one;
 
@@ -47,10 +60,10 @@ while (current1 !== null) {
 }
 output += "<br><br>";
 
-const mid1 = middleNode(head1);
+const result1 = swapPairs(head1);
 
 output += "<b>Example 1 Output:</b><br>";
-let out1 = mid1;
+let out1 = result1;
 while (out1 !== null) {
   output += out1.val;
   if (out1.next !== null) {
@@ -60,43 +73,77 @@ while (out1 !== null) {
 }
 output += "<br><br>";
 
-// Example 2: head = [1,2,3,4,5,6]
-const a = new ListNode(1);
-const b = new ListNode(2);
-const c = new ListNode(3);
-const d = new ListNode(4);
-const e = new ListNode(5);
-const f = new ListNode(6);
+// Example 2: head = []
+const head2 = null;
 
-a.next = b;
-b.next = c;
-c.next = d;
-d.next = e;
-e.next = f;
+output += "<b>Example 2 Input:</b><br>[]<br><br>";
 
-const head2 = a;
+const result2 = swapPairs(head2);
 
-output += "<b>Example 2 Input:</b><br>";
-let current2 = head2;
-while (current2 !== null) {
-  output += current2.val;
-  if (current2.next !== null) {
-    output += " -> ";
+output += "<b>Example 2 Output:</b><br>";
+if (result2 === null) {
+  output += "[]";
+} else {
+  let out2 = result2;
+  while (out2 !== null) {
+    output += out2.val;
+    if (out2.next !== null) {
+      output += " -> ";
+    }
+    out2 = out2.next;
   }
-  current2 = current2.next;
 }
 output += "<br><br>";
 
-const mid2 = middleNode(head2);
+// Example 3: head = [1]
+const head3 = new ListNode(1);
 
-output += "<b>Example 2 Output:</b><br>";
-let out2 = mid2;
-while (out2 !== null) {
-  output += out2.val;
-  if (out2.next !== null) {
+output += "<b>Example 3 Input:</b><br>1<br><br>";
+
+const result3 = swapPairs(head3);
+
+output += "<b>Example 3 Output:</b><br>";
+let out3 = result3;
+while (out3 !== null) {
+  output += out3.val;
+  if (out3.next !== null) {
     output += " -> ";
   }
-  out2 = out2.next;
+  out3 = out3.next;
+}
+output += "<br><br>";
+
+// Example 4: head = [1,2,3]
+const a = new ListNode(1);
+const b = new ListNode(2);
+const c = new ListNode(3);
+
+a.next = b;
+b.next = c;
+
+const head4 = a;
+
+output += "<b>Example 4 Input:</b><br>";
+let current4 = head4;
+while (current4 !== null) {
+  output += current4.val;
+  if (current4.next !== null) {
+    output += " -> ";
+  }
+  current4 = current4.next;
+}
+output += "<br><br>";
+
+const result4 = swapPairs(head4);
+
+output += "<b>Example 4 Output:</b><br>";
+let out4 = result4;
+while (out4 !== null) {
+  output += out4.val;
+  if (out4.next !== null) {
+    output += " -> ";
+  }
+  out4 = out4.next;
 }
 
 document.getElementById("output").innerHTML = output;
