cee8814e29cb10f52a700b92c3946e99bbb3245c
Jin Wang <specter01wj@gmail.com>
2026-02-28 19:25:46 -0500

    v4

--- mysol/Featured/Data Structures and Algorithms/4_Linked lists/Bonus/Fast_and_slow_pointers/M2095_Delete_the_Middle_Node_of_a_Linked_List/js_ts/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/4_Linked lists/Bonus/Fast_and_slow_pointers/M2095_Delete_the_Middle_Node_of_a_Linked_List/js_ts/ts/code.ts
@@ -8,94 +8,161 @@ class ListNode {
   }
 }
 
-function deleteDuplicates(head: ListNode | null): ListNode | null {
-  let current: ListNode | null = head;
+function deleteMiddle(head: ListNode | null, log: string[]): ListNode | null {
 
-  while (current !== null && current.next !== null) {
-    if (current.val === current.next.val) {
-      current.next = current.next.next;
-    } else {
-      current = current.next;
-    }
+  if (head === null) {
+    return null;
   }
+
+  if (head.next === null) {
+    return null;
+  }
+
+  let slow: ListNode = head;
+  let fast: ListNode | null = head;
+  let prev: ListNode | null = null;
+
+  let step: number = 0;
+
+  while (fast !== null && fast.next !== null) {
+    step++;
+    prev = slow;
+    slow = slow.next as ListNode;
+    fast = fast.next.next;
+
+    log.push(
+      "Step " + step +
+      ": prev=" + (prev as ListNode).val +
+      ", slow=" + slow.val +
+      ", fast=" + (fast === null ? "null" : fast.val)
+    );
+  }
+
+  log.push("Delete node (middle): " + slow.val);
+  (prev as ListNode).next = slow.next;
 
   return head;
 }
 
 (document.getElementById("title") as HTMLElement).innerText =
-  "83. Remove Duplicates from Sorted List (TS)";
+  "2095. Delete the Middle Node of a Linked List (TS)";
 
 let output: string = "";
 
-// Example 1: head = [1,1,2]
-const one: ListNode = new ListNode(1);
-const two: ListNode = new ListNode(1);
-const three: ListNode = new ListNode(2);
+// Example 1: [1,3,4,7,1,2,6]
+let a1: ListNode = new ListNode(1);
+let a2: ListNode = new ListNode(3);
+let a3: ListNode = new ListNode(4);
+let a4: ListNode = new ListNode(7);
+let a5: ListNode = new ListNode(1);
+let a6: ListNode = new ListNode(2);
+let a7: ListNode = new ListNode(6);
 
-one.next = two;
-two.next = three;
+a1.next = a2;
+a2.next = a3;
+a3.next = a4;
+a4.next = a5;
+a5.next = a6;
+a6.next = a7;
 
-const head1: ListNode = one;
+let head1: ListNode | null = a1;
 
-output += "<b>Example 1 Input:</b><br>";
-let current1: ListNode | null = head1;
-while (current1 !== null) {
-  output += current1.val;
-  if (current1.next !== null) {
-    output += " -> ";
-  }
-  current1 = current1.next;
+output += "<b>=== Example 1 ===</b><br>";
+output += "<b>Before:</b> ";
+let p1: ListNode | null = head1;
+while (p1 !== null) {
+  output += p1.val;
+  if (p1.next !== null) output += " -> ";
+  p1 = p1.next;
+}
+output += "<br>";
+
+let log1: string[] = [];
+head1 = deleteMiddle(head1, log1);
+
+for (let i: number = 0; i < log1.length; i++) {
+  output += log1[i] + "<br>";
+}
+
+output += "<b>After :</b> ";
+let q1: ListNode | null = head1;
+while (q1 !== null) {
+  output += q1.val;
+  if (q1.next !== null) output += " -> ";
+  q1 = q1.next;
 }
 output += "<br><br>";
 
-const result1: ListNode | null = deleteDuplicates(head1);
+// Example 2: [1,2,3,4]
+let b1: ListNode = new ListNode(1);
+let b2: ListNode = new ListNode(2);
+let b3: ListNode = new ListNode(3);
+let b4: ListNode = new ListNode(4);
 
-output += "<b>Example 1 Output:</b><br>";
-let out1: ListNode | null = result1;
-while (out1 !== null) {
-  output += out1.val;
-  if (out1.next !== null) {
-    output += " -> ";
-  }
-  out1 = out1.next;
+b1.next = b2;
+b2.next = b3;
+b3.next = b4;
+
+let head2: ListNode | null = b1;
+
+output += "<b>=== Example 2 ===</b><br>";
+output += "<b>Before:</b> ";
+let p2: ListNode | null = head2;
+while (p2 !== null) {
+  output += p2.val;
+  if (p2.next !== null) output += " -> ";
+  p2 = p2.next;
+}
+output += "<br>";
+
+let log2: string[] = [];
+head2 = deleteMiddle(head2, log2);
+
+for (let i: number = 0; i < log2.length; i++) {
+  output += log2[i] + "<br>";
+}
+
+output += "<b>After :</b> ";
+let q2: ListNode | null = head2;
+while (q2 !== null) {
+  output += q2.val;
+  if (q2.next !== null) output += " -> ";
+  q2 = q2.next;
 }
 output += "<br><br>";
 
-// Example 2: head = [1,1,2,3,3]
-const a: ListNode = new ListNode(1);
-const b: ListNode = new ListNode(1);
-const c: ListNode = new ListNode(2);
-const d: ListNode = new ListNode(3);
-const e: ListNode = new ListNode(3);
+// Example 3: [2,1]
+let c1: ListNode = new ListNode(2);
+let c2: ListNode = new ListNode(1);
 
-a.next = b;
-b.next = c;
-c.next = d;
-d.next = e;
+c1.next = c2;
 
-const head2: ListNode = a;
+let head3: ListNode | null = c1;
 
-output += "<b>Example 2 Input:</b><br>";
-let current2: ListNode | null = head2;
-while (current2 !== null) {
-  output += current2.val;
-  if (current2.next !== null) {
-    output += " -> ";
-  }
-  current2 = current2.next;
+output += "<b>=== Example 3 ===</b><br>";
+output += "<b>Before:</b> ";
+let p3: ListNode | null = head3;
+while (p3 !== null) {
+  output += p3.val;
+  if (p3.next !== null) output += " -> ";
+  p3 = p3.next;
 }
-output += "<br><br>";
+output += "<br>";
 
-const result2: ListNode | null = deleteDuplicates(head2);
+let log3: string[] = [];
+head3 = deleteMiddle(head3, log3);
 
-output += "<b>Example 2 Output:</b><br>";
-let out2: ListNode | null = result2;
-while (out2 !== null) {
-  output += out2.val;
-  if (out2.next !== null) {
-    output += " -> ";
-  }
-  out2 = out2.next;
+for (let i: number = 0; i < log3.length; i++) {
+  output += log3[i] + "<br>";
 }
+
+output += "<b>After :</b> ";
+let q3: ListNode | null = head3;
+while (q3 !== null) {
+  output += q3.val;
+  if (q3.next !== null) output += " -> ";
+  q3 = q3.next;
+}
+output += "<br>";
 
 (document.getElementById("output") as HTMLElement).innerHTML = output;
