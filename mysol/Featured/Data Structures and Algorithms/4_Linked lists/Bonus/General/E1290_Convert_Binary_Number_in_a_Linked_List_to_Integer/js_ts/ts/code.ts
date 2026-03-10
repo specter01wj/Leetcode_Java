565f078210bdc160f2c424981b14b8f1f2a5d1e1
Jin Wang <specter01wj@gmail.com>
2026-03-10 01:47:00 -0400

    v9

--- mysol/Featured/Data Structures and Algorithms/4_Linked lists/Bonus/General/E1290_Convert_Binary_Number_in_a_Linked_List_to_Integer/js_ts/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/4_Linked lists/Bonus/General/E1290_Convert_Binary_Number_in_a_Linked_List_to_Integer/js_ts/ts/code.ts
@@ -8,47 +8,32 @@ class ListNode {
   }
 }
 
-function removeElements(head: ListNode | null, val: number): ListNode | null {
+function getDecimalValue(head: ListNode | null): number {
 
-  const dummy: ListNode = new ListNode(0);
-  dummy.next = head;
+  let result: number = 0;
 
-  let current: ListNode = dummy;
-
-  while (current.next !== null) {
-    if (current.next.val === val) {
-      current.next = current.next.next;
-    } else {
-      current = current.next;
-    }
+  while (head !== null) {
+    result = result * 2 + head.val;
+    head = head.next;
   }
 
-  return dummy.next;
+  return result;
 }
 
 (document.getElementById("title") as HTMLElement).innerText =
-"203. Remove Linked List Elements (TS)";
+"1290. Convert Binary Number in a Linked List to Integer (TS)";
 
 let output: string = "";
 
-// Example 1: head = [1,2,6,3,4,5,6], val = 6
+// Example 1: head = [1,0,1]
 const a1: ListNode = new ListNode(1);
-const a2: ListNode = new ListNode(2);
-const a3: ListNode = new ListNode(6);
-const a4: ListNode = new ListNode(3);
-const a5: ListNode = new ListNode(4);
-const a6: ListNode = new ListNode(5);
-const a7: ListNode = new ListNode(6);
+const a2: ListNode = new ListNode(0);
+const a3: ListNode = new ListNode(1);
 
 a1.next = a2;
 a2.next = a3;
-a3.next = a4;
-a4.next = a5;
-a5.next = a6;
-a6.next = a7;
 
 const head1: ListNode = a1;
-const val1: number = 6;
 
 output += "<b>=== Example 1 ===</b><br>";
 output += "<b>Input :</b> ";
@@ -59,23 +44,16 @@ while (p1 !== null) {
   p1 = p1.next;
 }
 output += "<br>";
-output += "val = " + val1 + "<br>";
 
-const result1: ListNode | null = removeElements(head1, val1);
+const result1: number = getDecimalValue(head1);
 
-output += "<b>Output:</b> ";
-let q1: ListNode | null = result1;
-while (q1 !== null) {
-  output += q1.val;
-  if (q1.next !== null) output += " -> ";
-  q1 = q1.next;
-}
-output += "<br><br>";
+output += "<b>Output:</b> " + result1 + "<br><br>";
 
 
-// Example 2: head = [], val = 1
-const head2: ListNode | null = null;
-const val2: number = 1;
+// Example 2: head = [0]
+const b1: ListNode = new ListNode(0);
+
+const head2: ListNode = b1;
 
 output += "<b>=== Example 2 ===</b><br>";
 output += "<b>Input :</b> ";
@@ -86,53 +64,9 @@ while (p2 !== null) {
   p2 = p2.next;
 }
 output += "<br>";
-output += "val = " + val2 + "<br>";
 
-const result2: ListNode | null = removeElements(head2, val2);
+const result2: number = getDecimalValue(head2);
 
-output += "<b>Output:</b> ";
-let q2: ListNode | null = result2;
-while (q2 !== null) {
-  output += q2.val;
-  if (q2.next !== null) output += " -> ";
-  q2 = q2.next;
-}
-output += "<br><br>";
-
-
-// Example 3: head = [7,7,7,7], val = 7
-const c1: ListNode = new ListNode(7);
-const c2: ListNode = new ListNode(7);
-const c3: ListNode = new ListNode(7);
-const c4: ListNode = new ListNode(7);
-
-c1.next = c2;
-c2.next = c3;
-c3.next = c4;
-
-const head3: ListNode = c1;
-const val3: number = 7;
-
-output += "<b>=== Example 3 ===</b><br>";
-output += "<b>Input :</b> ";
-let p3: ListNode | null = head3;
-while (p3 !== null) {
-  output += p3.val;
-  if (p3.next !== null) output += " -> ";
-  p3 = p3.next;
-}
-output += "<br>";
-output += "val = " + val3 + "<br>";
-
-const result3: ListNode | null = removeElements(head3, val3);
-
-output += "<b>Output:</b> ";
-let q3: ListNode | null = result3;
-while (q3 !== null) {
-  output += q3.val;
-  if (q3.next !== null) output += " -> ";
-  q3 = q3.next;
-}
-output += "<br>";
+output += "<b>Output:</b> " + result2;
 
 (document.getElementById("output") as HTMLElement).innerHTML = output;
