0f150f07bcbf3c0e0dcb6ef57f64a930a0377212
Jin Wang <specter01wj@gmail.com>
2026-03-10 01:42:05 -0400

    v6

---
+++ mysol/Featured/Data Structures and Algorithms/4_Linked lists/Bonus/General/E1290_Convert_Binary_Number_in_a_Linked_List_to_Integer/src/com/jinwang/E1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.java
@@ -0,0 +1,10 @@
+package com.jinwang;
+
+public class E1290_Convert_Binary_Number_in_a_Linked_List_to_Integer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
