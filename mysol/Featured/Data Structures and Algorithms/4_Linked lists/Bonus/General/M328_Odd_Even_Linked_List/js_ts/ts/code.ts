3258f52cf169f8e8faaa2d75a1fe4d6bbbef4c6e
Jin Wang <specter01wj@gmail.com>
2026-03-11 21:09:02 -0400

    v4

--- mysol/Featured/Data Structures and Algorithms/4_Linked lists/Bonus/General/M328_Odd_Even_Linked_List/js_ts/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/4_Linked lists/Bonus/General/M328_Odd_Even_Linked_List/js_ts/ts/code.ts
@@ -8,47 +8,47 @@ class ListNode {
   }
 }
 
-function removeElements(head: ListNode | null, val: number): ListNode | null {
+function oddEvenList(head: ListNode | null): ListNode | null {
 
-  const dummy: ListNode = new ListNode(0);
-  dummy.next = head;
-
-  let current: ListNode = dummy;
-
-  while (current.next !== null) {
-    if (current.next.val === val) {
-      current.next = current.next.next;
-    } else {
-      current = current.next;
-    }
+  if (head === null || head.next === null) {
+    return head;
   }
 
-  return dummy.next;
+  let odd: ListNode = head;
+  let even: ListNode | null = head.next;
+  let evenHead: ListNode | null = even;
+
+  while (even !== null && even.next !== null) {
+    odd.next = even.next;
+    odd = odd.next as ListNode;
+
+    even.next = odd.next;
+    even = even.next;
+  }
+
+  odd.next = evenHead;
+
+  return head;
 }
 
 (document.getElementById("title") as HTMLElement).innerText =
-"203. Remove Linked List Elements (TS)";
+"328. Odd Even Linked List (TS)";
 
 let output: string = "";
 
-// Example 1: head = [1,2,6,3,4,5,6], val = 6
+// Example 1: head = [1,2,3,4,5]
 const a1: ListNode = new ListNode(1);
 const a2: ListNode = new ListNode(2);
-const a3: ListNode = new ListNode(6);
-const a4: ListNode = new ListNode(3);
-const a5: ListNode = new ListNode(4);
-const a6: ListNode = new ListNode(5);
-const a7: ListNode = new ListNode(6);
+const a3: ListNode = new ListNode(3);
+const a4: ListNode = new ListNode(4);
+const a5: ListNode = new ListNode(5);
 
 a1.next = a2;
 a2.next = a3;
 a3.next = a4;
 a4.next = a5;
-a5.next = a6;
-a6.next = a7;
 
 const head1: ListNode = a1;
-const val1: number = 6;
 
 output += "<b>=== Example 1 ===</b><br>";
 output += "<b>Input :</b> ";
@@ -59,9 +59,8 @@ while (p1 !== null) {
   p1 = p1.next;
 }
 output += "<br>";
-output += "val = " + val1 + "<br>";
 
-const result1: ListNode | null = removeElements(head1, val1);
+const result1: ListNode | null = oddEvenList(head1);
 
 output += "<b>Output:</b> ";
 let q1: ListNode | null = result1;
@@ -73,9 +72,23 @@ while (q1 !== null) {
 output += "<br><br>";
 
 
-// Example 2: head = [], val = 1
-const head2: ListNode | null = null;
-const val2: number = 1;
+// Example 2: head = [2,1,3,5,6,4,7]
+const b1: ListNode = new ListNode(2);
+const b2: ListNode = new ListNode(1);
+const b3: ListNode = new ListNode(3);
+const b4: ListNode = new ListNode(5);
+const b5: ListNode = new ListNode(6);
+const b6: ListNode = new ListNode(4);
+const b7: ListNode = new ListNode(7);
+
+b1.next = b2;
+b2.next = b3;
+b3.next = b4;
+b4.next = b5;
+b5.next = b6;
+b6.next = b7;
+
+const head2: ListNode = b1;
 
 output += "<b>=== Example 2 ===</b><br>";
 output += "<b>Input :</b> ";
@@ -86,9 +99,8 @@ while (p2 !== null) {
   p2 = p2.next;
 }
 output += "<br>";
-output += "val = " + val2 + "<br>";
 
-const result2: ListNode | null = removeElements(head2, val2);
+const result2: ListNode | null = oddEvenList(head2);
 
 output += "<b>Output:</b> ";
 let q2: ListNode | null = result2;
@@ -96,42 +108,6 @@ while (q2 !== null) {
   output += q2.val;
   if (q2.next !== null) output += " -> ";
   q2 = q2.next;
-}
-output += "<br><br>";
-
-
-// Example 3: head = [7,7,7,7], val = 7
-const c1: ListNode = new ListNode(7);
-const c2: ListNode = new ListNode(7);
-const c3: ListNode = new ListNode(7);
-const c4: ListNode = new ListNode(7);
-
-c1.next = c2;
-c2.next = c3;
-c3.next = c4;
-
-const head3: ListNode = c1;
-const val3: number = 7;
-
-output += "<b>=== Example 3 ===</b><br>";
-output += "<b>Input :</b> ";
-let p3: ListNode | null = head3;
-while (p3 !== null) {
-  output += p3.val;
-  if (p3.next !== null) output += " -> ";
-  p3 = p3.next;
-}
-output += "<br>";
-output += "val = " + val3 + "<br>";
-
-const result3: ListNode | null = removeElements(head3, val3);
-
-output += "<b>Output:</b> ";
-let q3: ListNode | null = result3;
-while (q3 !== null) {
-  output += q3.val;
-  if (q3.next !== null) output += " -> ";
-  q3 = q3.next;
 }
 output += "<br>";
 
