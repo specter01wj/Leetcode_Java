eac119623f42826a3c0694cc500dc775c72b4a79
Jin Wang <specter01wj@gmail.com>
2026-07-29 11:40:31 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Stacks/E1047_Remove_All_Adjacent_Duplicates_In_String/src/com/jinwang/E1047_Remove_All_Adjacent_Duplicates_In_String.java
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Stacks/E1047_Remove_All_Adjacent_Duplicates_In_String/src/com/jinwang/E1047_Remove_All_Adjacent_Duplicates_In_String.java
@@ -1,10 +1,39 @@
 package com.jinwang;
 
+import java.util.Stack;
+
 public class E1047_Remove_All_Adjacent_Duplicates_In_String {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		E1047_Remove_All_Adjacent_Duplicates_In_String solution = new E1047_Remove_All_Adjacent_Duplicates_In_String();
 
+        String input1 = "abbaca";
+        String input2 = "azxxzy";
+
+        System.out.println("Input: " + input1 + "\nOutput: " + solution.removeDuplicates(input1));
+        System.out.println();
+
+        System.out.println("Input: " + input2 + "\nOutput: " + solution.removeDuplicates(input2));
+	}
+	
+	public String removeDuplicates(String s) {
+		Stack<Character> stack = new Stack<>();
+		
+		for (char c : s.toCharArray()) {
+			if (!stack.empty() && stack.peek() == c) {
+				stack.pop();
+			} else {
+				stack.push(c);
+			}
+		}
+		
+		StringBuilder ans = new StringBuilder();
+		
+		while (!stack.empty()) {
+			ans.append(stack.pop());
+		}
+		
+		return ans.reverse().toString();
 	}
 
 }
