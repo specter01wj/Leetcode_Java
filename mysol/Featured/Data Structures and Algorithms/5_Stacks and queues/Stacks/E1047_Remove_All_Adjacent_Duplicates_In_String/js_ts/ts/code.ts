88bea1220d210f0eed161c24ae65c140b44de816
Jin Wang <specter01wj@gmail.com>
2026-07-29 11:49:56 -0400

    v4

--- mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Stacks/E1047_Remove_All_Adjacent_Duplicates_In_String/js_ts/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Stacks/E1047_Remove_All_Adjacent_Duplicates_In_String/js_ts/ts/code.ts
@@ -1,94 +1,47 @@
-function isValid(s: string): boolean {
+function removeDuplicates(s: string): string {
 
-  const matching = new Map<string, string>();
-  matching.set("(", ")");
-  matching.set("[", "]");
-  matching.set("{", "}");
+    const stack: string[] = [];
 
-  const stack: string[] = [];
+    for (const c of s) {
 
-  for (const c of s) {
+        if (stack.length > 0 && stack[stack.length - 1] === c) {
 
-    // Opening bracket
-    if (matching.has(c)) {
-      stack.push(c);
-    } else {
+            stack.pop();
 
-      // Closing bracket with no matching opening bracket
-      if (stack.length === 0) {
-        return false;
-      }
+        } else {
 
-      const previousOpening: string = stack.pop()!;
+            stack.push(c);
 
-      // Brackets do not match
-      if (matching.get(previousOpening) !== c) {
-        return false;
-      }
+        }
     }
-  }
 
-  // Valid only if no unmatched opening brackets remain
-  return stack.length === 0;
+    return stack.join("");
 }
 
 (document.getElementById("title") as HTMLElement).innerText =
-  "20. Valid Parentheses (TS)";
+    "1047. Remove All Adjacent Duplicates In String (TS)";
 
 let output: string = "";
 
 // Example 1
-const input1: string = "()";
+const input1: string = "abbaca";
 
 output += "<b>Example 1 Input:</b><br>";
 output += input1;
 output += "<br><br>";
 
 output += "<b>Example 1 Output:</b><br>";
-output += isValid(input1);
+output += removeDuplicates(input1);
 output += "<br><br>";
 
 // Example 2
-const input2: string = "()[]{}";
+const input2: string = "azxxzy";
 
 output += "<b>Example 2 Input:</b><br>";
 output += input2;
 output += "<br><br>";
 
 output += "<b>Example 2 Output:</b><br>";
-output += isValid(input2);
-output += "<br><br>";
-
-// Example 3
-const input3: string = "(]";
-
-output += "<b>Example 3 Input:</b><br>";
-output += input3;
-output += "<br><br>";
-
-output += "<b>Example 3 Output:</b><br>";
-output += isValid(input3);
-output += "<br><br>";
-
-// Example 4
-const input4: string = "([])";
-
-output += "<b>Example 4 Input:</b><br>";
-output += input4;
-output += "<br><br>";
-
-output += "<b>Example 4 Output:</b><br>";
-output += isValid(input4);
-output += "<br><br>";
-
-// Example 5
-const input5: string = "([)]";
-
-output += "<b>Example 5 Input:</b><br>";
-output += input5;
-output += "<br><br>";
-
-output += "<b>Example 5 Output:</b><br>";
-output += isValid(input5);
+output += removeDuplicates(input2);
 
 (document.getElementById("output") as HTMLElement).innerHTML = output;
