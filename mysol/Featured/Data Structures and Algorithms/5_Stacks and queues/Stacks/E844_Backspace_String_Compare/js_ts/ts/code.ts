ccfbc62a62087783043396598e0403aa6effbf3a
Jin Wang <specter01wj@gmail.com>
2026-07-30 03:40:26 -0400

    v8

--- mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Stacks/E844_Backspace_String_Compare/js_ts/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Stacks/E844_Backspace_String_Compare/js_ts/ts/code.ts
@@ -1,47 +1,64 @@
-function removeDuplicates(s: string): string {
-
-    const stack: string[] = [];
+function backspaceCompare(s: string, t: string): boolean {
+    const stackS: string[] = [];
+    const stackT: string[] = [];
 
     for (const c of s) {
-
-        if (stack.length > 0 && stack[stack.length - 1] === c) {
-
-            stack.pop();
-
-        } else {
-
-            stack.push(c);
-
+        if (c !== '#') {
+            stackS.push(c);
+        } else if (stackS.length > 0) {
+            stackS.pop();
         }
     }
 
-    return stack.join("");
+    for (const c of t) {
+        if (c !== '#') {
+            stackT.push(c);
+        } else if (stackT.length > 0) {
+            stackT.pop();
+        }
+    }
+
+    return stackS.join("") === stackT.join("");
 }
 
 (document.getElementById("title") as HTMLElement).innerText =
-    "1047. Remove All Adjacent Duplicates In String (TS)";
+    "844. Backspace String Compare (TS)";
 
 let output: string = "";
 
 // Example 1
-const input1: string = "abbaca";
+const s1: string = "ab#c";
+const t1: string = "ad#c";
 
 output += "<b>Example 1 Input:</b><br>";
-output += input1;
+output += 's = "' + s1 + '", t = "' + t1 + '"';
 output += "<br><br>";
 
 output += "<b>Example 1 Output:</b><br>";
-output += removeDuplicates(input1);
+output += backspaceCompare(s1, t1);
 output += "<br><br>";
 
 // Example 2
-const input2: string = "azxxzy";
+const s2: string = "ab##";
+const t2: string = "c#d#";
 
 output += "<b>Example 2 Input:</b><br>";
-output += input2;
+output += 's = "' + s2 + '", t = "' + t2 + '"';
 output += "<br><br>";
 
 output += "<b>Example 2 Output:</b><br>";
-output += removeDuplicates(input2);
+output += backspaceCompare(s2, t2);
+output += "<br><br>";
+
+// Example 3
+const s3: string = "a#c";
+const t3: string = "b";
+
+output += "<b>Example 3 Input:</b><br>";
+output += 's = "' + s3 + '", t = "' + t3 + '"';
+output += "<br><br>";
+
+output += "<b>Example 3 Output:</b><br>";
+output += backspaceCompare(s3, t3);
 
 (document.getElementById("output") as HTMLElement).innerHTML = output;
