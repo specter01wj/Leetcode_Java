4d73e5c415a230ea4236e496b2400d60d83a5e41
Jin Wang <specter01wj@gmail.com>
2026-08-06 21:49:10 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/E1544_Make_The_String_Great/src/com/jinwang/E1544_Make_The_String_Great.java
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/E1544_Make_The_String_Great/src/com/jinwang/E1544_Make_The_String_Great.java
@@ -1,10 +1,47 @@
 package com.jinwang;
 
+import java.util.Stack;
+
 public class E1544_Make_The_String_Great {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		E1544_Make_The_String_Great solution = new E1544_Make_The_String_Great();
 
+        String input1 = "leEeetcode";
+        String input2 = "abBAcC";
+        String input3 = "s";
+
+        System.out.println("Input: " + input1 +
+                "\nOutput: " + solution.makeGood(input1));
+        System.out.println();
+
+        System.out.println("Input: " + input2 +
+                "\nOutput: " + solution.makeGood(input2));
+        System.out.println();
+
+        System.out.println("Input: " + input3 +
+                "\nOutput: " + solution.makeGood(input3));
 	}
+	
+	public String makeGood(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (!stack.empty() && Math.abs(stack.peek() - c) == 32) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        for (char c : stack) {
+            ans.append(c);
+        }
+
+        return ans.toString();
+    }
 
 }
