c386f51803a00782bb22cca9318e9741fbd4aefd
Jin Wang <specter01wj@gmail.com>
2026-08-12 14:14:56 -0400

    v3

---
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Queues/Ch5_exp2/js_ts/js/code.js
@@ -0,0 +1,54 @@
+document.getElementById("title").innerText =
+    "Stack Basics (JS)";
+
+const stack = [];
+
+let output = "";
+
+// Push
+output += "<b>=== Push ===</b><br>";
+stack.push(1);
+stack.push(2);
+stack.push(3);
+output += "Stack: [" + stack.join(", ") + "]<br><br>";
+
+// Pop
+output += "<b>=== Pop ===</b><br>";
+output += "Pop: " + stack.pop() + "<br>";
+output += "Stack: [" + stack.join(", ") + "]<br>";
+
+output += "Pop: " + stack.pop() + "<br>";
+output += "Stack: [" + stack.join(", ") + "]<br><br>";
+
+// Push Again
+output += "<b>=== Push Again ===</b><br>";
+stack.push(5);
+output += "Stack: [" + stack.join(", ") + "]<br><br>";
+
+// Peek
+output += "<b>=== Peek ===</b><br>";
+output += "Top Element: " + stack[stack.length - 1] + "<br>";
+output += "Stack: [" + stack.join(", ") + "]<br><br>";
+
+// Size
+output += "<b>=== Size ===</b><br>";
+output += "Size: " + stack.length + "<br><br>";
+
+// Empty
+output += "<b>=== Empty ===</b><br>";
+output += "Is Empty? " + (stack.length === 0) + "<br><br>";
+
+// Pop Remaining Elements
+output += "<b>=== Pop Remaining Elements ===</b><br>";
+while (stack.length > 0) {
+    output += "Pop: " + stack.pop() + "<br>";
+    output += "Stack: [" + stack.join(", ") + "]<br>";
+}
+output += "<br>";
+
+// Final Check
+output += "<b>=== Final Check ===</b><br>";
+output += "Is Empty? " + (stack.length === 0) + "<br>";
+output += "Size: " + stack.length;
+
+document.getElementById("output").innerHTML = output;
