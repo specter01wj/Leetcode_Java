92ed239fb06d13efbe4b18d7b0dfac4743dfa32a
Jin Wang <specter01wj@gmail.com>
2026-08-13 15:41:55 -0400

    v1

---
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Monotonic/M1438_Longest_Continuous_Subarray_With_Absolute_Diff_Less_Than_or_Equal_to_Limit/src/com/jinwang/M1438_Longest_Continuous_Subarray_With_Absolute_Diff_Less_Than_or_Equal_to_Limit.java
@@ -0,0 +1,10 @@
+package com.jinwang;
+
+public class M1438_Longest_Continuous_Subarray_With_Absolute_Diff_Less_Than_or_Equal_to_Limit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
