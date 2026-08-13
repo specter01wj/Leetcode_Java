27b92ad88aee9b7543874700f0b450d567f6e425
Jin Wang <specter01wj@gmail.com>
2026-08-13 11:48:22 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Queues/E346_Moving_Average_from_Data_Stream/src/com/jinwang/E346_Moving_Average_from_Data_Stream.java
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Queues/E346_Moving_Average_from_Data_Stream/src/com/jinwang/E346_Moving_Average_from_Data_Stream.java
@@ -1,10 +1,50 @@
 package com.jinwang;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class E346_Moving_Average_from_Data_Stream {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MovingAverage movingAverage = new MovingAverage(3);
 
+		System.out.println("Input: 1");
+		System.out.println("Output: " + movingAverage.next(1));
+		System.out.println();
+		
+		System.out.println("Input: 10");
+		System.out.println("Output: " + movingAverage.next(10));
+		System.out.println();
+		
+		System.out.println("Input: 3");
+		System.out.println("Output: " + movingAverage.next(3));
+		System.out.println();
+		
+		System.out.println("Input: 5");
+		System.out.println("Output: " + movingAverage.next(5));
+	}
+	
+	static class MovingAverage {
+
+		private Queue<Integer> window;
+		private int maxSize;
+		private double sum;
+		public MovingAverage(int size) {
+			this.window = new LinkedList<>();
+			this.maxSize = size;
+			this.sum = 0.0;
+		}
+		public double next(int val) {
+			// Remove oldest value when window is full
+			if (window.size() == maxSize) {
+				sum -= window.poll();
+			}
+			// Add new value
+			window.add(val);
+			sum += val;
+			// Return moving average
+			return sum / window.size();
+		}
 	}
 
 }
