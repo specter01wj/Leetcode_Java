9255ae8932f0c56c6d5d472d39e22c0da874e694
Jin Wang <specter01wj@gmail.com>
2026-08-13 14:19:02 -0400

    v10

---
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Monotonic/M739_Daily_Temperatures/js_ts/ts/code.js
@@ -0,0 +1,40 @@
+function dailyTemperatures(temperatures) {
+    const stack = [];
+    const answer = new Array(temperatures.length).fill(0);
+    for (let i = 0; i < temperatures.length; i++) {
+        while (stack.length > 0 &&
+            temperatures[stack[stack.length - 1]] < temperatures[i]) {
+            const j = stack.pop();
+            answer[j] = i - j;
+        }
+        stack.push(i);
+    }
+    return answer;
+}
+document.getElementById("title").innerText =
+    "739. Daily Temperatures (TS)";
+let output = "";
+// Example 1
+const input1 = [73, 74, 75, 71, 69, 72, 76, 73];
+output += "<b>Example 1 Input:</b><br>";
+output += "[" + input1.join(", ") + "]";
+output += "<br><br>";
+output += "<b>Example 1 Output:</b><br>";
+output += "[" + dailyTemperatures(input1).join(", ") + "]";
+output += "<br><br>";
+// Example 2
+const input2 = [30, 40, 50, 60];
+output += "<b>Example 2 Input:</b><br>";
+output += "[" + input2.join(", ") + "]";
+output += "<br><br>";
+output += "<b>Example 2 Output:</b><br>";
+output += "[" + dailyTemperatures(input2).join(", ") + "]";
+output += "<br><br>";
+// Example 3
+const input3 = [30, 60, 90];
+output += "<b>Example 3 Input:</b><br>";
+output += "[" + input3.join(", ") + "]";
+output += "<br><br>";
+output += "<b>Example 3 Output:</b><br>";
+output += "[" + dailyTemperatures(input3).join(", ") + "]";
+document.getElementById("output").innerHTML = output;
