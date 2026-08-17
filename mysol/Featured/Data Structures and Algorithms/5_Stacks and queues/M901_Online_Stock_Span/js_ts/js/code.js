07bccd38bc93c1b98481e92ec1321bfce62f2986
Jin Wang <specter01wj@gmail.com>
2026-08-17 13:47:32 -0400

    v5

--- mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/M901_Online_Stock_Span/js_ts/js/code.js
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/M901_Online_Stock_Span/js_ts/js/code.js
@@ -1,65 +1,106 @@
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
+var StockSpanner = function() {
+    this.stack = [];
+};
+
+/** 
+ * @param {number} price
+ * @return {number}
  */
-var nextGreaterElement = function(nums1, nums2) {
+StockSpanner.prototype.next = function(price) {
+    let span = 1;
 
-    const nextGreater = new Map();
-    const stack = [];
-
-    for (const num of nums2) {
-
-        // Current number is the next greater element
-        while (stack.length > 0 && stack[stack.length - 1] < num) {
-            nextGreater.set(stack.pop(), num);
-        }
-
-        stack.push(num);
+    // Remove prices less than or equal to current price
+    while (this.stack.length > 0 && this.stack[this.stack.length - 1][0] <= price) {
+        span += this.stack.pop()[1];
     }
 
-    // Remaining numbers have no next greater element
-    while (stack.length > 0) {
-        nextGreater.set(stack.pop(), -1);
-    }
+    // Store current price and its span
+    this.stack.push([price, span]);
 
-    const answer = [];
-
-    for (let i = 0; i < nums1.length; i++) {
-        answer.push(nextGreater.get(nums1[i]));
-    }
-
-    return answer;
+    return span;
 };
 
 document.getElementById("title").innerText =
-    "496. Next Greater Element I (JS)";
+    "901. Online Stock Span (JS)";
 
 let output = "";
 
+const stockSpanner = new StockSpanner();
+
 // Example 1
-const nums1_1 = [4, 1, 2];
-const nums2_1 = [1, 3, 4, 2];
+const input1 = 100;
 
 output += "<b>Example 1 Input:</b><br>";
-output += "nums1 = [" + nums1_1.join(", ") + "]<br>";
-output += "nums2 = [" + nums2_1.join(", ") + "]";
+output += input1;
 output += "<br><br>";
 
 output += "<b>Example 1 Output:</b><br>";
-output += "[" + nextGreaterElement(nums1_1, nums2_1).join(", ") + "]";
+output += stockSpanner.next(input1);
 output += "<br><br>";
 
 // Example 2
-const nums1_2 = [2, 4];
-const nums2_2 = [1, 2, 3, 4];
+const input2 = 80;
 
 output += "<b>Example 2 Input:</b><br>";
-output += "nums1 = [" + nums1_2.join(", ") + "]<br>";
-output += "nums2 = [" + nums2_2.join(", ") + "]";
+output += input2;
 output += "<br><br>";
 
 output += "<b>Example 2 Output:</b><br>";
-output += "[" + nextGreaterElement(nums1_2, nums2_2).join(", ") + "]";
+output += stockSpanner.next(input2);
+output += "<br><br>";
+
+// Example 3
+const input3 = 60;
+
+output += "<b>Example 3 Input:</b><br>";
+output += input3;
+output += "<br><br>";
+
+output += "<b>Example 3 Output:</b><br>";
+output += stockSpanner.next(input3);
+output += "<br><br>";
+
+// Example 4
+const input4 = 70;
+
+output += "<b>Example 4 Input:</b><br>";
+output += input4;
+output += "<br><br>";
+
+output += "<b>Example 4 Output:</b><br>";
+output += stockSpanner.next(input4);
+output += "<br><br>";
+
+// Example 5
+const input5 = 60;
+
+output += "<b>Example 5 Input:</b><br>";
+output += input5;
+output += "<br><br>";
+
+output += "<b>Example 5 Output:</b><br>";
+output += stockSpanner.next(input5);
+output += "<br><br>";
+
+// Example 6
+const input6 = 75;
+
+output += "<b>Example 6 Input:</b><br>";
+output += input6;
+output += "<br><br>";
+
+output += "<b>Example 6 Output:</b><br>";
+output += stockSpanner.next(input6);
+output += "<br><br>";
+
+// Example 7
+const input7 = 85;
+
+output += "<b>Example 7 Input:</b><br>";
+output += input7;
+output += "<br><br>";
+
+output += "<b>Example 7 Output:</b><br>";
+output += stockSpanner.next(input7);
 
 document.getElementById("output").innerHTML = output;
