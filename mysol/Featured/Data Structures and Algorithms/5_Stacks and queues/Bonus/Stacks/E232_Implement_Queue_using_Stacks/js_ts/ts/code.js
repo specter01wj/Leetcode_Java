8f29155127f7df2b5bef60de594686b0ad60fa03
Jin Wang <specter01wj@gmail.com>
2026-08-19 14:45:28 -0400

    v6

---
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Stacks/E232_Implement_Queue_using_Stacks/js_ts/ts/code.js
@@ -0,0 +1,54 @@
+class MyQueue {
+    inputStack;
+    outputStack;
+    constructor() {
+        this.inputStack = [];
+        this.outputStack = [];
+    }
+    push(x) {
+        this.inputStack.push(x);
+    }
+    pop() {
+        if (this.outputStack.length === 0) {
+            while (this.inputStack.length > 0) {
+                this.outputStack.push(this.inputStack.pop());
+            }
+        }
+        return this.outputStack.pop();
+    }
+    peek() {
+        if (this.outputStack.length === 0) {
+            while (this.inputStack.length > 0) {
+                this.outputStack.push(this.inputStack.pop());
+            }
+        }
+        return this.outputStack[this.outputStack.length - 1];
+    }
+    empty() {
+        return this.inputStack.length === 0 && this.outputStack.length === 0;
+    }
+}
+document.getElementById("title").innerText =
+    "232. Implement Queue using Stacks (TS)";
+let output = "";
+const myQueue = new MyQueue();
+// Push 1
+output += "<b>Input: push(1)</b><br>";
+myQueue.push(1);
+output += "<br>";
+// Push 2
+output += "<b>Input: push(2)</b><br>";
+myQueue.push(2);
+output += "<br>";
+// Peek
+output += "<b>Input: peek()</b><br>";
+output += "Output: " + myQueue.peek();
+output += "<br><br>";
+// Pop
+output += "<b>Input: pop()</b><br>";
+output += "Output: " + myQueue.pop();
+output += "<br><br>";
+// Empty
+output += "<b>Input: empty()</b><br>";
+output += "Output: " + myQueue.empty();
+document.getElementById("output").innerHTML = output;
