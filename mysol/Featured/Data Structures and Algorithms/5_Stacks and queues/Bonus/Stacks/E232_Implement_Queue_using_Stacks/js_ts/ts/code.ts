0565bade028f2cd638711730691c10cf0cf08933
Jin Wang <specter01wj@gmail.com>
2026-08-19 14:45:12 -0400

    v4

--- mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Stacks/E232_Implement_Queue_using_Stacks/js_ts/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Stacks/E232_Implement_Queue_using_Stacks/js_ts/ts/code.ts
@@ -1,43 +1,71 @@
-function removeStars(s: string): string {
+class MyQueue {
 
-  const stack: string[] = [];
+  private inputStack: number[];
+  private outputStack: number[];
 
-  for (const c of s) {
-
-    if (c === "*") {
-      stack.pop();
-    } else {
-      stack.push(c);
-    }
+  constructor() {
+    this.inputStack = [];
+    this.outputStack = [];
   }
 
-  return stack.join("");
+  push(x: number): void {
+    this.inputStack.push(x);
+  }
+
+  pop(): number {
+    if (this.outputStack.length === 0) {
+      while (this.inputStack.length > 0) {
+        this.outputStack.push(this.inputStack.pop()!);
+      }
+    }
+
+    return this.outputStack.pop()!;
+  }
+
+  peek(): number {
+    if (this.outputStack.length === 0) {
+      while (this.inputStack.length > 0) {
+        this.outputStack.push(this.inputStack.pop()!);
+      }
+    }
+
+    return this.outputStack[this.outputStack.length - 1];
+  }
+
+  empty(): boolean {
+    return this.inputStack.length === 0 && this.outputStack.length === 0;
+  }
 }
 
 (document.getElementById("title") as HTMLElement).innerText =
-  "2390. Removing Stars From a String (TS)";
+  "232. Implement Queue using Stacks (TS)";
 
 let output: string = "";
 
-// Example 1
-const input1: string = "leet**cod*e";
+const myQueue: MyQueue = new MyQueue();
 
-output += "<b>Example 1 Input:</b><br>";
-output += input1;
+// Push 1
+output += "<b>Input: push(1)</b><br>";
+myQueue.push(1);
+output += "<br>";
+
+// Push 2
+output += "<b>Input: push(2)</b><br>";
+myQueue.push(2);
+output += "<br>";
+
+// Peek
+output += "<b>Input: peek()</b><br>";
+output += "Output: " + myQueue.peek();
 output += "<br><br>";
 
-output += "<b>Example 1 Output:</b><br>";
-output += removeStars(input1);
+// Pop
+output += "<b>Input: pop()</b><br>";
+output += "Output: " + myQueue.pop();
 output += "<br><br>";
 
-// Example 2
-const input2: string = "erase*****";
-
-output += "<b>Example 2 Input:</b><br>";
-output += input2;
-output += "<br><br>";
-
-output += "<b>Example 2 Output:</b><br>";
-output += removeStars(input2);
+// Empty
+output += "<b>Input: empty()</b><br>";
+output += "Output: " + myQueue.empty();
 
 (document.getElementById("output") as HTMLElement).innerHTML = output;
