f14c687742724ac414e5d33605c5a287960aa480
Jin Wang <specter01wj@gmail.com>
2026-08-19 14:27:03 -0400

    v3

---
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Stacks/M2390_Removing_Stars_From_a_String/js_ts/js/code.js
@@ -0,0 +1,65 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+
+    const nextGreater = new Map();
+    const stack = [];
+
+    for (const num of nums2) {
+
+        // Current number is the next greater element
+        while (stack.length > 0 && stack[stack.length - 1] < num) {
+            nextGreater.set(stack.pop(), num);
+        }
+
+        stack.push(num);
+    }
+
+    // Remaining numbers have no next greater element
+    while (stack.length > 0) {
+        nextGreater.set(stack.pop(), -1);
+    }
+
+    const answer = [];
+
+    for (let i = 0; i < nums1.length; i++) {
+        answer.push(nextGreater.get(nums1[i]));
+    }
+
+    return answer;
+};
+
+document.getElementById("title").innerText =
+    "496. Next Greater Element I (JS)";
+
+let output = "";
+
+// Example 1
+const nums1_1 = [4, 1, 2];
+const nums2_1 = [1, 3, 4, 2];
+
+output += "<b>Example 1 Input:</b><br>";
+output += "nums1 = [" + nums1_1.join(", ") + "]<br>";
+output += "nums2 = [" + nums2_1.join(", ") + "]";
+output += "<br><br>";
+
+output += "<b>Example 1 Output:</b><br>";
+output += "[" + nextGreaterElement(nums1_1, nums2_1).join(", ") + "]";
+output += "<br><br>";
+
+// Example 2
+const nums1_2 = [2, 4];
+const nums2_2 = [1, 2, 3, 4];
+
+output += "<b>Example 2 Input:</b><br>";
+output += "nums1 = [" + nums1_2.join(", ") + "]<br>";
+output += "nums2 = [" + nums2_2.join(", ") + "]";
+output += "<br><br>";
+
+output += "<b>Example 2 Output:</b><br>";
+output += "[" + nextGreaterElement(nums1_2, nums2_2).join(", ") + "]";
+
+document.getElementById("output").innerHTML = output;
