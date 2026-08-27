d4b87b21873edbbde8a0051eb375c765d82bcd9c
Jin Wang <specter01wj@gmail.com>
2026-08-27 12:52:32 -0400

    v3

--- mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Stacks/M946_Validate_Stack_Sequences/js_ts/js/code.js
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Stacks/M946_Validate_Stack_Sequences/js_ts/js/code.js
@@ -1,76 +1,55 @@
 /**
- * @param {string} s
- * @return {string}
+ * @param {number[]} pushed
+ * @param {number[]} popped
+ * @return {boolean}
  */
-var robotWithString = function(s) {
-    const count = new Array(26).fill(0);
-
-    for (const c of s) {
-        count[c.charCodeAt(0) - 97]++;
-    }
-
+var validateStackSequences = function(pushed, popped) {
     const stack = [];
-    let answer = "";
+    let j = 0;
 
-    let smallest = 0;
+    for (const num of pushed) {
+        stack.push(num);
 
-    for (const c of s) {
-        stack.push(c);
-        count[c.charCodeAt(0) - 97]--;
-
-        // Find the smallest remaining character in s
-        while (smallest < 26 && count[smallest] === 0) {
-            smallest++;
-        }
-
-        // Output characters that are no larger than the smallest remaining character
+        // Pop while the top matches the next expected popped value
         while (
             stack.length > 0 &&
-            (smallest === 26 ||
-                stack[stack.length - 1].charCodeAt(0) - 97 <= smallest)
+            j < popped.length &&
+            stack[stack.length - 1] === popped[j]
         ) {
-            answer += stack.pop();
+            stack.pop();
+            j++;
         }
     }
 
-    return answer;
+    return stack.length === 0;
 };
 
 document.getElementById("title").innerText =
-    "2434. Using a Robot to Print the Lexicographically Smallest String (JS)";
+    "946. Validate Stack Sequences (JS)";
 
 let output = "";
 
 // Example 1
-const input1 = "zza";
+const pushed1 = [1, 2, 3, 4, 5];
+const popped1 = [4, 5, 3, 2, 1];
 
 output += "<b>Example 1 Input:</b><br>";
-output += input1;
+output += "pushed = [" + pushed1.join(", ") + "], popped = [" + popped1.join(", ") + "]";
 output += "<br><br>";
 
 output += "<b>Example 1 Output:</b><br>";
-output += robotWithString(input1);
+output += validateStackSequences(pushed1, popped1);
 output += "<br><br>";
 
 // Example 2
-const input2 = "bac";
+const pushed2 = [1, 2, 3, 4, 5];
+const popped2 = [4, 3, 5, 1, 2];
 
 output += "<b>Example 2 Input:</b><br>";
-output += input2;
+output += "pushed = [" + pushed2.join(", ") + "], popped = [" + popped2.join(", ") + "]";
 output += "<br><br>";
 
 output += "<b>Example 2 Output:</b><br>";
-output += robotWithString(input2);
-output += "<br><br>";
-
-// Example 3
-const input3 = "bdda";
-
-output += "<b>Example 3 Input:</b><br>";
-output += input3;
-output += "<br><br>";
-
-output += "<b>Example 3 Output:</b><br>";
-output += robotWithString(input3);
+output += validateStackSequences(pushed2, popped2);
 
 document.getElementById("output").innerHTML = output;
