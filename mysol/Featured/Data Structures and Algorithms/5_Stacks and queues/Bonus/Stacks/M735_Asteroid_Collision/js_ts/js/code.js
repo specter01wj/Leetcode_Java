85530dd92802aab513b658fe84df88c81d42deb2
Jin Wang <specter01wj@gmail.com>
2026-08-30 18:52:09 -0400

    v4

--- mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Stacks/M735_Asteroid_Collision/js_ts/js/code.js
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Stacks/M735_Asteroid_Collision/js_ts/js/code.js
@@ -1,76 +1,86 @@
 /**
- * @param {string} s
- * @return {string}
+ * @param {number[]} asteroids
+ * @return {number[]}
  */
-var robotWithString = function(s) {
-    const count = new Array(26).fill(0);
-
-    for (const c of s) {
-        count[c.charCodeAt(0) - 97]++;
-    }
-
+var asteroidCollision = function(asteroids) {
     const stack = [];
-    let answer = "";
 
-    let smallest = 0;
+    for (const asteroid of asteroids) {
+        let destroyed = false;
 
-    for (const c of s) {
-        stack.push(c);
-        count[c.charCodeAt(0) - 97]--;
-
-        // Find the smallest remaining character in s
-        while (smallest < 26 && count[smallest] === 0) {
-            smallest++;
-        }
-
-        // Output characters that are no larger than the smallest remaining character
+        // Collision only happens when stack top moves right
+        // and current asteroid moves left
         while (
             stack.length > 0 &&
-            (smallest === 26 ||
-                stack[stack.length - 1].charCodeAt(0) - 97 <= smallest)
+            stack[stack.length - 1] > 0 &&
+            asteroid < 0
         ) {
-            answer += stack.pop();
+            if (stack[stack.length - 1] < -asteroid) {
+                stack.pop();
+            } else if (stack[stack.length - 1] === -asteroid) {
+                stack.pop();
+                destroyed = true;
+                break;
+            } else {
+                destroyed = true;
+                break;
+            }
+        }
+
+        if (!destroyed) {
+            stack.push(asteroid);
         }
     }
 
-    return answer;
+    return stack;
 };
 
 document.getElementById("title").innerText =
-    "2434. Using a Robot to Print the Lexicographically Smallest String (JS)";
+    "735. Asteroid Collision (JS)";
 
 let output = "";
 
 // Example 1
-const input1 = "zza";
+const input1 = [5, 10, -5];
 
 output += "<b>Example 1 Input:</b><br>";
-output += input1;
+output += "[" + input1.join(", ") + "]";
 output += "<br><br>";
 
 output += "<b>Example 1 Output:</b><br>";
-output += robotWithString(input1);
+output += "[" + asteroidCollision(input1).join(", ") + "]";
 output += "<br><br>";
 
 // Example 2
-const input2 = "bac";
+const input2 = [8, -8];
 
 output += "<b>Example 2 Input:</b><br>";
-output += input2;
+output += "[" + input2.join(", ") + "]";
 output += "<br><br>";
 
 output += "<b>Example 2 Output:</b><br>";
-output += robotWithString(input2);
+output += "[" + asteroidCollision(input2).join(", ") + "]";
 output += "<br><br>";
 
 // Example 3
-const input3 = "bdda";
+const input3 = [10, 2, -5];
 
 output += "<b>Example 3 Input:</b><br>";
-output += input3;
+output += "[" + input3.join(", ") + "]";
 output += "<br><br>";
 
 output += "<b>Example 3 Output:</b><br>";
-output += robotWithString(input3);
+output += "[" + asteroidCollision(input3).join(", ") + "]";
+output += "<br><br>";
+
+// Example 4
+const input4 = [3, 5, -6, 2, -1, 4];
+
+output += "<b>Example 4 Input:</b><br>";
+output += "[" + input4.join(", ") + "]";
+output += "<br><br>";
+
+output += "<b>Example 4 Output:</b><br>";
+output += "[" + asteroidCollision(input4).join(", ") + "]";
 
 document.getElementById("output").innerHTML = output;
