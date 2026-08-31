64f3cf4b12b3f66d1eb766ff81c42c707c5377b0
Jin Wang <specter01wj@gmail.com>
2026-08-30 20:19:07 -0400

    v7

--- mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Stacks/M155_Min_Stack/js_ts/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Stacks/M155_Min_Stack/js_ts/ts/code.ts
@@ -1,82 +1,62 @@
-function asteroidCollision(asteroids: number[]): number[] {
-  const stack: number[] = [];
+class MinStack {
+  private stack: number[];
+  private minStack: number[];
 
-  for (const asteroid of asteroids) {
-    let destroyed: boolean = false;
+  constructor() {
+    this.stack = [];
+    this.minStack = [];
+  }
 
-    // Collision only happens when stack top moves right
-    // and current asteroid moves left
-    while (
-      stack.length > 0 &&
-      stack[stack.length - 1] > 0 &&
-      asteroid < 0
+  push(value: number): void {
+    this.stack.push(value);
+
+    if (
+      this.minStack.length === 0 ||
+      value <= this.minStack[this.minStack.length - 1]
     ) {
-      if (stack[stack.length - 1] < -asteroid) {
-        stack.pop();
-      } else if (stack[stack.length - 1] === -asteroid) {
-        stack.pop();
-        destroyed = true;
-        break;
-      } else {
-        destroyed = true;
-        break;
-      }
-    }
-
-    if (!destroyed) {
-      stack.push(asteroid);
+      this.minStack.push(value);
     }
   }
 
-  return stack;
+  pop(): void {
+    const value: number = this.stack.pop()!;
+
+    if (value === this.minStack[this.minStack.length - 1]) {
+      this.minStack.pop();
+    }
+  }
+
+  top(): number {
+    return this.stack[this.stack.length - 1];
+  }
+
+  getMin(): number {
+    return this.minStack[this.minStack.length - 1];
+  }
 }
 
 (document.getElementById("title") as HTMLElement).innerText =
-  "735. Asteroid Collision (TS)";
+  "155. Min Stack (TS)";
 
 let output: string = "";
 
-// Example 1
-const input1: number[] = [5, 10, -5];
+const minStack = new MinStack();
 
-output += "<b>Example 1 Input:</b><br>";
-output += "[" + input1.join(", ") + "]";
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+
+output += "<b>Input:</b> getMin()<br>";
+output += "<b>Output:</b> " + minStack.getMin();
 output += "<br><br>";
 
-output += "<b>Example 1 Output:</b><br>";
-output += "[" + asteroidCollision(input1).join(", ") + "]";
+minStack.pop();
+
+output += "<b>Input:</b> top()<br>";
+output += "<b>Output:</b> " + minStack.top();
 output += "<br><br>";
 
-// Example 2
-const input2: number[] = [8, -8];
-
-output += "<b>Example 2 Input:</b><br>";
-output += "[" + input2.join(", ") + "]";
-output += "<br><br>";
-
-output += "<b>Example 2 Output:</b><br>";
-output += "[" + asteroidCollision(input2).join(", ") + "]";
-output += "<br><br>";
-
-// Example 3
-const input3: number[] = [10, 2, -5];
-
-output += "<b>Example 3 Input:</b><br>";
-output += "[" + input3.join(", ") + "]";
-output += "<br><br>";
-
-output += "<b>Example 3 Output:</b><br>";
-output += "[" + asteroidCollision(input3).join(", ") + "]";
-output += "<br><br>";
-
-// Example 4
-const input4: number[] = [3, 5, -6, 2, -1, 4];
-
-output += "<b>Example 4 Input:</b><br>";
-output += "[" + input4.join(", ") + "]";
-output += "<br><br>";
-
-output += "<b>Example 4 Output:</b><br>";
-output += "[" + asteroidCollision(input4).join(", ") + "]";
+output += "<b>Input:</b> getMin()<br>";
+output += "<b>Output:</b> " + minStack.getMin();
 
 (document.getElementById("output") as HTMLElement).innerHTML = output;
