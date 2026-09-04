32965b23a696783cbff94a9ed58c930193f34116
Jin Wang <specter01wj@gmail.com>
2026-09-04 16:16:58 -0400

    v2

--- mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Queues/E225_Implement_Stack_Using_Queues/src/com/jinwang/E225_Implement_Stack_Using_Queues.java
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Queues/E225_Implement_Stack_Using_Queues/src/com/jinwang/E225_Implement_Stack_Using_Queues.java
@@ -1,10 +1,60 @@
 package com.jinwang;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class E225_Implement_Stack_Using_Queues {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MyStack myStack = new MyStack();
 
+		// Example 1
+		myStack.push(1);
+		myStack.push(2);
+
+		System.out.println("Input: top()");
+		System.out.println("Output: " + myStack.top());
+		System.out.println();
+
+		System.out.println("Input: pop()");
+		System.out.println("Output: " + myStack.pop());
+		System.out.println();
+
+		System.out.println("Input: empty()");
+		System.out.println("Output: " + myStack.empty());
+	}
+
+}
+
+class MyStack {
+
+	private Queue<Integer> queue;
+
+	public MyStack() {
+		queue = new LinkedList<>();
+	}
+
+	public void push(int x) {
+		queue.offer(x);
+
+		// Move previous elements behind the new element
+		int size = queue.size();
+
+		for (int i = 0; i < size - 1; i++) {
+			queue.offer(queue.poll());
+		}
+	}
+
+	public int pop() {
+		return queue.poll();
+	}
+
+	public int top() {
+		return queue.peek();
+	}
+
+	public boolean empty() {
+		return queue.isEmpty();
 	}
 
 }
