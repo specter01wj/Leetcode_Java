a031b81209138f5773881eb0a620dc58950e6664
Jin Wang <specter01wj@gmail.com>
2026-09-09 12:17:17 -0400

    v10

---
+++ mysol/Hard/H1063_Number_Of_Valid_Subarrays/js_ts/ts/code.js
@@ -0,0 +1,30 @@
+function validSubarrays(nums) {
+    const stack = [];
+    let count = 0;
+    for (const num of nums) {
+        while (stack.length > 0 && stack[stack.length - 1] > num) {
+            stack.pop();
+        }
+        stack.push(num);
+        count += stack.length;
+    }
+    return count;
+}
+document.getElementById("title").innerText =
+    "1063. Number of Valid Subarrays (TS)";
+let output = "";
+// Example 1
+const nums1 = [1, 4, 2, 5, 3];
+output += "<b>Input:</b> nums = [" + nums1 + "]<br>";
+output += "<b>Output:</b> " + validSubarrays(nums1);
+output += "<br><br>";
+// Example 2
+const nums2 = [3, 2, 1];
+output += "<b>Input:</b> nums = [" + nums2 + "]<br>";
+output += "<b>Output:</b> " + validSubarrays(nums2);
+output += "<br><br>";
+// Example 3
+const nums3 = [2, 2, 2];
+output += "<b>Input:</b> nums = [" + nums3 + "]<br>";
+output += "<b>Output:</b> " + validSubarrays(nums3);
+document.getElementById("output").innerHTML = output;
