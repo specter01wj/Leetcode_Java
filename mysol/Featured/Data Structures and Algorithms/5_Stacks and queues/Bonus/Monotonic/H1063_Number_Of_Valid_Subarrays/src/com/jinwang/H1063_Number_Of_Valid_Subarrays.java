8d97b5ac80ff030fec04d6be4dd56deb760b153d
Jin Wang <specter01wj@gmail.com>
2026-09-09 12:10:45 -0400

    v3

--- mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Monotonic/H1063_Number_Of_Valid_Subarrays/src/com/jinwang/H1063_Number_Of_Valid_Subarrays.java
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Monotonic/H1063_Number_Of_Valid_Subarrays/src/com/jinwang/H1063_Number_Of_Valid_Subarrays.java
@@ -1,10 +1,49 @@
 package com.jinwang;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class H1063_Number_Of_Valid_Subarrays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		H1063_Number_Of_Valid_Subarrays solution =
+                new H1063_Number_Of_Valid_Subarrays();
 
+        // Example 1
+        int[] nums1 = {1, 4, 2, 5, 3};
+
+        System.out.println("Input: " + Arrays.toString(nums1));
+        System.out.println("Output: " + solution.validSubarrays(nums1));
+        System.out.println();
+
+        // Example 2
+        int[] nums2 = {3, 2, 1};
+
+        System.out.println("Input: " + Arrays.toString(nums2));
+        System.out.println("Output: " + solution.validSubarrays(nums2));
+        System.out.println();
+
+        // Example 3
+        int[] nums3 = {2, 2, 2};
+
+        System.out.println("Input: " + Arrays.toString(nums3));
+        System.out.println("Output: " + solution.validSubarrays(nums3));
 	}
+	
+	public int validSubarrays(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        int count = 0;
+
+        for (int num : nums) {
+            while (!stack.isEmpty() && stack.peek() > num) {
+                stack.pop();
+            }
+
+            stack.push(num);
+            count += stack.size();
+        }
+
+        return count;
+    }
 
 }
