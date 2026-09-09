aff09210f5578823c19bd937cbaf30df7688de1f
Jin Wang <specter01wj@gmail.com>
2026-09-09 10:56:32 -0400

    v1

---
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Monotonic/E1475_Final_Prices_With_A_Special_Discount_In_A_Shop/src/com/jinwang/E1475_Final_Prices_With_A_Special_Discount_In_A_Shop.java
@@ -0,0 +1,10 @@
+package com.jinwang;
+
+public class E1475_Final_Prices_With_A_Special_Discount_In_A_Shop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
