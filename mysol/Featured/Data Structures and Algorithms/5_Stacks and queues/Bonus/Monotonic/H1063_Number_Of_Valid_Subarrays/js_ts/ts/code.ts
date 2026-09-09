90fad8ab5d7b5366369c5655e2baaea3ccebd21c
Jin Wang <specter01wj@gmail.com>
2026-09-09 12:14:42 -0400

    v6

--- mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Monotonic/H1063_Number_Of_Valid_Subarrays/js_ts/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Monotonic/H1063_Number_Of_Valid_Subarrays/js_ts/ts/code.ts
@@ -1,45 +1,42 @@
-function finalPrices(prices: number[]): number[] {
-    const answer: number[] = [...prices];
+function validSubarrays(nums: number[]): number {
     const stack: number[] = [];
+    let count: number = 0;
 
-    for (let i = 0; i < prices.length; i++) {
-        while (
-            stack.length > 0 &&
-            prices[stack[stack.length - 1]] >= prices[i]
-        ) {
-            const index: number = stack.pop()!;
-            answer[index] = prices[index] - prices[i];
+    for (const num of nums) {
+        while (stack.length > 0 && stack[stack.length - 1] > num) {
+            stack.pop();
         }
 
-        stack.push(i);
+        stack.push(num);
+        count += stack.length;
     }
 
-    return answer;
+    return count;
 }
 
 (document.getElementById("title") as HTMLElement).innerText =
-    "1475. Final Prices With a Special Discount in a Shop (TS)";
+    "1063. Number of Valid Subarrays (TS)";
 
 let output: string = "";
 
 // Example 1
-const prices1: number[] = [8, 4, 6, 2, 3];
+const nums1: number[] = [1, 4, 2, 5, 3];
 
-output += "<b>Input:</b> prices = [" + prices1 + "]<br>";
-output += "<b>Output:</b> [" + finalPrices(prices1) + "]";
+output += "<b>Input:</b> nums = [" + nums1 + "]<br>";
+output += "<b>Output:</b> " + validSubarrays(nums1);
 output += "<br><br>";
 
 // Example 2
-const prices2: number[] = [1, 2, 3, 4, 5];
+const nums2: number[] = [3, 2, 1];
 
-output += "<b>Input:</b> prices = [" + prices2 + "]<br>";
-output += "<b>Output:</b> [" + finalPrices(prices2) + "]";
+output += "<b>Input:</b> nums = [" + nums2 + "]<br>";
+output += "<b>Output:</b> " + validSubarrays(nums2);
 output += "<br><br>";
 
 // Example 3
-const prices3: number[] = [10, 1, 1, 6];
+const nums3: number[] = [2, 2, 2];
 
-output += "<b>Input:</b> prices = [" + prices3 + "]<br>";
-output += "<b>Output:</b> [" + finalPrices(prices3) + "]";
+output += "<b>Input:</b> nums = [" + nums3 + "]<br>";
+output += "<b>Output:</b> " + validSubarrays(nums3);
 
 (document.getElementById("output") as HTMLElement).innerHTML = output;
