d8dd06ad7655a06e6c8e426a6f4e37bd9a9a9de7
Jin Wang <specter01wj@gmail.com>
2026-09-11 14:41:25 -0400

    v4

--- mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Monotonic/M1673_Find_The_Most_Competitive_Subsequence/js_ts/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Monotonic/M1673_Find_The_Most_Competitive_Subsequence/js_ts/ts/code.ts
@@ -1,42 +1,41 @@
-function validSubarrays(nums: number[]): number {
+function mostCompetitive(nums: number[], k: number): number[] {
     const stack: number[] = [];
-    let count: number = 0;
 
-    for (const num of nums) {
-        while (stack.length > 0 && stack[stack.length - 1] > num) {
+    for (let i = 0; i < nums.length; i++) {
+        while (
+            stack.length > 0 &&
+            stack[stack.length - 1] > nums[i] &&
+            stack.length + nums.length - i > k
+        ) {
             stack.pop();
         }
 
-        stack.push(num);
-        count += stack.length;
+        if (stack.length < k) {
+            stack.push(nums[i]);
+        }
     }
 
-    return count;
+    return stack;
 }
 
 (document.getElementById("title") as HTMLElement).innerText =
-    "1063. Number of Valid Subarrays (TS)";
+    "1673. Find the Most Competitive Subsequence (TS)";
 
 let output: string = "";
 
 // Example 1
-const nums1: number[] = [1, 4, 2, 5, 3];
+const nums1: number[] = [3, 5, 2, 6];
+const k1: number = 2;
 
-output += "<b>Input:</b> nums = [" + nums1 + "]<br>";
-output += "<b>Output:</b> " + validSubarrays(nums1);
+output += "<b>Input:</b> nums = [" + nums1 + "], k = " + k1 + "<br>";
+output += "<b>Output:</b> [" + mostCompetitive(nums1, k1) + "]";
 output += "<br><br>";
 
 // Example 2
-const nums2: number[] = [3, 2, 1];
+const nums2: number[] = [2, 4, 3, 3, 5, 4, 9, 6];
+const k2: number = 4;
 
-output += "<b>Input:</b> nums = [" + nums2 + "]<br>";
-output += "<b>Output:</b> " + validSubarrays(nums2);
-output += "<br><br>";
-
-// Example 3
-const nums3: number[] = [2, 2, 2];
-
-output += "<b>Input:</b> nums = [" + nums3 + "]<br>";
-output += "<b>Output:</b> " + validSubarrays(nums3);
+output += "<b>Input:</b> nums = [" + nums2 + "], k = " + k2 + "<br>";
+output += "<b>Output:</b> [" + mostCompetitive(nums2, k2) + "]";
 
 (document.getElementById("output") as HTMLElement).innerHTML = output;
