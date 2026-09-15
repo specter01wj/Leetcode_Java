2f0f26c96a219267ffdc40dae44cca389ce596da
Jin Wang <specter01wj@gmail.com>
2026-09-15 13:18:32 -0400

    v5

--- mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Monotonic/M2104_Sum_Of_Subarray_Ranges/js_ts/ts/code.ts
+++ mysol/Featured/Data Structures and Algorithms/5_Stacks and queues/Bonus/Monotonic/M2104_Sum_Of_Subarray_Ranges/js_ts/ts/code.ts
@@ -1,69 +1,72 @@
-function sumSubarrayMins(arr: number[]): number {
-    const mod: number = 1000000007;
-    const n: number = arr.length;
-
-    const left: number[] = new Array(n);
-    const right: number[] = new Array(n);
+function subArrayRanges(nums: number[]): number {
+    const n: number = nums.length;
+    let minSum: number = 0;
+    let maxSum: number = 0;
 
     const stack: number[] = [];
 
-    // Distance to previous smaller element
-    for (let i = 0; i < n; i++) {
+    // Sum of minimums
+    for (let i = 0; i <= n; i++) {
         while (
             stack.length > 0 &&
-            arr[stack[stack.length - 1]] > arr[i]
+            (i === n || nums[stack[stack.length - 1]] > nums[i])
         ) {
-            stack.pop();
-        }
+            const index: number = stack.pop()!;
+            const left: number =
+                stack.length === 0 ? index + 1 : index - stack[stack.length - 1];
+            const right: number = i - index;
 
-        left[i] =
-            stack.length === 0 ? i + 1 : i - stack[stack.length - 1];
+            minSum += nums[index] * left * right;
+        }
 
         stack.push(i);
     }
 
     stack.length = 0;
 
-    // Distance to next smaller or equal element
-    for (let i = n - 1; i >= 0; i--) {
+    // Sum of maximums
+    for (let i = 0; i <= n; i++) {
         while (
             stack.length > 0 &&
-            arr[stack[stack.length - 1]] >= arr[i]
+            (i === n || nums[stack[stack.length - 1]] < nums[i])
         ) {
-            stack.pop();
-        }
+            const index: number = stack.pop()!;
+            const left: number =
+                stack.length === 0 ? index + 1 : index - stack[stack.length - 1];
+            const right: number = i - index;
 
-        right[i] =
-            stack.length === 0 ? n - i : stack[stack.length - 1] - i;
+            maxSum += nums[index] * left * right;
+        }
 
         stack.push(i);
     }
 
-    let answer: number = 0;
-
-    for (let i = 0; i < n; i++) {
-        answer = (answer + arr[i] * left[i] * right[i]) % mod;
-    }
-
-    return answer;
+    return maxSum - minSum;
 }
 
 (document.getElementById("title") as HTMLElement).innerText =
-    "907. Sum of Subarray Minimums (TS)";
+    "2104. Sum of Subarray Ranges (TS)";
 
 let output: string = "";
 
 // Example 1
-const arr1: number[] = [3, 1, 2, 4];
+const nums1: number[] = [1, 2, 3];
 
-output += "<b>Input:</b> arr = [" + arr1 + "]<br>";
-output += "<b>Output:</b> " + sumSubarrayMins(arr1);
+output += "<b>Input:</b> nums = [" + nums1 + "]<br>";
+output += "<b>Output:</b> " + subArrayRanges(nums1);
 output += "<br><br>";
 
 // Example 2
-const arr2: number[] = [11, 81, 94, 43, 3];
+const nums2: number[] = [1, 3, 3];
 
-output += "<b>Input:</b> arr = [" + arr2 + "]<br>";
-output += "<b>Output:</b> " + sumSubarrayMins(arr2);
+output += "<b>Input:</b> nums = [" + nums2 + "]<br>";
+output += "<b>Output:</b> " + subArrayRanges(nums2);
+output += "<br><br>";
+
+// Example 3
+const nums3: number[] = [4, -2, -3, 4, 1];
+
+output += "<b>Input:</b> nums = [" + nums3 + "]<br>";
+output += "<b>Output:</b> " + subArrayRanges(nums3);
 
 (document.getElementById("output") as HTMLElement).innerHTML = output;
